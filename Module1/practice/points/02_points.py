38c4147b483bd3765013dbbef3ba10de9fb690f5
Rise777 <42608405+Rise777@users.noreply.github.com>
2021-03-20 14:41:46 +0300

    Update 02_points.py
    Не знаю как корректно вывести result

--- Module1/practice/points/02_points.py
+++ Module1/practice/points/02_points.py
@@ -1,9 +1,11 @@
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y): # magic
         self.x = x
         self.y = y
-
-
+    # Метод
+    def dist(self, other_point):
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
+    
 # Дан список точек
 points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)]
 
@@ -11,4 +13,14 @@ points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)
 
 # TODO: your core here...
 
-print("Координаты наиболее удаленной точки = ", ...)
+max_point = 0
+null_point = Point(0, 0)
+result = 0
+dist = 0
+for i in range(len(points)):
+    dist = null_point.dist(points[i])
+    if dist > max_point:
+        max_point = dist
+        result = points[i]
+
+print("Координаты наиболее удаленной точки = ", result)
