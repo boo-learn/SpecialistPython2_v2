5d332c516de5ad23d2cc8ca174989306d1d3a06c
Юрченко Евгений <71762690+boo-learn@users.noreply.github.com>
2021-03-20 16:29:15 +0300

    Create shop.py

---
+++ Module1/practice/shop.py
@@ -0,0 +1,18 @@
+items = [
+    {"type": "phone", "model": "Samsung A10", "cost": 24599},
+    {"type": "notebook", "model": "Lenovo Ideapad 5", "cost": 48200},
+    {"type": "notebook", "model": "HP 255", "cost": 40190},
+    {"type": "phone", "model": "Xiaomi Redmi 9", "cost": 12980},
+    {"type": "headphones", "model": "Apple AirPods Pro", "cost": 18900},
+    {"type": "headphones", "model": "Sennheiser HD 206", "cost": 1235},
+]
+
+# Задание-1:
+# Создайте class Item - класс для товара в интернет магазине
+# Преобразуйте список со словарями в список с объектами
+
+# Задание-2:
+# Выведите все товары в виде нумерованного списка, в формате:
+# 1. Phone "Xiaomi Redmi 9" 12980 rub.
+# 2. Phone ...
+# т.е. сначала тип товара с заглавной буквы, затем наименование в кавычках, затем цена с указанием валюты
