d06c27d21cd45d4bfd409aa5aeba07ef32ea150a
dssokolov-hub <81014902+dssokolov-hub@users.noreply.github.com>
2021-03-20 14:10:30 +0300

    Update 01_line.py

--- Module1/practice/points/01_line.py
+++ Module1/practice/points/01_line.py
@@ -13,3 +13,10 @@ points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 # my code
 
 print("Длина ломаной линии = ", ...)
+class Point:
+    def __init__(self, x, y):  # magic
+        self.x = x
+        self.y = y
+
+    def dist(self, other_point):
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
