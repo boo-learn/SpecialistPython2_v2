b67bb09a11f189b014670011efc5879d1a2f5ebe
GrayProphet <thegrayprophet@gmail.com>
2021-03-22 16:48:10 +0300

    Update shop.py

--- Module1/practice/shop.py
+++ Module1/practice/shop.py
@@ -1,18 +1,25 @@
-items = [
-    {"type": "phone", "model": "Samsung A10", "cost": 24599},
-    {"type": "notebook", "model": "Lenovo Ideapad 5", "cost": 48200},
-    {"type": "notebook", "model": "HP 255", "cost": 40190},
-    {"type": "phone", "model": "Xiaomi Redmi 9", "cost": 12980},
-    {"type": "headphones", "model": "Apple AirPods Pro", "cost": 18900},
-    {"type": "headphones", "model": "Sennheiser HD 206", "cost": 1235},
+
+class Item:
+    def __init__(self, type, model, cost):
+        self.type = type
+        self.model = model
+        self.cost = cost
+
+
+items1 = [
+    {"type": "Phone", "model": "Samsung A10", "cost": 24599},
+    {"type": "Notebook", "model": "Lenovo Ideapad 5", "cost": 48200},
+    {"type": "Notebook", "model": "HP 255", "cost": 40190},
+    {"type": "Phone", "model": "Xiaomi Redmi 9", "cost": 12980},
+    {"type": "Headphones", "model": "Apple AirPods Pro", "cost": 18900},
+    {"type": "Headphones", "model": "Sennheiser HD 206", "cost": 1235},
 ]
+items2 = []
+num = 1
+for i in items1:
+    items2.append(Item(i["type"], i["model"], i["cost"]))
 
-# Задание-1:
-# Создайте class Item - класс для товара в интернет магазине
-# Преобразуйте список со словарями в список с объектами
-
-# Задание-2:
-# Выведите все товары в виде нумерованного списка, в формате:
-# 1. Phone "Xiaomi Redmi 9" 12980 rub.
-# 2. Phone ...
-# т.е. сначала тип товара с заглавной буквы, затем наименование в кавычках, затем цена с указанием валюты
+for i in items2:
+    print('{}.'.format(num), i.type, '"{}"'.format(i.model), i.cost, "rub.")
+    num += 1
+Iphone = Item("phone", "Samsung A10", 24599)
