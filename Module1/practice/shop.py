35ce4beca31d19a313754b440975c33ca5419bb1
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-03-24 22:37:53 +0300

    Update shop.py

--- Module1/practice/shop.py
+++ Module1/practice/shop.py
@@ -9,10 +9,22 @@ items = [
 
 # Задание-1:
 # Создайте class Item - класс для товара в интернет магазине
+class Item:
+    def __init__(self, item_type, model, cost):
+        self.type = item_type
+        self.model = model
+        self.cost = cost
+
+
 # Преобразуйте список со словарями в список с объектами
+lst_items = []
+for item in items:
+    lst_items.append(Item(item["type"], item["model"], item["cost"]))
 
 # Задание-2:
 # Выведите все товары в виде нумерованного списка, в формате:
 # 1. Phone "Xiaomi Redmi 9" 12980 rub.
 # 2. Phone ...
 # т.е. сначала тип товара с заглавной буквы, затем наименование в кавычках, затем цена с указанием валюты
+for i in range(len(lst_items)):
+    print(f'{i+1}. {lst_items[i].type.capitalize()} "{lst_items[i].model}" {lst_items[i].cost} rub.')
