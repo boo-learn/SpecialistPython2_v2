c4c9e5ef87ba2f52f522595a553441b77c31ce3b
PortnovSergey <81014980+PortnovSergey@users.noreply.github.com>
2021-03-24 17:32:07 +0300

    Update shop.py

--- Module1/practice/shop.py
+++ Module1/practice/shop.py
@@ -1,3 +1,5 @@
+#!/usr/bin
+
 items = [
     {"type": "phone", "model": "Samsung A10", "cost": 24599},
     {"type": "notebook", "model": "Lenovo Ideapad 5", "cost": 48200},
@@ -11,8 +13,49 @@ items = [
 # Создайте class Item - класс для товара в интернет магазине
 # Преобразуйте список со словарями в список с объектами
 
+class Item:
+    def __init__(self, type, model, cost):
+        self.type = type
+        self.model = model
+        self.cost = cost
+
+
+def list_to_list(items):
+    output_list = []
+    for item in items:
+        product = Item(item["type"], item["model"], item["cost"])
+        output_list.append(product)
+    return output_list
+
+object_items = list_to_list(items)
+for item in object_items:
+    print(item)
+
+print("\n\n")
 # Задание-2:
 # Выведите все товары в виде нумерованного списка, в формате:
 # 1. Phone "Xiaomi Redmi 9" 12980 rub.
 # 2. Phone ...
 # т.е. сначала тип товара с заглавной буквы, затем наименование в кавычках, затем цена с указанием валюты
+
+def num_list(items):
+    for i,item in enumerate(items):
+        type_of_good = item["type"][0].upper() + item["type"][1:]
+        model_of_good = ' "' + item["model"] + '" '
+        cost_of_good = str(item["cost"]) + " rub."
+        res = str(i + 1) + ". " + type_of_good + model_of_good + cost_of_good
+        print(res)
+
+num_list(items)
+
+print("\n\n")
+
+def num_object_list(items):
+    for i,item in enumerate(items):
+        type_of_good = item.type[0].upper() + item.type[1:]
+        model_of_good = ' "' + item.model + '" '
+        cost_of_good = str(item.cost) + " rub."
+        res = str(i + 1) + ". " + type_of_good + model_of_good + cost_of_good
+        print(res)
+
+num_object_list(object_items)
