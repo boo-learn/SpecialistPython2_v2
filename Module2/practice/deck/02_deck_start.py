0f4ec951947c7d9268972c1ef598df366a1ae75b
Dmitry Solovov <75861361+dsolovov@users.noreply.github.com>
2021-03-27 10:52:05 +0300

    Update 02_deck_start.py

--- Module2/practice/deck/02_deck_start.py
+++ Module2/practice/deck/02_deck_start.py
@@ -1,10 +1,31 @@
 class Card:
+    value = {"2": 2,
+             "3": 3,
+             "4": 4,
+             "5": 5,
+             "6": 6,
+             "7": 7,
+             "8": 8,
+             "9": 9,
+             "10": 10,
+             "J": 10,
+             "Q": 10,
+             "K": 10,
+             "A": 11,
+             }
+    type = {
+        "diamonds": '\u2666',
+        "hearts": '\u2665',
+        "spades": '\u2660',
+        "clubs": '\u2663',
+    }
+
     def __init__(self, value, type):
-        pass
+        self.value = value
+        self.type = type
 
     def to_str(self):
-        pass
-
+        return f"{self.value}{Card.type[self.type]}"
 
 class Deck:
     def __init__(self):
