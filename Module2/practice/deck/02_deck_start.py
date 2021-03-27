ecf3d2d98de4ce87f6cbe6ad8224d85968dee9cf
PortnovSergey <81014980+PortnovSergey@users.noreply.github.com>
2021-03-27 10:54:38 +0300

    Update 02_deck_start.py

--- Module2/practice/deck/02_deck_start.py
+++ Module2/practice/deck/02_deck_start.py
@@ -1,14 +1,36 @@
+#!/usr/bin
+
 class Card:
     def __init__(self, value, type):
-        pass
+        self.value = value
+        self.type = type
 
     def to_str(self):
-        pass
+        string = str(self.value)
+        if self.type == "hearts":
+            return string + '\u2665'
+        elif self.type == "peaks":
+            return string + '\u2660'
+        elif self.type == "diamonds":
+            return string + '\u2663'
+        elif self.type == "clubs":
+            return string + '\u2663'
+
+    def equal_suit(self, card):
+        return self.type == card.type
+
+    def more(self, card):
+        return self.type == card.type and self.value > card.value
+
+    def less(self, card):
+        return self.type == card.type and self.value < card.value
 
 
 class Deck:
     def __init__(self):
         self.cards = []
+        card = Card(8, "clubs")
+        print(card.to_str())
 
     def show(self):
         pass
@@ -21,6 +43,3 @@ class Deck:
 
 
 deck = Deck()
-print(deck.show())
-deck.shuffle()
-print(deck.show())
