8e64936b7b6decc1b4328a1e18a56e6c384878b9
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-03-27 11:42:37 +0300

    Update 02_deck_start.py

--- Module2/practice/deck/02_deck_start.py
+++ Module2/practice/deck/02_deck_start.py
@@ -1,17 +1,35 @@
 class Card:
-    def __init__(self, value, type):
-        pass
+    HEARTS = 'Hearts'
+    DIAMONDS = 'Diamonds'
+    SPADES = 'Spades'
+    CLUBS = 'Clubs'
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+    suit_simbol = {HEARTS: '\u2665',
+                   DIAMONDS: '\u2666',
+                   CLUBS: '\u2663',
+                   SPADES: '\u2660'}
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
 
     def to_str(self):
-        pass
+        return f'{self.value}{Card.suit_simbol[self.suit]}'
 
 
 class Deck:
     def __init__(self):
         self.cards = []
+        for key in Card.suit_simbol:
+            for i in Card.values:
+                self.cards.append(Card(i, key))
 
     def show(self):
-        pass
+        tmp = []
+        for i in self.cards:
+            tmp.append(Card.to_str(i))
+        return f'deck[{len(self.cards)}]: {tmp}'
 
     def draw(self, x):
         pass
