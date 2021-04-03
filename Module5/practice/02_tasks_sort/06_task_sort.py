bceae9476b2b336df80e547451f8e715931c2992
dssokolov-hub <81014902+dssokolov-hub@users.noreply.github.com>
2021-04-03 14:09:41 +0300

    Update 06_task_sort.py

--- Module5/practice/02_tasks_sort/06_task_sort.py
+++ Module5/practice/02_tasks_sort/06_task_sort.py
@@ -11,3 +11,46 @@
 # 10 1 3 4 3 5 6 7 7 6 1
 # Выход
 # 5
+import random
+
+
+def gen_list(size, at=0, to=10):
+    nums = []
+    for _ in range(size):
+        nums.append(random.randint(at, to))
+    return nums
+
+
+def bubble_sort(nums, key=lambda x: x, reverse=False):
+    swapped = True
+    j = len(nums) - 1
+    while swapped:
+        swapped = False
+        # print("*****")
+        for i in range(j):
+            # print("i = ", i)
+            if reverse:
+                condition = key(nums[i]) < key(nums[i + 1])
+            else:
+                condition = key(nums[i]) > key(nums[i + 1])
+            if condition:
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+        j -= 1
+
+
+nums = gen_list(20)
+print(nums)
+bubble_sort(nums)
+print(nums)
+prizers = 0
+i = -1
+for _ in range(3):
+    prizers += 1
+    while nums[i - 1] == nums[i]:
+        prizers += 1
+        i -= 1
+    i -= 1
+print(prizers)
