c7a8d596db92b3b1b555d8a9869e52a33abf4403
GrayProphet <thegrayprophet@gmail.com>
2021-04-03 14:20:48 +0300

    Update 06_task_sort.py

--- Module5/practice/02_tasks_sort/06_task_sort.py
+++ Module5/practice/02_tasks_sort/06_task_sort.py
@@ -1,13 +1,44 @@
-# Призеры олимпиады
-# По результатам олимпиады участники награждаются дипломами.
-# Набравшие одинаковые  баллы  получают дипломы  одинаковой степени.
-# Призером олимпиады считается участник, получивший диплом  не хуже III степени.
-# По результатам олимпиады определите количество призеров.
-# Вход: натуральное число участников(N < 100) и далее N натуральных# чисел – результаты участников.
-# Выход: одно число – число призеров.
-# Пример:
-# Вход
-#
-# 10 1 3 4 3 5 6 7 7 6 1
-# Выход
-# 5
+import random
+
+
+def bubble_sort(nums, key=lambda x: x, reverse=False):
+    swapped = True
+    j = 0
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1 - j):
+            if reverse:
+                cond = key(nums[i]) < key(nums[i + 1])
+            else:
+                cond = key(nums[i]) > key(nums[i + 1])
+            if cond:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+        j += 1
+
+
+def gen_list(size, at=0, to=100):
+    data = []
+    for _ in range(size):
+        data.append(random.randint(at, to))
+    return data
+
+
+nums = gen_list(20)
+bubble_sort(nums, reverse=True)
+print(nums)
+
+place = nums[0]
+steps = 0
+num_peoples = 0
+for el in nums:
+    num_peoples+=1
+    if el < place:
+        steps+=1
+        place = el
+    if steps > 2:
+        break
+
+print(num_peoples)
+
+
