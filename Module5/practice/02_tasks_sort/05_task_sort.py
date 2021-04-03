ccdbb80b51a0a2938c6edfee55ad725b8d112139
GrayProphet <thegrayprophet@gmail.com>
2021-04-03 12:42:51 +0300

    Update 05_task_sort.py

--- Module5/practice/02_tasks_sort/05_task_sort.py
+++ Module5/practice/02_tasks_sort/05_task_sort.py
@@ -1,3 +1,34 @@
-# Анализ алгоритма сортировки “пузырьком”
-# Массив(список) отсортирован алгоритмом “пузырька”.
-# Определите, сколько обменов сделает алгоритм по возрастанию.
+import random
+
+
+def bubble_sort(nums):
+    coutn = 0
+    swapped = True
+    j = 0
+    while swapped:
+        swapped = False
+        # print("*****")
+        for i in range(len(nums) - 1 - j):
+            # print("i = ", i)
+            if nums[i] > nums[i + 1]:
+                coutn += 1
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+        j += 1
+    print(coutn)
+
+
+def gen_list(size, at=0, to=100):
+    data = []
+    for _ in range(size):
+        data.append(random.randint(at, to))
+    return data
+
+
+nums = gen_list(20)
+bubble_sort(nums)
+
+
+
