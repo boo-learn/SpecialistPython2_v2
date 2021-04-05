d24580726f641085d4ba5bc6759074c5dca7dd6d
Yuriy <81972162+owl-py@users.noreply.github.com>
2021-04-05 20:08:13 +0300

    Update 01_distance.py

--- Module1/practice/points/01_distance.py
+++ Module1/practice/points/01_distance.py
@@ -1,21 +1,18 @@
+from math import sqrt
+
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
 
-
-def distance():
-    """
-    Расстояние между двумя точками
-    """
-
+def distance(points: list): # можно перечислить по точкам, но в этом конкретном случае взял список
+							# и вычисление для первого и последнего элемента списка
+	return sqrt((points[-1].x - points[0].x) ** 2 + (points[-1].y - points[0].y) ** 2)
 
 # Дано две точки на координатной плоскости
-points1 = Point(2, 4)
+point1 = Point(2, 4)
 point2 = Point(5, -2)
 
 # Задание: Найдите расстояние между этими точками. Реализовав и используя функцию distance()
 
-# TODO: your core here...
-
-print("Расстояние между точками = ", ...)
+print("Расстояние между точками = ", distance([point1, point2]))
