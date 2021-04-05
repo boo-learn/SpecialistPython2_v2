0fa226e0af5896cf25b2fa523cbb127aba277e75
Michael Nikitenko <xm4dn355x@gmail.com>
2021-04-05 22:08:04 +0500

    Update 01_distance.py
    Решение от Никитенко Михаила

--- Module1/practice/points/01_distance.py
+++ Module1/practice/points/01_distance.py
@@ -1,21 +1,24 @@
+from math import sqrt
+
+
 class Point:
-    def __init__(self, x, y):
+    """Точка в двоичной системе координат"""
+    def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
 
 
-def distance():
-    """
-    Расстояние между двумя точками
-    """
+def distance(p1: Point, p2: Point) -> float:
+    """Расстояние между двумя точками"""
+    return round(sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2), 2)
 
 
-# Дано две точки на координатной плоскости
-points1 = Point(2, 4)
-point2 = Point(5, -2)
+if __name__ == '__main__':
+    # Дано две точки на координатной плоскости
+    point1 = Point(2, 4)
+    point2 = Point(5, -2)
 
-# Задание: Найдите расстояние между этими точками. Реализовав и используя функцию distance()
+    # Задание: Найдите расстояние между этими точками. Реализовав и используя функцию distance()
+    res = distance(point1, point2)
 
-# TODO: your core here...
-
-print("Расстояние между точками = ", ...)
+    print(f"Расстояние между точками = {res}")
