000f2814d2e80c1d1aecb6eec42dcf27f31c16d7
Deimos-szb <81982997+Deimos-szb@users.noreply.github.com>
2021-04-05 20:38:07 +0300

    Update 03_points.py

--- Module1/practice/points/03_points.py
+++ Module1/practice/points/03_points.py
@@ -9,6 +9,19 @@ points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)
 
 # Задание: найдите точку наиболее удаленную от начала координат и выведите ее координаты
 
-# TODO: your core here...
+point_zero = Point(0, 0)
 
-print("Координаты наиболее удаленной точки = ", ...)
+def distance(p1, p2):
+    """
+    Расстояние между двумя точками
+    """
+    import math
+    return math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
+
+distances = 0
+
+for point in points:
+    if distance(point_zero, point) > distances:
+        max_point = point
+
+print("Координаты наиболее удаленной точки = ", max_point.x, max_point.y)
