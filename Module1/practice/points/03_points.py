40d2b49ab282a513fac1ea8d68c4283d17c4add2
DzimLym <66909548+DzimLym@users.noreply.github.com>
2021-04-05 20:57:26 +0300

    Update 03_points.py

--- Module1/practice/points/03_points.py
+++ Module1/practice/points/03_points.py
@@ -4,11 +4,17 @@ class Point:
         self.y = y
 
 
+def distance(p1, p2):  # Расстояние между двумя точками
+    return ((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2) ** 0.5
+
+
 # Дан список точек
 points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)]
 
 # Задание: найдите точку наиболее удаленную от начала координат и выведите ее координаты
+maximum = Point(0, 0)
+for point in points:
+    if distance(Point(0, 0), maximum) < distance(Point(0, 0), point):
+        maximum = point
 
-# TODO: your core here...
-
-print("Координаты наиболее удаленной точки = ", ...)
+print(f"Координаты наиболее удаленной точки = {maximum.x}; {maximum.y}")
