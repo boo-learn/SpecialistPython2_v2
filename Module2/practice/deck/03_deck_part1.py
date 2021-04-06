a16ea8cc063f06ba40224495a1fecac549ca89a1
Deimos-szb <81982997+Deimos-szb@users.noreply.github.com>
2021-04-06 20:20:44 +0300

    Update 03_deck_part1.py

--- Module2/practice/deck/03_deck_part1.py
+++ Module2/practice/deck/03_deck_part1.py
@@ -1,35 +1,59 @@
+import random
+
 class Card:
-    pass
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
+    SUITS = ['♥', '♦', '♣', '♠']
+    VALUES = [str(i) for i in range(2, 11)]
+    for char in ['J', 'Q', 'K', 'A']:
+        VALUES.append(char)
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def __repr__(self):
+        return f'{self.value}{self.suit}'
 
 
-# Задание: Теперь создадим колоду из 52-ух карт и реализуем все методы
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
+
+    def is_more(self, other_card):
+        if self.value > other_card.value:
+            out = True
+        elif self.value == other_card.value:
+            suit_self = Card.SUITS.index(self.suit)
+            suit_other = Card.SUITS.index(other_card.suit)
+            out = suit_self > suit_other
+        else:
+            out = False
+        return out
+
+    def is_less(self, other_card):
+        return not self.is_more(other_card)
+
 class Deck:
     def __init__(self):
-        # Список карт в колоде. Каждым элементом списка будет объект класса Card
-        self.cards = []
+        self.deck = []
+        for suit in Card.SUITS:
+            for value in Card.VALUES:
+                self.deck.append(Card(value, suit))
 
-    def show(self):
-        pass
+    def __repr__(self):
+        return f'deck[{len(self.deck)}]: ' + str(self.deck)
 
-    def draw(self, x):
-        pass
+    def draw(self, how_much):
+        out = []
+        for i in range(how_much):
+            out.append(self.deck.pop(0))
+        return out
 
     def shuffle(self):
-        pass
+        random.shuffle(self.deck)
 
+deck1 = Deck()
+deck1.shuffle()
 
-# Создаем колоду
-deck = Deck()
-# Выводим колоду в формате указанном в основном задании
-print(deck.show())
-# Тусуем колоду
-deck.shuffle()
-print(deck.show())
+card1, card2 = deck1.draw(2)
 
-# Возьмем 5 карт "в руку"
-hand = deck.draw(5)
-# Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
-print(deck.show())
-# Выводим список карт "в руке"(список hand)
-print(...)
+print(card1.is_more(card2))
+print(card1.is_less(card2))
