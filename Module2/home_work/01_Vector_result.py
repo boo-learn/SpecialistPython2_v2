b98d479f5cc0b896383429c20424fe5a40ef57db
Michael Nikitenko <xm4dn355x@gmail.com>
2021-04-07 19:38:37 +0500

    Update 01_Vector_result.py
    Решение Михаила Никитенко

--- Module2/home_work/01_Vector_result.py
+++ Module2/home_work/01_Vector_result.py
@@ -1,5 +1,48 @@
-# Сюда отправляем решение задачи "Вектор"
-# Само задание в файле 01_Vector_task.md
-
 class Vector:
-    pass
+    """Вектор в двухмерном пространстве"""
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'Vector ({self.x}, {self.y})'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __add__(self, other):
+        self.x = self.x + other.x
+        self.y = self.y + other.y
+        return self
+
+    def __sub__(self, other):
+        self.x = self.x - other.x
+        self.y = self.y - other.y
+        return self
+
+    def __mul__(self, k: int):
+        self.x = self.x * k
+        self.y = self.y * k
+        return self
+
+
+if __name__ == '__main__':
+    print('Задаём два вектора')
+    vector1 = Vector(1, 2)
+    vector2 = Vector(4, 5)
+    print(vector1)
+    print(vector2)
+
+    print('Умножим векторы на скаляры')
+    vector1 = vector1 * 2
+    vector2 = vector2 * -1
+    print(vector1)
+    print(vector2)
+
+    print('Сложим оба вектора')
+    vector3 = vector1 + vector2
+    print(vector3)
+
+    print('Вычтем из вектора вектор')
+    vector3 = vector1 - vector2
+    print(vector3)
