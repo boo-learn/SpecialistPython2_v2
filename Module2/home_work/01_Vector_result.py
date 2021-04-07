ea9127c13c1fd8ecd7c64c50c938c3a708a2fcbe
DzimLym <66909548+DzimLym@users.noreply.github.com>
2021-04-07 18:21:26 +0300

    Update 01_Vector_result.py

--- Module2/home_work/01_Vector_result.py
+++ Module2/home_work/01_Vector_result.py
@@ -1,5 +1,32 @@
-# Сюда отправляем решение задачи "Вектор"
-# Само задание в файле 01_Vector_task.md
-
 class Vector:
-    pass
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):  # to_str
+        return f"[{self.x}, {self.y}]"
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, factor):
+        return Vector(self.x * factor, self.y * factor)
+
+
+vector_a = Vector(2, 4)
+vector_b = Vector(1, 2)
+print(vector_a)
+if vector_a == vector_b:
+    print(f"Векторы {vector_a} и {vector_b} равны")
+else:
+    print(f"Векторы {vector_a} и {vector_b} не равны")
+print(vector_a + vector_b)
+print(vector_a - vector_b)
+print(vector_a * 5)
