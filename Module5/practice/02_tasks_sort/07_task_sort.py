d29754b92104f9902f784c5aed098b1c49637d06
dssokolov-hub <81014902+dssokolov-hub@users.noreply.github.com>
2021-04-07 19:49:49 +0300

    Update 07_task_sort.py

--- Module5/practice/02_tasks_sort/07_task_sort.py
+++ Module5/practice/02_tasks_sort/07_task_sort.py
@@ -13,3 +13,34 @@
 # 70
 # Пояснение:
 # Возможен такой порядок: 10 2 50 1 10
+
+import random
+
+
+def gen_list(size, at=0, to=10):
+    nums = []
+    for _ in range(size):
+        nums.append(random.randint(at, to))
+    return nums
+
+
+def bubble_sort(nums, key=lambda x: x, reverse=False):
+    swapped = True
+    j = len(nums) - 1
+    while swapped:
+        swapped = False
+        for i in range(j):
+            if reverse:
+                condition = key(nums[i]) < key(nums[i + 1])
+            else:
+                condition = key(nums[i]) > key(nums[i + 1])
+            if condition:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+        j -= 1
+
+l = gen_list(6, 0, 20)
+print(l)
+bubble_sort(l, reverse=True)
+num_check = len(l) - len(l) // 2
+print(sum(l[:num_check]))
