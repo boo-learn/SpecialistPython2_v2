dbabb5b94ff3a3a147be2a0a29a5df802a61ccdc
Deimos-szb <81982997+Deimos-szb@users.noreply.github.com>
2021-04-07 17:42:24 +0300

    Update 06_task_deck.py

--- Module2/practice/deck/deck_tasks/06_task_deck.py
+++ Module2/practice/deck/deck_tasks/06_task_deck.py
@@ -1,1 +1,96 @@
-# Сюда отправляем решение шестой задачи с колодой
+"""Задание-6 “Игра в две колоды”
+
+Создайте две колоды по 52 карты. Перемешайте их вместе - в итоге получится
+одна колода из 104 карт. Выбросите/вытяните половину карт. Узнайте,
+какой/каких мастей в колоде осталось больше всего?"""
+
+import random
+
+
+class Card:
+    SUITS = ['♥', '♦', '♣', '♠']
+    VALUES = [str(i) for i in range(2, 11)] + ['J', 'Q', 'K', 'A']
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit        
+
+    def __repr__(self):        
+        return f'{self.value}{self.suit}'
+
+    def __gt__(self, other):
+        if self.value == other.value:
+            return Card.SUITS.index(self.suit) < Card.SUITS.index(other.suit)
+        else:
+            return Card.VALUES.index(self.value) > Card.VALUES.index(other.value)
+    def __eq__(self, other):
+        return (self.suit == other.suit) & (self.value == other.value)
+
+    def __lt__(self, other):
+        return not self.__gt__(other)
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
+
+
+class Deck:
+    def __init__(self, list_cards = None):
+        self.count = 0
+        self.deck = list_cards if list_cards else []
+        for suit in Card.SUITS:
+            for value in Card.VALUES:
+                self.deck.append(Card(value, suit))
+
+    def __repr__(self):
+        return f'deck[{len(self.deck)}]: ' + ', '.join([str(card) for card in self.deck])
+
+    def __len__(self):
+        return len(self.deck)
+
+    def __getitem__(self, key):        
+        return self.deck[key]
+
+    def __iter__(self):
+        self.count = 0
+        return self
+
+    def __next__(self):
+        if self.count >= len(self.deck):
+            raise StopIteration
+        card = self.deck[self.count]
+        self.count += 1        
+        return card
+
+    def __add__(self, other):
+        return Deck(self.deck + other.deck)
+
+    def draw(self, how_much):
+        out = []
+        for i in range(how_much):
+            try:
+                out.append(self.deck.pop(0))
+            except IndexError:
+                break
+        return out
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+
+deck_1 = Deck()
+deck_2 = Deck()
+deck = deck_1 + deck_2
+deck.shuffle()
+
+deck.draw(len(deck) // 2)
+count = dict.fromkeys(Card.SUITS, 0)
+for card in deck:
+    count[card.suit] += 1
+
+max_cards = max(count.values())
+print('В оставшейся половине двойной колоды масти(ей) ', end='')
+for k, v in count.items():
+    if v == max_cards:
+        print(k, end=' ')
+print('больше всего.')
+
