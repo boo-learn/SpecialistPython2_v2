24a01762fa39c72ffbc99251f2b2d99e388ef784
AnnaBobyleva <81982995+AnnaBobyleva@users.noreply.github.com>
2021-04-08 20:57:37 +0300

    Update 04_task_sort.py

--- Module5/practice/02_tasks_sort/04_task_sort.py
+++ Module5/practice/02_tasks_sort/04_task_sort.py
@@ -1,3 +1,20 @@
 # Сумма наибольших по модулю
 # Дан массив(список) чисел.
 # Найти: сумму 10-ти самых больших элементов по модулю.
+nums = [2, 10, 55, 12, 8, 77, 17, -15, -99, 11, 23, 56, 66]
+
+def bubble_sort(nums):
+    swapped = True
+    j = 0
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1 - j):
+            if abs(nums[i]) > abs(nums[i + 1]):
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+        j += 1
+
+bubble_sort(nums)
+#print(nums)
+#print(nums[-10:])
+print(sum(nums[-10:]))
