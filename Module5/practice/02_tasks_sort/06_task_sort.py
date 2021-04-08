11888768af5eff5703dd07ee98ce4908123d5d49
YuryTomgorov <81982991+YuryTomgorov@users.noreply.github.com>
2021-04-08 21:26:37 +0300

    Update 06_task_sort.py

--- Module5/practice/02_tasks_sort/06_task_sort.py
+++ Module5/practice/02_tasks_sort/06_task_sort.py
@@ -1,13 +1,32 @@
-# Призеры олимпиады
-# По результатам олимпиады участники награждаются дипломами.
-# Набравшие одинаковые  баллы  получают дипломы  одинаковой степени.
-# Призером олимпиады считается участник, получивший диплом  не хуже III степени.
-# По результатам олимпиады определите количество призеров.
-# Вход: натуральное число участников(N < 100) и далее N натуральных# чисел – результаты участников.
-# Выход: одно число – число призеров.
-# Пример:
-# Вход
-#
-# 10 1 3 4 3 5 6 7 7 6 1
-# Выход
-# 5
+def bubble_sort(nums, key=lambda x: x, reverse=False):
+    j = 0
+    swapped = True
+    while swapped:
+        swapped = False
+        # print("*****")
+        for i in range(len(nums) - j - 1):
+            # print("i = ", i)
+            if reverse:
+                expr = key(nums[i]) < key(nums[i + 1])
+            else:
+                expr = key(nums[i]) > key(nums[i + 1])
+            if expr:
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+        j += 1
+    return nums
+
+a = 4
+p = 0
+gen = [1, 3, 4, 3, 5, 6, 7, 7, 6, 1]
+print('before: ', gen)
+bubble_sort(gen)
+print('sort: ', gen)
+
+for i in gen:
+    if i > a:
+        p += 1
+
+print('prizea: ', p)
