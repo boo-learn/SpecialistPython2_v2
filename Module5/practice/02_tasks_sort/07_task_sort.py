c28638b76256b20137c6e6a3818002bdad035a69
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-04-09 16:28:30 +0300

    Update 07_task_sort.py

--- Module5/practice/02_tasks_sort/07_task_sort.py
+++ Module5/practice/02_tasks_sort/07_task_sort.py
@@ -13,3 +13,38 @@
 # 70
 # Пояснение:
 # Возможен такой порядок: 10 2 50 1 10
+
+def bubble_sort(nums, key=lambda x: x, reverse=False):
+    count = 0
+    swapped = True
+    j = 0
+    while swapped:
+        swapped = False
+        # print("*****")
+        for i in range(len(nums) - 1 - j):
+            # print("i = ", i)
+            if reverse:
+                cond = key(nums[i]) < key(nums[i + 1])
+            else:
+                cond = key(nums[i]) > key(nums[i + 1])
+            if cond:
+                count += 1
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+        j += 1
+    # print(f'{count=}')
+
+
+def gen_list(size, at=-100, to=100):
+    import random
+    nums = []
+    for _ in range(size):
+        nums.append(random.randint(at, to))
+    return nums
+
+
+purchases = gen_list(22, 0, 100)
+print(*purchases)
+bubble_sort(purchases)
+print(*purchases)
+print(sum(purchases[int(len(purchases) / 2):]))
