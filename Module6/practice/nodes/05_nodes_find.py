7f87797743426c83a5b9b82195c5382ebe1edc32
Michael Nikitenko <xm4dn355x@gmail.com>
2021-04-09 22:00:15 +0500

    Update 05_nodes_find.py
    Михаил Никитенко

--- Module6/practice/nodes/05_nodes_find.py
+++ Module6/practice/nodes/05_nodes_find.py
@@ -1,11 +1,21 @@
 class Node:
-    """
-    Класс для узла списка. Хранит значение и указатель на следующий узел.
-    """
+    """Класс для узла списка. Хранит значение и указатель на следующий узел."""
 
     def __init__(self, value=None, next=None):
         self.value = value
         self.next = next
+
+
+def len_nodes(start_node):
+    """
+    Возвращает целое число - кол-во нод у цепочке
+    """
+    count = 1
+    node = start_node
+    while node.next:
+        count += 1
+        node = node.next
+    return count
 
 
 def find_node_by_value(start_node, value):
@@ -13,7 +23,18 @@ def find_node_by_value(start_node, value):
     Возвращает индекс ноды с заданным значением(value).
     Если нода с указанным значение(value) не найдена, дублируйте поведение методы .index() списка
     """
-    pass
+    list_len = len_nodes(start_node)
+    index = 0
+    while iter != list_len:
+        start_node = start_node.next
+        try:
+            if start_node.value == value:
+                print(f'{index=} {start_node.value=}')
+                break
+        except AttributeError:
+            print('В списке нет такого элемента')
+            break
+        index += 1
 
 
 def gen_names_list(size=None):
@@ -25,16 +46,20 @@ def gen_names_list(size=None):
     return names[:size]
 
 
-# Дан список из произвольного количества имен
-names = gen_names_list()
-# print(names)
+if __name__ == '__main__':
+    # Дан список из произвольного количества имен
+    names = gen_names_list()
+    print(names)
 
-# TODO: скопируйте цепочку нод из предыдущей задачи
-...
+    # TODO: скопируйте цепочку нод из предыдущей задачи
+    prev = None
+    for name in names[::-1]:
+        node = Node(value=name, next=prev)
+        prev = node
 
 
-# TODO: Передайте первую ноду и искомое значение в функцию find_node_by_value()
-#  напишите реализацию функции find_node_by_value()
-first_node = ...
-value = ...
-find_node_by_value(first_node, value)
+    # TODO: Передайте первую ноду и искомое значение в функцию find_node_by_value()
+    #  напишите реализацию функции find_node_by_value()
+    first_node = node
+    value = "Линус"
+    find_node_by_value(first_node, value)
