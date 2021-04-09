7e527f01d36e14cf3ff99402f2856d6afd867109
Deimos-szb <81982997+Deimos-szb@users.noreply.github.com>
2021-04-09 20:38:01 +0300

    Update 05_nodes_find.py

--- Module6/practice/nodes/05_nodes_find.py
+++ Module6/practice/nodes/05_nodes_find.py
@@ -13,7 +13,16 @@ def find_node_by_value(start_node, value):
     Возвращает индекс ноды с заданным значением(value).
     Если нода с указанным значение(value) не найдена, дублируйте поведение методы .index() списка
     """
-    pass
+
+    print(value)
+    count = 0
+    cur_node = start_node
+    while cur_node.value != value:
+        if cur_node.next == None:
+            raise ValueError(f'{value} not in Nodes')
+        cur_node = cur_node.next
+        count += 1
+    return count
 
 
 def gen_names_list(size=None):
@@ -27,14 +36,17 @@ def gen_names_list(size=None):
 
 # Дан список из произвольного количества имен
 names = gen_names_list()
-# print(names)
+print(names)
 
 # TODO: скопируйте цепочку нод из предыдущей задачи
-...
+prev_node = Node(names[-1])
+for name in names[::-1][1:]:
+    next_node = Node(name, prev_node)
+    prev_node = next_node
 
 
 # TODO: Передайте первую ноду и искомое значение в функцию find_node_by_value()
 #  напишите реализацию функции find_node_by_value()
-first_node = ...
-value = ...
-find_node_by_value(first_node, value)
+
+value = 'Ирина'
+print(find_node_by_value(prev_node, value))
