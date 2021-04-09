da1f6703f70e20c30ae5ebfb1a0db005c579f406
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-04-09 19:47:58 +0300

    Update 10_task_sort.py

--- Module5/practice/02_tasks_sort/10_task_sort.py
+++ Module5/practice/02_tasks_sort/10_task_sort.py
@@ -4,3 +4,27 @@
 # Необходимо разбить их на две кучи таким образом, чтобы веса куч отличались не более чем в 2 раза.
 # Если этого сделать нельзя, то указать это.
 
+def gen_list(size, at=-100, to=100):
+    import random
+    nums = []
+    for _ in range(size):
+        nums.append(random.randint(at, to))
+    return nums
+
+
+rocks = gen_list(5, 1, 100)
+print(rocks)
+max_weight = max(rocks)
+pile1 = []
+pile2 = []
+pile1.append(max_weight)
+rocks.remove(max_weight)
+for rock in rocks:
+    if sum(pile1) > sum(pile2):
+        pile2.append(rock)
+    else:
+        pile1.append(rock)
+if sum(pile1) > sum(pile2) * 2:
+    print('Нельзя распределить камни указанным образом')
+else:
+    print(f'{pile1=} {pile2=}')
