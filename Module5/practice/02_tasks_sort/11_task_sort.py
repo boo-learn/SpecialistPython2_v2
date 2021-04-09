3f1bfc49cbe2d42c7a42d43add2037c20ded16a1
Komarovio <55410623+Komarovio@users.noreply.github.com>
2021-04-09 18:11:29 +0300

    Update 11_task_sort.py

--- Module5/practice/02_tasks_sort/11_task_sort.py
+++ Module5/practice/02_tasks_sort/11_task_sort.py
@@ -1,2 +1,36 @@
 # Анаграммы*
 # Задается словарь (список слов). Найти в нем все анаграммы (слова, составленные из одних и тех же букв).
+
+
+def bubble_sort(nums, key=lambda x: x, reverse=False):
+    j = 0
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - j - 1):
+            if reverse:
+                cond = key(nums[i]) < key(nums[i + 1])
+            else:
+                cond = key(nums[i]) > key(nums[i + 1])
+            if cond:
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+        j += 1
+    return nums
+
+
+words = ['qwertyuiop', 'twoplussix', 'poiuytrewq', 'plustwosix', 'poiuqwerty']
+
+
+anagramms = {}
+for w in words:
+    ls = list(w)
+    ls_sort = bubble_sort(ls)
+    ls = "".join(ls_sort)
+    anagramms.setdefault(ls, [])
+    anagramms[ls].append(w)
+
+for a in anagramms.values():
+    print(*a)
