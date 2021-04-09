7000be9940c8539a33f2b548f29aef4996631e7f
Michael Nikitenko <xm4dn355x@gmail.com>
2021-04-09 19:41:34 +0500

    Update 06_task_sort.py
    Михаил Никитенко

--- Module5/practice/02_tasks_sort/06_task_sort.py
+++ Module5/practice/02_tasks_sort/06_task_sort.py
@@ -11,3 +11,44 @@
 # 10 1 3 4 3 5 6 7 7 6 1
 # Выход
 # 5
+
+
+def gen_list(size, at=-100, to=100):
+    import random
+    return [random.randint(at, to) for _ in range(size)]
+
+
+def bubble_sort(nums, key=lambda x: x, reverse=False):
+    swapped = True
+    j = 0
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1 - j):
+            if reverse:
+                expr = key(nums[i]) < key(nums[i + 1])
+            else:
+                expr = key(nums[i]) > key(nums[i + 1])
+            if expr:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+        j += 1
+    return nums
+
+
+def rewarding(people: list):
+    people = bubble_sort(people, reverse=True)
+    prev = people[0]
+    prizes = 0
+    steps = 1
+    for human in people:
+        if steps <= 3:
+            prizes += 1
+            if prev != human:
+                steps += 1
+        prev = human
+    return prizes - 1
+
+if __name__ == '__main__':
+    olympigs = [10, 1, 3, 4, 3, 5, 6, 7, 7, 6, 1]
+    winners = rewarding(olympigs)
+    print(f'Призёров всего : {winners}')
