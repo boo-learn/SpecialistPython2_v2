741c5ce30b6840b4e4dbd8b62ec25839a305cf09
Michael Nikitenko <xm4dn355x@gmail.com>
2021-04-09 21:18:21 +0500

    Update 01_nodes_create.py
    Михаил Никитенко

--- Module6/practice/nodes/01_nodes_create.py
+++ Module6/practice/nodes/01_nodes_create.py
@@ -1,3 +1,4 @@
+
 class Node:
     """
     Класс для узла списка. Хранит значение и указатель на следующий узел.
@@ -13,16 +14,20 @@ node0 = Node("Иван")
 node1 = Node("Алекс")
 node2 = Node("Петр")
 node3 = Node("Анна")
-# TODO: Доавьте еще несколько нод с именами
+node4 = Node('Жак')
+node5 = Node('Мишаня')
 
 # Связываем ноды
 node0.next = node1
 node1.next = node2
 node2.next = node3
-# TODO: Свяжите добавленные ноды вместе
+node3.next = node4
+node4.next = node5
 
-# Пройдемся по значениям всех нод, начиная с первой
-current_node = node0
-while current_node:
-    print(current_node.value)
-    current_node = current_node.next
+
+if __name__ == '__main__':
+    # Пройдемся по значениям всех нод, начиная с первой
+    current_node = node0
+    while current_node:
+        print(current_node.value)
+        current_node = current_node.next
