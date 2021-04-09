c18555e8a02ec7cfa1ebef991e6cc3ffb5a6cf3d
PortnovSergey <81014980+PortnovSergey@users.noreply.github.com>
2021-04-09 15:35:32 +0300

    Update 09_task_sort.py

--- Module5/practice/02_tasks_sort/09_task_sort.py
+++ Module5/practice/02_tasks_sort/09_task_sort.py
@@ -1,3 +1,25 @@
+#!/usr/bin
+
+def bubble_sort(arr):
+    for j in range(len(arr)):
+        flag = False
+        for i in range(len(arr) - j - 1):
+            if arr[i] > arr[i + 1]:
+                flag = True
+                buf = arr[i + 1]
+                arr[i + 1] = arr[i]
+                arr[i] = buf
+        if not flag:
+            break
+    return arr
+
+def to_list(keytoval):
+    outpt = []
+    outpt.append(keytoval['surname'])
+    outpt.append(keytoval['name'])
+    outpt.append(keytoval['position'])
+    return outpt
+
 # Список сотрудников
 # Дан список, элементами которого являются сотрудники, представленные в виде словаря(dict).
 # Пример данных:
@@ -7,5 +29,10 @@ employees = [
    {"name": "Алексей", "surname": "Петров", "position": "Строитель"},
    {"name": "Иван", "surname": "Сидоров", "position": "Строитель"},
 ]
+
+lst = []
+for elem in employees:
+    lst.append(to_list(elem))
+print(bubble_sort(lst))
 # Выведите список сотрудников(без указания должности) в формате: Фамилия Имя, в отсортированном порядке.
 # Примечание: если фамилии сотрудников совпадают, при сортировке учесть имя.
