4bdfb7b444bc8e60fdf449b320a99ab2e8322d98
Michael Nikitenko <xm4dn355x@gmail.com>
2021-04-09 21:40:00 +0500

    Update 04_nodes_index.py
    Михаил Никитенко

--- Module6/practice/nodes/04_nodes_index.py
+++ Module6/practice/nodes/04_nodes_index.py
@@ -1,11 +1,21 @@
 class Node:
-    """
-    Класс для узла списка. Хранит значение и указатель на следующий узел.
-    """
+    """Класс для узла списка. Хранит значение и указатель на следующий узел."""
 
     def __init__(self, value=None, next=None):
         self.value = value
         self.next = next
+
+
+def len_nodes(start_node):
+    """
+    Возвращает целое число - кол-во нод у цепочке
+    """
+    count = 1
+    node = start_node
+    while node.next:
+        count += 1
+        node = node.next
+    return count
 
 
 def print_node_by_index(start_node, index):
@@ -14,7 +24,11 @@ def print_node_by_index(start_node, index):
     Если index = 0, выводим значение ноды start_node
     Считаем, что index гарантированно > 0
     """
-    pass
+    iter = 0
+    while iter != index:
+        start_node = start_node.next
+        iter += 1
+    print(start_node.value)
 
 
 def gen_names_list(size=None):
@@ -26,16 +40,20 @@ def gen_names_list(size=None):
     return names[:size]
 
 
-# Дан список из произвольного количества имен
-names = gen_names_list()
-# print(names)
+if __name__ == '__main__':
+    # Дан список из произвольного количества имен
+    names = gen_names_list()
+    print(names)
 
-# TODO: скопируйте цепочку нод из предыдущей задачи
-...
+    # TODO: скопируйте цепочку нод из предыдущей задачи
+    prev = None
+    for name in names:
+        node = Node(value=name, next=prev)
+        prev = node
 
 
-# TODO: Передайте первую ноду и индекс ноды, значение которой нужно вывести, в функцию print_node_by_index()
-#  напишите реализацию функции print_node_by_index()
-first_node = ...
-index = ...
-print_node_by_index(first_node, index)
+    # TODO: Передайте первую ноду и индекс ноды, значение которой нужно вывести, в функцию print_node_by_index()
+    #  напишите реализацию функции print_node_by_index()
+    first_node = node
+    index = 2
+    print_node_by_index(first_node, index)
