0cf5428d55a7f08bccc9e9a16143848693102d0b
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-04-10 08:56:16 +0300

    Update 01_Vector_result.py

--- Module2/home_work/01_Vector_result.py
+++ Module2/home_work/01_Vector_result.py
@@ -2,4 +2,28 @@
 # Само задание в файле 01_Vector_task.md
 
 class Vector:
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, numb):
+        return Vector(self.x * numb, self.y * numb)
+
+
+vector1 = Vector(0, 5)
+vector2 = Vector(5, 0)
+
+print(f'{vector1}')
+print(f'{vector2}')
+print(f'{(vector1 + vector2)}')
+print(f'{(vector1 - vector2)}')
+print(f'{(vector1 * 5)}')
