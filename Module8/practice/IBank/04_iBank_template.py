cd8ec93d4464cd50df6b8c4a77ffb5dd552c639d
dssokolov-hub <81014902+dssokolov-hub@users.noreply.github.com>
2021-04-10 16:42:54 +0300

    Update 04_iBank_template.py

--- Module8/practice/IBank/04_iBank_template.py
+++ Module8/practice/IBank/04_iBank_template.py
@@ -51,4 +51,37 @@ class AccountBase(ABC):
         """
         return f"..."
 
+class Account(AccountBase):
 
+    def deposite(self, amount):
+        self.balance += amount
+
+    def transfer(self, target_account, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            target_account.balance += amount
+        else:
+            print("Недостаточно средств на счете")
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("Недостаточно средств на счете")
+
+    def full_info(self):
+        return f'{self.name} баланс {self.balance} руб. паспорт: {self.passport8} т.{self.phone_number}'
+
+    def __repr__(self):
+        name_list = self.name.split(' ')
+        name = f'{name_list[0]} {name_list[1][0]}.{name_list[2][0]}.'
+        return f'{name} баланс {self.balance} руб.'
+
+
+acc1 = Account("Иванов Иван Иванович", 10101010, 89000000001)
+acc1.deposite(100)
+print(acc1)
+print(acc1.full_info())
+acc2 = Account("Иванов Петр Иванович", 10101011, 89000110001)
+acc1.transfer(acc2, 50)
+print(acc2)
