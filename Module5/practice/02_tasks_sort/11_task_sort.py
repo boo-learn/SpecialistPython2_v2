c002009211ed0243e4c3b8a181b6f20d36ce4845
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-04-10 09:40:04 +0300

    Update 11_task_sort.py

--- Module5/practice/02_tasks_sort/11_task_sort.py
+++ Module5/practice/02_tasks_sort/11_task_sort.py
@@ -1,2 +1,20 @@
 # Анаграммы*
 # Задается словарь (список слов). Найти в нем все анаграммы (слова, составленные из одних и тех же букв).
+
+words = ['qwerty', 'ytrewq', 'wasd', 'sadw', 'zxc', 'vbnm', 'rtyuiqw', 'qwer', 'werqty', 'czx', 'y6u6', '6yu6']
+tmp_check = {}
+result = []
+
+while words:
+    tmp_check = tmp_check.fromkeys(words[0])
+    for word in words:
+        if tmp_check == dict.fromkeys(word):
+            result.append(word)
+    if len(result) == 1:
+        pass
+    else:
+        print('*' * 40)
+        print(*result)
+    for word in result:
+        words.remove(word)
+    result = []
