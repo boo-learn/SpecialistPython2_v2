a1015450fb114ef024aa7ac89e88c1c9d76d73dc
PortnovSergey <81014980+PortnovSergey@users.noreply.github.com>
2021-04-10 12:04:47 +0300

    Update 01_task_graph.py

--- Module7/practice/01_task_graph.py
+++ Module7/practice/01_task_graph.py
@@ -1,6 +1,31 @@
-# "Лабиринт"
-# см. Картинку на гугло-диске в Модуле-7
-# P-1, P-2 … - точки старта
-# Определите: Из каких точек можно дойти до выхода(F), а из каких нет
+#!/usr/bin
 
-# Сюда отправляем полное решение
+graph = [
+    [1],
+    [0, 3, 5],
+    [5],
+    [1, 7],
+    [8],
+    [6, 2, 1],
+    [5],
+    [3],
+    [4]
+]
+
+visited = [False] * (len(graph))
+prev = [None] * (len(graph))
+
+# вершина выхода
+start = 6
+
+
+def dfs(v):
+    visited[v] = True
+    for w in graph[v]:
+        if not visited[w]:  # посещён ли текущий сосед?
+            dfs(w)
+
+dfs(start)
+for i in [4, 2, 0, 3]:
+    if visited[i]:
+        print(f"From vertex {i} exit can be reached")
