9d5e22927e425ba8b3a81e17ba7f7f5d32851e6b
Eruungait <77881015+Eruungait@users.noreply.github.com>
2021-04-10 14:05:50 +0300

    Update 02_task_graph.py

--- Module7/practice/02_task_graph.py
+++ Module7/practice/02_task_graph.py
@@ -1,10 +1,66 @@
-# "Лабиринт с ключем"
-# см. Картинку на гугло-диске в Модуле-7
-# P-1, P-2 … - стартовые точки
-# Посетив клетку K, можно подобрать ключ, который отпирают любую дверь.
-# Определите:
-# Из каких точек можно добраться до точки F?
-# Какую дверь нужно открыть, чтобы добраться до точки P-2?
+def dfs(graph, v):
+    visited = [False] * (len(graph))
+
+    def _dfs(v):
+        visited[v] = True
+        for w in graph[v]:
+            if not visited[w]:  # посещён ли текущий сосед?
+                _dfs(w)
+
+    _dfs(v)
+    return visited
 
 
-# Сюда отправляем полное решение
+graph_with_doors = [
+    [1],  # 0
+    [0, 2],  # 1
+    [7, 1],  # 2
+    [6],  # 3
+    [],  # 4
+    [6],  # 5
+    [5, 3],  # 6
+    [2],  # 7
+    [9],  # 8
+    [8]  # 9
+]
+
+graph_without_doors = [
+    [10],  # 0
+    [10, 2, 5],  # 1
+    [7, 1],  # 2
+    [6],  # 3
+    [],  # 4
+    [6],  # 5
+    [5, 3],  # 6
+    [2],  # 7
+    [9],  # 8
+    [8],  # 9
+    [9, 0, 1]  # 10
+]
+
+point_finish = 3
+key_point = 7
+start_points = {
+    "P-1": 0,
+    "P-2": 8,
+    "P-3": 4,
+    "P-4": 2,
+    'P-5': 6,
+}
+door_points = {
+    'D-1': 9,
+    'D-2': 5,
+}
+
+for point_name, vertex in start_points.items():
+    visited = dfs(graph_with_doors, vertex)
+    if visited[point_finish]:
+        print(f"Из точки {point_name} можно дойти до финиша")
+    elif visited[key_point]:
+        visited = dfs(graph_without_doors, vertex)
+        if visited[point_finish]:
+            print(f"Из точки {point_name} можно дойти до финиша")
+        else:
+            print(f"Из точки {point_name} нельзя дойти до финиша")
+    else:
+        print(f"Из точки {point_name} нельзя дойти до финиша")
