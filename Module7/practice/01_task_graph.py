8fc027145fd729c26b12eb5c23d483e443ffa950
Roschupkin Andrey <81983142+a-roschupkin@users.noreply.github.com>
2021-04-12 19:49:14 +0300

    Update 01_task_graph.py

--- Module7/practice/01_task_graph.py
+++ Module7/practice/01_task_graph.py
@@ -1,6 +1,36 @@
-# "Лабиринт"
-# см. Картинку на гугло-диске в Модуле-7
-# P-1, P-2 … - точки старта
-# Определите: Из каких точек можно дойти до выхода(F), а из каких нет
+graph = [
+    # список смежности
+    [1],  # 0
+    [4, 2, 0],  # 1
+    [1, 3, 4],  # 2
+    [2],  # 3
+    [1, 2, 5, 6],  # 4
+    [4],  # 5
+    [4, 7, 8],  # 6
+    [6],  # 7
+    [6],  # 8
+    [10],  # 10
+    [9],  # 9
+]
 
-# Сюда отправляем полное решение
+
+def start_bfs(start):
+    lengths = [None] * (len(graph))
+    lengths[start] = start
+    queue = [start]
+    while queue:
+        cur_vertex = queue.pop(0)
+        for vertex in graph[cur_vertex]:
+            if lengths[vertex] is None:
+                lengths[vertex] = lengths[cur_vertex] + 1
+                queue.append(vertex)
+    return lengths
+
+
+start_point = 7
+finish_point = 8
+lengths = start_bfs(start_point).copy()
+if lengths[finish_point]:
+    print(f"Из точки {start_point} можно дойти до точки {finish_point}")
+else:
+    print(f"Из точки {start_point} нельзя дойти до точки {finish_point}")
