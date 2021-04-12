c84262e985e906892e00c484698bc380f4a612a9
Michael Nikitenko <xm4dn355x@gmail.com>
2021-04-12 16:16:15 +0500

    Update 09_task_sort.py
    Михаил Никитенко

--- Module5/practice/02_tasks_sort/09_task_sort.py
+++ Module5/practice/02_tasks_sort/09_task_sort.py
@@ -9,3 +9,26 @@ employees = [
 ]
 # Выведите список сотрудников(без указания должности) в формате: Фамилия Имя, в отсортированном порядке.
 # Примечание: если фамилии сотрудников совпадают, при сортировке учесть имя.
+
+from pprint import pprint
+
+
+def bubble_sort(nums, key=lambda x: x, reverse=False):
+   swapped = True
+   j = 0
+   while swapped:
+      swapped = False
+      for i in range(len(nums) - 1 - j):
+         if reverse:
+            expr = key(nums[i]) < key(nums[i + 1])
+         else:
+            expr = key(nums[i]) > key(nums[i + 1])
+         if expr:
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            swapped = True
+      j += 1
+   return nums
+
+
+if __name__ == '__main__':
+   pprint(bubble_sort(employees, key=(lambda x: (x['position'], x['surname'], x['name']))))
