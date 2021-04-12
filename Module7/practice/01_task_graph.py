b66acfd392467d665a98cfcc13b82d9e524e62a4
Deimos-szb <81982997+Deimos-szb@users.noreply.github.com>
2021-04-12 19:47:42 +0300

    Update 01_task_graph.py

--- Module7/practice/01_task_graph.py
+++ Module7/practice/01_task_graph.py
@@ -4,3 +4,35 @@
 # Определите: Из каких точек можно дойти до выхода(F), а из каких нет
 
 # Сюда отправляем полное решение
+
+
+graph = [
+    [1],  # 0
+    [0, 2, 4],  # 1
+    [1, 3, 4],  # 2
+    [2],  # 3
+    [1, 2, 5, 6],  # 4
+    [4],  # 5
+    [4, 7, 8],  # 6
+    [6],  # 7
+    [6],  # 8
+    [10],  # 9
+    [9],  # 10
+]
+
+starts = [0, 3, 9]
+finish = 8
+for start in starts:
+    lengths = [None] * (len(graph))
+    lengths[start] = 0
+    queue = [start]
+    while queue:
+        cur_vertex = queue.pop(0)
+        for vertex in graph[cur_vertex]:
+            if lengths[vertex] is None:
+                lengths[vertex] = lengths[cur_vertex] + 1
+                queue.append(vertex)
+    if lengths[finish]:
+        print(f'Из точки {start} в точку {finish} пройти можно за {lengths[finish]} шага(ов).')
+    else:
+        print(f'Из точки {start} в точку {finish} пройти низя.')
