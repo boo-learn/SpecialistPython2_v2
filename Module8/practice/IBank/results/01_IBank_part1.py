f0e74861f465864e0878bd77a6dc80675cfde117
Deimos-szb <81982997+Deimos-szb@users.noreply.github.com>
2021-04-13 19:32:55 +0300

    Update 01_IBank_part1.py

--- Module8/practice/IBank/results/01_IBank_part1.py
+++ Module8/practice/IBank/results/01_IBank_part1.py
@@ -1,1 +1,119 @@
-# Сюда отправляем готовое решение IBank часть-1
+# from generators import get_user_data
+from abc import ABC, abstractmethod
+
+
+class AccountBase(ABC):
+    def __init__(self, name, passport8, phone_number, start_balance=0):
+        self.name = name
+        self.passport8 = passport8
+        self.phone_number = phone_number
+        self.balance = start_balance
+
+    @abstractmethod
+    def transfer(self, target_account, amount):
+        """
+        Перевод денег на счет другого клиента
+        :param target_account: счет клиента для перевода
+        :param amount: сумма перевода
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def deposite(self, amount):
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        """
+        pass
+
+    @abstractmethod
+    def withdraw(self, amount):
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        """
+        pass
+
+    @abstractmethod
+    def full_info(self):
+        """
+        Полная информация о счете в формате: "Иванов Иван Петрович баланс: 100 руб. паспорт: 12345678 т.89002000203"
+        """
+        return f"..."
+
+    @abstractmethod
+    def __repr__(self):
+        """
+        :return: Информацию о счете в виде строки в формате "Иванов И.П. баланс: 100 руб."
+        """
+        return f"..."
+
+
+class Account(AccountBase):
+    accounts = []
+
+    def __init__(self, name, passport8, phone_number, start_balance=0):
+        self.name = name
+        self.passport8 = passport8
+        self.phone_number = phone_number
+        self.balance = start_balance
+        Account.accounts.append(self)
+
+    def transfer(self, target_account, amount):
+        """
+        Перевод денег на счет другого клиента
+        :param target_account: счет клиента для перевода
+        :param amount: сумма перевода
+        :return:
+        """
+        if target_account in [acc.passport8 for acc in Account.accounts]:
+            if amount <= self.balance:
+                self.balance -= amount
+                target_account.balance += amount
+            else:
+                print('Недостаточно средств на счете.')
+        else:
+            print('Получатель не найден среди клиентов банка.')
+
+    def deposite(self, amount):
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        """
+        self.balance += amount
+
+    def withdraw(self, amount):
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        """
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print('Недостаточно средств на счете.')
+
+    def full_info(self):
+        """
+        Полная информация о счете в формате: "Иванов Иван Петрович баланс: 100 руб. паспорт: 12345678 т.89002000203"
+        """
+        return f"{self.name} баланс: {self.balance}. Пасспорт: {self.passport8}. тел.: {self.phone_number}"
+
+    def __repr__(self):
+        """
+        :return: Информацию о счете в виде строки в формате "Иванов И.П. баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.balance}."
+
+
+man_1 = Account('Petr', 12345678, '+71235675656', 1000)
+man_2 = Account('Vasya', 43214442, '+71235675656', 5000)
+man_3 = Account('Kolya', 87654321, '+71235675656', 100)
+
+man_1.full_info()
+
+man_2.transfer(87654321, 3000)
+
+print(man_2)
+
+print(man_3)
