e6363b778a0f1f1acbb6e7b85242961bee9af70a
Michael Nikitenko <xm4dn355x@gmail.com>
2021-04-13 21:33:43 +0500

    Update 01_IBank_part1.py

--- Module8/practice/IBank/results/01_IBank_part1.py
+++ Module8/practice/IBank/results/01_IBank_part1.py
@@ -1,1 +1,119 @@
-# Сюда отправляем готовое решение IBank часть-1
+# from generators import get_user_data
+from abc import ABC, abstractmethod
+
+EMPLOYEE_PASSWORD = "123"
+
+
+class AccountBase(ABC):
+    def __init__(self, name, passport8, phone_number, start_balance=0):
+        self.name = name
+        self.passport8 = passport8
+        self.phone_number = phone_number
+        self.balance = start_balance
+
+    @abstractmethod
+    def transfer(self, target_account, amount):
+        """
+        Перевод денег на счет другого клиента
+        :param target_account: счет клиента для перевода
+        :param amount: сумма перевода
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def deposit(self, amount):
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        """
+        pass
+
+    @abstractmethod
+    def withdraw(self, amount):
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        """
+        pass
+
+    @abstractmethod
+    def full_info(self):
+        """
+        Полная информация о счете в формате: "Иванов Иван Петрович баланс: 100 руб. паспорт: 12345678 т.89002000203"
+        """
+        return f"..."
+
+    @abstractmethod
+    def __repr__(self):
+        """
+        :return: Информацию о счете в виде строки в формате "Иванов И.П. баланс: 100 руб."
+        """
+        return f"..."
+
+
+class Account(AccountBase):
+    def __init__(self, name, passport8, phone_number, start_balance=0):
+        super().__init__(name, passport8, phone_number, start_balance)
+
+    def transfer(self, target_account, amount):
+        """
+        Перевод денег на счет другого клиента
+        :param target_account: счет клиента для перевода
+        :param amount: сумма перевода
+        :return:
+        """
+        if amount <= self.balance:
+            target_account.deposit(amount)
+            self.withdraw(amount)
+
+    def deposit(self, amount):
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        """
+        if self.balance + amount >= 0:
+            self.balance += amount
+            return self.balance
+        else:
+            return False
+
+    def withdraw(self, amount):
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        """
+        if self.balance >= amount:
+            self.balance -= amount
+            return amount
+        else:
+            return False
+
+    def full_info(self):
+        """
+        Полная информация о счете в формате: "Иванов баланс: 100 руб. паспорт: 12345678 т.89002000203"
+        """
+        return f"{self.name} баланс: {self.balance} руб. паспорт: {self.passport8} т.{self.phone_number}"
+
+    def __repr__(self):
+        """
+        :return: Информацию о счете в виде строки в формате "Иванов И.П. баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.balance} руб."
+
+
+if __name__ == '__main__':
+    ivan = Account('Иван', '12345678', 90000000000, start_balance=1000)
+    oleg = Account('Иван', '23456789', 90000000001, start_balance=10000)
+    print(ivan)
+    print(oleg)
+    ivan.deposit(100)
+    print(ivan)
+    ivan.withdraw(1000000)
+    print(ivan)
+    ivan.transfer(oleg, 1000000)
+    print(ivan)
+    print(oleg)
+    ivan.transfer(oleg, 500)
+    print(ivan)
+    print(oleg)
