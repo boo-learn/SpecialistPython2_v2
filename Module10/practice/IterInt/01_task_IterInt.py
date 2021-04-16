06f430744ce505f1fc6135361fa0f3abd67a0027
Deimos-szb <81982997+Deimos-szb@users.noreply.github.com>
2021-04-16 20:14:16 +0300

    Update 01_task_IterInt.py

--- Module10/practice/IterInt/01_task_IterInt.py
+++ Module10/practice/IterInt/01_task_IterInt.py
@@ -2,17 +2,24 @@
 # возможность итерировать по цифрам числа
 
 class IterInt(int):
-    pass
+    def __init__(self, x):
+        super().__init__()
+        self.count = 0
+
+    def __iter__(self):
+        self.count = 0
+        return self
+
+    def __next__(self):
+        try:
+            num = str(self)[self.count]
+            self.count += 1
+        except IndexError:
+            raise StopIteration
+        return num
 
 
 n = IterInt(12346)
 
 for digit in n:
     print("digit = ", digit)
-
-# Выведет:
-# digit = 1
-# digit = 2
-# digit = 3
-# digit = 4
-# digit = 6
