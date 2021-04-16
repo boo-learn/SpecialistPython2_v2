e2577b62a4f771f62a7c8954fcb22c8bc71c4212
Michael Nikitenko <xm4dn355x@gmail.com>
2021-04-16 17:52:25 +0500

    Update 02_task_collect.py
    Михаил Никитенко

--- Module9/practice/collection_tasks/02_task_collect.py
+++ Module9/practice/collection_tasks/02_task_collect.py
@@ -8,4 +8,38 @@
 # Для решения задачи подберите наиболее удобную структуру и заполните
 # Выведите: учеников, изучающих только один язык
 
-# TODO: your code here...
+
+from collections import Counter
+
+
+students = [
+    {'name': 'Вася', 'surname': 'Пупкин', 'languages': ['english', 'french']},
+    {'name': 'Женя', 'surname': 'Скарибидис', 'languages': ['english', ]},
+    {'name': 'Петя', 'surname': 'Пёс', 'languages': ['french', ]},
+    {'name': 'Эмиль', 'surname': 'Гусейнов', 'languages': ['english', ]},
+    {'name': 'Витя', 'surname': 'Евстафьев', 'languages': ['english', 'french']},
+    {'name': 'Саша', 'surname': 'Лищенко', 'languages': ['french', ]},
+    {'name': 'Серёга', 'surname': 'Кувалда', 'languages': ['english', ]},
+    {'name': 'Маша', 'surname': 'Загорская', 'languages': ['english', 'french']},
+    {'name': 'Кристина', 'surname': 'Шаманова', 'languages': ['english', ]},
+    {'name': 'Данил', 'surname': '1JZ-GE', 'languages': ['html']},  # Что-то настроение хорошее))
+]
+
+def count_single_language_students(students):
+    counter = Counter()
+    for student in students:
+        languages = student['languages']
+        if len(languages) == 1:
+            counter[languages[0]] += 1
+    return counter
+
+
+if __name__ == '__main__':
+    print('Иностранные языки у учеников')
+    counter = count_single_language_students(students)
+    res = ''
+    single_languaged = 0
+    for c in counter.most_common():
+        single_languaged += c[1]
+        res += f'язык: {c[0]} учеников: {c[1]}\n'
+    print(f'Среди учеников изучают один язык: {single_languaged}\n' + res)
