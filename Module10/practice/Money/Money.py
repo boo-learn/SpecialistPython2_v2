a6a2ed7fa544dcc83bb708d29d1c478d5404bde4
YuryTomgorov <81982991+YuryTomgorov@users.noreply.github.com>
2021-04-16 19:03:39 +0300

    Update Money.py

--- Module10/practice/Money/Money.py
+++ Module10/practice/Money/Money.py
@@ -1,3 +1,5 @@
-class Money:
-    pass
-    # TODO: your code here
+    def perevod_v_rubli(self):
+        while self.kopeyki >100:
+            if self.kopeyki > 100:
+                self.kopeyki -= 100
+                self.rubli += 1
