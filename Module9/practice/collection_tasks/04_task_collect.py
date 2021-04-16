497a1c15d163952494069c3a9ab422ffd82d4d8a
Michael Nikitenko <xm4dn355x@gmail.com>
2021-04-16 19:41:46 +0500

    Update 04_task_collect.py
    Михаил Никитенко

--- Module9/practice/collection_tasks/04_task_collect.py
+++ Module9/practice/collection_tasks/04_task_collect.py
@@ -6,4 +6,30 @@
 # Например, слово «Help» превратится в «…. . .-.. .—.».
 # Обратите внимание, что строчные и заглавные буквы кодируются одинаково.
 
-# TODO: your code here...
+
+from collections import OrderedDict
+
+text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore" \
+       " magna aliqua."
+d = {
+    'a': '-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.', 'h': '....', 'i': '..',
+    'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-', 'r': '.-.',
+    's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-', 'y': '-.--', 'z': '--..', '0': '-----',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+    '9': '----.', ',': '.-.-.-', '.': '......', ' ': ' '}
+morze_alphabet = OrderedDict(sorted(d.items(), key=lambda t: t[0]))
+
+
+def morze(text):
+    text = text.strip().lower()
+    res = ''
+    for letter in text:
+        res += morze_alphabet[letter]
+    res += '..-.-'  # Сигнал конец связи
+    return res
+
+
+if __name__ == '__main__':
+    print('Азбука морзе')
+    print(text)
+    print(morze(text))
