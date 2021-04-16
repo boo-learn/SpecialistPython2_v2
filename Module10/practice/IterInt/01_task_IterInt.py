bbe5b5092239071f89aa785d532e5c1adada6941
Michael Nikitenko <xm4dn355x@gmail.com>
2021-04-16 22:18:47 +0500

    Update 01_task_IterInt.py
    Михаил Никитенко

--- Module10/practice/IterInt/01_task_IterInt.py
+++ Module10/practice/IterInt/01_task_IterInt.py
@@ -2,13 +2,35 @@
 # возможность итерировать по цифрам числа
 
 class IterInt(int):
-    pass
+    def __init__(self, value):
+        try:
+            self.value = int(value)
+        except ValueError:
+            raise ValueError(f'Необходимо ввести целое число, вместо этого было введено {type(value)} {value}')
+        self.iteration = 0
+
+    def __str__(self):
+        return f'IterInt {self.value}'
+
+    def __iter__(self):
+        self.iteration = 0
+        return self
+
+    def __next__(self):
+        if self.iteration >= len(str(self.value)):
+            self.iteration = 0
+            raise StopIteration
+        value = int(str(self.value)[self.iteration])
+        self.iteration += 1
+        return value
 
 
 n = IterInt(12346)
 
 for digit in n:
     print("digit = ", digit)
+
+n = IterInt('1s23456f')
 
 # Выведет:
 # digit = 1
