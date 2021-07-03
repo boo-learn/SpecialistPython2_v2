cbd548b361c8f5d93821a8a6e79e34cf70e96d0c
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-07-03 17:00:18 +0300

    Update 02_card.py

--- Module2/practice/deck/02_card.py
+++ Module2/practice/deck/02_card.py
@@ -1,13 +1,15 @@
 # Начнем с создания карты
 class Card:
     def __init__(self, value, suit):
-        pass
+        self.value = value
+        self.suit = suit
 
     def to_str(self):
-        pass
+        suit = {'Hearts': '\u2665', 'Diamonds': '\u2666', 'Spades': '\u2660', 'Clubs': '\u2663'}
+        return f'{self.value}{suit[self.suit]}'
 
     def equal_suit(self, other_card):
-        pass
+        return self.suit == other_card.suit
 
 
 # Создадим несколько карт
@@ -17,6 +19,11 @@ card2 = Card("A", "Diamonds")
 # Выведем карты на экран в виде: 10♥ и A♦
 print(card1.to_str())
 print(card2.to_str())
+
+# print('\u2661', '\u2665')
+# print('\u2662', '\u2666')
+# print('\u2667', '\u2663')
+# print('\u2664', '\u2660')
 
 # Проверим, одинаковые ли масти у карт
 if card1.equal_suit(card2):
