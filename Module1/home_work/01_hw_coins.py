c08522b3f8fa44765244ad7c7f5a7139f2c93376
Anna Zhuravleva <zhuravlevahanna@gmail.com>
2021-07-03 15:59:55 +0300

    Update 01_hw_coins.py

--- Module1/home_work/01_hw_coins.py
+++ Module1/home_work/01_hw_coins.py
@@ -10,8 +10,17 @@ class Coin:
         """
         Подбрасывание монетки
         """
-        self.side = ...  # random: heads/tails
+        self.side = random.choice(['head', 'tail'])
 
+
+if __name__ == '__main__':
+    coins = [Coin()] * random.choice(range(100))
+    heads = 0
+    for coin in coins:
+        coin.flip()
+        heads += coin.side == 'head'
+    print(f'Среди выпавших монеток {round(heads/len(coins), 4) * 100}%'
+          f' - орлы')
 
 # Задание: создайте список из n-монеток. Подбросьте(flip) все монетки.
 # выведите соотношение выпавших орлов и решек в процентах
