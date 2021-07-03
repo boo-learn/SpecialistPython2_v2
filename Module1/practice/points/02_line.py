ca85f4e39f9317fa12d17b122290cd791e18cd1c
alexs2011 <kir-al93@yandex.ru>
2021-07-03 16:34:44 +0700

    Update 02_line.py

--- Module1/practice/points/02_line.py
+++ Module1/practice/points/02_line.py
@@ -4,11 +4,17 @@ class Point:
         self.y = y
 
 
+def distance(p1, p2):
+    return ((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2) ** 0.5
+
+
 # Ломаная линия задана произвольным количеством последовательных точек
 points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 
 # Задание: Найдите длину ломаной линии
 
-# TODO: your core here...
+length = 0
+for i in range(len(points) - 1):
+    length += distance(points[i], points[i + 1])
 
-print("Длина ломаной линии = ", ...)
+print("Длина ломаной линии = ", length)
