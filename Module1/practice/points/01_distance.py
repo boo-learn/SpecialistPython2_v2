b2d3841d24470dc18ba2de9f029773830bf0319d
Anattanifakku <83123616+Anattanifakku@users.noreply.github.com>
2021-07-03 20:29:17 +1100

    Update 01_distance.py

--- Module1/practice/points/01_distance.py
+++ Module1/practice/points/01_distance.py
@@ -4,18 +4,16 @@ class Point:
         self.y = y
 
 
-def distance():
-    """
-    Расстояние между двумя точками
-    """
-
-
-# Дано две точки на координатной плоскости
 points1 = Point(2, 4)
 point2 = Point(5, -2)
 
-# Задание: Найдите расстояние между этими точками. Реализовав и используя функцию distance()
 
-# TODO: your core here...
-
-print("Расстояние между точками = ", ...)
+def distance(x, y):
+    x = points1.x
+    x1 = point2.x
+    y = points1.y
+    y1 = point2.y
+    dist = (((x1 - x) ** 2) + ((y1 - y) ** 2)) ** 0.5
+    dist = round(dist, 2)
+    return dist
+print(distance(points1, point2))
