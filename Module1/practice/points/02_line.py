85aabbfa6da6521020a11f0609bd0e244a0ede7f
AIKarpukhin <86874081+AIKarpukhin@users.noreply.github.com>
2021-07-03 12:54:43 +0300

    Update 02_line.py

--- Module1/practice/points/02_line.py
+++ Module1/practice/points/02_line.py
@@ -12,3 +12,23 @@ points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 # TODO: your core here...
 
 print("Длина ломаной линии = ", ...)
+
+
+def leng(p1, p2):
+    """
+    Расстояние между двумя точками
+    """
+    #     x1-x2**2 + y1-y2**2
+    return ((p1.x-p2.x)**2 + (p1.y-p2.y)**2)**0.5
+    
+
+# Задание: Найдите расстояние между этими точками. Реализовав и используя функцию distance()
+length = 0
+for i in range(len(points)-1):
+    length = leng(points[i], points[i+1]) + length 
+
+
+
+print("Расстояние между точками = ", length)
+#© 2021 GitHub, Inc.
+
