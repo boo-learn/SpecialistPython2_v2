3352ffe0f2943a8bc3ddcf47d1a45d60ee907556
JackMan1989 <86956534+JackMan1989@users.noreply.github.com>
2021-07-05 12:48:32 +0300

    Update 02_line.py

--- Module1/practice/points/02_line.py
+++ Module1/practice/points/02_line.py
@@ -4,11 +4,19 @@ class Point:
         self.y = y
 
 
+def crocked_line_length(points_list: list):
+    line_len = 0
+    for i in range(1, len(points_list)):
+        line_len += ((points_list[i].x - points_list[i - 1].x)**2 + (points_list[i].y - points_list[i - 1].y)**2)**0.5
+    return line_len
+
+
 # Ломаная линия задана произвольным количеством последовательных точек
 points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
+
 
 # Задание: Найдите длину ломаной линии
 
 # TODO: your core here...
 
-print("Длина ломаной линии = ", ...)
+print("Длина ломаной линии = ", crocked_line_length(points))
