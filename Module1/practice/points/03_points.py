510f0f72e2f0e700cc195f84d8ceb3651f01cb8c
Timur <58703012+tims0n777@users.noreply.github.com>
2021-07-05 15:24:16 +0500

    Update 03_points.py

--- Module1/practice/points/03_points.py
+++ Module1/practice/points/03_points.py
@@ -8,7 +8,21 @@ class Point:
 points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)]
 
 # Задание: найдите точку наиболее удаленную от начала координат и выведите ее координаты
+def distance(point1: Point, point2: Point):
+    '''
+    Расстояние между двумя точками
+    :param point1: объект типа Point
+    :param point2: объект типа Point
+    :return: возвращает скалярное расстояние между двумя точками
+    '''
+    return ((point1.x - point2.x)**2 + (point1.y - point2.y)**2)**0.5
 
-# TODO: your core here...
+p0 = Point(0, 0)
+d = 0
 
-print("Координаты наиболее удаленной точки = ", ...)
+for pnt in points:
+    if d < distance(p0, pnt):
+        d = distance(p0, pnt)
+        p_max = pnt
+
+print(f"Координаты наиболее удаленной точки = ({p_max.x}, {p_max.y})")
