1a02040fea005dd217db6f6d37dc4551cfcccbd9
pisarev <2368404+pisarev@users.noreply.github.com>
2021-07-05 12:36:54 +0300

    Update 04_color_points.py

--- Module1/practice/points/04_color_points.py
+++ Module1/practice/points/04_color_points.py
@@ -1,11 +1,12 @@
+import math
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color = ""):
         self.x = x
         self.y = y
-        self.color = ...
+        self.color = color
 
-    def dist_to(self, other_point):
-        ...
+    def distance(self, point):
+        return math.sqrt((self.x - point.x) ** 2 + (self.y - point.y) ** 2)
 
 
 # Дан список точек нарисованных красным(red) и зеленым(green) цветами
@@ -23,8 +24,25 @@ points = [
 # Задание-1: доработайте конструкто class Point для хранения цвета точки
 # Задание-2: реализуйте метод dist_to()
 # Задание-3: вычислите площади треугольников образованных точками разных цветов
+class Triangle:
+    a = 0
+    b = 0
+    c = 0
+    def __init__(self, points):
+        self.a = points[0].distance(rpoints[1])
+        self.b = points[0].distance(rpoints[2])
+        self.c = points[1].distance(rpoints[2])
+    def area(self):
+        p = (self.a + self.b + self.c) / 2
+        return math.sqrt(p * (p - self.a) * (p - self.b) * (p - self.c))
 
-# TODO: your core here...
+rpoints = []
+gpoints = []
+for point in points:
+    if point.color == "red":
+        rpoints.append(point)
+    elif point.color == "green":
+        gpoints.append(point)
 
-print("Площадь красного треугольника = ", ...)
-print("Площадь зеленого треугольника = ", ...)
+print("Площадь зеленого треугольника = ", Triangle(rpoints).area())
+print("Площадь красного треугольника = ", Triangle(gpoints).area())
