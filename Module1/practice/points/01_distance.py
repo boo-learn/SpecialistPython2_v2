5270a040c395cfa3b409b12dcb14a9ef3122065c
Timur <58703012+tims0n777@users.noreply.github.com>
2021-07-05 15:20:59 +0500

    Update 01_distance.py

--- Module1/practice/points/01_distance.py
+++ Module1/practice/points/01_distance.py
@@ -3,19 +3,18 @@ class Point:
         self.x = x
         self.y = y
 
-
-def distance():
-    """
+def distance(point1: Point, point2: Point):
+    '''
     Расстояние между двумя точками
-    """
-
+    :param point1: объект типа Point
+    :param point2: объект типа Point
+    :return: возвращает скалярное расстояние между двумя точками
+    '''
+    return ((point1.x - point2.x)**2 + (point1.y - point2.y)**2)**0.5
 
 # Дано две точки на координатной плоскости
-points1 = Point(2, 4)
+point1 = Point(2, 4)
 point2 = Point(5, -2)
 
 # Задание: Найдите расстояние между этими точками. Реализовав и используя функцию distance()
-
-# TODO: your core here...
-
-print("Расстояние между точками = ", ...)
+print("Расстояние между точками = ", distance(point1, point2))
