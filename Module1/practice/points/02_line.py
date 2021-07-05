1eb27b19ac25e999691b75ebf1b50ac6f05e65ae
Timur <58703012+tims0n777@users.noreply.github.com>
2021-07-05 15:23:08 +0500

    Update 02_line.py

--- Module1/practice/points/02_line.py
+++ Module1/practice/points/02_line.py
@@ -7,8 +7,24 @@ class Point:
 # Ломаная линия задана произвольным количеством последовательных точек
 points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 
+def distance(point1: Point, point2: Point):
+    '''
+    Расстояние между двумя точками
+    :param point1: объект типа Point
+    :param point2: объект типа Point
+    :return: возвращает скалярное расстояние между двумя точками
+    '''
+    return ((point1.x - point2.x)**2 + (point1.y - point2.y)**2)**0.5
+
 # Задание: Найдите длину ломаной линии
 
-# TODO: your core here...
+i = 0
+d = 0
 
-print("Длина ломаной линии = ", ...)
+while i < len(points):
+    if i > 0:
+        d += distance(points[i-1], points[i])
+    i += 1
+
+
+print("Длина ломаной линии = ", d)
