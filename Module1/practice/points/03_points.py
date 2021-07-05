169c75a14ea38c4d7e2820360ebdff5d59bb7f78
ua6mq <86956414+ua6mq@users.noreply.github.com>
2021-07-05 14:02:46 +0300

    Update 03_points.py

--- Module1/practice/points/03_points.py
+++ Module1/practice/points/03_points.py
@@ -11,4 +11,18 @@ points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)
 
 # TODO: your core here...
 
-print("Координаты наиболее удаленной точки = ", ...)
+def distance(p1, p2):
+    """
+    Расстояние между двумя точками
+    """
+    return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
+max = 0
+i = 0
+# TODO: your core here...
+while i < len(points)-1:
+    point1 = points[i]
+    point2 = points[i+1]
+    if distance(point1,point2) > max: max = distance(point1,point2)
+    i+=1
+
+print("Координаты наиболее удаленной точки = ", max)
