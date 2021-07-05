0a1adcc4587ab5c15c60169379dac9312ee49bb2
RBearThom <86956501+RBearThom@users.noreply.github.com>
2021-07-05 14:07:52 +0300

    Update 04_color_points.py

--- Module1/practice/points/04_color_points.py
+++ Module1/practice/points/04_color_points.py
@@ -1,11 +1,11 @@
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        self.color = ...
+        self.color = color
 
     def dist_to(self, other_point):
-        ...
+        return ( (self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2 ) ** 0.5
 
 
 # Дан список точек нарисованных красным(red) и зеленым(green) цветами
@@ -24,7 +24,25 @@ points = [
 # Задание-2: реализуйте метод dist_to()
 # Задание-3: вычислите площади треугольников образованных точками разных цветов
 
-# TODO: your core here...
 
-print("Площадь красного треугольника = ", ...)
-print("Площадь зеленого треугольника = ", ...)
+def triangle_area(p1, p2, p3):
+    return abs(p1.x*p2.y + p2.x*p3.y + p3.x*p1.y - p2.x*p1.y - p3.x*p2.y - p1.x*p3.y) / 2
+
+
+def count_colored_triangle_area(points, color):
+    area = 0
+    triangle = []
+    for point in points:
+        if point.color == color:
+            triangle.append(point)
+    num_points = len(triangle)
+    if num_points != 3:
+        print(f"Warning: invalid number of '{color}' points for a triangle: {num_points}")
+    else:
+        area = triangle_area(*triangle)
+    return area
+
+
+print("Площадь красного треугольника = ", count_colored_triangle_area(points, "red"))
+print("Площадь зеленого треугольника = ", count_colored_triangle_area(points, "green"))
+
