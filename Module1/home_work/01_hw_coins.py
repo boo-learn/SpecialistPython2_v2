b338bee8f098de0f8aab7209a22ff0ab0d81e074
Bioalchemist <81476572+Bioalchemist@users.noreply.github.com>
2021-07-06 09:38:20 +0300

    Update 01_hw_coins.py

--- Module1/home_work/01_hw_coins.py
+++ Module1/home_work/01_hw_coins.py
@@ -1,16 +1,27 @@
-import random
-
-
 class Coin:
     def __init__(self):
         # heads-орел/tails-решка
         self.side = None
 
     def flip(self):
-        """
-        Подбрасывание монетки
-        """
-        self.side = ...  # random: heads/tails
+        sides = ['tails','heads']
+        return random.choice(sides)# Подбрасывание монетки
+
+
+n = 100
+coins = []
+for i in range(n):
+    coins.append(Coin())
+numb_heads = 0
+numb_tails = 0
+
+coin_list = []
+for coin in coins:
+    if  coin.flip == "tails":
+        numb_tails += 1
+    else:
+        numb_heads += 1
+print (f"решка: {numb_tails/n*100} орел: {numb_heads/n*100} ")
 
 
 # Задание: создайте список из n-монеток. Подбросьте(flip) все монетки.
