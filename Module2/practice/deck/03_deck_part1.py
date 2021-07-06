3fafad8b676e4b1578e745db81f7e78154aaa94c
RBearThom <86956501+RBearThom@users.noreply.github.com>
2021-07-06 10:55:51 +0300

    Update 03_deck_part1.py

--- Module2/practice/deck/03_deck_part1.py
+++ Module2/practice/deck/03_deck_part1.py
@@ -1,23 +1,62 @@
+import random
+
 class Card:
-    pass
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
+    suits = {
+        "Hearts":   '\u2665',
+        "Spades":   '\u2660',
+        "Clubs":    '\u2663',
+        "Diamonds": '\u2666'
+    }
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+        pass
+
+    def to_str(self):
+        return f"{self.value}{Card.suits[self.suit]}"
+        pass
+
+    def equal_suit(self, other_card):
+        return  self.suit == other_card.suit
 
 
 # Задание: Теперь создадим колоду из 52-ух карт и реализуем все методы
 class Deck:
+    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     def __init__(self):
         # Список карт в колоде. Каждым элементом списка будет объект класса Card
         self.cards = []
+        for suit, _ in Card.suits.items():
+            for value in Deck.values:
+                self.cards.append(Card(value, suit))
+
 
     def show(self):
-        pass
+        deck_str = f"deck[{len(self.cards)}]: "
+        for i, card in enumerate(self.cards):
+            deck_str += f"{card.to_str()}"
+            if i < len(self.cards)-1:
+                deck_str += ", "
+        return deck_str
 
     def draw(self, x):
-        pass
+        cards_draw = self.cards[0:x]
+        self.cards = self.cards[x:]
+        return cards_draw
 
     def shuffle(self):
+        random.shuffle(self.cards)
         pass
 
+
+def show_hand(hand_list):
+    hand_str = f"hand[{len(hand_list)}]: "
+    for i, card in enumerate(hand_list):
+        hand_str += f"{card.to_str()}"
+        if i < len(hand_list) - 1:
+            hand_str += ", "
+    return hand_str
 
 # Создаем колоду
 deck = Deck()
@@ -32,4 +71,4 @@ hand = deck.draw(5)
 # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
 print(deck.show())
 # Выводим список карт "в руке"(список hand)
-print(...)
+print(show_hand(hand))
