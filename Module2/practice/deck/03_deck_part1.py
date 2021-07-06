f50f189070017dcc6364ae4be5e3ba32c05b155d
Khasan09 <86655038+Khasan09@users.noreply.github.com>
2021-07-06 11:05:53 +0300

    Update 03_deck_part1.py

--- Module2/practice/deck/03_deck_part1.py
+++ Module2/practice/deck/03_deck_part1.py
@@ -1,5 +1,5 @@
 class Card:
-    pass
+    passCancel changes
     # TODO: сюда копируем реализацию класса карты из предыдущего задания
 
 
@@ -33,3 +33,56 @@ hand = deck.draw(5)
 print(deck.show())
 # Выводим список карт "в руке"(список hand)
 print(...)
+
+
+
+
+
+import random
+
+
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def to_str(self):
+        icons = {
+            "Hearts": '\u2665',
+            "Diamonds": '\u2666',
+            "Clubs": '\u2663',
+            "Spades": '\u2660',
+        }
+        return f"{self.value}{icons[self.suit]}"
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
+
+
+# Задание: Теперь создадим колоду из 52-ух карт и реализуем все методы
+class Deck:
+    def __init__(self):
+        values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        self.cards = []
+        # Список карт в колоде. Каждым элементом списка будет объект класса Card
+        for suit in suits:
+            for value in values:
+                card = Card(value, suit)
+                self.cards.append(card)
+
+    def show(self):
+        deck_str = f"deck[{len(self.cards)}]"
+        for card in self.cards:
+            deck_str += card.to_str() + ","
+        return deck_str
+
+    def draw(self, x):
+        _draw = []
+        for i in range(0, x):
+            _draw.append([self.cards])
+            del self.cards[i]
+        return _draw
+
+    def shuffle(self):
+        random.shuffle(self.cards)
