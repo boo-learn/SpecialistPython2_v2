d398866b2368729b374ae95f340b000c6fc5bd50
RBearThom <86956501+RBearThom@users.noreply.github.com>
2021-07-06 20:51:33 +0300

    Update 02_task_deck.py

--- Module2/practice/deck/deck_tasks/02_task_deck.py
+++ Module2/practice/deck/deck_tasks/02_task_deck.py
@@ -1,1 +1,104 @@
-# Сюда отправляем решение второй задачи с колодой
+import random
+
+
+class Card:
+    HEARTS   = "Hearts"
+    SPADES   = "Spades"
+    CLUBS    = "Clubs"
+    DIAMONDS = "Diamonds"
+    suits = {
+        HEARTS:   '\u2665',
+        SPADES:   '\u2660',
+        CLUBS:    '\u2663',
+        DIAMONDS: '\u2666'
+    }
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+        pass
+
+    def __repr__(self):
+        return f"{self.value}{Card.suits[self.suit]}"
+        pass
+
+    def __gt__(self, other):
+        value_index_self  = Deck.values.index(self.value)
+        value_index_other = Deck.values.index(other.value)
+        if value_index_self == value_index_other:
+            suit_index_self = Deck.suits_order.index(self.suit)
+            suit_index_other = Deck.suits_order.index(other.suit)
+            if suit_index_self > suit_index_other:
+                return True
+        if value_index_self > value_index_other:
+            return True
+        return False
+
+    def __lt__(self, other):
+        value_index_self  = Deck.values.index(self.value)
+        value_index_other = Deck.values.index(other.value)
+        if value_index_self == value_index_other:
+            suit_index_self = Deck.suits_order.index(self.suit)
+            suit_index_other = Deck.suits_order.index(other.suit)
+            if suit_index_self < suit_index_other:
+                return True
+        if value_index_self < value_index_other:
+            return True
+        return False
+    
+    def __eq__(self, other):
+        return self.value == other.value and self.suit == other.suit
+
+
+class Deck:
+    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    suits_order = ["Spades", "Clubs", "Diamonds", "Hearts"]
+    
+    def __init__(self):
+        self.cards = []
+        for suit, _ in Card.suits.items():
+            for value in Deck.values:
+                self.cards.append(Card(value, suit))
+
+    def __repr__(self):
+        deck_str = f"deck[{len(self.cards)}]: "
+        for i, card in enumerate(self.cards):
+            deck_str += f"{card}"
+            if i < len(self.cards) - 1:
+                deck_str += ", "
+        return deck_str
+
+    def draw(self, x):
+        cards_draw = self.cards[0:x]
+        self.cards = self.cards[x:]
+        return cards_draw
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+        pass
+
+
+def main():
+    deck = Deck()
+    deck.shuffle()
+    #print(deck)
+    cards_drawn = deck.draw(10)
+    #print(deck)
+    count_cards = [0 ,0 ,0, 0]
+    print(cards_drawn)
+    for card in cards_drawn:
+        count_cards[Deck.suits_order.index(card.suit)] += 1
+    print(count_cards)
+    
+    max_suits = max(count_cards)
+    all_max_suits_inds = []
+    for i, num_suits in enumerate(count_cards):
+        if num_suits == max_suits:
+            all_max_suits_inds.append(i)
+    for i in all_max_suits_inds:
+        print(f"Most of drawn cards are '{Deck.suits_order[i]}'.")
+    
+    
+    
+if __name__ == "__main__":
+    main()
