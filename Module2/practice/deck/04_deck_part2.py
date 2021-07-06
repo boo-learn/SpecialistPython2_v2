4f6a4ffff0227b9666ca96e8389aa9d7a8e8a885
vikaafonina <86655057+vikaafonina@users.noreply.github.com>
2021-07-06 11:56:55 +0300

    Update 04_deck_part2.py

--- Module2/practice/deck/04_deck_part2.py
+++ Module2/practice/deck/04_deck_part2.py
@@ -26,3 +26,47 @@ if card1.more(card2):
     print(f"{card1.to_str()} больше {card2.to_str()}")
 if card1.less(card2):
     print(f"{card1.to_str()} меньше {card2.to_str()}")
+class Card:
+    pass
+def more(self, other_card):
+    while card1 > card2
+        return ("more than")
+
+    # TODO: сюда копируем реализацию класса карты из предыдущего задания
+    #  реализуем новые методы
+    def more(self, other_card):
+        pass
+
+    def less(self, other_card):
+        pass
+def less(self, other_card):
+    while card1 < card2
+        return ("less than")
+
+
+class Deck:
+    pass
+    # TODO: сюда копируем реализацию класса колоды из предыдущего задания
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+        self.value = value
+        self.suit = suit
+    def to_str(self):
+        icons = {
+            "Hearts": "\u2665",
+            "Clubs": "\u2663",
+            "Diamonds": "\u2666",
+            "Spades":"\u2660"
+        }
+        return f"{self.value}{icons[self.suit]}"
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
+        # return("same suit")
+# class Deck:
+#     pass
+#     # TODO: сюда копируем реализацию класса колоды из предыдущего задания
+
+
+deck = Deck()
