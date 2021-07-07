254a6ca16d9d18940fecfc67c920516ec4f33313
AlexanderKotyuk <86956454+AlexanderKotyuk@users.noreply.github.com>
2021-07-07 16:02:23 +0300

    Update 09_task_sort.py

--- Module5/practice/02_tasks_sort/09_task_sort.py
+++ Module5/practice/02_tasks_sort/09_task_sort.py
@@ -9,3 +9,21 @@ employees = [
 ]
 # Выведите список сотрудников(без указания должности) в формате: Фамилия Имя, в отсортированном порядке.
 # Примечание: если фамилии сотрудников совпадают, при сортировке учесть имя.
+
+f_lambda = lambda name: name.lower()
+swapped = True
+
+while swapped:
+    swapped = False
+    for i in range(len(employees) - 1):
+        if employees[i]['surname'].lower() > employees[i+1]['surname'].lower():
+            employees[i], employees[i+1] = employees[i+1], employees[i]
+            swapped = True
+        elif employees[i]['surname'].lower() == employees[i+1]['surname'].lower():
+            if employees[i]['name'].lower() > employees[i+1]['name'].lower():
+                employees[i], employees[i + 1] = employees[i + 1], employees[i]
+                swapped = True
+
+
+for emp in employees:
+    print(emp['surname'], emp['name'])
