dc9dadb8cf6ec9aacb8343b4d077824aed635557
pisarev <2368404+pisarev@users.noreply.github.com>
2021-07-07 12:06:08 +0300

    Update 02_base_sort.py

--- Module5/practice/01_bases_sort/02_base_sort.py
+++ Module5/practice/01_bases_sort/02_base_sort.py
@@ -1,26 +1,41 @@
-# Все алгоритмы сортировки из examples/ оберните в функции
+import random
 
-def bubble_sort():
-    pass
-
-
-def sort_choice():
-    pass
-
-
-def quick_sort():
-    pass
-
+count = 0
+def quick_sort(data, lindex, rindex):
+    count = 0
+    i = lindex
+    j = rindex
+    p = (lindex + rindex) // 2
+    while True:
+        while data[i] < data[p]:
+            i += 1
+        while data[j] > data[p]:
+            j -= 1
+        if i <= j:
+            if i < j:
+                if p == i:
+                    p = j
+                elif p == j:
+                    p = i
+                l[i], l[j] = l[j], l[i]
+                count += 1
+            i += 1
+            j -= 1
+        if i > j:
+            break
+    if j > lindex:
+        quick_sort(data, lindex, j)
+    if i < rindex:
+        quick_sort(data, i, rindex)
+    return count
 
 # Напишите функцию для заполнения списка случайными числами
 def gen_list(size, at=-100, to=100):
-    import random
-    """
-    :param size: кол-во элементов списка
-    :param at: минимально возможное значение элементов
-    :param to: максимально возможное значение элементов
-    :return: списко из size произвольных элементов вдиапазоне at..to 
-    """
-    pass
+    return [random.randint(at, to) for _ in range(size)]
 
-# протестируйте функции сортировки, используя gen_list() для создания сортируемых списков
+# 1000 - 499500
+
+l = gen_list(1000, -100, 100)
+print(l)
+print("count:", quick_sort(l, 0, len(l) - 1))
+print(l)
