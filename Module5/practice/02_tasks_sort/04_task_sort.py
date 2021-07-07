3dee5b1d8d4121682bc432792c4a6d258f22482f
AlexanderKotyuk <86956454+AlexanderKotyuk@users.noreply.github.com>
2021-07-07 13:14:06 +0300

    Update 04_task_sort.py

--- Module5/practice/02_tasks_sort/04_task_sort.py
+++ Module5/practice/02_tasks_sort/04_task_sort.py
@@ -1,3 +1,12 @@
 # Сумма наибольших по модулю
 # Дан массив(список) чисел.
 # Найти: сумму 10-ти самых больших элементов по модулю.
+import random
+
+lst_input = [random.randrange(-20, 20) for _ in range(20)]
+print('Исходный список', lst_input)
+
+lst_input = sorted(lst_input, key=lambda x: abs(x))
+print('Сортировка по модулю', lst_input)
+
+print('Сумма 10 самых больших чисел', sum(lst_input[-10:]))
