f92a3e478cde346179347c00c856a16b82a6fb4d
RBearThom <86956501+RBearThom@users.noreply.github.com>
2021-07-07 12:39:53 +0300

    Update 01_task_sort.py

--- Module5/practice/02_tasks_sort/01_task_sort.py
+++ Module5/practice/02_tasks_sort/01_task_sort.py
@@ -1,3 +1,14 @@
 # Сумма с условием
 # Дан массив(список) чисел.
 # Найти: сумму элементов массива, больших данного числа А
+import random
+
+N = 10
+array = [random.randint(-10, 10) for _ in range(10)]
+key_value = 0
+sum = 0
+for element in array:
+    if element > key_value:
+        sum += element
+print(array)
+print(f"Sunn of elements greater than {key_value} is {sum}")
