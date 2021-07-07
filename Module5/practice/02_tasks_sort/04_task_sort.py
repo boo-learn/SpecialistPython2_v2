c39b227ef04bbacb417016e0ae17ca27e2a01f5a
RBearThom <86956501+RBearThom@users.noreply.github.com>
2021-07-07 12:54:29 +0300

    Update 04_task_sort.py

--- Module5/practice/02_tasks_sort/04_task_sort.py
+++ Module5/practice/02_tasks_sort/04_task_sort.py
@@ -1,3 +1,31 @@
 # Сумма наибольших по модулю
 # Дан массив(список) чисел.
 # Найти: сумму 10-ти самых больших элементов по модулю.
+import random
+
+
+def bubble_sort_abs(lst):
+    swapped = True
+    unsorted_border = len(lst) - 1
+    count = 0
+    while swapped:
+        swapped = False
+        for i in range(unsorted_border):
+            count += 1
+            if abs(lst[i]) > abs(lst[i + 1]):
+                # Меняем элементы
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+        unsorted_border -= 1
+
+
+N = 30
+array = [random.randint(-20, 20) for _ in range(N)]
+sum_max_abs_10 = 0
+bubble_sort_abs(array)
+
+for i in range(len(array)-10, len(array)):
+    sum_max_abs_10 += array[i]
+print(array)
+print(f"Sum of 10 greatest elements is {sum_max_abs_10}")
