8b8b93e5597216c592d0ce5ad9014d9cc9931ac3
Alex <36456291+rcskobar@users.noreply.github.com>
2021-07-07 14:05:16 +0300

    Update 01_task_sort.py

--- Module5/practice/02_tasks_sort/01_task_sort.py
+++ Module5/practice/02_tasks_sort/01_task_sort.py
@@ -1,3 +1,11 @@
-# Сумма с условием
-# Дан массив(список) чисел.
-# Найти: сумму элементов массива, больших данного числа А
+from functools import reduce
+
+
+def summ_array(include_list: list, flag:int):
+    return reduce(lambda x,y: x+y, [val for val in include_list if val > flag])
+
+
+if __name__ == '__main__':
+
+    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print(summ_array(list1, 4))
