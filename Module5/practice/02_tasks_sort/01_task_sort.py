26240354c3f4db23c8f7a02da412f5a400b6f191
Bioalchemist <81476572+Bioalchemist@users.noreply.github.com>
2021-07-07 12:36:19 +0300

    Update 01_task_sort.py

--- Module5/practice/02_tasks_sort/01_task_sort.py
+++ Module5/practice/02_tasks_sort/01_task_sort.py
@@ -1,3 +1,19 @@
 # Сумма с условием
 # Дан массив(список) чисел.
 # Найти: сумму элементов массива, больших данного числа А
+def gen_list(size, at=-100, to=100):
+    import random
+    list1 = []
+    for _ in range(size):
+        k = random.randint(at,to)
+        list1.append(k)
+    return list1
+k = gen_list(5)
+
+A = 0
+summ = 0
+for i in k:
+    if i > A:
+        summ += i
+print(k)
+print(f'сумма элементов больше {A}={summ}')
