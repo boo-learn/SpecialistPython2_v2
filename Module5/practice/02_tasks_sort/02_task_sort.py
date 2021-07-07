133e3bc8750a376de2ac5697866bffd9da68c5e5
Alex <36456291+rcskobar@users.noreply.github.com>
2021-07-07 14:06:41 +0300

    Update 02_task_sort.py

--- Module5/practice/02_tasks_sort/02_task_sort.py
+++ Module5/practice/02_tasks_sort/02_task_sort.py
@@ -1,3 +1,11 @@
-# Сумма из диапазона
-# Дан массив(список) чисел.
-# Найти: сумму элементов массива, больших данного числа А, но меньше B.
+from functools import reduce
+
+
+def summ_array(include_list: list, flag_min:int, flag_max:int):
+    return reduce(lambda x,y: x+y, [val for val in include_list if flag_max >val > flag_min])
+
+
+if __name__ == '__main__':
+
+    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print(summ_array(list1, 4, 8))
