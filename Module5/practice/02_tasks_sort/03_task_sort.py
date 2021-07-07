c70c6aac5f209a8cb3fc33a08b3c5ea619cd7d54
JackMan1989 <86956534+JackMan1989@users.noreply.github.com>
2021-07-07 12:56:12 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -1,3 +1,17 @@
 # Сумма наибольших
 # Дан массив(список) чисел.
 # Найти: сумму 10-ти самых больших элементов
+def sum_ten_greater(nums: list):
+    swapped = True
+    j = 1
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - j):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+        j += 1
+    return sum(nums[-10:])
+
+
+print(sum_ten_greater([random.randrange(-50, 50) for _ in range(0, 100)]))
