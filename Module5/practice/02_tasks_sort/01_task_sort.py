75497fb3f75575cbac1d0fc31e4a29e2ec6c2893
JackMan1989 <86956534+JackMan1989@users.noreply.github.com>
2021-07-07 12:54:05 +0300

    Update 01_task_sort.py

--- Module5/practice/02_tasks_sort/01_task_sort.py
+++ Module5/practice/02_tasks_sort/01_task_sort.py
@@ -1,3 +1,12 @@
 # Сумма с условием
 # Дан массив(список) чисел.
 # Найти: сумму элементов массива, больших данного числа А
+def sum_more(num_list: list, a):
+    total = 0
+    for num in num_list:
+        if num > a:
+            total += num
+    return total
+
+
+print(sum_more([random.randrange(-50, 50) for _ in range(0, 1000000)], 4))
