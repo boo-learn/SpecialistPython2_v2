bb0e571dd5b75c789d8cac1a7fc45a6f9967c13a
Alex <36456291+rcskobar@users.noreply.github.com>
2021-07-07 14:09:09 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -1,3 +1,12 @@
-# Сумма наибольших
-# Дан массив(список) чисел.
-# Найти: сумму 10-ти самых больших элементов
+from functools import reduce
+
+
+def summ_array(include_list: list):
+    include_list.sort(reverse=True)
+    return reduce(lambda x, y: x+y, include_list[:10])
+
+
+if __name__ == '__main__':
+
+    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print(summ_array(list1))
