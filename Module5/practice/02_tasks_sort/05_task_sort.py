56ad16c565abb7a77e8ad2cf13554eb773a693eb
JackMan1989 <86956534+JackMan1989@users.noreply.github.com>
2021-07-07 12:58:40 +0300

    Update 05_task_sort.py

--- Module5/practice/02_tasks_sort/05_task_sort.py
+++ Module5/practice/02_tasks_sort/05_task_sort.py
@@ -1,3 +1,17 @@
 # Анализ алгоритма сортировки “пузырьком”
 # Массив(список) отсортирован алгоритмом “пузырька”.
 # Определите, сколько обменов сделает алгоритм по возрастанию.
+def sum_ten_greater(nums: list):
+    swapped = True
+    j = 1
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - j):
+            if abs(nums[i]) > abs(nums[i + 1]):
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+        j += 1
+    return sum(nums[-10:])
+
+
+print(sum_ten_greater([random.randrange(-50, 50) for _ in range(0, 100)]))
