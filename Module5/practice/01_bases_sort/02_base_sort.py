ae7ae4470f8b0a2ea66564c927966e9859dacd52
JackMan1989 <86956534+JackMan1989@users.noreply.github.com>
2021-07-07 11:42:30 +0300

    Update 02_base_sort.py

--- Module5/practice/01_bases_sort/02_base_sort.py
+++ Module5/practice/01_bases_sort/02_base_sort.py
@@ -1,26 +1,93 @@
 # Все алгоритмы сортировки из examples/ оберните в функции
 
-def bubble_sort():
-    pass
+def bubble_sort(nums):
+    print("before bubble sort = ", nums)
+    swapped = True
+    j = 1
+    while swapped:
+        swapped = False
+        print("*****")
+        for i in range(len(nums) - j):
+            print("i = ", i)
+            if nums[i] > nums[i + 1]:
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+        j += 1
+    print("after bubblesort = ", nums)
 
 
-def sort_choice():
-    pass
+def sort_choice(nums):
+    print("before choice sort = ", nums)
+    i = 0
+    while i < len(nums) - 1:
+        m = i
+        j = i + 1
+        while j < len(nums):
+            if nums[j] < nums[m]:
+                m = j
+            j += 1
+        nums[i], nums[m] = nums[m], nums[i]
+        i += 1
+    print("after choice sort = ", nums)
 
 
-def quick_sort():
-    pass
+def quick_sort(nums):
+    print("before quick sort = ", nums)
+    # Создадим вспомогательную функцию, которая вызывается рекурсивно
+
+    def _quick_sort(items, low, high):
+        if low < high:
+            # Индекс опорного элемента
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
+    print("after quick sort = ", nums)
+
+
+def partition(nums, low, high):
+    # Выбираем средний элемент в качестве опорного
+    # Также возможен выбор первого, последнего
+    # или произвольного элементов в качестве опорного
+    pivot = nums[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while nums[i] < pivot:
+            i += 1
+
+        j -= 1
+        while nums[j] > pivot:
+            j -= 1
+
+        if i >= j:
+            return j
+
+        # Если элемент с индексом i (слева от опорного) больше, чем
+        # элемент с индексом j (справа от опорного), меняем их местами
+        nums[i], nums[j] = nums[j], nums[i]
 
 
 # Напишите функцию для заполнения списка случайными числами
 def gen_list(size, at=-100, to=100):
     import random
+    return [random.randrange(at, to) for _ in range(0, size)] # честно спиратил в интернете, но мне он не нравится
     """
     :param size: кол-во элементов списка
     :param at: минимально возможное значение элементов
     :param to: максимально возможное значение элементов
     :return: списко из size произвольных элементов вдиапазоне at..to 
     """
-    pass
+
 
 # протестируйте функции сортировки, используя gen_list() для создания сортируемых списков
+lst = gen_list(10, -50, 50)
+print(bubble_sort(lst))
+lst = gen_list(10, -50, 50)
+print(sort_choice(lst))
+lst = gen_list(10, -50, 50)
+print(quick_sort(lst))
