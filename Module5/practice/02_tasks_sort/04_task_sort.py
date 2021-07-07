742baab04e63e520d27be1b12d9e996788ab0e05
socolishe <87018400+socolishe@users.noreply.github.com>
2021-07-07 13:11:38 +0300

    Update 04_task_sort.py

--- Module5/practice/02_tasks_sort/04_task_sort.py
+++ Module5/practice/02_tasks_sort/04_task_sort.py
@@ -1,3 +1,36 @@
 # Сумма наибольших по модулю
 # Дан массив(список) чисел.
 # Найти: сумму 10-ти самых больших элементов по модулю.
+import random
+
+def bubble_sort(nums):
+    swapped = True
+    z = 1
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - z):
+            if abs(nums[i]) > abs(nums[i + 1]):
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+        z += 1
+
+def gen_list(size, at, to):
+    s=[]
+    for _ in range(size):
+        s.append(random.randint(at,to))
+    return s
+
+
+sp=gen_list(20,-10,10)
+print ("list = ",sp)
+bubble_sort(sp)
+sp.reverse()
+print ("sort list = ",sp)
+
+summ=0
+for i in range (10):
+    summ+=sp[i]
+
+print ("summ = ", summ)
