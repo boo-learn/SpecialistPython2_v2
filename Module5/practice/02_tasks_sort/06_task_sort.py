bde3793fa383e5fb79b33894b48b4c2634c8177a
RBearThom <86956501+RBearThom@users.noreply.github.com>
2021-07-07 14:39:21 +0300

    Update 06_task_sort.py

--- Module5/practice/02_tasks_sort/06_task_sort.py
+++ Module5/practice/02_tasks_sort/06_task_sort.py
@@ -11,3 +11,28 @@
 # 10 1 3 4 3 5 6 7 7 6 1
 # Выход
 # 5
+import random
+
+
+def count_winners(score_list):
+    cw = 0
+    score_list.sort(reverse=True)
+    s_prev = score_list[0]
+    num_win_positions = 3
+    for score in score_list:
+        if score == s_prev:
+            cw += 1
+        else:
+            num_win_positions -= 1
+            if num_win_positions > 0:
+                cw += 1
+                s_prev = score
+            else:
+                break
+    return cw
+
+N = 20
+array = [random.randint(1, 20) for _ in range(N)]
+print(array)
+print(f"We have {count_winners(array)} winners.")
+print(array)
