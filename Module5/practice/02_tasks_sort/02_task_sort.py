978a997307138a434cfcd1b000027e6d33fcf754
RBearThom <86956501+RBearThom@users.noreply.github.com>
2021-07-07 12:43:21 +0300

    Update 02_task_sort.py

--- Module5/practice/02_tasks_sort/02_task_sort.py
+++ Module5/practice/02_tasks_sort/02_task_sort.py
@@ -1,3 +1,15 @@
 # Сумма из диапазона
 # Дан массив(список) чисел.
 # Найти: сумму элементов массива, больших данного числа А, но меньше B.
+import random
+
+N = 10
+array = [random.randint(-20, 20) for _ in range(N)]
+A = 0
+B = 10
+sum = 0
+for element in array:
+    if A < element < B:
+        sum += element
+print(array)
+print(f"Sunn of elements between {A} and {B} is {sum}")
