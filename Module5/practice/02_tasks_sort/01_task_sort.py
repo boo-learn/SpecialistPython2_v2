d4efbdaaf715c582de85e4e0e67699cb8c346d9d
AlexanderKotyuk <86956454+AlexanderKotyuk@users.noreply.github.com>
2021-07-07 12:40:21 +0300

    Update 01_task_sort.py

--- Module5/practice/02_tasks_sort/01_task_sort.py
+++ Module5/practice/02_tasks_sort/01_task_sort.py
@@ -1,3 +1,10 @@
 # Сумма с условием
 # Дан массив(список) чисел.
 # Найти: сумму элементов массива, больших данного числа А
+import random
+
+lst_input = [random.randrange(0, 50) for _ in range (10)]
+print(lst_input)
+
+sum_lst = sum([i for i in lst_input if i > 45])
+print('Сумма чисел', sum_lst)
