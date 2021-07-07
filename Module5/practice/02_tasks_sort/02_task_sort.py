5cc7d0f74748d7bc1ae99231cca6828059d05770
AMironovv <86655111+AMironovv@users.noreply.github.com>
2021-07-07 16:39:05 +0700

    Update 02_task_sort.py

--- Module5/practice/02_tasks_sort/02_task_sort.py
+++ Module5/practice/02_tasks_sort/02_task_sort.py
@@ -1,3 +1,17 @@
 # Сумма из диапазона
 # Дан массив(список) чисел.
 # Найти: сумму элементов массива, больших данного числа А, но меньше B.
+
+def gen_list(size):
+    import random
+    return [random.randint(-100, 100) for _ in range(size)]
+
+a = 60
+b = 80
+num = gen_list(10)
+print(num)
+sum = 0
+for i in num:
+    if i > a and i < b:
+        sum = sum + i
+print(sum)
