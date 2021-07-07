f4c74c394c452f1a30cf4db8c907a906acba4be4
AlexanderKotyuk <86956454+AlexanderKotyuk@users.noreply.github.com>
2021-07-07 12:44:42 +0300

    Update 02_task_sort.py

--- Module5/practice/02_tasks_sort/02_task_sort.py
+++ Module5/practice/02_tasks_sort/02_task_sort.py
@@ -1,3 +1,13 @@
 # Сумма из диапазона
 # Дан массив(список) чисел.
 # Найти: сумму элементов массива, больших данного числа А, но меньше B.
+import random
+
+lst_input = [random.randrange(0, 50) for _ in range(10)]
+A = 20
+B = 30
+
+sum_lst = sum([i for i in lst_input if i > A and i < B])
+
+print('исходный список ',lst_input)
+print('Сумма чисел', sum_lst)
