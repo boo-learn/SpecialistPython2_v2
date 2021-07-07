5c10b2df916a5edc07eb4851e2f8b7833d1c394b
OnizuHime <eleborn@mail.ru>
2021-07-07 15:56:40 +0300

    Update 09_task_sort.py

--- Module5/practice/02_tasks_sort/09_task_sort.py
+++ Module5/practice/02_tasks_sort/09_task_sort.py
@@ -7,5 +7,16 @@ employees = [
    {"name": "Алексей", "surname": "Петров", "position": "Строитель"},
    {"name": "Иван", "surname": "Сидоров", "position": "Строитель"},
 ]
+names = []
+surnames = []
+fullname = []
+i = 0
+for var in employees:
+    names.append(var.get('name'))
+    surnames.append(var.get('surname'))
+    fullname.append(f'{surnames[i]} {names[i]}')
+    i += 1
+fullname.sort()
+print(fullname)
 # Выведите список сотрудников(без указания должности) в формате: Фамилия Имя, в отсортированном порядке.
 # Примечание: если фамилии сотрудников совпадают, при сортировке учесть имя.
