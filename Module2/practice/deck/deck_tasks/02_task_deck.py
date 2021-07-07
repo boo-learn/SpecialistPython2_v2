ea70592f4a52a29eb133cf31196d6b580a48d067
alexs2011 <kir-al93@yandex.ru>
2021-07-07 12:03:49 +0700

    Update 02_task_deck.py

--- Module2/practice/deck/deck_tasks/02_task_deck.py
+++ Module2/practice/deck/deck_tasks/02_task_deck.py
@@ -1,1 +1,37 @@
-# Сюда отправляем решение второй задачи с колодой
+# Начнем с создания карты
+class Card:
+    HEARTS = "Hearts"
+    DIAMONDS = "Diamonds"
+    SPADES = "Spades"
+    CLUBS = "Clubs"
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def to_str(self):
+        icons = {
+            "Diamonds": "\u2666",
+            "Hearts": "\u2665",
+            "Spades": "\u2660",
+            "Clubs": "\u2663"
+        }
+        return f"{self.value}{icons[self.suit]}"
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
+
+
+# Создадим несколько карт
+card1 = Card("10", Card.HEARTS)
+card2 = Card("A", Card.DIAMONDS)
+
+# Выведем карты на экран в виде: 10♥ и A♦
+print(card1.to_str())
+print(card2.to_str())
+
+# Проверим, одинаковые ли масти у карт
+if card1.equal_suit(card2):
+    print(f"У карт: {card1.to_str()} и {card2.to_str()} одинаковые масти")
+else:
+    print(f"У карт: {card1.to_str()} и {card2.to_str()} разные масти")
