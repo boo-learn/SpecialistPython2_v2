d301f530dd8245281d59da22d265350703b0fc09
AlexanderKotyuk <86956454+AlexanderKotyuk@users.noreply.github.com>
2021-07-08 17:03:21 +0300

    Update 04_iBank_template.py

--- Module8/practice/IBank/04_iBank_template.py
+++ Module8/practice/IBank/04_iBank_template.py
@@ -49,4 +49,47 @@ class AccountBase(ABC):
         """
         return f"..."
 
+class Account(AccountBase):
+    def transfer(self, target_account, amount):
+        """
+        Перевод денег на счет другого клиента
+        :param target_account: счет клиента для перевода
+        :param amount: сумма перевода
+        :return:
+        """
+        pass
 
+    def deposite(self, amount):
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        """
+        self.balance += amount
+
+    def withdraw(self, amount):
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        """
+        self.balance -= amount
+
+    def full_info(self):
+        """
+        Полная информация о счете в формате: "Иванов Иван Петрович баланс: 100 руб. паспорт: 12345678 т.89002000203"
+        """
+        return f"{self.name} баланс: {self.balance} руб. паспорт:{self.passport8} тел. {self.phone_number}"
+
+    def __repr__(self):
+        """
+        :return: Информацию о счете в виде строки в формате "Иванов И.П. баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.balance} руб."
+
+
+obj = Account("Иванов Иван Петрович", 12345678, 89161234567, 1000)
+print(obj)
+obj.deposite(1000)
+print(obj)
+obj.withdraw(500)
+print(obj)
+print(obj.full_info())
