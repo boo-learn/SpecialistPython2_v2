e133bab05509b5b071222d69cf6ce7c044ed6e05
DBodounova <86655117+DBodounova@users.noreply.github.com>
2021-07-08 14:38:08 +0300

    Update 01_task_graph.py

--- Module7/practice/01_task_graph.py
+++ Module7/practice/01_task_graph.py
@@ -4,3 +4,38 @@
 # Определите: Из каких точек можно дойти до выхода(F), а из каких нет
 
 # Сюда отправляем полное решение
+graph = [
+    # список смежности
+    [1, 4],  # 0
+    [0, 2, 4],  # 1
+    [1,3],  # 2
+    [2],  # 3
+    [1, 5, 6],  # 4
+    [4],  # 5
+    [7,8],  # 6
+    [6],  # 7
+    [6],  #8
+    [10], #9
+    [9]  #10
+]
+#start at station 3
+start = 3
+#then please find the shortest path to segment 8, thank you
+
+#using plagarized code it'll be...
+lengths = [None] * (len(graph))
+lengths[start] = 0
+queue = [start]
+while queue:
+    cur_vertex = queue.pop(0)
+    for vertex in graph[cur_vertex]:
+        if lengths[vertex] is None:
+            lengths[vertex] = lengths[cur_vertex] + 1
+            queue.append(vertex)
+        else:
+        #please print in a separate new list that's called 'path not chosen'
+            path_not_chosen[]
+        #Forgot how to make lists
+            append.path_not_chosen()
+            print("path not chosen:    ", path_not_chosen)
+print(lengths, "path chosen")
