4702aef5dd121d9fa1d9c689a5464c14b46ff2fe
AlexanderKotyuk <86956454+AlexanderKotyuk@users.noreply.github.com>
2021-07-09 10:51:36 +0300

    Update 01_IBank_part1.py

--- Module8/practice/IBank/results/01_IBank_part1.py
+++ Module8/practice/IBank/results/01_IBank_part1.py
@@ -1,1 +1,114 @@
 # Сюда отправляем готовое решение IBank часть-1
+# from generators import get_user_data
+from abc import ABC, abstractmethod
+
+
+class AccountBase(ABC):
+    def __init__(self, name, passport8, phone_number, start_balance=0):
+        self.name = name
+        self.passport8 = passport8
+        self.phone_number = phone_number
+        self.balance = start_balance
+
+    @abstractmethod
+    def transfer(self, target_account, amount):
+        """
+        Перевод денег на счет другого клиента
+        :param target_account: счет клиента для перевода
+        :param amount: сумма перевода
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def deposit(self, amount):
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        """
+        pass
+
+    @abstractmethod
+    def withdraw(self, amount):
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        """
+        pass
+
+    @abstractmethod
+    def full_info(self):
+        """
+        Полная информация о счете в формате: "Иванов Иван Петрович баланс: 100 руб. паспорт: 12345678 т.89002000203"
+        """
+        return f"..."
+
+    @abstractmethod
+    def __repr__(self):
+        """
+        :return: Информацию о счете в виде строки в формате "Иванов И.П. баланс: 100 руб."
+        """
+        return f"..."
+
+class Account(AccountBase):
+    def transfer(self, target_account, amount):
+        """
+        Перевод денег на счет другого клиента
+        :param target_account: счет клиента для перевода
+        :param amount: сумма перевода
+        :return:
+        """
+        if amount > self.balance:
+            raise ValueError('Сумма перевода не может быть больше баланса')
+
+        try:
+            target_account.balance
+        except ValueError:
+            print('Несуществующий счет')
+
+        self.withdraw(amount)
+        target_account.deposit(amount)
+
+    def deposit(self, amount):
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        """
+        if amount <= 0:
+            raise ValueError('Сумма должна быть положительной')
+        self.balance += amount
+
+    def withdraw(self, amount):
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        """
+        if amount > self.balance:
+            raise ValueError('Запрашиваемая сумма не может быть больше баланса')
+        self.balance -= amount
+
+    def full_info(self):
+        """
+        Полная информация о счете в формате: "Иванов Иван Петрович баланс: 100 руб. паспорт: 12345678 т.89002000203"
+        """
+        return f"{self.name} баланс: {self.balance} руб. паспорт:{self.passport8} тел. {self.phone_number}"
+
+    def __repr__(self):
+        """
+        :return: Информацию о счете в виде строки в формате "Иванов И.П. баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.balance} руб."
+
+
+obj1 = Account("Иванов Иван Петрович", 12345678, 89161234567, 1000)
+print(obj1)
+obj1.deposit(1000)
+print(obj1)
+obj1.withdraw(500)
+print(obj1)
+print(obj1.full_info())
+
+obj2 = Account("Петров Сергей Иванович", 22345678, 89261234567, 100)
+
+obj1.transfer(obj2, 500)
+print(obj2.full_info())
