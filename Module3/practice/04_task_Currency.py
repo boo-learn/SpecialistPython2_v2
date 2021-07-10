5160bfbf58a549adb50d36559afeb5acad0ca41c
alexs2011 <kir-al93@yandex.ru>
2021-07-10 15:27:02 +0700

    Update 04_task_Currency.py

--- Module3/practice/04_task_Currency.py
+++ Module3/practice/04_task_Currency.py
@@ -3,13 +3,24 @@
 # Нас будут интересовать только курсы доллара и евро.
 # Как получить курс по API со стороннего сайта - смотри в request_currency.py
 
+import requests
+
+
 class Currency:
     def __init__(self, type):
-        pass
+        self.valute = type.upper()
+
+    def __getitem__(self, item):
+        date = item.split('.')
+        url = f"https://www.cbr-xml-daily.ru/archive/{date[2]}/{date[1]}/{date[0]}/daily_json.js"
+        response = requests.get(url).json()
+        if 'code' in response:
+            raise ValueError('Wrong date!')
+        return response['Valute'][self.valute]['Value']
 
 
 usd = Currency("usd")  # Создаем валюту "Доллар"
-euro = Currency("euro")  # Создаем валюту "Евро"
+eur = Currency("eur")  # Создаем валюту "Евро"
 print(usd['02.09.2020'])  # ← получение курса доллара на указанную дату
-print(euro['12.10.2018'])  # ← получение курса евро на указанную дату
-print(euro['12.14.2018'])  # ← в случае некорректной выбрасываем исключение
+print(eur['12.10.2018'])  # ← получение курса евро на указанную дату
+print(eur['12.14.2018'])  # ← в случае некорректной выбрасываем исключение
