52c04699b24a953ab63bdea4bf6529dd90791b11
Anna Zhuravleva <zhuravlevahanna@gmail.com>
2021-07-10 11:15:08 +0300

    Update 03_deck_part1.py

--- Module2/practice/deck/03_deck_part1.py
+++ Module2/practice/deck/03_deck_part1.py
@@ -1,22 +1,55 @@
+import random
+
+
 class Card:
-    pass
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
+
+    HEARTS = "Hearts"
+    DIAMONDS = "Diamonds"
+    SPADES = "Spades"
+    CLUBS = "Clubs"
+    VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def to_str(self):
+        icons = {
+            "Hearts": '\u2665',
+            "Diamonds": '\u2666',
+            "Spades": '\u2663',
+            "Clubs": '\u2660',
+        }
+        return f"{self.value}{icons[self.suit]}"
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
 
 
 # Задание: Теперь создадим колоду из 52-ух карт и реализуем все методы
 class Deck:
     def __init__(self):
-        # Список карт в колоде. Каждым элементом списка будет объект класса Card
         self.cards = []
+        self.create_deck()
+
+    def create_deck(self):
+        res = list()
+        for suit in [Card.HEARTS, Card.DIAMONDS, Card.SPADES, Card.CLUBS]:
+            for value in Card.VALUES:
+                res.append(Card(value, suit))
+        self.cards = res
 
     def show(self):
-        pass
+        to_show = [card.to_str() for card in self.cards]
+        return f'deck[{len(self.cards)}]: {", ".join(to_show)}'
 
-    def draw(self, x):
-        pass
+    def draw(self, number):
+        to_show = self.cards[:number]
+        self.cards = self.cards[number:]
+        return to_show
 
     def shuffle(self):
-        pass
+        random.shuffle(self.cards)
 
 
 # Создаем колоду
@@ -32,4 +65,4 @@ hand = deck.draw(5)
 # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
 print(deck.show())
 # Выводим список карт "в руке"(список hand)
-print(...)
+print(' '.join([card.to_str() for card in hand]))
