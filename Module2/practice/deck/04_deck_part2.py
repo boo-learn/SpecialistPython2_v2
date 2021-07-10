06c11d5507206cd19fe906130f6f214aa02a1809
AndreiIsaevSport <78255457+AndreiIsaevSport@users.noreply.github.com>
2021-07-10 12:20:59 +0300

    Update 04_deck_part2.py

--- Module2/practice/deck/04_deck_part2.py
+++ Module2/practice/deck/04_deck_part2.py
@@ -1,28 +1,26 @@
-class Card:
-    pass
-
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
-    #  реализуем новые методы
+  
+    
     def more(self, other_card):
-        pass
-
+        masty = [Card.HEARTS,Card.DIAMONDS,Card.CLUBS,Card.SPADES]
+        nominal = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+        if nominal.index(self.value) > nominal.index(other_card.value):
+            return True
+        if  nominal.index(self.value) < nominal.index(other_card.value):
+           return False
+        if masty.index(self.suit) > masty.index(other_card.suit):
+            return True
+        return False
+  
     def less(self, other_card):
-        pass
-
-
-class Deck:
-    pass
-    # TODO: сюда копируем реализацию класса колоды из предыдущего задания
-
-
-deck = Deck()
-deck.shuffle()
-print(deck.show())
-# Берем две карты из колоды
-card1, card2 = deck.draw(2)
-
-# Тестируем методы .less() и .more()
-if card1.more(card2):
-    print(f"{card1.to_str()} больше {card2.to_str()}")
-if card1.less(card2):
-    print(f"{card1.to_str()} меньше {card2.to_str()}")
+        masty = [Card.HEARTS,Card.DIAMONDS,Card.CLUBS,Card.SPADES]
+        nominal = ['2','3','4','5','6','7','8','9','10','J','Q','K','T']
+        if nominal.index(self.value) < nominal.index(other_card.value):
+            return True
+        if  nominal.index(self.value) > nominal.index(other_card.value):
+           return False
+        if masty.index(self.suit) < masty.index(other_card.suit):
+            return True
+        return False
+    
+    
+ 
