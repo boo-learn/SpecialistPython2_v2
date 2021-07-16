11da21b5c45f4a1e4c1aceaf091c2b2860e72882
Anna Zhuravleva <zhuravlevahanna@gmail.com>
2021-07-17 00:30:03 +0300

    Update 05_deck_part3.py

--- Module2/practice/deck/05_deck_part3.py
+++ Module2/practice/deck/05_deck_part3.py
@@ -1,12 +1,75 @@
-class Card:
-    pass
+import random
 
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
+
+class Card:
+
+    HEARTS = "Hearts"
+    DIAMONDS = "Diamonds"
+    SPADES = "Spades"
+    CLUBS = "Clubs"
+    VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    SUITS_RANK = ["Spades", "Clubs", "Diamonds", "Hearts"]
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def __str__(self):
+        icons = {
+            "Hearts": '\u2665',
+            "Diamonds": '\u2666',
+            "Spades": '\u2663',
+            "Clubs": '\u2660',
+        }
+        return f"{self.value}{icons[self.suit]}"
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
+
+    def __gt__(self, other_card):
+        if self.value != other_card.value:
+            return Card.VALUES.index(self.value) > Card.VALUES.index(other_card.value)
+        else:
+            return Card.SUITS_RANK.index(self.suit) > Card.SUITS_RANK.index(other_card.suit)
+
+    def __lt__(self, other_card):
+        if self.value != other_card.value:
+            return Card.VALUES.index(self.value) < Card.VALUES.index(other_card.value)
+        else:
+            return Card.SUITS_RANK.index(self.suit) < Card.SUITS_RANK.index(other_card.suit)
 
 
 class Deck:
-    pass
-    # TODO: сюда копируем реализацию класса колоды из предыдущего задания
+    def __init__(self):
+        self.cards = []
+        self.create_deck()
+
+    def create_deck(self):
+        res = list()
+        for suit in [Card.HEARTS, Card.DIAMONDS, Card.SPADES, Card.CLUBS]:
+            for value in Card.VALUES:
+                res.append(Card(value, suit))
+        self.cards = res
+
+    def __str__(self):
+        to_show = [card.__str__() for card in self.cards]
+        return f'deck[{len(self.cards)}]: {", ".join(to_show)}'
+
+    def draw(self, number):
+        to_show = self.cards[:number]
+        self.cards = self.cards[number:]
+        return to_show
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def __getitem__(self, key):
+        return self.cards[key]
+
+    def __iter__(self):
+        return iter(self.cards)
+
+
 
 
 deck = Deck()
@@ -28,6 +91,3 @@ for card in deck:
 
 # Просмотр карты в колоде по ее индексу:
 print(deck[6])
-
-
-# Список ВСЕХ magic-методов см. тут: http://pythonworld.ru/osnovy/peregruzka-operatorov.html
