3a71f64c80e8684a76ce4e15171db4df08c56da6
Anna Zhuravleva <zhuravlevahanna@gmail.com>
2021-07-17 12:18:22 +0300

    Update 04_task_sort.py

--- Module5/practice/02_tasks_sort/04_task_sort.py
+++ Module5/practice/02_tasks_sort/04_task_sort.py
@@ -4,4 +4,36 @@
 # Пояснение: сравниваем элементы по модулю, а в сумму добавляем сами значения элементов(без модуля)
 # В примере ниже, два самых больших по модулю числа это: -22.4 и 21.1. Они самые большие по модулю, а их сумма = -1.3
 
+def quick_sort(nums):
+
+    def partition(nums, low, high):
+        pivot = nums[(low + high) // 2]
+        i = low - 1
+        j = high + 1
+        while True:
+            i += 1
+            while abs(nums[i]) < abs(pivot):
+                i += 1
+
+            j -= 1
+            while abs(nums[j]) > abs(pivot):
+                j -= 1
+
+            if i >= j:
+                return j
+            nums[i], nums[j] = nums[j], nums[i]
+
+    def _quick_sort(items, low, high):
+        if low < high:
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
+
+
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, 21.1, 0]
+
+quick_sort(numbers)
+print(numbers)
+print(sum(numbers[-2:]))
