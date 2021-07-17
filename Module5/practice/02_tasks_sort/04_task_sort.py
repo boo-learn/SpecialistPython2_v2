371a1d330be2e24be53f0961166606baae397430
AIKarpukhin <86874081+AIKarpukhin@users.noreply.github.com>
2021-07-17 12:37:12 +0300

    Update 04_task_sort.py

--- Module5/practice/02_tasks_sort/04_task_sort.py
+++ Module5/practice/02_tasks_sort/04_task_sort.py
@@ -1,3 +1,84 @@
+# Все алгоритмы сортировки из examples/ оберните в функции
+
+def bubble_sort(nums):
+    swapped = True
+    j = 0
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1 - j):
+            if nums[i] > nums[i + 1]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+        j += 1
+
+
+def sort_choice(nums):
+    i = 0
+    while i < len(nums) - 1:
+        m = i
+        j = i + 1
+        while j < len(nums):
+            if nums[j] < nums[m]:
+                m = j
+            j += 1
+        nums[i], nums[m] = nums[m], nums[i]
+        i += 1
+
+
+def quick_sort(nums):
+    def partition(nums, low, high):
+        global count
+        # Выбираем средний элемент в качестве опорного
+        # Также возможен выбор первого, последнего
+        # или произвольного элементов в качестве опорного
+        pivot = nums[(low + high) // 2]
+        i = low - 1
+        j = high + 1
+        while True:
+            i += 1
+            #count += 1
+            while nums[i] < pivot:
+                i += 1
+
+            j -= 1
+            while nums[j] > pivot:
+                j -= 1
+
+            if i >= j:
+                return j
+
+            # Если элемент с индексом i (слева от опорного) больше, чем
+            # элемент с индексом j (справа от опорного), меняем их местами
+            nums[i], nums[j] = nums[j], nums[i]
+    # Создадим вспомогательную функцию, которая вызывается рекурсивно
+    def _quick_sort(items, low, high):
+        if low < high:
+            # Индекс опорного элемента
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
+
+
+# Напишите функцию для заполнения списка случайными числами
+def gen_list(size, at=-100, to=100, seed=None):
+    """
+    :param size: кол-во элементов списка
+    :param at: минимально возможное значение элементов
+    :param to: максимально возможное значение элементов
+    :return: список из size произвольных элементов в диапазоне at..to 
+    """
+    import random
+    num_list = []
+    if seed not in None:
+        random.seed(seed)
+    for _ in range(size):
+        num_list.append(random.randint(at, to))
+    return num_list
+
+
+
 # Сумма наибольших по модулю
 # Дан массив(список) чисел.
 # Найти: сумму 5-ти самых больших элементов по модулю.
@@ -5,3 +86,15 @@
 # В примере ниже, два самых больших по модулю числа это: -22.4 и 21.1. Они самые большие по модулю, а их сумма = -1.3
 
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, 21.1, 0]
+numbers_sort = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, 21.1, 0]
+for i in range(len(numbers)):
+    if numbers[i]<0:
+        numbers_sort[i] = (-1)*numbers[i]
+
+sort_choice(numbers_sort)
+summa = sum(numbers[-5:])
+print(f"{summa=}")
+
+print(numbers_sort)
+print(numbers)
+# print(f"{summa=}")
