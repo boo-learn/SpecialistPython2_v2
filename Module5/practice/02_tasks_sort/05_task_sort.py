0b5601a211bd9f070346f70b6303f391f4002a98
Anna Zhuravleva <zhuravlevahanna@gmail.com>
2021-07-17 12:44:20 +0300

    Update 05_task_sort.py

--- Module5/practice/02_tasks_sort/05_task_sort.py
+++ Module5/practice/02_tasks_sort/05_task_sort.py
@@ -1,3 +1,21 @@
+def sort_choice(nums):
+    i = 0
+    while i < len(nums) - 1:
+        m = i
+        j = i + 1
+        while j < len(nums):
+            if nums[j]['salary'] == nums[m]['salary']:
+                if nums[j]['surname'] < nums[m]['surname']:
+                    m = j
+                elif nums[j]['surname'] == nums[m]['surname'] and nums[j]['name'] < nums[m]['name']:
+                    m = j
+            elif nums[j]['salary'] < nums[m]['salary']:
+                m = j
+            j += 1
+        nums[i], nums[m] = nums[m], nums[i]
+        i += 1
+
+
 # Дан список сотрудников:
 staff = [
     {
@@ -29,6 +47,12 @@ staff = [
 
 # 1. Выведите список сотрудников, отсортированный по уменьшению их заработной платы.
 # Если у нескольких сотрудников одинаковая ЗП, то добавьте сортировку по Фамилии
+sort_choice(staff)
 print("Список сотрудников отсортированный по уменьшению ЗП:")
+for el in staff:
+    print(el)
+print()
 
 # 2. Найдите сумму зарплат трех самых низкооплачиваемых сотрудников:
+print("Сумма зарплат трех самых низкооплачиваемых сотрудников:")
+print(sum((el['salary'] for el in staff[:3])))
