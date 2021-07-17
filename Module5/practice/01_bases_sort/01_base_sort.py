0e08aecebeec37b31812ff8438b3acdcd6b56f65
alexs2011 <kir-al93@yandex.ru>
2021-07-17 14:37:51 +0700

    Update 01_base_sort.py

--- Module5/practice/01_bases_sort/01_base_sort.py
+++ Module5/practice/01_bases_sort/01_base_sort.py
@@ -2,3 +2,20 @@
 # Раскомментируйте print-ы, изучите вывод в консоли.
 # Вспомнив теорию, оптимизируйте алгоритм сортировки...
 
+
+nums = [5, 2, 1, 8, 4]
+print("before sort = ", nums)
+swapped = True
+not_process = 0
+while swapped:
+    swapped = False
+    print("*****")
+    for i in range(len(nums) - 1 - not_process):
+        print("i = ", i)
+        if nums[i] > nums[i + 1]:
+            # Меняем элементы
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            # Устанавливаем swapped в True для следующей итерации
+            swapped = True
+    not_process += 1
+print("after sort = ", nums)
