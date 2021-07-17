cf089f948d7530b82aae554c2125b7d82be67738
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-07-17 14:16:01 +0300

    Update 06_task_sort.py

--- Module5/practice/02_tasks_sort/06_task_sort.py
+++ Module5/practice/02_tasks_sort/06_task_sort.py
@@ -13,3 +13,20 @@
 # 70
 # Пояснение:
 # Возможен такой порядок: 10 2 50 1 10
+def sort_choice(nums, key=lambda el: el, reverse=False):
+    i = 0
+    while i < len(nums) - 1:
+        m = i
+        j = i + 1
+        while j < len(nums):
+            compare = key(nums[j]) > key(nums[m]) if reverse else key(nums[j]) < key(nums[m])
+            if compare:
+                m = j
+            j += 1
+        nums[i], nums[m] = nums[m], nums[i]
+        i += 1
+
+
+recipe = [5, 2, 1, 10, 50, 10]
+sort_choice(recipe)
+print(sum(recipe[-len(recipe)//2:]))
