665fdb49f8195129a1445c71e47d96839321e99a
Anna Zhuravleva <zhuravlevahanna@gmail.com>
2021-07-17 12:13:10 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -2,5 +2,35 @@
 # Дан список чисел.
 # Найти: сумму 5-ти самых больших элементов
 
-numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
 
+def quick_sort(nums):
+
+    def partition(nums, low, high):
+        pivot = nums[(low + high) // 2]
+        i = low - 1
+        j = high + 1
+        while True:
+            i += 1
+            while nums[i] < pivot:
+                i += 1
+
+            j -= 1
+            while nums[j] > pivot:
+                j -= 1
+
+            if i >= j:
+                return j
+            nums[i], nums[j] = nums[j], nums[i]
+
+    def _quick_sort(items, low, high):
+        if low < high:
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
+
+
+numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
+quick_sort(numbers)
+print(sum(numbers[-5:]))
