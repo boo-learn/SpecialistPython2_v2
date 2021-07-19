eb3b69ee8a159d682aa880697952d0b61ef297ef
alexs2011 <kir-al93@yandex.ru>
2021-07-19 14:54:40 +0700

    Update LinkedList.py

--- Module6/practice/LinkedList.py
+++ Module6/practice/LinkedList.py
@@ -7,29 +7,71 @@ class Node:
         self.value = value
         self.next = next
 
+    def __str__(self):
+        return f"Node: {self.value}"
+
 
 class LinkedList:
-    def __init__(self):
+    def __init__(self, *args):
         self.first = None
         self.last = None
+        self.__len = 0
+        if args:
+            for agr in args:
+                self.add(agr)
 
     def __str__(self):
-        # FIXME: убрать вывод запятой после последнего элемента
         if self.first is not None:
             current = self.first
-            out = 'LinkedList [' + str(current.value) + ','
+            out = 'LinkedList [' + str(current.value)
             while current.next is not None:
                 current = current.next
-                out += str(current.value) + ','
+                out += ',' + str(current.value)
             return out + ']'
         return 'LinkedList []'
+
+    def __getitem__(self, idx):
+        if type(idx) is not int:
+            raise TypeError('LinkedList indices must be integers')
+        if idx >= len(self) or abs(idx) > len(self):
+            raise IndexError('LinkedList index out of range')
+
+        idx = idx % len(self)
+        cur_node = self.first
+        while idx > 0:
+            idx -= 1
+            cur_node = cur_node.next
+        return cur_node.value
+
+    def __setitem__(self, idx, value):
+        if type(idx) is not int:
+            raise TypeError('LinkedList indices must be integers')
+        if idx >= len(self) or abs(idx) > len(self):
+            raise IndexError('LinkedList assignment index out of range')
+
+        idx = idx % len(self)
+        cur_node = self.first
+        while idx > 0:
+            idx -= 1
+            cur_node = cur_node.next
+        cur_node.value = value
+
+    def __iter__(self):
+        self.cur = -1
+        return self
+
+    def __next__(self):
+        if self.cur < len(self) - 1:
+            self.cur += 1
+            return self[self.cur]
+        else:
+            raise StopIteration
 
     def clear(self):
         """
         Очищаем список
         """
-        # TODO: реализовать очистку списка
-        raise TypeError("Not implemented")
+        self.__init__()
 
     def add(self, value):
         """
@@ -43,6 +85,7 @@ class LinkedList:
         else:
             self.last.next = new_node
             self.last = new_node
+        self.__len += 1
 
     def push(self, value):
         """
@@ -54,13 +97,32 @@ class LinkedList:
         else:
             new_node = Node(value, self.first)
             self.first = new_node
+        self.__len += 1
 
     def insert(self, value, index):
         """
         Вставляет узел со значением value на позицию index
         """
-        # TODO: реализовать вставку
-        raise TypeError("Not implemented")
+        if index == 0:
+            self.push(value)
+            return
+        if index >= self.len():
+            self.add(value)
+            return
+
+        cur_idx = 0
+        current = self.first
+        while current.next is not None:
+            if cur_idx == index - 1:
+                break
+            current = current.next
+            cur_idx += 1
+
+        prev_node = current
+        next_node = current.next
+        new_node = Node(value, next_node)
+        prev_node.next = new_node
+        self.__len += 1
 
     def find(self, value):
         """
@@ -68,19 +130,21 @@ class LinkedList:
         :param value: значение искомого элемента
         :return: индекс искомого элемента, или ???, если элемент не найден
         """
-        # TODO: реализовать поиск элемента
-        #   подумать над возвращаемым значением, если элемент со значение value не найден
-        raise TypeError("Not implemented")
+        idx = 0
+        cur_node = self.first
+        while cur_node:
+            if cur_node.value == value:
+                return idx
+            else:
+                idx += 1
+                cur_node = cur_node.next
+        raise ValueError(f"{value} is not in LinkedList")
 
     def len(self):
-        # TODO: сделать более быструю реализацию, т.к. каждый раз проходка по всем элементам - долго
-        length = 0
-        if self.first is not None:
-            current = self.first
-            while current.next is not None:
-                current = current.next
-                length += 1
-        return length + 1  # +1 для учета self.first
+        return self.__len
+
+    def __len__(self):
+        return self.len()
 
 
 if __name__ == "__main__":
@@ -89,24 +153,16 @@ if __name__ == "__main__":
     L.add(1)
     L.add(2)
     L.add(3)
+    L.insert(-4, 0)
 
     print("list = ", L)
 
-    # TODO: реализовать интерфейс итерации
-    # for el in L:
-    #     print(el)
-    # Напомню принцип работы итератора:
-    # iterator_L = iter(L) L.__iter__()
-    # next(iterator_L) it.__next__()
-    # next(iterator_L)
-    # next(iterator_L)
-    # next(iterator_L)
+    for el in L:
+        print(el)
 
-    # TODO: реализовать обращение по индексу и изменение значение по индексу
-    # print(L[0])
-    # L[0] = "new"
-    # print(L[0])
+    print(L[0])
+    L[0] = "new"
+    print(L[0])
 
-    # TODO: реализовать создание нового списка с задание начальных элементов
-    # L = LinkedList(2, 4, 6, -12)
-    # print(L)
+    L = LinkedList(2, 4, 6, -12)
+    print(L)
