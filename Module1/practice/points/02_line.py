2cf53025e3dc967fa46c68a258968a83513ee353
marisedas2101 <77969389+marisedas2101@users.noreply.github.com>
2021-07-19 20:52:31 +0300

    Update 02_line.py

--- Module1/practice/points/02_line.py
+++ Module1/practice/points/02_line.py
@@ -1,3 +1,4 @@
+import math
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -9,6 +10,7 @@ points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 
 # Задание: Найдите длину ломаной линии
 
-# TODO: your core here...
+line_sum = sum([math.sqrt((points[i + 1].y - points[i].y) ** 2 + (points[i + 1].x - points[i].x) ** 2) for i, _ in
+                enumerate(points) if i + 1 != len(points)])
+print("Длина ломаной линии = ", line_sum)
 
-print("Длина ломаной линии = ", ...)
