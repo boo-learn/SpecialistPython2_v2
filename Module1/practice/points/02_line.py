2fb13b6d9c1853aa02578a573edb571ed74e5d2d
efajunk <53151777+efajunk@users.noreply.github.com>
2021-07-19 21:07:35 +0300

    Update 02_line.py

--- Module1/practice/points/02_line.py
+++ Module1/practice/points/02_line.py
@@ -4,6 +4,22 @@ class Point:
         self.y = y
 
 
+def distance(x_point1, y_point1, x_point2, y_point2) -> object:
+    """
+    Расстояние между двумя точками
+    """
+    result = ((x_point1 - x_point2) ** 2 + (y_point1 - y_point2) ** 2) ** 0.5
+    return result
+
+def distance_addition(points_list):
+    result = []
+    for i in range(len(points_list)):
+        if i + 1 < len(points_list):
+            result.append(distance(points_list[i].x, points_list[i].y, points_list[i + 1].x, points_list[i + 1].y))
+        else:
+            return result
+
+
 # Ломаная линия задана произвольным количеством последовательных точек
 points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 
@@ -11,4 +27,4 @@ points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 
 # TODO: your core here...
 
-print("Длина ломаной линии = ", ...)
+print(f'Длина ломаной линии = {sum(distance_addition(points)):.2f}')
