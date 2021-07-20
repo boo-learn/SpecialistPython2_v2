fba6fa3d9d01b25e5b33033b30958dabb7158c64
MarinaDrenina <87034452+MarinaDrenina@users.noreply.github.com>
2021-07-20 17:20:27 +0300

    Update 01_hw_coins.py

--- Module1/home_work/01_hw_coins.py
+++ Module1/home_work/01_hw_coins.py
@@ -1,5 +1,7 @@
-import random
+# Задание: создайте список из n-монеток. Подбросьте(flip) все монетки.
+# выведите соотношение выпавших орлов и решек в процентах
 
+import random
 
 class Coin:
     def __init__(self):
@@ -7,11 +9,17 @@ class Coin:
         self.side = None
 
     def flip(self):
-        """
-        Подбрасывание монетки
-        """
-        self.side = ...  # random: heads/tails
+        side=["heads","tails"]
+        self.side = random.choice(side)
 
+coin_side=[]
+n=10
+for i in range(n):
+    coin_=Coin()
+    coin_.flip()
+    coin_side.append(coin_.side)
 
-# Задание: создайте список из n-монеток. Подбросьте(flip) все монетки.
-# выведите соотношение выпавших орлов и решек в процентах
+heads_count=len([el for el in coin_side if el=="heads"])
+print(coin_side)
+print("Доля решек:", '{:.0%}'.format((len(coin_side)-heads_count)/len(coin_side)))
+print("Доля орлов:", '{:.0%}'.format(heads_count/len(coin_side)))
