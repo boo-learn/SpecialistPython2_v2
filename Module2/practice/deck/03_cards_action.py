49c13051fccccce84f90c6095dd655d6037bc403
Caser281 <67740673+Caser281@users.noreply.github.com>
2021-07-20 20:22:08 +0300

    Update 03_cards_action.py

--- Module2/practice/deck/03_cards_action.py
+++ Module2/practice/deck/03_cards_action.py
@@ -1,9 +1,52 @@
 class Card:
-    pass
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
+	def __init__(self, value, suit):
+		self.value = value
+		self.suit = suit
+	
+	def to_str(self):
+		if self.suit == 'Hearts':
+			return f"{self.value}♥"
+		elif self.suit == 'Diamonds':
+			return f"{self.value}♦"
+		elif self.suit == 'Spades':
+			return f"{self.value}♠"
+		elif self.suit == 'Clubs':
+			return f"{self.value}♣"
 
+	def equal_suit(self, other_card):
+		return self.suit == other_card.suit
+
+
+hearts_cards = []
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+for value in values:
+	hearts_cards.append(Card(value, 'Hearts'))
+
+
+diamonds_cards = []
+for value in values:
+	diamonds_cards.append(Card(value, 'Diamonds'))
+
+
+spades_cards = []
+for value in values:
+	spades_cards.append(Card(value, 'Spades'))
+
+
+clubs_cards = []
+for value in values:
+	clubs_cards.append(Card(value, 'Clubs'))
 
 cards = []
-# TODO-1: в список cards добавьте ВСЕ карты всех мастей
 
-# TODO-2: Выведите карты в формате: cards[кол-во]2♥, 3♥, 4♥ ... A♥, 2♦, 3♦ ... A♦, ....
+
+for card in (hearts_cards + diamonds_cards + spades_cards + clubs_cards):
+	cards.append(card)
+
+
+cards_str = cards[0].to_str()
+for card in cards[1:]:
+	cards_str += ',' + card.to_str()
+
+
+print(cards_str)
