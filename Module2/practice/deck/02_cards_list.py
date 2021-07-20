fdb736d408afb58e0c72196a6b60774b4e7f716e
MarinaDrenina <87034452+MarinaDrenina@users.noreply.github.com>
2021-07-20 20:00:21 +0300

    Update 02_cards_list.py

--- Module2/practice/deck/02_cards_list.py
+++ Module2/practice/deck/02_cards_list.py
@@ -1,14 +1,18 @@
 class Card:
-    pass
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
+     def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
 
-
-hearts_cards = []
 # TODO-1: добавьте в список hearts_cards все червовые карты(от 2-ки до туза)
+values=[2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+hearts_cards = [str(card)+'\u2665' for card in values]
 
-diamonds_cards = []
 # TODO-2: добавьте в список diamonds_cards все бубновые карты(от туза до 2-ки)
+diamonds_cards = [str(card)+'\u2666' for card in values]
 
 # TODO-3: выведите все карты из списка hearts_cards в терминал через запятую в одну строку:
 # Пример вывода: 2♥, 3♥, 4♥ ... A♥
+line=""
+line=",".join(hearts_cards)
+print(line)
 
