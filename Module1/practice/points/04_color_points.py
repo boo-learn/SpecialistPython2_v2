290475acb05be784830b96d69e09ff3a696fee4d
marisedas2101 <77969389+marisedas2101@users.noreply.github.com>
2021-07-20 11:58:34 +0300

    Update 04_color_points.py

--- Module1/practice/points/04_color_points.py
+++ Module1/practice/points/04_color_points.py
@@ -1,11 +1,14 @@
+import math
+
+
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        self.color = ...
+        self.color = color
 
     def dist_to(self, other_point):
-        ...
+        return math.sqrt((self.y - other_point.y) ** 2 + (self.x - other_point.x) ** 2)
 
 
 # Дан список точек нарисованных красным(red) и зеленым(green) цветами
@@ -18,13 +21,20 @@ points = [
     Point(10, -2, "green"),
     Point(-12, 0, "red")
 ]
+
+
 # Все точки одного цвета соеденены линиями и образуют треугольник
+def triangle_area(a1, a2, a3):
+    p = (a1 + a2 + a3) / 2
+    return math.sqrt(p * (p - a1) * (p - a2) * (p - a3))
 
-# Задание-1: доработайте конструкто class Point для хранения цвета точки
-# Задание-2: реализуйте метод dist_to()
-# Задание-3: вычислите площади треугольников образованных точками разных цветов
 
-# TODO: your core here...
+def choose_color_triangle(color):
+    return list(filter(lambda x: x.color == color, points))
 
-print("Площадь красного треугольника = ", ...)
-print("Площадь зеленого треугольника = ", ...)
+reds = choose_color_triangle('red')
+greens = choose_color_triangle('green')
+print("Площадь красного треугольника = ",
+      triangle_area(reds[0].dist_to(reds[1]), reds[0].dist_to(reds[2]), reds[1].dist_to(reds[2])))
+print("Площадь зеленого треугольника = ",
+      triangle_area(greens[0].dist_to(greens[1]), greens[0].dist_to(reds[2]), reds[1].dist_to(reds[2])))
