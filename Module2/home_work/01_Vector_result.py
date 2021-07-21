485cdf39b6bbea8e237665110639a11d2aa6b759
Caser281 <67740673+Caser281@users.noreply.github.com>
2021-07-21 03:50:20 +0300

    Update 01_Vector_result.py

--- Module2/home_work/01_Vector_result.py
+++ Module2/home_work/01_Vector_result.py
@@ -1,5 +1,16 @@
-# Сюда отправляем решение задачи "Вектор"
-# Само задание в файле 01_Vector_task.md
-
 class Vector:
-    pass
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def vector_sum(self, other_vector):
+		self.x = self.x + other_vector.x
+		self.y = self.y + other_vector.y
+
+	def vector_difference(self, other_vector):
+		self.x = self.x - other_vector.x
+		self.y = self.y - other_vector.y
+
+	def vector_scalar(self, scalar):
+		self.x = self.x * scalar
+		self.y = self.y * scalar
