5b15e17e721b37a4915a6d25534257af15d5fb91
efajunk <53151777+efajunk@users.noreply.github.com>
2021-07-21 16:46:49 +0300

    Update 01_task_deck.py

--- Module2/practice/deck/deck_tasks/01_task_deck.py
+++ Module2/practice/deck/deck_tasks/01_task_deck.py
@@ -1,4 +1,16 @@
 from deck_total import Card, Deck
 
+play_deck = Deck()
+play_deck.shuffle()
+
+card1, card2 = play_deck.draw(2)
+
+if card1 > card2:
+    print(f"{card1} больше {card2}")
+elif card1 < card2:
+    print(f"{card1} меньше {card2}")
+elif card1 == card2:
+    print(f"{card1} равна {card2}")
+
 # Создайте колоду из 52 карт. Перемешайте ее. Вытяните две карты сверху.
 # Сравните эти карты и выведите сообщение формата: “карта A♦ больше J♣”
