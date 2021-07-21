7bed0bab8768a7271606d4665365230c8590f953
NickShustov <48993223+NickShustov@users.noreply.github.com>
2021-07-21 19:01:31 +0300

    Update 05_deck_part2.py

--- Module2/practice/deck/05_deck_part2.py
+++ Module2/practice/deck/05_deck_part2.py
@@ -1,18 +1,73 @@
+import random
+
+
 class Card:
     pass
 
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
-    #  реализуем новые методы
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+
+    def to_str(self):
+        icons = {'Hearts': '\u2665',
+                     'Diamonds': '\u2666',
+                     'Clubs': '\u2663',
+                     'Spades': '\u2660'}
+        return f'{self.value}{icons[self.suit]}'
+
+    def equal_suit(self, other_card):
+        return other_card.suit == self.suit
+
+
     def more(self, other_card):
-        pass
+        icons_values = {'Hearts': 4,
+                        'Diamonds': 3,
+                        'Clubs': 2,
+                        'Spades': 1}
+        if self.value != other_card.value:
+            return self.value > other_card.value
+        else:
+            return icons_values[self.suit] > icons_values[other_card.suit]
 
     def less(self, other_card):
-        pass
+        icons_values = {'Hearts': 4,
+                        'Diamonds': 3,
+                        'Clubs': 2,
+                        'Spades': 1}
+        if self.value != other_card.value:
+            return self.value < other_card.value
+        else:
+            return icons_values[self.suit] < icons_values[other_card.suit]
 
 
 class Deck:
-    pass
-    # TODO: сюда копируем реализацию класса колоды из предыдущего задания
+    def __init__(self):
+        # Список карт в колоде. Каждым элементом списка будет объект класса Card
+        self.cards = []
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        for suit in suits:
+            for value in values:
+                self.cards.append(Card(value, suit))
+
+    def show(self):
+        # Принцип работы данного метода прописан в 00_task_deck.md
+        cards_str = f"deck[{len(self.cards)}]{self.cards[0].to_str()}"
+        for card in self.cards[1:]:
+            cards_str += "," + card.to_str()
+        return cards_str
+
+    def draw(self, x):
+        # Принцип работы данного метода прописан в 00_task_deck.md
+        taken_card = []
+        for _ in range(x):
+            card = self.cards.pop(0)
+            taken_card.append(card)
+        return taken_card
+
+    def shuffle(self):
+        random.shuffle(self.cards)
 
 
 deck = Deck()
