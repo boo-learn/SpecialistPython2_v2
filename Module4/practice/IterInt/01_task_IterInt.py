79f8af3b6a59b71eba5b76b688e48967463e8a15
marisedas2101 <77969389+marisedas2101@users.noreply.github.com>
2021-07-23 14:28:07 +0300

    Update 01_task_IterInt.py

--- Module4/practice/IterInt/01_task_IterInt.py
+++ Module4/practice/IterInt/01_task_IterInt.py
@@ -1,8 +1,24 @@
+
 # Разработать класс IterInt, который наследует функциональность стандартного типа int, но добавляет
 # возможность итерировать по цифрам числа
 
 class IterInt(int):
-    pass
+
+    def __iter__(self):
+        self.last_idx = None
+        return self
+
+    def __next__(self):
+        self.lst = list(str(self))
+        for idx, n in enumerate(self.lst):
+            self.lst[idx] = int(n)
+        if self.last_idx is None:
+            self.last_idx = 0
+        else:
+            self.last_idx += 1
+        if self.last_idx >= len(self.lst):
+            raise StopIteration
+        return self.lst[self.last_idx]
 
 
 n = IterInt(12346)
