fbdc87ea98207e6300990d0ffbd1ed302ca131e1
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-07-24 00:36:42 +0300

    Update 03_cards_action.py

--- Module2/practice/deck/03_cards_action.py
+++ Module2/practice/deck/03_cards_action.py
@@ -1,9 +1,30 @@
 class Card:
-    pass
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
+    HEARTS = 'Hearts'
+    DIAMONDS = 'Diamonds'
+    SPADES = 'Spades'
+    CLUBS = 'Clubs'
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def __str__(self):
+        suit = {'Spades': '\u2660', 'Clubs': '\u2663', 'Diamonds': '\u2666', 'Hearts': '\u2665'}
+        return f'{self.value}{suit[self.suit]}'
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
 
 
 cards = []
-# TODO-1: в список cards добавьте ВСЕ карты всех мастей
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+suits = [Card.SPADES, Card.CLUBS, Card.DIAMONDS, Card.HEARTS]
+for suit in suits:
+    for value in values:
+        card = Card(value, suit)
+        cards.append(card)
 
-# TODO-2: Выведите карты в формате: cards[кол-во]2♥, 3♥, 4♥ ... A♥, 2♦, 3♦ ... A♦, ....
+deck_string = f'deck[{len(cards)}]: '
+for card in cards:
+    deck_string += str(card) + ", "
+print(deck_string)
