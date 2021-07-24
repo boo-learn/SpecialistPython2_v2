f43f791a12bb6221b313dd2db456c4ec266aed00
Anna Zhuravleva <zhuravlevahanna@gmail.com>
2021-07-24 11:42:10 +0300

    Update maze1.py

--- Module7/practice/01_Maze-1/maze1.py
+++ Module7/practice/01_Maze-1/maze1.py
@@ -2,7 +2,42 @@
 
 # Опишите список смежности по изображению лабиринта из файла task.md
 graph = [
-    ...
+    [1],
+    [5],
+    [6],
+    [7],
+    [5, 8],
+    [4, 1],
+    [2, 10],
+    [3],
+    [4, 9],
+    [8, 10],
+    [6, 9, 11, 14],
+    [10],
+    [8],
+    [],
+    [10, 15],
+    [14]
 ]
 
 # Решите задачу и выведите ответ в нужном формате
+
+
+def dfs(v):
+    visited[v] = True
+    for w in graph[v]:
+        if not visited[w]:  # посещён ли текущий сосед?
+            dfs(w)
+
+
+my_dict = {0: 'S-1', 12: 'S-2', 3: 'S-3'}
+finish = 14
+
+for start in my_dict.keys():
+    visited = [False] * (len(graph))
+    dfs(start)
+    if visited[finish]:
+        print(f'Из точки {my_dict[start]} можно дойти до финиша')
+    else:
+        print(f'Из точки {my_dict[start]} нельзя дойти до финиша')
+    #print(visited)
