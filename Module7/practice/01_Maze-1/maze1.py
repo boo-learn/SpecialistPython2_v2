903a591bea26714eedb64022a3b13c6a625fc022
parfenov1976 <83123644+parfenov1976@users.noreply.github.com>
2021-07-24 11:42:10 +0300

    Update maze1.py

--- Module7/practice/01_Maze-1/maze1.py
+++ Module7/practice/01_Maze-1/maze1.py
@@ -2,7 +2,42 @@
 
 # Опишите список смежности по изображению лабиринта из файла task.md
 graph = [
-    ...
+    [1],                # 0
+    [0, 5],             # 1
+    [6],                # 2
+    [7],                # 3
+    [5, 8],             # 4
+    [1, 4],             # 5
+    [2, 10],            # 6
+    [3],                # 7
+    [4, 9, 12],         # 8
+    [8, 10],            # 9
+    [6, 9, 11, 14],     # 10
+    [10],               # 11
+    [8],                # 12
+    [],                 # 13
+    [10, 15],           # 14
+    [14]                # 15
+
 ]
+
+start_points = {'S-1': 0, 'S-2': 12, 'S-3': 3}
+finish = 14
+for item in start_points.items():
+    start = item[1]
+    lengths = [None] * (len(graph))
+    lengths[start] = 0
+    queue = [start]
+    while queue:
+        cur_vertex = queue.pop(0)
+        for vertex in graph[cur_vertex]:
+            if lengths[vertex] is None:
+                lengths[vertex] = lengths[cur_vertex] + 1
+                queue.append(vertex)
+    if lengths[finish] is not None:
+        print(f'Из точки{item[0]} можно дойти до финиша')
+    else:
+        print(f'Из точки{item[0]} нельзя дойти до финиша')
+
 
 # Решите задачу и выведите ответ в нужном формате
