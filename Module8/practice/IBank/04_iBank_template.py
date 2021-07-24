90f61b2f681bc3665e4edc32ec69e0c6601ba4e6
alexs2011 <kir-al93@yandex.ru>
2021-07-24 20:10:18 +0700

    Update 04_iBank_template.py

--- Module8/practice/IBank/04_iBank_template.py
+++ Module8/practice/IBank/04_iBank_template.py
@@ -20,7 +20,7 @@ class AccountBase(ABC):
         pass
 
     @abstractmethod
-    def deposite(self, amount):
+    def deposit(self, amount):
         """
         Внесение суммы на текущий счет
         :param amount: сумма
@@ -50,3 +50,66 @@ class AccountBase(ABC):
         return f"..."
 
 
+class Account(AccountBase):
+    def __init__(self, name, passport8, phone_number, start_balance=0):
+        AccountBase.__init__(self, name, passport8, phone_number, start_balance=0)
+        self.history = []
+
+    def transfer(self, target_account, amount):
+        """
+        Перевод денег на счет другого клиента
+        :param target_account: счет клиента для перевода
+        :param amount: сумма перевода
+        :return:
+        """
+        self.withdraw(amount)
+        target_account.deposit(amount)
+        self.history.append({'transfer': {'to': target_account.passport8} | {'amount': amount}})
+
+    def deposit(self, amount):
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        """
+        self.balance += amount
+        self.history.append({'deposit': amount})
+
+    def withdraw(self, amount):
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        """
+        if self.balance < amount:
+            raise ValueError("На счёту недостаточно средств")
+        self.balance -= amount
+        self.history.append({'withdraw': amount})
+
+    def full_info(self):
+        """
+        Полная информация о счете в формате: "Иванов Иван Петрович баланс: 100 руб. паспорт: 12345678 т.89002000203"
+        """
+        return f"{self.name} баланс: {self.balance} руб. паспорт: {self.passport8} т. {self.phone_number}"
+
+    def __repr__(self):
+        """
+        :return: Информацию о счете в виде строки в формате "Иванов И.П. баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.balance} руб."
+
+    def account_history(self):
+        print(self.history)
+
+
+class CreditAccount(Account):
+    pass
+
+
+account1 = Account("Иван", 1234, "...")
+account2 = Account("Пётр", 5678, "...", start_balance=300)
+print(account1.full_info())
+account1.deposit(100)
+print(account1.full_info())
+account1.transfer(account2, 100)
+print(account1.full_info())
+print(account2.full_info())
+account1.account_history()
