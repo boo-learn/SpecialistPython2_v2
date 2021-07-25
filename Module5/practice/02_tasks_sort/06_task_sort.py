25829b552511307ac682d5cb4fae06625de45f56
efajunk <53151777+efajunk@users.noreply.github.com>
2021-07-25 15:49:17 +0300

    Update 06_task_sort.py

--- Module5/practice/02_tasks_sort/06_task_sort.py
+++ Module5/practice/02_tasks_sort/06_task_sort.py
@@ -9,7 +9,28 @@
 # Пример
 # Вход:
 # 5 2 1 10 50 10
+
 # Выход:
 # 70
 # Пояснение:
 # Возможен такой порядок: 10 2 50 1 10
+
+list_of_products = [int(i) for i in input(': ').split()]
+sorted(list_of_products)
+trick_list = []
+index_count = 1
+cheap = sorted(list_of_products[:int(len(list_of_products) / 2)], reverse=True)
+expensive = sorted(list_of_products[int(len(list_of_products) / 2):], reverse=True)
+
+che_count = 0
+exp_count = 0
+for i in range(int(len(list_of_products))):
+    if i % 2 == 0:
+        trick_list.append(cheap[che_count])
+        che_count += 1
+    else:
+        trick_list.append(expensive[exp_count])
+        exp_count += 1
+
+print(trick_list)
+print(sum(expensive))
