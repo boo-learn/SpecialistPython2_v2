c4483541e672cd8d80530d0b46e7ebd354b45bb9
marisedas2101 <77969389+marisedas2101@users.noreply.github.com>
2021-07-25 21:05:03 +0300

    Update 05_task_sort.py

--- Module5/practice/02_tasks_sort/05_task_sort.py
+++ Module5/practice/02_tasks_sort/05_task_sort.py
@@ -29,6 +29,21 @@ staff = [
 
 # 1. Выведите список сотрудников, отсортированный по уменьшению их заработной платы.
 # Если у нескольких сотрудников одинаковая ЗП, то добавьте сортировку по Фамилии
+def bubble_sort(nums):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i]['salary'] < nums[i + 1]['salary']:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+            elif nums[i]['salary'] == nums[i + 1]['salary'] and nums[i]['surname'] > nums[i + 1]['surname']:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+
+
+bubble_sort(staff)
+print(staff)
 print("Список сотрудников отсортированный по уменьшению ЗП:")
 
 # 2. Найдите сумму зарплат трех самых низкооплачиваемых сотрудников:
