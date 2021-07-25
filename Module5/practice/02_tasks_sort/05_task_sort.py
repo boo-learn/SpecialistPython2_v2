25d6738a0986af726a5c7d1c1baa0affb15a26f1
VladimirShandra <87034414+VladimirShandra@users.noreply.github.com>
2021-07-25 19:45:20 +0300

    Update 05_task_sort.py

--- Module5/practice/02_tasks_sort/05_task_sort.py
+++ Module5/practice/02_tasks_sort/05_task_sort.py
@@ -1,7 +1,7 @@
 # Дан список сотрудников:
 staff = [
     {
-        'name': 'Григорий',
+        'name': 'Алексей',
         'surname': 'Петров',
         'salary': 124300
     },
@@ -29,6 +29,32 @@ staff = [
 
 # 1. Выведите список сотрудников, отсортированный по уменьшению их заработной платы.
 # Если у нескольких сотрудников одинаковая ЗП, то добавьте сортировку по Фамилии
-print("Список сотрудников отсортированный по уменьшению ЗП:")
+print("Список сотрудников отсортированный по уменьшению ЗП:", )
+
+for _ in range(len(staff)): #Сортировка по уровню з/п
+    i = 0
+    while i < len(staff) - 1:
+        if staff[i].get("salary") < staff[i + 1].get("salary"):
+            staff[i], staff[i + 1] = staff[i + 1], staff[i]
+        i += 1
+for _ in staff: #Сортировка по имени и фамилии (сначала сравниваются фамилии потом имена у однофамильцев)
+    j = 0
+    while j < len(staff) - 1:
+        if staff[j].get("salary") == staff[j + 1].get("salary"):
+            if staff[j].get("surname") > staff[j + 1].get("surname"):
+                staff[j], staff[j + 1] = staff[j + 1], staff[j]
+            elif staff[j].get("surname") == staff[j + 1].get("surname") \
+                    and staff[j].get("name") > staff[j + 1].get("name"):
+                staff[j], staff[j + 1] = staff[j + 1], staff[j]
+        j += 1
+for el in staff:
+    print(f"{el.get('name')}, {el.get('surname')}, {el.get('salary')}")
 
 # 2. Найдите сумму зарплат трех самых низкооплачиваемых сотрудников:
+
+sum_min_salaries = 0
+for el in staff[-3:]:
+    sum_min_salaries += el.get("salary")
+    
+print(f"Cумма зарплат трех самых низкооплачиваемых сотрудников равне: {sum_min_salaries}")
+
