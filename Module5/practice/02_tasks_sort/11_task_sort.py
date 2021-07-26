e1657ee3dd3679384e92b3187e5345c34a7ccb7a
MarinaDrenina <87034452+MarinaDrenina@users.noreply.github.com>
2021-07-26 18:24:18 +0300

    Update 11_task_sort.py

--- Module5/practice/02_tasks_sort/11_task_sort.py
+++ Module5/practice/02_tasks_sort/11_task_sort.py
@@ -1,2 +1,17 @@
 # Анаграммы*
 # Задается словарь (список слов). Найти в нем все анаграммы (слова, составленные из одних и тех же букв).
+
+worlds=["кот","тик","ток","кто","тир","кит"]
+sort_char=[]
+for world in worlds:
+    sort_char.append(sorted(world))
+print(sort_char)
+for el in sort_char:
+    if sort_char.count(el)>1:
+        my_worlds = []
+        for i in range(sort_char.count(el)):
+            my_index=sort_char.index(el)
+            my_worlds.append(worlds[my_index])
+            worlds.remove(worlds[my_index])
+            sort_char.remove(el)
+        print(my_worlds)
