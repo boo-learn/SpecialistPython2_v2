11a0ea4cac250369b15c0ecb351f922d77d69b9f
MarinaDrenina <87034452+MarinaDrenina@users.noreply.github.com>
2021-07-26 18:18:21 +0300

    Update 08_task_sort.py

--- Module5/practice/02_tasks_sort/08_task_sort.py
+++ Module5/practice/02_tasks_sort/08_task_sort.py
@@ -3,3 +3,15 @@
 # Телефон задан в виде строки формата nn-nn-nn. Например, 23-45-67
 
 phones = ["25-17-58", "11-34-85", "54-61-56", "34-61-72", "25-17-55", "34-56-56"]
+int_phones=[]
+
+for phone in phones:
+    int_phones.append(int(phone.replace("-","")))
+    int_phones.sort()
+    phones=[]
+for phone in int_phones:
+    phone=str(phone)
+    new_phone=f"{phone[:2]}-{phone[2:4]}-{phone[4:]}"
+    print(new_phone)
+    phones.append(new_phone)
+print(phones)
