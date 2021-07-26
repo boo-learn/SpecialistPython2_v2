ff4fed1c34791029ee73f969b6b8908a9ef88c46
MarinaDrenina <87034452+MarinaDrenina@users.noreply.github.com>
2021-07-26 18:23:38 +0300

    Update 10_task_sort.py

--- Module5/practice/02_tasks_sort/10_task_sort.py
+++ Module5/practice/02_tasks_sort/10_task_sort.py
@@ -4,3 +4,17 @@
 # Необходимо разбить их на две кучи таким образом, чтобы веса куч отличались не более чем в 2 раза.
 # Если этого сделать нельзя, то указать это.
 
+stone=[2,3,4,2,1,15,2,8,9,16]
+if max(stone)>(sum(stone)-max(stone))*2:
+    print ("Невозможно")
+else:
+    stone.sort()
+    sum_small=1
+    sum_big=max(stone)
+    i=0
+    while sum_big/sum_small>2:
+        sum_small=sum(stone[:i+1])
+        sum_big=sum(stone[i+1:])
+        i+=1
+    print(stone[:i])
+    print(stone[i:])
