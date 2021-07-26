745c7ff79a25a1340ffc067bd82d36cdc078a2b0
Caser281 <67740673+Caser281@users.noreply.github.com>
2021-07-26 20:07:07 +0300

    Update 02_nodes_from_list.py

--- Module6/practice/nodes/02_nodes_from_list.py
+++ Module6/practice/nodes/02_nodes_from_list.py
@@ -1,8 +1,4 @@
 class Node:
-    """
-    Класс для узла списка. Хранит значение и указатель на следующий узел.
-    """
-
     def __init__(self, value=None, next=None):
         self.value = value
         self.next = next
@@ -29,9 +25,15 @@ names = gen_names_list()
 print(names)
 
 # TODO: создайте цепочку нод из имен списка
-...
+name_dict = []
+for name in names:
+	name_node = Node(name)
+	name_dict.append(name_node)
 
+for name_node in name_dict:
+	if name_dict.index(name_node) < len(name_dict) - 1:
+		name_node.next = name_dict[name_dict.index(name_node)+1]
 
 # TODO: Передайте первую ноду в функцию print_nodes_value(), чтобы получить значения всех нод в цепочке
-first_node = ...
+first_node = name_dict[0]
 print_nodes_value(first_node)
