e471c46b0f9f4026d40b12b80a48d331365f39dc
marisedas2101 <77969389+marisedas2101@users.noreply.github.com>
2021-07-28 19:35:03 +0300

    Update IBank_part1.py

--- Module8/practice/IBank/results/IBank_part1.py
+++ Module8/practice/IBank/results/IBank_part1.py
@@ -13,30 +13,49 @@ class Account:
         :param amount: сумма перевода
         :return:
         """
-        pass
+        if self.balance >= amount:
+            target_account.balance += amount
+            self.balance -= amount
+        else:
+            raise Exception('Недостаточно средств для проведения операции')
 
     def deposit(self, amount):
         """
         Внесение суммы на текущий счет
         :param amount: сумма
         """
-        pass
+        if amount:
+            self.balance += amount
 
     def withdraw(self, amount):
         """
         Снятие суммы с текущего счета
         :param amount: сумма
         """
-        pass
+        if self.balance - amount >= 0:
+            self.balance -= amount
+        else:
+            raise Exception('Недостаточно средств для проведения операции')
 
     def full_info(self):
         """
         Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
         """
-        return f"..."
+        return f"{self.name} баланс: {self.balance} руб. паспорт: {self.passport} т. {self.phone_number}"
 
     def __repr__(self):
         """
         :return: Информацию о счете в виде строки в формате "Иванов И.П. баланс: 100 руб."
         """
-        return f"..."
+        return f"{self.name} баланс: {self.balance} руб."
+
+
+iv = Account('Иван', '5555 222222', '+7-913-976-7642', 100)
+an = Account('Андрей', '5545 112222', '+7-913-987-7642', 200)
+iv.withdraw(10)
+print(iv.balance)
+iv.deposit(100)
+print(iv.balance)
+iv.transfer(an, 15)
+print(iv.balance)
+print(an.balance)
