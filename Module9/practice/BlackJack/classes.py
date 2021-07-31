a1fc4e880d1b8d78688412a73372ad8d17bd4b54
alexs2011 <kir-al93@yandex.ru>
2021-07-31 18:50:16 +0700

    Update classes.py

--- Module9/practice/BlackJack/classes.py
+++ Module9/practice/BlackJack/classes.py
@@ -1,1 +1,108 @@
+import random
+
+
+class Card:
+    HEARTS = "Hearts"
+    DIAMONDS = "Diamonds"
+    SPADES = "Spades"
+    CLUBS = "Clubs"
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+        self.points = Card.set_points(value)
+
+    def __str__(self):
+        icons = {
+            "Hearts": '\u2665',
+            "Diamonds": '\u2666',
+            "Spades": '\u2663',
+            "Clubs": '\u2660',
+        }
+        return f"{self.value}{icons[self.suit]}"
+
+    @staticmethod
+    def set_points(value):
+        if value.isdigit():
+            return int(value)
+        if value == 'A':
+            return 11
+        return 10
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
+
+    def __gt__(self, other_card):  # >
+        if Deck.values.index(self.value) == Deck.values.index(other_card.value):
+            return Deck.suits.index(self.suit) > Deck.suits.index(other_card.suit)
+        else:
+            return Deck.values.index(self.value) > Deck.values.index(other_card.value)
+
+    def __lt__(self, other_card):  # <
+        if Deck.values.index(self.value) == Deck.values.index(other_card.value):
+            return Deck.suits.index(self.suit) < Deck.suits.index(other_card.suit)
+        else:
+            return Deck.values.index(self.value) < Deck.values.index(other_card.value)
+
+
+class Deck:
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    suits = [Card.CLUBS, Card.SPADES, Card.DIAMONDS, Card.HEARTS]
+
+    def __init__(self):
+        # Список карт в колоде. Каждым элементом списка будет объект класса Card
+        self.cards = []
+        #          0    1    2    3 ...
+        self.last_card_index = -1
+        for suit in Deck.suits:
+            for value in Deck.values:
+                card = Card(value, suit)
+                self.cards.append(card)
+
+    def __str__(self):
+        s = f'deck[{len(self.cards)}]:'
+        # str(card) --> card.__str__()
+        for card in self.cards:
+            s = s + str(card) + ","
+        return s
+
+    def __iter__(self):
+        self.last_card_index = -1
+        return self
+
+    def __next__(self):
+        self.last_card_index += 1
+        if self.last_card_index >= len(self.cards):
+            raise StopIteration
+        return self.cards[self.last_card_index]
+
+    def draw(self, x):
+        cards = []
+        for _ in range(x):
+            cards.append(self.cards.pop(0))
+        return cards
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+
+class Hand:
+    def __init__(self):
+        self.hand = []
+
+    def sum_points(self, is_ace_collapse=False):
+        summa = 0
+        if not is_ace_collapse:
+            for card in self.hand:
+                summa += card.points
+        else:
+            for card in self.hand:
+                if card.points == 11:
+                    summa += 1
+                else:
+                    summa += card.points
+        return summa
+
+    def append(self, value):
+        self.hand.append(value)
 
