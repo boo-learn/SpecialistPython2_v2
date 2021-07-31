d2138ad1336d17bcb0782d1cb935e8cc80616ffc
Anna Zhuravleva <zhuravlevahanna@gmail.com>
2021-07-31 14:59:24 +0300

    Update BlackJack.py

--- Module9/practice/BlackJack/BlackJack.py
+++ Module9/practice/BlackJack/BlackJack.py
@@ -1,8 +1,88 @@
-# Возьмите классы Deck и Card с GIST'а второго занятия и доработайте их
-from ... import Deck, Card
+import random
 
-player_money = 100  # Деньги игрока
-rate_value = 10  # Размер ставки
+
+class Card:
+    HEARTS = "Hearts"
+    DIAMONDS = "Diamonds"
+    SPADES = "Spades"
+    CLUBS = "Clubs"
+    suits = [CLUBS, SPADES, DIAMONDS, HEARTS]
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    POINTS = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,
+              '9': 9, '10': 10, 'J': 10, 'K': 10, 'Q': 10, 'A': 11}
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def __repr__(self):
+        icons = {
+            "Hearts": '\u2665',
+            "Diamonds": '\u2666',
+            "Spades": '\u2663',
+            "Clubs": '\u2660',
+        }
+        return f"{self.value}{icons[self.suit]}"
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
+
+    def __gt__(self, other_card): # >
+        if Card.values.index(self.value) == Card.values.index(other_card.value):
+            return Card.suits.index(self.suit) > Card.suits.index(other_card.suit)
+        else:
+            return Card.values.index(self.value) > Card.values.index(other_card.value)
+
+    def __lt__(self, other_card): # <
+        if Card.values.index(self.value) == Card.values.index(other_card.value):
+            return Card.suits.index(self.suit) < Card.suits.index(other_card.suit)
+        else:
+            return Card.values.index(self.value) < Card.values.index(other_card.value)
+
+    @property
+    def get_points(self):
+        return Card.POINTS[self.value]
+
+
+# card1.more(card2)
+
+class Deck:
+
+    def __init__(self):
+        # Список карт в колоде. Каждым элементом списка будет объект класса Card
+        self.cards = []
+        self.last_card_index = -1
+        for suit in Card.suits:
+            for value in Card.values:
+                card = Card(value, suit)
+                self.cards.append(card)
+
+    def __repr__(self):
+        s = f'deck[{len(self.cards)}]:'
+        # str(card) --> card.__str__()
+        for card in self.cards:
+            s = s + str(card) + ","
+        return s
+
+    def __iter__(self):
+        self.last_card_index = -1
+        return self
+
+    def __next__(self):
+        self.last_card_index += 1
+        if self.last_card_index >= len(self.cards):
+            raise StopIteration
+        return self.cards[self.last_card_index]
+
+    def draw(self, x):
+        cards = []
+        for _ in range(x):
+            cards.append(self.cards.pop(0))
+        return cards
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
 
 deck = Deck()
 
@@ -16,51 +96,14 @@ def sum_points(cards):
     # Совет: храните кол-во очков за карту внутри класса Колоды(колода "знает", сколько дает очков каждая карта)
 
     #  Сначала считаем сумму карт, считая ТУЗ за 11-очков
-    sum_points = ...
+    sum_points = sum((card.get_points for card in cards))
     # Если сумма > 21, то перечитываем сумму, считая ТУЗ за 1(единицу)
     if sum_points > 21:
-        ...
-
+        new_sum_points = 0
+        for card in cards:
+            if card.value == 'A':
+                new_sum_points += 1
+            else:
+                new_sum_points += card.get_points
+        return new_sum_points
     return sum_points
-
-
-while True:
-    # 0. Игрок делает ставку
-    player_money -= rate_value
-    # 1. В начале игры перемешиваем колоду
-    # 2. Игроку выдаем две карты
-    player_cards = ...
-    # 3. Дилер берет одну карту
-    dealer_cards = ...
-    # 4. Отображаем в консоли карты игрока и дилера
-    # 5. Проверяем нет ли у игрока блэкджека (21 очко)
-    if sum_points(player_cards) == 21:
-        # Выплачиваем выигрышь 3 и 2
-        player_money += rate_value * 1.5
-        print("Black Jack!!! Игрок победил")
-        # Заканчиваем игру
-    # Если нет блэкджека, то
-    while True:  # Игрок добирает карты пока не скажет "достаточно" или не сделает перебор (>21)
-        player_choice = input("еще(1)/достаточно(0): ")
-        if player_choice == "1":
-            # Раздаем еще одну карту
-            # Если перебор (>21), заканчиваем добор
-            if sum_points(player_cards) > 21:
-                print(f"Перебор: {sum_points(player_cards)} очков")
-                ...
-                break
-        if player_choice == "0":
-            # Заканчиваем добирать карты
-            break
-
-    # Если у игрока не 21(блэкджек) и нет перебора, то
-    if ...:
-        print("Диллер добирает карты")
-        while True:  # дилер начинает набирать карты.
-            ...  # Смотри подробные правила добора дилера в задании
-
-    # Выясняем кто набрал больше очков. Выплачиваем/забираем ставку
-    if sum_points(player_cards) > sum_points(dealer_cards):
-        ...
-    else:
-        ...
