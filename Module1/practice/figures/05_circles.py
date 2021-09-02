c72d113b1288c28ca2f7fa56171507316c9dcc7d
CrystalColeCrystal <86655027+CrystalColeCrystal@users.noreply.github.com>
2021-09-02 22:31:24 +0300

    Update 05_circles.py

--- Module1/practice/figures/05_circles.py
+++ Module1/practice/figures/05_circles.py
@@ -1,14 +1,16 @@
+import math
 class Circle:
     def __init__(self, center_coords, radius):
-        ...
+        self.center_coords=center_coords
+        self.radius=radius
+    def dist_cen(self, other_circle):
+        return math.sqrt((self.center_coords[0]-other_circle.center_coords[0])**2+(self.center_coords[1]-other_circle.center_coords[1])**2)
 
     def intersect(self, other_circle):
-        """
-        Проверяет пересекается ли текущая окружность с other_circle
-        :return: True/False
-        """
-        ...
-
+       if self.dist_cen(other_circle)<= self.radius+other_circle.radius:
+           return True
+       else:
+           return False
 
 # Окружности заданы координатами центров и радиусами
 circle1 = Circle((6, -8), 5)
