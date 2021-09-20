c68d1189b0d76861926e04391b638522665df08f
Oleg Vorobiev <53373374+olegchibis@users.noreply.github.com>
2021-09-20 20:35:57 +0300

    Update 02_line.py

--- Module1/practice/points/02_line.py
+++ Module1/practice/points/02_line.py
@@ -10,5 +10,14 @@ points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 # Задание: Найдите длину ломаной линии
 
 # TODO: your core here...
+def dist(plist):
+    sum_dist = 0
+    for i in range(1,len(plist)-1):
+        temp_dist = ((plist[i].x - plist[i+1].x) ** 2 + (plist[i].y - plist[i+1].y) ** 2) ** .5
+        sum_dist += temp_dist
+    return sum_dist
 
-print("Длина ломаной линии = ", ...)
+
+res_dist = dist(points)
+
+print("Длина ломаной линии = ", res_dist)
