58086fca7fbcda065fba053670285f4acdecb49a
Александр Петров <64920524+vortep72@users.noreply.github.com>
2021-09-20 20:25:09 +0300

    Update 02_line.py

--- Module1/practice/points/02_line.py
+++ Module1/practice/points/02_line.py
@@ -7,8 +7,17 @@ class Point:
 # Ломаная линия задана произвольным количеством последовательных точек
 points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 
+def section():
+    section1 = ((points[0].x - points[1].x) ** 2 + (points[0].y - points[1].y) ** 2) ** 0.5
+    section2 = ((points[1].x - points[2].x) ** 2 + (points[1].y - points[2].y) ** 2) ** 0.5
+    section3 = ((points[2].x - points[3].x) ** 2 + (points[2].y - points[3].y) ** 2) ** 0.5
+    section4 = ((points[3].x - points[4].x) ** 2 + (points[3].y - points[4].y) ** 2) ** 0.5
+
+    return section1 + section2 + section3 + section4
+
+
 # Задание: Найдите длину ломаной линии
 
 # TODO: your core here...
 
-print("Длина ломаной линии = ", ...)
+print(f"Длина ломаной линии = {section()}")
