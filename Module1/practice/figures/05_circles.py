3d8a5dbb65e37c71cd05c1865669f07a974adb83
paul-izum <63441928+paul-izum@users.noreply.github.com>
2021-09-21 20:28:25 +0300

    Update 05_circles.py

--- Module1/practice/figures/05_circles.py
+++ Module1/practice/figures/05_circles.py
@@ -1,13 +1,26 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def dist_to(self, other_point):
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
+
+
 class Circle:
     def __init__(self, center_coords, radius):
-        ...
+        self.center_coords = Point(*center_coords) 
+        self.radius = radius
 
     def intersect(self, other_circle):
         """
         Проверяет пересекается ли текущая окружность с other_circle
         :return: True/False
         """
-        ...
+        dist = self.center_coords.dist_to(other_circle.center_coords)
+        return self.radius + other_circle.radius <= dist
+
+
 
 
 # Окружности заданы координатами центров и радиусами
