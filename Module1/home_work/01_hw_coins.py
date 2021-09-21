eae37cd0e9ae7278cf0d32940ae6e69658fda861
Александр Петров <64920524+vortep72@users.noreply.github.com>
2021-09-21 17:41:43 +0300

    Update 01_hw_coins.py

--- Module1/home_work/01_hw_coins.py
+++ Module1/home_work/01_hw_coins.py
@@ -3,15 +3,33 @@ import random
 
 class Coin:
     def __init__(self):
-        # heads-орел/tails-решка
         self.side = None
 
     def flip(self):
         """
         Подбрасывание монетки
         """
-        self.side = ...  # random: heads/tails
+        sides = ['heads', 'tails']      # heads-орел/tails-решка
+        self.side = random.choice(sides)  # random: heads/tails
+        return self.side
 
 
-# Задание: создайте список из n-монеток. Подбросьте(flip) все монетки.
-# выведите соотношение выпавших орлов и решек в процентах
+count = int(input("Введите количество монет: "))
+coins = []
+heads = 0
+tails = 0
+while count > 0:
+    coins.append(Coin())
+    count -= 1
+sides_coins = []
+for coin in coins:
+    sides_coins.append(coin.flip())
+for char in sides_coins:
+    if char == 'heads':
+        heads += 1
+    if char == 'tails':
+        tails += 1
+count = heads + tails
+
+print(f'Орёл выпал в {heads * 100 / count}% подбрасывания монетки')
+print(f'Решка выпала в {tails * 100 / count}% подбрасывания монетки')
