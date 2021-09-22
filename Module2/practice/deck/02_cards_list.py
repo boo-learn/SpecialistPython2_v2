38a4abaea59d032b2c9aba13797e997d991c2e42
Александр Петров <64920524+vortep72@users.noreply.github.com>
2021-09-22 19:42:13 +0300

    Update 02_cards_list.py

--- Module2/practice/deck/02_cards_list.py
+++ Module2/practice/deck/02_cards_list.py
@@ -1,14 +1,46 @@
 class Card:
-    pass
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        symbols = {
+            "Spades": "\u2660",
+            "Hearts": "\u2665",
+            "Diamonds": "\u2666",
+            "Clubs": "\u2663"
+        }
+        return f"{self.value}{symbols[self.suit]}"
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
+
+
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']     # Список значений карт
+
+
+spades_cards = []
+for value in values:
+    spades_cards.append(Card(value, 'Spades'))
+    for card in spades_cards:
+        print(card.to_str(), end=", ")
 
 
 hearts_cards = []
-# TODO-1: добавьте в список hearts_cards все червовые карты(от 2-ки до туза)
+for value in values:
+    hearts_cards.append(Card(value, 'Hearts'))
+    for card in hearts_cards:
+        print(card.to_str(), end=", ")
+
 
 diamonds_cards = []
-# TODO-2: добавьте в список diamonds_cards все бубновые карты(от туза до 2-ки)
+for value in values:
+    diamonds_cards.append(Card(value, 'Diamonds'))
+    for card in diamonds_cards:
+        print(card.to_str(), end=", ")
 
-# TODO-3: выведите все карты из списка hearts_cards в терминал через запятую в одну строку:
-# Пример вывода: 2♥, 3♥, 4♥ ... A♥
-
+clubs_cards = []
+for value in values:
+    clubs_cards.append(Card(value, 'Clubs'))
+    for card in clubs_cards:
+        print(card.to_str(), end=", ")
