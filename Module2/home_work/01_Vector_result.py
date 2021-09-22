3135d842d5a8f182d3d5b5addd59eafb9d166c36
AyratZagitov <89096208+AyratZagitov@users.noreply.github.com>
2021-09-22 14:54:09 +0300

    Update 01_Vector_result.py

--- Module2/home_work/01_Vector_result.py
+++ Module2/home_work/01_Vector_result.py
@@ -1,5 +1,20 @@
-# Сюда отправляем решение задачи "Вектор"
-# Само задание в файле 01_Vector_task.md
-
 class Vector:
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def plus(self, other_vector):
+        return (self.x + other_vector.x, self.y + other_vector.y)
+
+    def minus(self, other_vector):
+        return (self.x - other_vector.x, self.y - other_vector.y)
+
+    def mult_scalar(self, scl):
+        return (self.x * scl, self.y * scl)
+
+
+v1 = Vector(5, 10)
+v2 = Vector(0, -6)
+print(v1.plus(v2))
+print(v1.minus(v2))
+print(v1.mult_scalar(-4))
