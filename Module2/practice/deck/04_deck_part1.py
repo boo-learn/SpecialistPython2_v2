cbf9f091d5219d52d9f9fbf5fb53c0b3761f798c
svzolotova1 <91084552+svzolotova1@users.noreply.github.com>
2021-09-22 20:38:16 +0300

    Update 04_deck_part1.py

--- Module2/practice/deck/04_deck_part1.py
+++ Module2/practice/deck/04_deck_part1.py
@@ -1,18 +1,38 @@
 class Card:
-    pass
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
 
+    def to_str(self):
+        symbols = {
+            'Hearts': '\u2665',
+            'Diamonds': '\u2666',
+            'Clubs': '\u2663',
+            'Spades': '\u2660'
+        }
+        return f'{self.value}{symbols[self.suit]}'
+
+    def equal_suit(self, other_card):  # проверяет, одинаковые ли масти у карт
+        return self.suit == other_card.suit
 
 # Задание: Теперь создадим колоду из 52-ух карт и реализуем все методы
 class Deck:
     def __init__(self):
         # Список карт в колоде. Каждым элементом списка будет объект класса Card
         self.cards = []
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        for suit in suits:
+            for value in values:
+                card = Card(value, suit)
+                self.cards.append(card)
 
     def show(self):
-        # Принцип работы данного метода прописан в 00_task_deck.md
-        pass
-
+        # Принцип работы данного метода прописан в 00_task_deck.md отображает все карты колоды в формате:
+        # deck[12]: 3♥, 4♦, A♣, …
+        cards_str = ", ".join([card.to_str() for card in self.cards])
+        return f'deck[{len(self.cards)}]{cards_str}'
+    
     def draw(self, x):
         # Принцип работы данного метода прописан в 00_task_deck.md
         pass
