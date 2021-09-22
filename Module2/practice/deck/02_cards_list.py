3b6e92db575939451553df165fe89d7be4a3254a
dmnilov <89096332+dmnilov@users.noreply.github.com>
2021-09-22 19:36:21 +0300

    Update 02_cards_list.py

--- Module2/practice/deck/02_cards_list.py
+++ Module2/practice/deck/02_cards_list.py
@@ -1,14 +1,36 @@
 class Card:
-    pass
-    # TODO: сюда копируем реализацию класса карты из предыдущего задания
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
 
+    def to_str(self):
+        symbols = {
+            'Hearts': '\u2665',
+            'Diamonds': '\u2666',
+            'Clubs': '\u2663',
+            'Spades': '\u2660'
+        }
+        return f'{self.value}{symbols[self.suit]}'
+
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
+
+
+
+
+values = ["2", "3", "4", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 hearts_cards = []
-# TODO-1: добавьте в список hearts_cards все червовые карты(от 2-ки до туза)
+for value in values:
+    H_card = Card(value, "Hearts")
+    hearts_cards.append(H_card)
 
 diamonds_cards = []
-# TODO-2: добавьте в список diamonds_cards все бубновые карты(от туза до 2-ки)
+for value in values:
+    d_card = Card(value, "Diamonds")
+    diamonds_cards.append(d_card)
 
-# TODO-3: выведите все карты из списка hearts_cards в терминал через запятую в одну строку:
-# Пример вывода: 2♥, 3♥, 4♥ ... A♥
 
+for card in hearts_cards:
+    print(card.to_str(), end=", ")
