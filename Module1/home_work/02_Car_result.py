c4fab19e67f54efc4956f036bf62c2fe2dc72dbf
svzolotova1 <91084552+svzolotova1@users.noreply.github.com>
2021-09-22 18:22:52 +0300

    Update 02_Car_result.py

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -1,5 +1,34 @@
-# Сюда отправляем решение задачи "Автомобиль"
-# Само задание в файле 02_Сar_task.md
-
 class Car:
-    pass
+
+    def __init__(self, gas, capacity, gas_per_km):
+        self.gas = gas #сколько бензина в баке
+        self.capacity = capacity #вместимость бака
+        self.gas_per_km = gas_per_km #расход топлива на 100 км
+   
+    def fill(self, gas_fill):
+        gas_def = self.capacity - self.gas
+        if gas_fill > gas_def:
+            self.gas = self.capacity
+            print(f'Машина заполнена полностью, не стоит вливать {gas_fill - gas_def} лишних литров')
+            return self.gas
+        elif gas_fill == gas_def:
+            self.gas = self.capacity
+            print('Машина заполнена полностью')
+            return self.gas
+        else:
+            self.gas = self.gas + gas_fill
+            print(f'Машина заполнена на {self.gas} литр(ов)')
+            return self.gas
+    
+    def ride(self, ride_km):
+         gas_ride = ride_km * self.gas_per_km / 100
+         if gas_ride < self.gas:
+             print(f'Проехали {ride_km} километров. Осталось бензина ->{self.gas - gas_ride} л ')
+         elif gas_ride == self.gas:
+             print(f'Проехали {ride_km} километров. К сожалению бензина не осталось')
+         else:
+             print(f'Проехали {self.gas * 100 / self.gas_per_km} километров. К сожалению бензина не осталось. Осталось проехать {ride_km - self.gas * 100 / self.gas_per_km} км')
+    
+car1 = Car(0, 60, 12)
+car1.fill(60)
+car1.ride(1200)
