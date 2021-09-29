2afa3d36349c2b0a897da2fd054e9af2fe647ef7
PchelinDA <91084692+PchelinDA@users.noreply.github.com>
2021-09-29 18:54:42 +0300

    Update 01_nodes_create.py

--- Module6/practice/nodes/01_nodes_create.py
+++ Module6/practice/nodes/01_nodes_create.py
@@ -13,12 +13,21 @@ node0 = Node("Иван")
 node1 = Node("Алекс")
 node2 = Node("Петр")
 node3 = Node("Анна")
+node4 = Node("Семён")
+node5 = Node("Евгений")
+node6 = Node("Георгий")
+node7 = Node("Кристина")
 # TODO: Добавьте еще несколько нод с именами
 
 # Связываем ноды
 node0.next = node1
 node1.next = node2
 node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+node6.next = node7
+
 # TODO: Свяжите добавленные выше ноды вместе
 
 # Пройдемся по значениям всех нод, начиная с первой
