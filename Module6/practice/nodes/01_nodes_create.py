537a53a8b581790220befcd8cf328a36c67c2aca
Золотова Светлана <91084552+svzolotova1@users.noreply.github.com>
2021-09-29 18:55:44 +0300

    Update 01_nodes_create.py

--- Module6/practice/nodes/01_nodes_create.py
+++ Module6/practice/nodes/01_nodes_create.py
@@ -7,19 +7,34 @@ class Node:
         self.value = value
         self.next = next
 
+    def __str__(self):
+        return f'Node:{self.value}, next:{self.next}'
+
 
 # Создаем ноды(узлы)
 node0 = Node("Иван")
 node1 = Node("Алекс")
 node2 = Node("Петр")
 node3 = Node("Анна")
+
 # TODO: Добавьте еще несколько нод с именами
+node4 = Node("Кевин")
+node5 = Node("Поль")
+node6 = Node("Рэнуа")
+node7 = Node("Семён")
+node8 = Node("Колян")
 
 # Связываем ноды
 node0.next = node1
 node1.next = node2
 node2.next = node3
+
 # TODO: Свяжите добавленные выше ноды вместе
+node3.next = node4
+node4.next = node5
+node5.next = node6
+node6.next = node7
+node7.next = node8
 
 # Пройдемся по значениям всех нод, начиная с первой
 current_node = node0
