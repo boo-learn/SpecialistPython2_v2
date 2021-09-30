681dd9cd32585e01ea484b0c47fdc0c36bbf9568
dmnilov <89096332+dmnilov@users.noreply.github.com>
2021-09-30 21:08:35 +0300

    Update maze2.py

--- Module7/practice/02_Maze-2/maze2.py
+++ Module7/practice/02_Maze-2/maze2.py
@@ -1,5 +1,55 @@
-# Скопируйте решение из предыдущей задачи(Maze-1) и адаптируйте под текущий лабиринт
-# Чем меньше пришлось вносить изменений в код программы, тем лучше было решение предыдущей задачи
+# Выверите и скопируйте сюда наиболее подходяший алгоритм для работы с графом(bfs/dfs)
 
+# Опишите список смежности по изображению лабиринта из файла task.md
+graph = [
+    # список смежности
+    [1],         # 0
+    [0, 2],   # 1
+    [1, 3],         # 2
+    [2, 7],      # 3
+    [5],         # 4
+    [4, 6],      # 5
+    [5],            # 6
+    [3, 11],             # 7
+    [9, 12],             # 8
+    [8, 10],             # 9
+    [9, 11],             # 10
+    [7, 10, 15],             # 11
+    [8, 13],             # 12
+    [12, 14],             # 13
+    [13],             # 14
+    [11],             # 15
+]
 
 # Решите задачу и выведите ответ в нужном формате
+
+
+start = {
+    "s-1": 1,
+    "s-2": 5,
+    "s-3": 14
+}
+finish = 15
+
+
+def bts(graph, start_point):
+    start = start_point
+    lengths = [None] * (len(graph))
+    lengths[start] = 0
+    queue = [start]
+    while queue:
+        cur_vertex = queue.pop(0)
+        for vertex in graph[cur_vertex]:
+            if lengths[vertex] is None:
+                lengths[vertex] = lengths[cur_vertex] + 1
+                queue.append(vertex)
+    return lengths
+
+for point_name, vertex in start.items():
+    visited = bts(graph, vertex)
+    if visited[finish]:
+        print(f'Из точки {point_name} можно дойти до финиша')
+    else:
+        print(f'Из точки {point_name} нельзя дойти до финиша')
+
+
