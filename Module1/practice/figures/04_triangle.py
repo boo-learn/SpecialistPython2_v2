aa5bdc4aef8f53603b8f76d3cd9e465648bc6510
AleksandrYulgu <88583852+AleksandrYulgu@users.noreply.github.com>
2021-10-24 14:59:07 +0300

    Update 04_triangle.py

--- Module1/practice/figures/04_triangle.py
+++ Module1/practice/figures/04_triangle.py
@@ -4,7 +4,8 @@ class Point:
         self.y = y
 
     def dist_to(self, other_point):
-        ...
+        return ((other_point.x - self.x) ** 2 + (other_point.y - self.y) ** 2) ** 0.5
+
 
 
 class Triangle:
@@ -14,12 +15,16 @@ class Triangle:
         self.point3 = p3
 
     def perimeter(self):
-        ...
+        return self.point1.dist_to(self.point2) + self.point2.dist_to(self.point3) + self.point3.dist_to(self.point1)
 
     def area(self):
         # Для нахождения площади, используйте формулу Герона
-        ...
-
+        a = self.point1.dist_to(self.point2)
+        b = self.point2.dist_to(self.point3)
+        c = self.point3.dist_to(self.point1)
+        p = (a + b + c) / 2
+        return (p(p - a)(p - b)(p - c)) ** 0.5
+        
 
 # Треугольник задан координатами трех точек
 triangle1 = Triangle(Point(2, 4), Point(12, 8), Point(-2, 0))
@@ -27,5 +32,5 @@ triangle1 = Triangle(Point(2, 4), Point(12, 8), Point(-2, 0))
 
 # TODO: your core here...
 
-print("Периметр треугольника = ", ...)
-print("Площадь треугольника = ", ...)
+print("Периметр треугольника = ", triangle1.perimeter())
+print("Площадь треугольника = ", triangle1.area())
