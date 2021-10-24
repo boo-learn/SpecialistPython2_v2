6d037cae2511f82192c152888eb3c8dd71d70516
AleksandrYulgu <88583852+AleksandrYulgu@users.noreply.github.com>
2021-10-24 11:30:20 +0300

    Update 02_line.py

--- Module1/practice/points/02_line.py
+++ Module1/practice/points/02_line.py
@@ -9,6 +9,15 @@ points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 
 # Задание: Найдите длину ломаной линии
 
-# TODO: your core here...
+def distance(a, b):
+    ab = ((b.x - a.x)**2 + (b.y - a.y)**2)**0.5
+    return ab
 
-print("Длина ломаной линии = ", ...)
+def long(list_point):
+    long = 0
+    for el in range(len(list_point)-1):
+        long += distance(list_point[el], list_point[el+1])
+    return long
+
+
+print("Длина ломаной линии = ", long(points))
