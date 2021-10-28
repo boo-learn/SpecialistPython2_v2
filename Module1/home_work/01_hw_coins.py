fa5f0cef08ffcec428b9d4a8244816f985b163cc
AleksandrYulgu <88583852+AleksandrYulgu@users.noreply.github.com>
2021-10-28 20:26:11 +0300

    Update 01_hw_coins.py

--- Module1/home_work/01_hw_coins.py
+++ Module1/home_work/01_hw_coins.py
@@ -10,8 +10,34 @@ class Coin:
         """
         Подбрасывание монетки
         """
-        self.side = ...  # random: heads/tails
+        sides = ('heads', 'tails')
+        self.side = random.choice(sides)
+        return self.side
 
+coin1 = Coin()
+coin2 = Coin()
+coin3 = Coin()
+coin4 = Coin()
+coin5 = Coin()
+coin6 = Coin()
+coin7 = Coin()
+coin8 = Coin()
+coin9 = Coin()
+coin10 = Coin()
+
+coins = [coin1, coin10, coin9, coin8, coin7, coin6, coin5, coin4, coin3, coin2]
+def heads_at_tails(lst):
+    head = 0
+    tail = 0
+    for el in lst:
+        if el.flip() == 'heads':
+            head +=1
+        else:
+            tail +=1
+    return f'Соотношение heads = {(head/len(lst))*100}%, tails = {(tail/len(lst))*100}%'
+
+
+print(heads_at_tails(coins))
 
 # Задание: создайте список из n-монеток. Подбросьте(flip) все монетки.
 # выведите соотношение выпавших орлов и решек в процентах
