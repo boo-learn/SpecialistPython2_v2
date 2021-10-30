bcdc830363abb64506493a03cd2aa20d9e0d9ad4
Roman-dell <91017879+Roman-dell@users.noreply.github.com>
2021-10-30 20:56:39 +0300

    Update 02_Car_result.py

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -2,4 +2,38 @@
 # Само задание в файле 02_Сar_task.md
 
 class Car:
-    pass
+    def __init__(self, gas, capaciti, gas_per_km,km=0):
+        self.gas = gas
+        self.capaciti = capaciti
+        self.gas_per_km = gas_per_km
+        self.km=km
+
+    def fill(self, litre):
+        to_full = self.capaciti - self.gas
+        if litre > to_full:
+            self.gas = self.capaciti
+            print(f"Не вместилось литров: {litre - to_full}")
+        else:
+            self.gas = litre
+            print(f"В машине {self.gas} литров")
+
+
+    def ride(self, distance):
+        if distance > (self.gas / self.gas_per_km):
+            print('Проехали', int(self.gas / self.gas_per_km), 'км, кончился бензин')
+            self.km += int(self.gas / self.gas_per_km)
+            self.gas = 0
+  
+        else:
+            print('Проехали', distance, 'км')
+            self.km += distance
+            return self.km
+
+car1 = Car(15, 50, 0.8)
+car1.fill(20)
+car1.ride(200)
+car1.fill(20)
+car1.ride(200)
+print('Общий пробег автомобиля ', car1.km, 'км.')
+
+
