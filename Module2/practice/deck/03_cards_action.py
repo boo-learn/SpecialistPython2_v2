612472d171a8c23ce680c3815ea87f4b6329140e
Roman-dell <91017879+Roman-dell@users.noreply.github.com>
2021-10-31 11:38:59 +0300

    Update 03_cards_action.py

--- Module2/practice/deck/03_cards_action.py
+++ Module2/practice/deck/03_cards_action.py
@@ -7,3 +7,44 @@ cards = []
 # TODO-1: в список cards добавьте ВСЕ карты всех мастей
 
 # TODO-2: Выведите карты в формате: cards[кол-во]2♥, 3♥, 4♥ ... A♥, 2♦, 3♦ ... A♦, ....
+class Card:
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        icon_suits = {'Hearts': '\u2661', 'Diamonds': '\u2662', 'Clubs': '\u2667', 'Spades': '\u2664'}
+        return f'{self.value}{icon_suits[self.suit]}'
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
+
+
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+hearts_cards = []
+for value in values:
+    card = Card(value, 'Hearts')
+    hearts_cards.append(card)
+
+diamonds_cards = []
+for value in values:
+    card = Card(value, 'Diamonds')
+    diamonds_cards.append(card)
+
+clubs_cards = []
+for value in values:
+    card = Card(value, 'Clubs')
+    clubs_cards.append(card)
+
+spades_cards = []
+for value in values:
+    card = Card(value, 'Spades')
+    spades_cards.append(card)
+
+old_cards=spades_cards+clubs_cards+hearts_cards+diamonds_cards
+
+cards_str = ''
+for card in old_cards:
+    cards_str += card.to_str()
+
+print(*cards_str,sep=',')
