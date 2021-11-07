a5ca7945fe09b8ca47d2c5beee6846938960056d
Semidelskaya Natalya <91018002+SevenMakes@users.noreply.github.com>
2021-11-07 16:40:53 +0300

    Update SuperStr.py

--- Module4/practice/SuperStr/SuperStr.py
+++ Module4/practice/SuperStr/SuperStr.py
@@ -7,3 +7,19 @@
 # 2. метод is_palindrome(), который возвращает True или False в зависимости от того,
 # является ли строка палиндромом. Регистрами символов пренебрегать.
 # Пустую строку считать палиндромом.
+class SuperStr (str):
+
+    def is_repeatance(self, s: str):
+        if s in self:
+            s += s
+        return self == s
+
+
+    def is_palindrome(self):
+        return self == self[::-1]
+
+my_str = SuperStr('12321')
+print(my_str.is_palindrome())
+
+my_str = SuperStr('123123')
+print(my_str.is_repeatance('120'))
