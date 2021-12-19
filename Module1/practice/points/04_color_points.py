9fff7f8f6aaf7efe49097396e53f6de539d31922
Семёнов Михаил <96371173+Muxa12012@users.noreply.github.com>
2021-12-19 12:44:12 +0300

    Update 04_color_points.py

--- Module1/practice/points/04_color_points.py
+++ Module1/practice/points/04_color_points.py
@@ -1,11 +1,11 @@
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        self.color = ...
+        self.color = color
 
-    def dist_to(self, other_point):
-        ...
+    def dist_to(self, op):
+        return ((self.x - op.x) ** 2 + (self.y - op.y) ** 2) ** 0.5
 
 
 # Дан список точек нарисованных красным(red) и зеленым(green) цветами
@@ -24,7 +24,26 @@ points = [
 # Задание-2: реализуйте метод dist_to()
 # Задание-3: вычислите площади треугольников образованных точками разных цветов
 
-# TODO: your core here...
-
-print("Площадь красного треугольника = ", ...)
-print("Площадь зеленого треугольника = ", ...)
+r = [[], []]
+g = [[], []]
+pr = 0
+pg = 0
+for a in points:
+    if a.color == "green":
+        g[0].append(a)
+    else:
+        r[0].append(a)
+for i in range(3):
+    r[1].append(r[0][i - 1].dist_to(r[0][i]))
+    g[1].append(g[0][i - 1].dist_to(g[0][i]))
+pr = sum(r[1]) / 2
+pg = sum(g[1]) / 2
+sr = pr
+sg = pg
+for i in range(3):
+    sr *= pr - r[1][i]
+    sg *= pg - g[1][i]
+sr **= 0.5
+sg **= 0.5
+print("Площадь красного треугольника = ", sr)
+print("Площадь зеленого треугольника = ", sg)
