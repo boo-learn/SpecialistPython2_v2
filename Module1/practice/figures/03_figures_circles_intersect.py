ce0f2cdfe307967e71e649c22e983f1d6336f889
Elena Bochkova <planeado@gmail.com>
2022-04-02 15:43:45 +0300

    Update 03_figures_circles_intersect.py

--- Module1/practice/figures/03_figures_circles_intersect.py
+++ Module1/practice/figures/03_figures_circles_intersect.py
@@ -1,6 +1,15 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def dist_to(self, other_point):
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
+
 class Circle:
     def __init__(self, center_coords, radius):
-        ...
+        self.center = Point(*(center_coords))
+        self.radius = radius
 
     def intersect(self, other_circle):
         """
@@ -8,14 +17,15 @@ class Circle:
         :return: True/False
         """
         # TODO-1: реализуйте метод
-        ...
+        return self.center.dist_to(other_circle.center) <= self.radius + other_circle.radius
 
 
 # Окружности заданы координатами центров и радиусами
-circle1 = Circle((6, -8), 5)
-circle2 = Circle((2, 4), 4)
+circle1 = Circle((6, -8), 15)
+circle2 = Circle((13, -8), 4)
 
 if circle1.intersect(circle2):
     print("Окружности пересекаются")
 else:
     print("Окружности НЕ пересекаются")
+
