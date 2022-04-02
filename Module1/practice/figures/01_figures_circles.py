e6eb56991c37b64a5477990ce177f225952f7d01
Elena Bochkova <planeado@gmail.com>
2022-04-02 15:32:17 +0300

    Update 01_figures_circles.py

--- Module1/practice/figures/01_figures_circles.py
+++ Module1/practice/figures/01_figures_circles.py
@@ -1,28 +1,34 @@
+from math import pi
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 class Circle:
     def __init__(self, center_coords, radius):
-        ...
+        self.center = Point(*(center_coords))
+        self.radius = radius
 
     def length(self):
         """
         :return: длину окружности
         """
-        # TODO-1: реализуйте метод
-        ...
+        return 2 * pi * self.radius
 
     def area(self):
         """
         :return: площадь окружности
         """
-        # TODO-2: реализуйте метод
-        ...
+        return pi * (self.radius) ** 2
 
 
 # Окружности заданы координатами центров и радиусами
 circle1 = Circle((6, -8), 5)
 circle2 = Circle((2, 4), 4)
 
-print(f"Длина окружности радиусом {...} = {...}")
-print(f"Длина окружности радиусом {...} = {...}")
+print(f"Длина окружности радиусом {circle1.radius} = {circle1.length()}")
+print(f"Длина окружности радиусом {circle2.radius} = {circle2.length()}")
 
-print(f"Площадь окружности радиусом {...} = {...}")
-print(f"Площадь окружности радиусом {...} = {...}")
+print(f"Площадь окружности радиусом {circle1.radius} = {circle1.area()}")
+print(f"Площадь окружности радиусом {circle2.radius} = {circle2.area()}")
