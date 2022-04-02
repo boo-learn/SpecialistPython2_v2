51a732e3f0da659357866d14c62589407be00042
ivales <i.leskin@mail.ru>
2022-04-02 13:17:41 +0300

    Update 06_color_points[add].py

--- Module1/practice/points/06_color_points[add].py
+++ Module1/practice/points/06_color_points[add].py
@@ -1,11 +1,14 @@
+import math
+
+
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        self.color = ...
+        self.color = color
 
     def dist_to(self, other_point):
-        ...
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
 
 
 # Дан список точек нарисованных красным(red) и зеленым(green) цветами
@@ -24,7 +27,18 @@ points = [
 # TODO-2: реализуйте метод dist_to()
 # TODO-3: вычислите площади треугольников образованных точками разных цветов
 
-# your core here...
+def square_of_triangle(a,b,c):
+    p=(a+b+c)/2
+    return math.sqrt(p*(p-a)*(p-b)*(p-c))
 
-print("Площадь красного треугольника = ", ...)
-print("Площадь зеленого треугольника = ", ...)
+# your core here...
+points_red, points_green=[]
+for point in points:
+    if point.color=="red":
+        points_red.append(point)
+    else:
+        points_green.append(point)
+red_square=square_of_triangle(points_red[0].dist_to(points_red[1]),points_red[1].dist_to(points_red[2]),points_red[0].dist_to(points_red[2]))
+green_square=square_of_triangle(points_green[0].dist_to(points_green[1]),points_green[1].dist_to(points_green[2]),points_green[0].dist_to(points_green[2]))
+print("Площадь красного треугольника = ", red_square)
+print("Площадь зеленого треугольника = ", green_square)
