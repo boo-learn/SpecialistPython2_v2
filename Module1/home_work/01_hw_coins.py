999611f4dbb0bf56786afdc75c4ab015623ef8d3
Elena Bochkova <planeado@gmail.com>
2022-04-08 23:47:38 +0300

    Update 01_hw_coins.py

--- Module1/home_work/01_hw_coins.py
+++ Module1/home_work/01_hw_coins.py
@@ -10,7 +10,33 @@ class Coin:
         """
         Подбрасывание монетки
         """
-        self.side = ...  # random: heads/tails
+        self.side = 'heads' if random.randint(0,1) else 'trails'  # random: heads/tails
+
+# спрашиваем количество подброшенных монеток
+while True:
+    try:
+        n = int(input("Сколько монеток хотите подбросить: "))
+        if n <= 0:
+            raise ValueError
+        break
+    except ValueError:
+        print(f"{n} - это не целое положительное число")
+
+# создаем список монеток и подбрасываем каждую
+coins = [Coin() for x in range(0, n)]
+for coin in coins:
+    coin.flip()
+
+# Считаем количество орлов
+heads = 0
+for coin in coins:
+    if coin.side == 'heads':
+        heads += 1
+
+# подсчитываем проценты орлов и решек и выводим их
+heads_procent = heads * 100 / len(coins)
+trails_percent = (len(coins) - heads) * 100 / len(coins)
+print(f"Процент орлов {heads_procent}%, процент решек {trails_percent}%")
 
 
 # Задание: создайте список из n-монеток. Подбросьте(flip) все монетки.
