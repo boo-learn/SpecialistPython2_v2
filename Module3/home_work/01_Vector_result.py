9e155c9230b827b8702c84a35ff1e807d6905f71
ivales <i.leskin@mail.ru>
2022-04-10 11:57:01 +0300

    module 3 hw done

--- Module3/home_work/01_Vector_result.py
+++ Module3/home_work/01_Vector_result.py
@@ -2,4 +2,14 @@
 # Само задание в файле 01_Vector_task.md
 
 class Vector:
-    pass
+    def __init__(self, point=(0, 0)):
+        self.x, self.y = point
+
+    def __add__(self, other):
+        return Vector((self.x + other.x, self.y + other.y))
+
+    def __sub__(self, other):
+        return Vector((self.x - other.x, self.y - other.y))
+
+    def __mul__(self, other):
+        return Vector((self.x * other, self.y * other))
