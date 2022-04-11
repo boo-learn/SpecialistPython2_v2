10a217579f0b9539e5d487c096b26daba07bd5d8
PopovArtemDm <103452059+PopovArtemDm@users.noreply.github.com>
2022-04-11 21:21:17 +0300

    Update 01_distance.py

--- Module1/practice/points/01_distance.py
+++ Module1/practice/points/01_distance.py
@@ -1,14 +1,12 @@
-class Point:
+claclass Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
 
 def distance(p1, p2):
-    """
-    Расстояние между двумя точками
-    """
-    # TODO: напишите тело функции
+   i = ((p1[x] - p2[x] ** 2) + (p1[y] - p2[y] ** 2))
+   return i
 
 # Дано две точки на координатной плоскости
 point1 = Point(2, 4)
