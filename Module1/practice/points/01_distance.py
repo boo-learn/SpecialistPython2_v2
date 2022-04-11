9220db6cd011df5984dfc337bdbb5a6b96a6ad88
TeKuJIaCoM6pepoc <103452077+TeKuJIaCoM6pepoc@users.noreply.github.com>
2022-04-11 21:38:07 +0300

    Update 01_distance.py

--- Module1/practice/points/01_distance.py
+++ Module1/practice/points/01_distance.py
@@ -9,10 +9,11 @@ def distance(p1, p2):
     Расстояние между двумя точками
     """
     # TODO: напишите тело функции
-
+    ret = ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0,5
+    return ret
 # Дано две точки на координатной плоскости
-point1 = Point(2, 4)
-point2 = Point(5, -2)
+point1 = Point(5, 4)
+point2 = Point(5, 10)
 
 dist = distance(point1, point2)  # Передаем объекты point1 и point2 в функцию
 
