043afc7b8497d8dd5b46a1922c12875bd20a4ca4
noReaLl <103452127+noReaLl@users.noreply.github.com>
2022-04-13 00:54:41 +0300

    Update 02_backpack_par1.py

--- Module2/practice/backpack/02_backpack_par1.py
+++ Module2/practice/backpack/02_backpack_par1.py
@@ -1,5 +1,11 @@
 class Item:
-    ...
+    def __init__(self, name, weight, cost):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
+
+    def show(self):
+        return f"{self.name:10} вес:{self.weight:3} цена:{self.cost:5}"
     # TODO: сюда копируем реализацию класса из предыдущего задания
 
 
@@ -12,17 +18,24 @@ class BackPack:  # рюкзак
         Добавляет предмет(item) в этот рюкзак
         """
         # TODO: реализуйте метод
+        self.items.append(item)
 
     def show_items(self):
         """
         Вывод все предметы, содержащиеся в рюкзаке в виде нумерованного списка
         """
         # TODO: реализуйте метод
+        i = 1
+        for it in self.items:
+            print(f'{i}) - {it.show()}')
+            i += 1
 
 
 # Создаем предметы
 item1 = Item("Гиря", 25, 500)
 item2 = Item("Арбуз", 4, 300)
+item3 = Item("Ноутбук", 2.5, 22500)
+item4 = Item("Кот", 0.5, 250)
 
 # Создаем пустой рюкзак
 backpack = BackPack()
@@ -30,6 +43,9 @@ backpack = BackPack()
 # Добавляем пару предметов в рюкзак
 backpack.add_item(item1)
 backpack.add_item(item2)
+backpack.add_item(item3)
+backpack.add_item(item4)
 
 # Выводим все предметы в рюкзаке
+
 backpack.show_items()
