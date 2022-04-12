e56914654e5e8c2e2df005db3028536a657cd19f
noReaLl <103452127+noReaLl@users.noreply.github.com>
2022-04-13 00:56:08 +0300

    Update 03_backpack_part2.py

--- Module2/practice/backpack/03_backpack_part2.py
+++ Module2/practice/backpack/03_backpack_part2.py
@@ -1,10 +1,44 @@
 class Item:
-    ...
+
     # TODO: сюда копируем реализацию класса из предыдущего задания
+    def __init__(self, name, weight, cost):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
+
+    def show(self):
+        return f"{self.name:10} вес:{self.weight:3} цена:{self.cost:5}"
 
 
 class BackPack:  # рюкзак
-    ...
+    def __init__(self, max_weight):
+        self.items = []  # Предметы, которые хранятся в рюкзаке
+        self.max_weight = max_weight
+
+    def add_item(self, item: Item):
+        """
+        Добавляет предмет(item) в этот рюкзак
+        """
+        # TODO: реализуйте метод
+        if type(item) == Item:
+            current_weight = 0
+            for it in self.items:
+                current_weight += it.weight
+            if ((current_weight + item.weight) < self.max_weight):
+                self.items.append(item)
+            else:
+                print(f"Предмет {item.name} слишком тяжелый")
+
+    def show_items(self):
+        """
+        Вывод все предметы, содержащиеся в рюкзаке в виде нумерованного списка
+        """
+        # TODO: реализуйте метод
+        i = 1
+        for it in self.items:
+            print(f'{i}) - {it.show()}')
+            i += 1
+
     # TODO: сюда копируем реализацию класса из предыдущего задания
     # TODO: добавьте новое свойство .max_weight - максимальный суммарный вес предметов, которые можно положить в рюкзак
 
@@ -13,12 +47,20 @@ class BackPack:  # рюкзак
         Возвращает суммарный вес всех предметов в рюкзаке
         """
         # TODO: реализуйте метод
+        sum_allweight = 0
+        for items in self.items:
+            sum_allweight += items.weight
+        return sum_allweight
 
     def sum_cost(self):
         """
         Возвращает суммарную стоимость всех предметов в рюкзаке
         """
         # TODO: реализуйте метод
+        sum_allcost = 0
+        for items in self.items:
+            sum_allcost += items.cost
+        return sum_allcost
 
 
 # Создаем предметы
@@ -35,6 +77,11 @@ backpack.add_item(item1)
 backpack.add_item(item2)
 backpack.add_item(item3)
 backpack.add_item(item4)
+
+print("-----------------------")
+print(f"вес всех предметов в рюкзаке - {backpack.sum_weight()}")
+print(f"стоимость всех предметов в рюкзаке - {backpack.sum_cost()}")
+print("-----------------------")
 # TODO: Если предмет не помещается в рюкзак по весу - вывести сообщение "Предмет {name} слишком тяжелый",
 #  и сам предмет не должен быть добавлен в рюкзак.
 #  Если предмет помещает, то добавляем его в рюкзак.
