4e9fca422f9d2223829d840dacc5cde9423b2d6f
Satanichia129 <103451977+Satanichia129@users.noreply.github.com>
2022-04-13 19:42:24 +0300

    Update 01_book_to_str.py

--- Module3/practice/books/01_book_to_str.py
+++ Module3/practice/books/01_book_to_str.py
@@ -1,3 +1,4 @@
+Моё решение:
 class Author:
     def __init__(self, name, surname):
         self.name = name
@@ -6,7 +7,7 @@ class Author:
     def short_name(self):
         # TODO-1: метод возвращает строку в формате: М.Булгаков
         #  пояснение: Первую букву имени, фамилию целиком
-        ...
+        return f"{self.name[0:1]}.{self.surname}"
 
 
 class Book:
@@ -19,11 +20,11 @@ class Book:
     def to_str(self):
         # TODO-2: метод возвращает строку в формате: "Вьюга" author:М.Булгаков publish:1926 год
         #  пояснение: Название книги выводим в кавычках(""), у имени автора только первую букву
-        ...
+        return f'"{self.name}" author: {self.author.short_name()} publish: {self.year} year'
 
 
-author = Author("Михаил", "Булгаков")
-book = Book("Вьюга", author, 1926, 25)
+author = Author("Евгений", "Сидаров")
+book = Book("Книга века", author, 1926, 25)
 
 # TODO-3: Измените имя, фамилию автора и название книги, проверьте, что программа корректно работает с новыми значениями
 print(book.to_str())
