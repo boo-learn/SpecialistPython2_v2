299b1175759f299e6bf2333411c5c2c5ef600357
Анастасия <103452261+Crazy-Hedgehog@users.noreply.github.com>
2022-04-14 11:18:14 +0300

    Update 02_Car_result.py

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -1,5 +1,50 @@
-# Сюда отправляем решение задачи "Автомобиль"
-# Само задание в файле 02_Сar_task.md
-
 class Car:
-    pass
+    def __init__(self, name, gas, capacity, gas_per_km, mileage=0):
+        self.name = name
+        self.gas = gas
+        self.capacity = capacity
+        self.gas_per_km = gas_per_km
+        self.mileage = mileage
+
+    def fill(self, volume):
+        if volume >= 0:
+            if volume > self.capacity - self.gas:
+                print(f'{volume - (self.capacity - self.gas)} литров не вместилось в бак.')
+                self.gas = self.capacity
+            else:
+                self.gas += volume
+        else:
+            print('Вы ввели некорректное значение.')
+
+    def ride(self, path):
+        fuel_dist = self.gas / self.gas_per_km
+        if path >= 0:
+            if fuel_dist >= path:
+                self.mileage += path
+                self.gas -= path * self.gas_per_km
+                print(f'Вы проехали {path} км.')
+            else:
+                self.mileage += fuel_dist
+                self.gas = 0
+                print(f'Вам не хватило бензина. Вы проехали {fuel_dist} км.')
+        else:
+            print('Вы ввели некорректное значение.')
+
+
+# вспомогательная функция
+def show_car(car):
+    return f"{car.name} {car.gas} {car.capacity} {car.gas_per_km} {car.mileage}"
+
+car = Car('Ford', 10, 60, 0.5)
+
+print(show_car(car))
+
+volume = float(input('Сколько литров Вы хотите залить? '))
+car.fill(volume)
+
+print(show_car(car))
+
+path = float(input('Сколько километров Вы хотите проехать? '))
+car.ride(path)
+
+print(show_car(car))
