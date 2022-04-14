dcced19d161c2670f42f068fae377a276241ce5f
anton.shmelev <103452127+noReaLl@users.noreply.github.com>
2022-04-14 19:24:02 +0300

    Update 06_deck_part2.py

--- Module3/practice/deck/06_deck_part2.py
+++ Module3/practice/deck/06_deck_part2.py
@@ -1,6 +1,16 @@
+import random
+
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        dict_1 = {"Hearts": '\u2665', "Diamonds": '\u2666', "Clubs": '\u2663', "Spades": '\u2660'}
+        return f"{self.value}{dict_1[self.suit]}"
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
 
 
 class Deck:
@@ -9,20 +19,35 @@ class Deck:
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
         self.cards = []
-        # TODO-0: конструктор копируем из предыдущей задачи
+        # TODO-1: конструктор добавляет в список self.cards все(52) карты
+        for i in range(len(suits)):
+            for value in values:
+                self.cards.append(Card(value, suits[i]))
 
     def show(self):
-        # TODO-0: копируем из предыдущей задачи
-        ...
+        # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
+        cards_str = list()
+
+        for card in self.cards:
+            cards_str.append(card.to_str())
+
+        return (f"cards:[{len(self.cards)}] " + ", ".join(cards_str))
+
 
     def draw(self, x):
         # TODO-1: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+        hand=list()
+        for i in range(x):
+            hand.append(self.cards[i])
+        for i in range(x):
+            del self.cards[0]
+        return hand
 
     def shuffle(self):
         # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
         #   Подсказка: https://www.w3schools.com/python/ref_random_shuffle.asp
-        ...
+        random.shuffle(self.cards)
+
 
 
 # Создаем колоду
@@ -38,4 +63,9 @@ hand = deck.draw(5)
 # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
 print(deck.show())
 # Выводим список карт "в руке"(список hand)
-print(...)
+cards_strinhand=list()
+for card in hand:
+    cards_strinhand.append(card.to_str())
+
+print(f"карты в руке:" + ", ".join(cards_strinhand))
+# print(...)
