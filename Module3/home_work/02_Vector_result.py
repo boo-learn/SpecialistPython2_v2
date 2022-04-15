94f18a63eff0ebc81f3f07066bbf8100556f9ab4
SA272 <62008704+SA272@users.noreply.github.com>
2022-04-15 20:48:08 +0300

    Update 02_Vector_result.py

--- Module3/home_work/02_Vector_result.py
+++ Module3/home_work/02_Vector_result.py
@@ -1,5 +1,28 @@
-# Сюда отправляем решение задачи "Вектор"
-# Само задание в файле 01_Vector_task.md
-
 class Vector:
-    pass
+    def __init__(self, x, y):
+        self.x = int(x)
+        self.y = int(y)
+
+    def __str__(self):
+        return f'{self.x}i + {self.y}j'
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __mul__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Vector(self.x * other, self.y * other)
+        raise ValueError("Число!!")
+
+a = Vector(3, 6)
+b = Vector(1, 2)
+c = a + b
+d = a - b
+f = a * 5
+print(f"{a}, {b}")
+print(c)
+print(d)
+print(f)
