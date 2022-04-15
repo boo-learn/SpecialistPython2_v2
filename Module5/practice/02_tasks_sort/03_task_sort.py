a6e1964021ee294a8f5cba8c11117a1e3e7981e1
anton.shmelev <103452127+noReaLl@users.noreply.github.com>
2022-04-15 21:18:55 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -1,6 +1,10 @@
-# Сумма наибольших
-# Дан список чисел.
-# Найти: сумму 5-ти самых больших элементов
-
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
+for i in range(len(numbers)-1):
+    for j in range (len(numbers)-1):
+        if numbers[j]>numbers[j+1]:
+            numbers[j],numbers[j+1]=numbers[j+1],numbers[j]
+summa=0
+for i in range(len(numbers)-5,len(numbers),1):
+    summa+=numbers[i]
 
+print(summa)
