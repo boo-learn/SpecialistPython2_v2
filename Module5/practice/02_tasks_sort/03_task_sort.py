18a20ddde9f51615f49c4749e1a01acde89d9239
Satanichia129 <103451977+Satanichia129@users.noreply.github.com>
2022-04-15 21:26:59 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -4,3 +4,16 @@
 
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
 
+n = 0
+swapped = True
+while swapped:
+    swapped = False
+    n += 1
+    for i in range(len(numbers) - n):
+        if numbers[i] > numbers[i + 1]:
+            numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+            swapped = True
+
+summ = 0
+for numb in numbers[:5:-1]:
+    summ += numb
