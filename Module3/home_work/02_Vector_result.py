75b9baf02bd156de8e0ae4959708268d5389a04f
Rozhkov-Aleksey <103452344+Rozhkov-Aleksey@users.noreply.github.com>
2022-04-16 19:26:36 +0300

    Update 02_Vector_result.py

--- Module3/home_work/02_Vector_result.py
+++ Module3/home_work/02_Vector_result.py
@@ -1,5 +1,32 @@
-# Сюда отправляем решение задачи "Вектор"
-# Само задание в файле 01_Vector_task.md
-
+#Можно было и без точек, но так красивее мне кажется.
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def show(self):
+        return f"({self.x},{self.y})"
 class Vector:
-    pass
+    def __init__(self, dot1, dot2):
+        self.a = dot2.x - dot1.x
+        self.b = dot2.y - dot1.y
+    def show(self):
+        return f"({self.a},{self.b})"
+
+    def sum_v(self, vec1):
+        return Vector(Point(0,0), Point(self.a + vec1.a, self.b + vec1.b))
+    def sub_v(self, vec1):
+        return Vector(Point(0, 0), Point(self.a - vec1.a, self.b - vec1.b))
+    def mul_v(self, mult):
+        return Vector(Point(0, 0), Point(self.a*mult, self.b*mult))
+
+A = Point(2,4)
+B = Point(5,8)
+C = Point(6,3)
+AB = Vector(A, B)
+AC = Vector(A, C)
+k = 3
+print(f"AB {AB.show()}")
+print(f"AC {AC.show()}")
+print(f"AB+AC {Vector.sum_v(AB,AC).show()}")
+print(f"AB-AC {Vector.sub_v(AB,AC).show()}")
+print(f"AB*k {Vector.mul_v(AB,k).show()}")
