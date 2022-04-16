b1feb6485686d3bbd69d2e670f08320f6d2b5d42
SA272 <62008704+SA272@users.noreply.github.com>
2022-04-16 13:08:30 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -4,3 +4,27 @@
 
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
 
+def bubble_sort(nums):
+    swapped = True
+    j = 0
+    while swapped:
+        swapped = False
+        # print("*****")
+        for i in range(len(nums) - 1 - j):
+            # print("i = ", i)
+            if nums[i] > nums[i + 1]:
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+        j += 1
+
+bubble_sort(numbers)
+print(numbers)
+
+new_numbers = numbers[-4:]
+res = 0
+for elem in new_numbers:
+    res += elem
+
+print(res)
