23cbefddb7d1b09a37e425080007ca86ad3fdda1
EvilMercerP <103452544+EvilMercerP@users.noreply.github.com>
2022-04-18 21:05:28 +0300

    Update 02_nodes_from_list.py

--- Module6/practice/nodes/02_nodes_from_list.py
+++ Module6/practice/nodes/02_nodes_from_list.py
@@ -24,14 +24,32 @@ def gen_names_list(size=None):
     return names[:size]
 
 
+
 # Дан список из произвольного количества имен
 names = gen_names_list()
 print(names)
 
 # TODO: создайте цепочку нод из имен списка
+node0 = Node("")
+node1 = Node("")
+node2 = Node("")
+node3 = Node("")
+node4 = Node("")
+node5 = Node("")
+node6 = Node("")
+node7 = Node("")
+node8 = Node("")
+node9 = Node("")
 ...
-
-
+node0.next = node1
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+node6.next = node7
+node7.next = node8
+node8.next = node9
 # TODO: Передайте первую ноду в функцию print_nodes_value(), чтобы получить значения всех нод в цепочке
-first_node = ...
+first_node = node0
 print_nodes_value(first_node)
