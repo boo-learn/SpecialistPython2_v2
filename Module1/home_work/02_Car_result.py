fee08e7910c507d943f0c133164777e65ec7c321
anton.shmelev <103452127+noReaLl@users.noreply.github.com>
2022-04-18 15:31:10 +0300

    Update 02_Car_result.py

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -1,5 +1,32 @@
-# Сюда отправляем решение задачи "Автомобиль"
-# Само задание в файле 02_Сar_task.md
-
 class Car:
-    pass
+    def __init__(self, gas, capasity, gas_per_km, probeg=0):
+        if gas > capasity: gas = capasity
+        self.gas = gas
+        self.capasity = capasity
+        self.gas_per_km = gas_per_km
+        self.probeg = probeg
+
+    def fill(self, gas_to_fill):
+        self.gas += gas_to_fill
+        if self.gas + gas_to_fill > self.capasity:
+            print(f"в бак не влезло {self.gas - self.capasity} литров")
+            self.gas = self.capasity
+
+    def ride(self, range):
+        if range < (self.gas / self.gas_per_km * 100):
+            print(f"бензина хватает на все {range}км")
+            self.probeg += range
+            self.fill(range / 100 * self.gas_per_km * -1)
+        else:
+            print(f"проехали {self.gas / self.gas_per_km * 100}км \n"
+                  f"на остальные {range - self.gas / self.gas_per_km * 100} бензина не хватит")
+            self.probeg += self.gas / self.gas_per_km * 100
+            self.gas = 0
+
+
+car1 = Car(30, 50, 7)
+car1.fill(50)
+print(f"в баке залито {car1.gas}л")
+car1.ride(700)
+print(f"в баке залито {car1.gas}л")
+print(f"пробег машины составляет {car1.probeg}км")
