2fe3bb11afc2dc59b9ac68227d8714549f373fc6
anton.shmelev <103452127+noReaLl@users.noreply.github.com>
2022-04-18 21:27:11 +0300

    Update 03_nodes_len.py

--- Module6/practice/nodes/03_nodes_len.py
+++ Module6/practice/nodes/03_nodes_len.py
@@ -21,7 +21,11 @@ def len_nodes(start_node):
     """
     Возвращает целое число - кол-во нод у цепочке
     """
-    pass
+    number_of_nodes=0
+    while start_node:
+        number_of_nodes+=1
+        start_node = start_node.next
+    return number_of_nodes
 
 
 # Дан список из произвольного количества имен
@@ -29,8 +33,14 @@ names = gen_names_list()
 print(names)
 
 # TODO: скопируйте цепочку нод из предыдущей задачи
-...
+start_node = Node(names[0])
+current_node = start_node
+for name in names[1:]:
+    node = Node(name)
+    current_node.next = node
+    current_node = node
 
 # TODO: Передайте первую ноду в функцию len_nodes(), чтобы получить количество нод в цепочке
-first_node = ...
+first_node = start_node
+print(len_nodes(first_node))
 len_nodes(first_node)
