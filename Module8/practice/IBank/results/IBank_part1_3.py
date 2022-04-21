6238bd3bbdf6f31db91801785522df48d0fadeac
gureeva-vd <103522445+gureeva-vd@users.noreply.github.com>
2022-04-21 21:44:42 +0400

    Update IBank_part1_3.py

--- Module8/practice/IBank/results/IBank_part1_3.py
+++ Module8/practice/IBank/results/IBank_part1_3.py
@@ -6,39 +6,11 @@ class Account:
         self.__balance = start_balance
 
     def transfer(self, target_account, amount):
-        """
-        Перевод денег на счет другого клиента
-        :param target_account: счет клиента для перевода
-        :param amount: сумма перевода
-        :return:
-        """
-        pass
-
-    def deposit(self, amount):
-        """
-        Внесение суммы на текущий счет
-        :param amount: сумма
-        """
-        pass
-
-    def withdraw(self, amount):
-        """
-        Снятие суммы с текущего счета
-        :param amount: сумма
-        """
-        pass
-
-    def full_info(self):
-        """
-        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
-        """
-        return f"..."
-
-    def __repr__(self):
-        """
-        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
-        """
-        return f"..."
+        if self.__balance >= amount:
+            self.__balance = self.__balance - amount
+            target_account.__balance += amount
+        else:
+            return ValueError("Недостаточно средств")
 
 
 account1 = Account("Иван", "3230 634563", "+7-900-765-12-34", 1000)
@@ -47,16 +19,17 @@ account2 = Account("Алексей", "+7-901-744-22-99", "3232 456124", 200)
 print(account1)
 print(account2)
 
-# Переводим деньги с первого аккаунт на второй:
-account1.transfer(account2, 500)
-
-# Проверяем изменения баланса:
+try:
+    account1.transfer(account2, 500)
+except ValueError as e:
+    print(e)
 print(account1)
 print(account2)
 
-# Переводим еще с первого аккаунт на второй:
-account1.transfer(account2, 600)
-
-# Проверяем изменения баланса:
+try:
+    account1.transfer(account2, 600)
+except ValueError as e:
+    print(e)
 print(account1)
 print(account2)
+
