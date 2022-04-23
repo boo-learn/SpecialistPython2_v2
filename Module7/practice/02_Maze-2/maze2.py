71164d227edeb095fb781b5bb2f1199a80777e4e
ivales <83123684+ivales@users.noreply.github.com>
2022-04-23 11:48:02 +0300

    Update maze2.py

--- Module7/practice/02_Maze-2/maze2.py
+++ Module7/practice/02_Maze-2/maze2.py
@@ -3,3 +3,50 @@
 
 
 # Решите задачу и выведите ответ в нужном формате
+def dfs(start_point, graph):
+    visited = [False] * len(graph)
+
+    def _dfs(v):
+        visited[v] = True
+        for w in graph[v]:
+            if not visited[w]:
+                _dfs(w)
+
+    _dfs(start_point)
+    return visited
+
+
+def start_finish(start, finish):
+    if dfs(start, graph)[finish]:
+        print(f'Из точки {start} можно дойти до точки {finish}')
+    else:
+        print(f'Из точки {start} нельзя дойти до точки {finish}')
+
+
+graph = [
+    [1],
+    [0, 2],
+    [1, 3],
+    [2, 7],
+    [5],
+    [6, 4],
+    [5],
+    [3, 11],
+    [9, 12],
+    [8, 10],
+    [9, 11],
+    [7, 10, 15],
+    [8, 13],
+    [12, 14],
+    [13],
+    [11]
+]
+
+s1 = 1
+s2 = 5
+s3 = 15
+finish = 14
+
+start_finish(s1, finish)
+start_finish(s2, finish)
+start_finish(s3, finish)
