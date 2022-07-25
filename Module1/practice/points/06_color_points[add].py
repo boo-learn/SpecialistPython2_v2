df90d3dff34a47c172e731539361410d8ad58fd8
Fotum <MaDMaDDneSS@gmail.com>
2022-07-25 14:54:11 +0300

    Update 06_color_points[add].py
    Соколов Юрий
    
    Немного захардкодил фильтрацию на цвета... Вероятно можно сделать как-то попроще и без хардкода.

--- Module1/practice/points/06_color_points[add].py
+++ Module1/practice/points/06_color_points[add].py
@@ -1,11 +1,34 @@
+import math
+
+
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        self.color = ...
+        self.color = color
 
     def dist_to(self, other_point):
-        ...
+        """
+        Расстояние между двумя точками
+        """
+        return math.sqrt(
+            math.pow((other_point.x - self.x), 2) +
+            math.pow((other_point.y - self.y), 2)
+        )
+
+
+class Triangle:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def get_area(self):
+        return abs((
+                           (self.b.x - self.a.x) * (self.c.y - self.a.y) -
+                           (self.c.x - self.a.x) * (self.b.y - self.a.y)
+                   ) / 2
+                   )
 
 
 # Дан список точек нарисованных красным(red) и зеленым(green) цветами
@@ -19,12 +42,17 @@ points = [
     Point(-12, 0, "red")
 ]
 # Все точки одного цвета соединены линиями и образуют треугольник
+triFirst = []
+triSecond = []
 
-# TODO-1: доработайте конструктор class Point для хранения цвета точки
-# TODO-2: реализуйте метод dist_to()
-# TODO-3: вычислите площади треугольников образованных точками разных цветов
+for pt in points:
+    if pt.color == "red":
+        triFirst.append(pt)
+    elif pt.color == "green":
+        triSecond.append(pt)
 
-# your core here...
+triRed = Triangle(triFirst[0], triFirst[1], triFirst[2])
+triGreen = Triangle(triSecond[0], triSecond[1], triSecond[2])
 
-print("Площадь красного треугольника = ", ...)
-print("Площадь зеленого треугольника = ", ...)
+print("Площадь красного треугольника =", triRed.get_area())
+print("Площадь зеленого треугольника =", triGreen.get_area())
