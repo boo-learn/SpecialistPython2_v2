fdebf0a33cd3d83a8b6d31e49b6ffcfc42da1365
vln-oss <109944033+vln-oss@users.noreply.github.com>
2022-07-25 14:55:51 +0300

    Update 04_long_points.py

--- Module1/practice/points/04_long_points.py
+++ Module1/practice/points/04_long_points.py
@@ -1,7 +1,15 @@
+from math import sqrt
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def dist_to(self, other_point):
+        # TODO-1: реализуйте метод
+        # Подсказка: смотри пример, Module1/examples/06_object_methods.py
+        return sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
 
 
 # Дан список из произвольного количества точек:
@@ -9,4 +17,14 @@ points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)
 
 # TODO: найдите точку наиболее удаленную от начала координат и выведите ее координаты
 
-print("Координаты наиболее удаленной точки = ", ...)
+zero_point = Point(0, 0)
+
+seek_distance = 0
+
+for point in points:
+    cur_dist = point.dist_to(zero_point)
+    if cur_dist > seek_distance:
+        seek_point = point
+        seek_distance = cur_dist
+
+print(f"Координаты наиболее удаленной точки = ( {seek_point.x} : {seek_point.y} )  Растояние = {seek_distance:.2f}")
