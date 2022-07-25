2d1b147de29f92fc38584379f4ff8f0a93651b34
GavrilovaMaria <109950547+GavrilovaMaria@users.noreply.github.com>
2022-07-25 22:27:10 +0300

    Update 02_points.py

--- Module1/practice/points/02_points.py
+++ Module1/practice/points/02_points.py
@@ -4,6 +4,9 @@ class Point:
         self.y = y
 
 
+def distance(p1, p2):
+    return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
+
 # Дан список из произвольного количества точек:
 points = [Point(3, 8), Point(7, -5), Point(10, -2), Point(0, 6), Point(-12, 0)]
 # И произвольная точка на плоскости:
@@ -13,3 +16,5 @@ random_point = Point(-12, 10)
 # Совет: используйте функцию distance() из предыдущего задания
 # Подсказка: смотри пример, Module1/examples/05_objects_list.py
 
+for i in points:
+    print (distance(i, random_point))
