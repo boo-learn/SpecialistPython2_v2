918805dacbf39639c253ab02ec2e185f6caf998f
IggIOK <56275131+IggIOK@users.noreply.github.com>
2022-07-25 14:16:19 +0300

    Update 05_line.py

--- Module1/practice/points/05_line.py
+++ Module1/practice/points/05_line.py
@@ -4,9 +4,17 @@ class Point:
         self.y = y
 
 
+def dist_between_points(p1, p2):
+    return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
+
+
 # Ломаная линия задана произвольным количеством последовательных точек
 points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 
-# TODO: Найдите длину ломаной линии
+sum_length = 0
 
-print("Длина ломаной линии = ", ...)
+for cur_segm in range(1, len(points)):
+    p_from, p_to = points[cur_segm - 1], points[cur_segm]
+    sum_length += dist_between_points(p_from, p_to)
+
+print("Длина ломаной линии = ", sum_length)
