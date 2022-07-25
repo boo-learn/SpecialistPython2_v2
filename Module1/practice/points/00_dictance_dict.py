2de036919c6eb87781de8c89dcb52085634a34f8
utenkov-maxim <74677702+utenkov-maxim@users.noreply.github.com>
2022-07-25 11:54:11 +0300

    Update 00_dictance_dict.py
    Утенков Максим

--- Module1/practice/points/00_dictance_dict.py
+++ Module1/practice/points/00_dictance_dict.py
@@ -1,13 +1,22 @@
+import math
+
+
 def distance(p1, p2):
     """
     Расстояние между двумя точками
     """
     # TODO: напишите тело функции
 
+    # AB = √(xb - xa)2 + (yb - ya)2
+
+    return math.sqrt((p2["x"] - p1["x"]) * (p2["x"] - p1["x"]) + (p2["y"] - p1["y"]) * (p2["y"] - p1["y"]))
+
 
 # Даны две точки на координатной плоскости
-point1 = {"x": 2, "y": 5}
-point2 = {"x": -2, "y": 4}
+# point1 = {"x": 2, "y": 5}
+# point2 = {"x": -2, "y": 4}
+point1 = {"x": 3, "y": 0}
+point2 = {"x": 0, "y": 4}
 
 dist = distance(point1, point2)
 
