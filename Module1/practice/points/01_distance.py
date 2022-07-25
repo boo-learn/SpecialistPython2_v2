1863e8299af49a728b872035e7f94a56ae2429b2
RafaelKhalikov <109956457+RafaelKhalikov@users.noreply.github.com>
2022-07-25 13:12:00 +0300

    Update 01_distance.py

--- Module1/practice/points/01_distance.py
+++ Module1/practice/points/01_distance.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -8,12 +11,12 @@ def distance(p1, p2):
     """
     Расстояние между двумя точками
     """
-    # TODO: напишите тело функции
+    return sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
 
 
 # Даны две точки на координатной плоскости
 point1 = Point(2, 4)
-point2 = Point(5, -2)
+point2 = Point(5, 4)
 
 dist = distance(point1, point2)  # Передаем объекты point1 и point2 в функцию
 
