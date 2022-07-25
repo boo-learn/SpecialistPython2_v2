8f1cb2fd32892895255d515ff267052ef2285901
utenkov-maxim <74677702+utenkov-maxim@users.noreply.github.com>
2022-07-25 16:28:52 +0300

    Update 01_people_age.py

--- Module1/practice/peoples/01_people_age.py
+++ Module1/practice/peoples/01_people_age.py
@@ -2,20 +2,24 @@ class People:
     def __init__(self, name, surname, age):
         self.name = name
         self.surname = surname
-        self.age = age
+        self.__age = age
 
     def change_age(self, new_age):
         # Корректным возрастом считаем целое число от 1 до 100
         # Если задан некорректный возраст, выводим "некорректное значение для возраста" и оставляем старое значение
         # Метод ничего не возвращает в качестве результата, а только меняем свойство self.age на значение new_age
         # TODO: напишите реализацию данного метода
-        ...
+
+        if type(new_age) == int and 1 <= new_age <= 100:
+            self.__age = new_age
+        else:
+            print("некорректное значение для возраста")
 
     def full_name(self):
         return f"{self.surname} {self.name}"
 
     def full_info(self):
-        return f"Человек: {self.surname} {self.name} и ему {self.age} лет"
+        return f"Человек: {self.surname} {self.name} и ему {self.__age} лет"
 
 
 # Создадим двух человек:
