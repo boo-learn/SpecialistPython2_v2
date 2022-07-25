97ad85e9c28ff2c489bbe10df77fa1495031ced4
iLeyawiin <109950415+iLeyawiin@users.noreply.github.com>
2022-07-25 13:22:58 +0300

    Update 06_object_methods.py

--- Module1/examples/06_object_methods.py
+++ Module1/examples/06_object_methods.py
@@ -1,16 +1,22 @@
+import math
+from math import sqrt
+
+
 class Point:
-    # Конструктор - тоже является методом, но с зарезервированным именем
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    # Метод - функция внутри класса
+
     def dist_to(self, other_point):
-        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
+        return math.sqrt((other_point.x-self.x)**2 + (other_point.y-self.y)**2)
 
 
-point1 = Point(10, -8)
-point2 = Point(12, 5)
+# Дано две точки на координатной плоскости
+point1 = Point(2, 4)
+point2 = Point(5, -2)
 
+
+# TODO-2: выведите расстояние между точками используя метод dist_to()
 dist = point1.dist_to(point2)
-print("Расстояние между точками =", dist)
+print(f"Расстояние между точками = {dist}")
