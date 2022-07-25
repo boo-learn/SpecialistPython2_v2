f6814fee3a8d426c028c30d70ec1389627e22113
vln-oss <109944033+vln-oss@users.noreply.github.com>
2022-07-25 13:10:02 +0300

    Update 02_points.py

--- Module1/practice/points/02_points.py
+++ Module1/practice/points/02_points.py
@@ -1,15 +1,24 @@
+from math import sqrt
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+def distance(p1, p2):
+    """
+    Расстояние между двумя точками
+    """
+    return sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2)
 
 # Дан список из произвольного количества точек:
 points = [Point(3, 8), Point(7, -5), Point(10, -2), Point(0, 6), Point(-12, 0)]
 # И произвольная точка на плоскости:
 random_point = Point(-12, 10)
 
+for point in points:
+    print(distance(point,random_point))
+
 # TODO: выведите расстояние от каждой точки(из списка) до точки random_point
 # Совет: используйте функцию distance() из предыдущего задания
 # Подсказка: смотри пример, Module1/examples/05_objects_list.py
-
