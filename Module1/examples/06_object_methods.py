9dc4d171ac3aebe0bb99e764aa1c2a8fe75addc3
Scherbakova Natalya <19natasha94@bk.ru>
2022-07-25 20:52:02 +0300

    Update 06_object_methods.py

--- Module1/examples/06_object_methods.py
+++ Module1/examples/06_object_methods.py
@@ -1,16 +1,17 @@
 class Point:
-    # Конструктор - тоже является методом, но с зарезервированным именем
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    # Метод - функция внутри класса
     def dist_to(self, other_point):
+        # TODO-1: реализуйте метод
+        # Подсказка: смотри пример, Module1/examples/06_object_methods.py
         return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
 
 
-point1 = Point(10, -8)
-point2 = Point(12, 5)
+# Дано две точки на координатной плоскости
+point1 = Point(2, 4)
+point2 = Point(5, -2)
 
-dist = point1.dist_to(point2)
-print("Расстояние между точками =", dist)
+# TODO-2: выведите расстояние между точками используя метод dist_to()
+print(f"Расстояние между точками = {point1.dist_to(point2)}")
