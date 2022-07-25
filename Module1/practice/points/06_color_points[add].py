27a454af7004400bc427101dc3a7ca8b417e56aa
utenkov-maxim <74677702+utenkov-maxim@users.noreply.github.com>
2022-07-25 14:55:16 +0300

    Update 06_color_points[add].py

--- Module1/practice/points/06_color_points[add].py
+++ Module1/practice/points/06_color_points[add].py
@@ -1,11 +1,14 @@
+import math
+
+
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        self.color = ...
+        self.color = color
 
     def dist_to(self, other_point):
-        ...
+        return math.sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
 
 
 # Дан список точек нарисованных красным(red) и зеленым(green) цветами
@@ -26,5 +29,20 @@ points = [
 
 # your core here...
 
-print("Площадь красного треугольника = ", ...)
-print("Площадь зеленого треугольника = ", ...)
+# red_points = []
+# green_points = []
+#
+# for p in points:
+#     if p.color == "red":
+#         red_points.append(p)
+#     if p.color == "green":
+#         green_points.append(p)
+
+red_points = (list)(filter(lambda p: p.color == "red", points))
+green_points = (list)(filter(lambda p: p.color == "green", points))
+
+red_s = 0.5 * ((red_points[1].x - red_points[0].x) * (red_points[2].y - red_points[0].y) - (red_points[2].x - red_points[0].x) * (red_points[2].y - red_points[1].y))
+green_s = 0.5 * ((green_points[1].x - green_points[0].x) * (green_points[2].y - green_points[0].y) - (green_points[2].x - green_points[0].x) * (green_points[2].y - green_points[1].y))
+
+print("Площадь красного треугольника = ", red_s)
+print("Площадь зеленого треугольника = ", green_s)
