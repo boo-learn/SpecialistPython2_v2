ff582f0a1cf93cfc0613782478b51437b5c21b68
RafaelKhalikov <109956457+RafaelKhalikov@users.noreply.github.com>
2022-07-25 14:24:33 +0300

    Update 04_long_points.py

--- Module1/practice/points/04_long_points.py
+++ Module1/practice/points/04_long_points.py
@@ -1,12 +1,28 @@
+from math import sqrt
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    def dist_to(self, other_point):
+        # Подсказка: смотри пример, Module1/examples/06_object_methods.py
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
 
-# Дан список из произвольного количества точек:
+
 points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)]
+point_start = Point(0, 0)
+max_point = None
+max_dist = 0
 
 # TODO: найдите точку наиболее удаленную от начала координат и выведите ее координаты
+for p in points:
+    distance = point_start.dist_to(p)
+    if distance > max_dist:
+        max_dist = distance
+        max_point = p
 
-print("Координаты наиболее удаленной точки = ", ...)
+print("Наибольшая дистанция = ", max_dist)
+print(f"Точка с наибольшей дистанцией = [{max_point.x},{max_point.y}]")
+
