e14b77776d41c411b8fd95123f6b9e465d9209f1
ngospodarik <109979798+ngospodarik@users.noreply.github.com>
2022-07-25 21:30:15 +0300

    Update 04_long_points.py

--- Module1/practice/points/04_long_points.py
+++ Module1/practice/points/04_long_points.py
@@ -3,10 +3,22 @@ class Point:
         self.x = x
         self.y = y
 
+    def dist_to_zero(self, other_point):
+
+        dist = ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
+        return dist
 
 # Дан список из произвольного количества точек:
 points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)]
 
 # TODO: найдите точку наиболее удаленную от начала координат и выведите ее координаты
 
-print("Координаты наиболее удаленной точки = ", ...)
+max_dist = 0
+req_point = 0
+for point in points:
+    dist_to_z = point.dist_to_zero(Point(0,0))
+    if dist_to_z > max_dist:
+        max_dist = dist_to_z
+        req_point = point.x, point.y
+
+print("Координаты наиболее удаленной точки = ", req_point)
