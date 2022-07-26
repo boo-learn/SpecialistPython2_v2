4b2e83d44a614bc8aff54c0d31b7a117f831a056
Fotum <MaDMaDDneSS@gmail.com>
2022-07-26 13:56:32 +0300

    Update 06_deck_part2.py
    Соколов Юрий

--- Module3/practice/deck/06_deck_part2.py
+++ Module3/practice/deck/06_deck_part2.py
@@ -1,28 +1,41 @@
+import random
+
+
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value: str, suit: str):
+        self.value = value
+        self.suit = suit
+
+    def to_str(self):
+        suit_pic = {"Hearts": "\u2665", "Diamonds": "\u2666", "Clubs": "\u2663", "Spades": "\u2660"}
+        return f"{self.value}{suit_pic.get(self.suit)}"
+
+    def equal_suit(self, other):
+        return self.suit == other.suit
 
 
 class Deck:
     def __init__(self):
         # Список карт в колоде. Каждым элементом списка будет объект класса Card
+        self.cards = []
+
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-        self.cards = []
-        # TODO-0: конструктор копируем из предыдущей задачи
+        for st in suits:
+            suit_cards = [Card(value, st) for value in values]
+            self.cards.extend(suit_cards)
 
     def show(self):
-        # TODO-0: копируем из предыдущей задачи
-        ...
+        return f"deck[{len(self.cards)}]: {', '.join([card.to_str() for card in self.cards])}"
 
     def draw(self, x):
-        # TODO-1: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+        if x > len(self.cards):
+            raise ValueError("Нельзя взять в руку больше карт, чем осталось в колоде")
+
+        return [self.cards.pop(i) for i in range(x)]
 
     def shuffle(self):
-        # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
-        #   Подсказка: https://www.w3schools.com/python/ref_random_shuffle.asp
-        ...
+        random.shuffle(self.cards)
 
 
 # Создаем колоду
@@ -38,4 +51,4 @@ hand = deck.draw(5)
 # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
 print(deck.show())
 # Выводим список карт "в руке"(список hand)
-print(...)
+print(", ".join([crd.to_str() for crd in hand]))
