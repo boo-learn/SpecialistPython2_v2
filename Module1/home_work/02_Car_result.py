52198c2cf317e71196c97efc7627453d5428118c
nadezhda1989 <106016937+nadezhda1989@users.noreply.github.com>
2022-07-26 15:51:35 +0300

    Update 02_Car_result.py

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -3,3 +3,24 @@
 
 class Car:
     pass
+    def __init__(self, gas, capacity, gas_per_km, car_mileage):
+        self.gas = gas
+        self.capacity = capacity
+        self.gas_per_km = gas_per_km
+        self.car_mileage = car_mileage
+
+    def fill(self, petrol):
+        if self.gas + petrol > self.capacity:
+            print("Лишних литров: ", self.gas + petrol - capacity)
+            self.gas = self.capacity
+        else:
+            self.gas += petrol
+    def ride(self, km):
+        if km * self.gas_per_km > self.gas:
+            self.car_mileage = self.car_mileage + (self.gas / self.gas_per_km)
+            return print(f" Машина проехала {self.gas/self.gas_per_km}, км, бензин закончился")
+        self.mileage = self.mileage + km
+        return print(f" машина проехала {km} км")
+car1 = Car(30, 70, 5, 500)
+
+car1.ride(50)
