b570d52b6cb30fdbe6f598e56eb0b88dd4a12197
vln-oss <109944033+vln-oss@users.noreply.github.com>
2022-07-26 09:52:08 +0300

    Update 02_Car_result.py

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -1,5 +1,40 @@
-# Сюда отправляем решение задачи "Автомобиль"
-# Само задание в файле 02_Сar_task.md
-
 class Car:
-    pass
+    def __init__(self, gas, capacity, gas_per_km):
+        self.gas = gas
+        self.capacity = capacity
+        self.gas_per_km = gas_per_km
+        self.__mileage = 0
+
+    def get_mileage(self):
+        return self.__mileage
+
+    def fill(self, amount):
+
+        if self.gas + amount > self.capacity:
+            print("Лишних литров: ", self.gas + amount - self.capacity)
+            self.gas = self.capacity
+        else:
+            self.gas += amount
+
+    def ride(self, km):
+
+        if self.gas / self.gas_per_km * 100 >= km:
+            l_ride = km
+        else:
+            l_ride = self.gas / self.gas_per_km * 100
+
+        self.gas = self.gas - l_ride * self.gas_per_km / 100
+
+        self.__mileage += l_ride
+
+        print(f'Проехали {l_ride:.2f} километров. Остаток топлива {self.gas:.2f} л.')
+
+
+car = Car(10, 60, 10)
+car.fill(10)
+car.fill(100)
+
+car.ride(30)
+car.ride(600)
+
+print(f"Пробег  ", car.get_mileage())
