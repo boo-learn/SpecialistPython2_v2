68f8c9ea412957ff043f09e861b33be896936d70
Skandy-git <61092155+Skandy-git@users.noreply.github.com>
2022-07-26 11:16:46 +0300

    Update 02_Car_result.py
    ДЗ Скворцова Никиты

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -2,4 +2,36 @@
 # Само задание в файле 02_Сar_task.md
 
 class Car:
-    pass
+
+    def __init__(self, gas, capacity, gas_per_km):
+        self.gas = gas
+        self.capacity = capacity
+        self.gas_per_km = gas_per_km
+        self.mileage = 0
+
+    def fill(self, liters):
+        if self.gas + liters > self.capacity:
+            print(f"Бак заполнен полностью. Обнаружено {self.gas + liters - self.capacity} лишних литров")
+            self.gas = self.capacity
+            return
+        self.gas = self.gas + liters
+
+    def ride(self, km):
+        available_km = int(self.gas / self.gas_per_km)
+        if available_km >= km:
+            self.gas = self.gas - self.gas_per_km * km
+            print(f'Проехали требуемые {km} км')
+            self.mileage += km
+        else:
+            self.gas = self.gas - self.gas_per_km * available_km
+            print(f'Проехали {available_km} км из {km} км требуемых')
+            self.mileage += available_km
+
+
+car1 = Car(15, 65, 5)
+
+car1.ride(4)
+
+car1.fill(65)
+
+car1.ride(13)
