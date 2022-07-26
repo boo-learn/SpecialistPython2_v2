92c86ffce47eb29668dfbd98cce18b322c9c65de
Fotum <MaDMaDDneSS@gmail.com>
2022-07-26 12:50:03 +0300

    Update 02_card_equal.py
    Соколов Юрий

--- Module3/practice/deck/02_card_equal.py
+++ Module3/practice/deck/02_card_equal.py
@@ -1,21 +1,36 @@
-# Начнем с создания карты
-class Card:
-    def __init__(self, value, suit):
-        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
-        self.suit = suit  # Масть карты
+class Suit:
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
 
     def to_str(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
-
-    def equal_suit(self, other_card):
-        # TODO-1: метод возвращает True - если масти карт равны или False - если нет
-        ...
+        return f"{self.symbol}{self.name}"
 
 
-# Создадим несколько карт
-card1 = Card("10", "Hearts")
-card2 = Card("A", "Diamonds")
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def to_str(self):
+        return f"{self.value}{self.suit.symbol}"
+
+    def equal_suit(self, other):
+        if self.suit.name == other.suit.name:
+            return True
+        else:
+            return False
+
+
+# Инициализация мастей
+hearts = Suit("Hearts", "\u2665")
+diamonds = Suit("Diamonds", "\u2666")
+clubs = Suit("Clubs", "\u2663")
+spades = Suit("Spades", "\u2660")
+
+# Карты
+card1 = Card("10", hearts)
+card2 = Card("A", spades)
 
 # Проверим, одинаковые ли масти у карт
 if card1.equal_suit(card2):
