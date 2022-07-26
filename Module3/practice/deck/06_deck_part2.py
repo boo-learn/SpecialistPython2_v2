a9804b2d3bb6cd6d36b6b60e555fd46a37fae199
utenkov-maxim <74677702+utenkov-maxim@users.noreply.github.com>
2022-07-26 14:25:40 +0300

    Update 06_deck_part2.py

--- Module3/practice/deck/06_deck_part2.py
+++ Module3/practice/deck/06_deck_part2.py
@@ -1,6 +1,16 @@
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        # TODO-0: скопируйте реализацию метода из предыдущей задачи
+        suits = {'Diamonds': '\u2666', 'Hearts': '\u2665', 'Spades': '\u2660', 'Clubs': '\u2663'}
+        return f"{self.value}{suits[self.suit]}"
+
+    def equal_suit(self, other_card):
+        # TODO-1: метод возвращает True - если масти карт равны или False - если нет
+        return self.suit == other_card.suit
 
 
 class Deck:
@@ -10,19 +20,23 @@ class Deck:
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
         self.cards = []
         # TODO-0: конструктор копируем из предыдущей задачи
+        for s in suits:
+            for v in values:
+                self.cards.append(Card(v, s))
 
     def show(self):
         # TODO-0: копируем из предыдущей задачи
-        ...
+        return f"deck[{len(self.cards)}]" + ', '.join([c.to_str() for c in self.cards])
 
     def draw(self, x):
         # TODO-1: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+        return [self.cards.pop(0) for i in range(x)]
 
     def shuffle(self):
         # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
         #   Подсказка: https://www.w3schools.com/python/ref_random_shuffle.asp
-        ...
+        from random import shuffle
+        shuffle(self.cards)
 
 
 # Создаем колоду
@@ -38,4 +52,4 @@ hand = deck.draw(5)
 # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
 print(deck.show())
 # Выводим список карт "в руке"(список hand)
-print(...)
+print(', '.join([c.to_str() for c in hand]))
