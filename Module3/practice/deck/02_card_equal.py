6c3187e0be742eac33b748394dc211f656e048e3
vln-oss <109944033+vln-oss@users.noreply.github.com>
2022-07-26 13:02:46 +0300

    Update 02_card_equal.py

--- Module3/practice/deck/02_card_equal.py
+++ Module3/practice/deck/02_card_equal.py
@@ -3,14 +3,22 @@ class Card:
     def __init__(self, value, suit):
         self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
         self.suit = suit  # Масть карты
+        self.suit_dict = {"Hearts":'\u2665', "Diamonds":'\u2666',"Spades": '\u2660',"Clubs":'\u2663'}
 
     def to_str(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return f"{self.value}{self.suit_dict[self.suit]}"
 
     def equal_suit(self, other_card):
-        # TODO-1: метод возвращает True - если масти карт равны или False - если нет
-        ...
+      self.suit == other_card.suit
+
+
+# Создадим несколько карт
+card1 = Card("10", "Hearts")
+card2 = Card("A", "Diamonds")
+
+# Выведем карты на экран в виде: 10♥ и A♦
+print(card1.to_str())
+print(card2.to_str())
 
 
 # Создадим несколько карт
@@ -22,3 +30,4 @@ if card1.equal_suit(card2):
     print(f"У карт: {card1.to_str()} и {card2.to_str()} одинаковые масти")
 else:
     print(f"У карт: {card1.to_str()} и {card2.to_str()} разные масти")
+
