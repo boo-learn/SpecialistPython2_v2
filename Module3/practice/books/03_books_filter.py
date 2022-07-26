10750e7cb38514bf07c74ce42a0d98de97863010
YuryKuzin <109950460+YuryKuzin@users.noreply.github.com>
2022-07-26 12:02:00 +0300

    Update 03_books_filter.py

--- Module3/practice/books/03_books_filter.py
+++ Module3/practice/books/03_books_filter.py
@@ -4,8 +4,7 @@ class Author:
         self.surname = surname
 
     def short_name(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return self.name[0] + "." + self.surname
 
 
 class Book:
@@ -16,8 +15,8 @@ class Book:
         self.pages = pages
 
     def to_str(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        # TODO-2: метод возвращает строку в формате: "Вьюга" author:М.Булгаков publish:1926 год
+        return f'"{self.name}" author: {self.author.short_name()} publish:{self.year}'
 
 
 author1 = Author("Михаил", "Булгаков")
@@ -30,7 +29,19 @@ books_catalog = [
     Book("Сияние", author2, 2014, 544),
     Book("Оно", author2, 1986, 320),
 ]
+i = 0
+# TODO-1: Выведите нумерованный список книг, используя для каждой книги ее строковое представление(метод .to_str())
+# for book in (books_catalog):
+#     i = i + 1
+#     print(f'{i}. {book.to_str()}')
+
 print("Найти все книги по фамилии автора")
 surname = input("Фамилия автора: ")
-# TODO-1: Выведите нумерованный список книг автора с фамилией surname
-#  примечание: если книг для автора с указанной фамилией нет - выведите "Книги не найдены"
+    # TODO-1: Выведите нумерованный список книг автора с фамилией surname
+    #  примечание: если книг для автора с указанной фамилией нет - выведите "Книги не найдены"
+for book in (books_catalog):
+    if book.author.surname == surname:
+        i = i + 1
+        print(f'{i}. {book.to_str()}')
+    else:
+        print("Книги не найдены")
