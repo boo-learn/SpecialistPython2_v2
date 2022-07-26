487345161c6928060a0b9a03c97dd2105b7b75d0
Fotum <MaDMaDDneSS@gmail.com>
2022-07-26 12:55:37 +0300

    Update 03_cards_list.py
    Соколов Юрий

--- Module3/practice/deck/03_cards_list.py
+++ Module3/practice/deck/03_cards_list.py
@@ -1,21 +1,36 @@
+class Suit:
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
+
+    def to_str(self):
+        return f"{self.symbol}{self.name}"
+
+
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def to_str(self):
+        return f"{self.value}{self.suit.symbol}"
+
+    def equal_suit(self, other):
+        if self.suit.name == other.suit.name:
+            return True
+        else:
+            return False
 
 
+# Инициализация мастей
+hearts = Suit("Hearts", "\u2665")
+diamonds = Suit("Diamonds", "\u2666")
+
+# Карты
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-hearts_cards = []
-# TODO-1: добавьте в список hearts_cards все червовые карты(от 2-ки до туза)
 
-diamonds_cards = []
-# TODO-2: добавьте в список diamonds_cards все бубновые карты(от туза до 2-ки)
+hearts_cards = [Card(value, hearts) for value in values]
+diamond_cards = [Card(value, diamonds) for value in values]
 
-# TODO-3: выведите все карты из списка hearts_cards в терминал через запятую в одну строку:
-# Пример вывода: 2♥, 3♥, 4♥ ... A♥
-
-
-cards = [
-    Card("2", "Hearts"),
-    Card("3", "Hearts"),
-    Card("4", "Hearts"),
-    ...]
+print(", ".join([card.to_str() for card in hearts_cards]))
+print(", ".join([card.to_str() for card in diamond_cards]))
