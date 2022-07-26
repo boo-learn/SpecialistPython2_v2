07224253f0718d90e8bcd99c09c96891f617c878
Black Cranium <mikhail.zabolotskih@gmail.com>
2022-07-26 17:00:00 +0300

    Update 06_color_points[add].py

--- Module1/practice/points/06_color_points[add].py
+++ Module1/practice/points/06_color_points[add].py
@@ -1,15 +1,15 @@
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        self.color = ...
+        self.color = color
 
     def dist_to(self, other_point):
-        ...
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
 
 
 # Дан список точек нарисованных красным(red) и зеленым(green) цветами
-# Точно известно что точек каждого цвета ровно три, но порядок точек в списке произвольный
+# Точно известно, что точек каждого цвета ровно три, но порядок точек в списке произвольный
 points = [
     Point(2, 7, "red"),
     Point(12, 7, "green"),
@@ -19,12 +19,18 @@ points = [
     Point(-12, 0, "red")
 ]
 # Все точки одного цвета соединены линиями и образуют треугольник
+red_triangle = [point for point in points if point.color == "red"]
+green_triangle = [point for point in points if point.color == "green"]
 
-# TODO-1: доработайте конструктор class Point для хранения цвета точки
-# TODO-2: реализуйте метод dist_to()
-# TODO-3: вычислите площади треугольников образованных точками разных цветов
 
-# your core here...
+# Площадь треугольника по формуле Герона
+def s_triangle(triangle):
+    a = triangle[0].dist_to(triangle[1])
+    b = triangle[1].dist_to(triangle[2])
+    c = triangle[2].dist_to(triangle[0])
+    p = (a + b + c) / 2
+    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
 
-print("Площадь красного треугольника = ", ...)
-print("Площадь зеленого треугольника = ", ...)
+
+print("Площадь красного треугольника = ", s_triangle(red_triangle))
+print("Площадь зеленого треугольника = ", s_triangle(green_triangle))
