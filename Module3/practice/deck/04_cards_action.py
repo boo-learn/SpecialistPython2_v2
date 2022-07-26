b5cb1e374b7e858307ece546d5d069bee89a9c58
Fotum <MaDMaDDneSS@gmail.com>
2022-07-26 13:14:03 +0300

    Update 04_cards_action.py
    Соколов Юрий

--- Module3/practice/deck/04_cards_action.py
+++ Module3/practice/deck/04_cards_action.py
@@ -1,12 +1,25 @@
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value: str, suit: str):
+        self.value = value
+        self.suit = suit
 
+    def to_str(self):
+        suit_pic = {"Hearts": "\u2665", "Diamonds": "\u2666", "Clubs": "\u2663", "Spades": "\u2660"}
+        return f"{self.value}{suit_pic.get(self.suit)}"
+
+    def equal_suit(self, other):
+        return self.suit == other.suit.name
+
+
+# Инициализация мастей
+suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+# Карты
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 cards = []
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-# TODO-1: в список cards добавьте ВСЕ карты всех мастей
+for s in suits:
+    suit_cards = [Card(value, s) for value in values]
+    cards.extend(suit_cards)
 
-# TODO-2: Выведите карты в формате: cards[кол-во]2♥, 3♥, 4♥ ... A♥, 2♦, 3♦ ... A♦, ...
-#  кол-во берем от размера списка cards
+print(f"cards[{len(cards)}] {', '.join([card.to_str() for card in cards])}")
