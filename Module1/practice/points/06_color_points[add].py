702ac41e3561be85098a625ff477767ed441319f
ASidorovskaya <asidorovskaya@mail.ru>
2022-07-26 14:30:11 +0300

    Update 06_color_points[add].py

--- Module1/practice/points/06_color_points[add].py
+++ Module1/practice/points/06_color_points[add].py
@@ -1,15 +1,26 @@
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        self.color = ...
+        self.color = color
 
     def dist_to(self, other_point):
-        ...
+        if other_point.color == self.color:
+            return ((other_point.x - self.x) ** 2 + (other_point.y - self.y) ** 2) ** 0.5
+        else:
+            return 0
 
 
-# Дан список точек нарисованных красным(red) и зеленым(green) цветами
-# Точно известно что точек каждого цвета ровно три, но порядок точек в списке произвольный
+def triangle_area(p1, p2, p3):
+    distp1_p2 = p1.dist_to(p2)
+    distp2_p3 = p2.dist_to(p3)
+    distp3_p1 = p3.dist_to(p1)
+    semiperimeter = (distp1_p2 + distp2_p3 + distp3_p1) * 0.5
+    area = (semiperimeter * (semiperimeter - distp1_p2) * (semiperimeter - distp2_p3) * (
+            semiperimeter - distp3_p1)) ** 0.5
+    return area
+
+
 points = [
     Point(2, 7, "red"),
     Point(12, 7, "green"),
@@ -18,6 +29,19 @@ points = [
     Point(10, -2, "green"),
     Point(-12, 0, "red")
 ]
+
+red_triangle = []
+green_triangle = []
+
+for point in points:
+    if point.color == "green":
+        green_triangle.append(point)
+    else:
+        red_triangle.append(point)
+
+# Дан список точек нарисованных красным(red) и зеленым(green) цветами
+# Точно известно что точек каждого цвета ровно три, но порядок точек в списке произвольный
+
 # Все точки одного цвета соединены линиями и образуют треугольник
 
 # TODO-1: доработайте конструктор class Point для хранения цвета точки
@@ -26,5 +50,6 @@ points = [
 
 # your core here...
 
-print("Площадь красного треугольника = ", ...)
-print("Площадь зеленого треугольника = ", ...)
+print("Площадь красного треугольника = ", triangle_area(red_triangle[0], red_triangle[1], red_triangle[2]))
+print("Площадь зеленого треугольника = ", triangle_area(green_triangle[0], green_triangle[1], green_triangle[2]))
+
