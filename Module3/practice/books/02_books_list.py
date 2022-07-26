f22e9484946094a7175855e1f9410360fd850fc3
IggIOK <56275131+IggIOK@users.noreply.github.com>
2022-07-26 11:52:19 +0300

    Update 02_books_list.py

--- Module3/practice/books/02_books_list.py
+++ Module3/practice/books/02_books_list.py
@@ -4,8 +4,7 @@ class Author:
         self.surname = surname
 
     def short_name(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return f"{self.name[0]}.{self.surname}"
 
 
 class Book:
@@ -16,8 +15,7 @@ class Book:
         self.pages = pages
 
     def to_str(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return f'"{self.name}" ' + f'author:{self.author.short_name()} ' + f'publish:{self.year}'
 
 
 author1 = Author("Михаил", "Булгаков")
@@ -31,4 +29,7 @@ books_catalog = [
     Book("Оно", author2, 1986, 320),
 ]
 
-# TODO-1: Выведите нумерованный список книг, используя для каждой книги ее строковое представление(метод .to_str())
+cnt = 1
+for book in books_catalog:
+    print(cnt, ', ', book.to_str())
+    cnt += 1
