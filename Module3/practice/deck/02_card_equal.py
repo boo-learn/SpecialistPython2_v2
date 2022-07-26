7216fc204958a3047a00e497e91f24d2ec474970
Sym0n92 <109950407+Sym0n92@users.noreply.github.com>
2022-07-26 14:05:40 +0400

    Update 02_card_equal.py

--- Module3/practice/deck/02_card_equal.py
+++ Module3/practice/deck/02_card_equal.py
@@ -1,21 +1,32 @@
 # Начнем с создания карты
+
 class Card:
     def __init__(self, value, suit):
         self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
         self.suit = suit  # Масть карты
+        self.suit_dict = {
+            'Hearts': '\u2665',
+            'Diamonds': '\u2666',
+            'Spades': '\u2660',
+            'Clubs': '\u2663'
+        }
 
     def to_str(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return self.value + self.suit_dict.get(self.suit)
 
     def equal_suit(self, other_card):
         # TODO-1: метод возвращает True - если масти карт равны или False - если нет
-        ...
+        return self.suit == other_card.suit
+
 
 
 # Создадим несколько карт
-card1 = Card("10", "Hearts")
-card2 = Card("A", "Diamonds")
+card1 = Card("10", "Spades")
+card2 = Card("A", "Hearts")
+
+# Выведем карты на экран в виде: 10♥ и A♦
+print(card1.to_str())
+print(card2.to_str())
 
 # Проверим, одинаковые ли масти у карт
 if card1.equal_suit(card2):
