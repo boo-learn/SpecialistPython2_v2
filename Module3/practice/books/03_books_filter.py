dc21039e6ce64477ff093966c730f6c5f548e6a0
Fotum <MaDMaDDneSS@gmail.com>
2022-07-26 11:49:47 +0300

    Update 03_books_filter.py
    Соколов Юрий

--- Module3/practice/books/03_books_filter.py
+++ Module3/practice/books/03_books_filter.py
@@ -4,8 +4,7 @@ class Author:
         self.surname = surname
 
     def short_name(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return f"{self.name[0]}.{self.surname}"
 
 
 class Book:
@@ -16,9 +15,19 @@ class Book:
         self.pages = pages
 
     def to_str(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return f'"{self.name}" author: {self.author.short_name()} publish: {self.year} год'
 
+
+author1 = Author("Михаил", "Булгаков")
+author2 = Author(name="Стивен", surname="Кинг")
+
+books_catalog = [
+    Book("Вьюга", author1, 1926, 25),
+    Book("Мастер и Маргарита", author1, 1967, 480),
+    Book("Собачье сердце", author1, 1987, 352),
+    Book("Сияние", author2, 2014, 544),
+    Book("Оно", author2, 1986, 320),
+]
 
 author1 = Author("Михаил", "Булгаков")
 author2 = Author(name="Стивен", surname="Кинг")
@@ -32,5 +41,13 @@ books_catalog = [
 ]
 print("Найти все книги по фамилии автора")
 surname = input("Фамилия автора: ")
-# TODO-1: Выведите нумерованный список книг автора с фамилией surname
-#  примечание: если книг для автора с указанной фамилией нет - выведите "Книги не найдены"
+
+filtered_books = [book for book in books_catalog if book.author.surname.lower() == surname.lower()]
+
+if len(filtered_books) != 0:
+    book_nm = 1
+    for book in filtered_books:
+        print(f"{book_nm}. {book.to_str()}")
+        book_nm += 1
+else:
+    print("Книги не найдены")
