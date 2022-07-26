39dea1bcf1d4ef9cb2e51418eeebfe4f7a8b2e6a
IggIOK <56275131+IggIOK@users.noreply.github.com>
2022-07-26 12:51:51 +0300

    Update 01_card_to_str.py

--- Module3/practice/deck/01_card_to_str.py
+++ Module3/practice/deck/01_card_to_str.py
@@ -4,9 +4,15 @@ class Card:
         self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
         self.suit = suit  # Масть карты
 
+        self.to_unic_char = {
+            "Hearts": "\u2665",
+            "Diamonds": "\u2666",
+            "Spades": "\u2660",
+            "Clubs": "\u2663"
+        }
+
     def to_str(self):
-        # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
-        ...
+        return f"{self.value}{self.to_unic_char[self.suit]}"
 
 
 # Создадим несколько карт
@@ -16,9 +22,3 @@ card2 = Card("A", "Diamonds")
 # Выведем карты на экран в виде: 10♥ и A♦
 print(card1.to_str())
 print(card2.to_str())
-
-# Пример, вывод иконок мастей:
-print('\u2661', '\u2665')
-print('\u2662', '\u2666')
-print('\u2667', '\u2663')
-print('\u2664', '\u2660')
