d0fe517eabe3680ec515426c9bd4ae6566c3ae45
utenkov-maxim <74677702+utenkov-maxim@users.noreply.github.com>
2022-07-26 14:06:47 +0300

    Update 04_cards_action.py

--- Module3/practice/deck/04_cards_action.py
+++ Module3/practice/deck/04_cards_action.py
@@ -1,12 +1,27 @@
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        # TODO-0: скопируйте реализацию метода из предыдущей задачи
+        suits = {'Diamonds': '\u2666', 'Hearts': '\u2665', 'Spades': '\u2660', 'Clubs': '\u2663'}
+        return f"{self.value}{suits[self.suit]}"
+
+    def equal_suit(self, other_card):
+        # TODO-1: метод возвращает True - если масти карт равны или False - если нет
+        return self.suit == other_card.suit
 
 
 cards = []
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 # TODO-1: в список cards добавьте ВСЕ карты всех мастей
+
+for s in suits:
+    for v in values:
+        cards.append(Card(v, s))
 
 # TODO-2: Выведите карты в формате: cards[кол-во]2♥, 3♥, 4♥ ... A♥, 2♦, 3♦ ... A♦, ...
 #  кол-во берем от размера списка cards
+print(f"cards[{len(cards)}]" + ', '.join([c.to_str() for c in cards]))
