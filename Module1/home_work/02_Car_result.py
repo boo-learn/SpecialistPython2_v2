f4c8279e5d03e1920ab7616f7db2441928c8d858
ngospodarik <109979798+ngospodarik@users.noreply.github.com>
2022-07-26 12:33:56 +0300

    Update 02_Car_result.py

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -1,5 +1,27 @@
-# Сюда отправляем решение задачи "Автомобиль"
-# Само задание в файле 02_Сar_task.md
-
 class Car:
-    pass
+    def __init__(self, gas, capacity, gas_per_km):
+        self.gas = gas
+        self.capacity = capacity
+        self.gas_per_km = gas_per_km
+        self.mileage = 0
+
+
+    def fill(self, liters):
+        if (liters + self.gas) > self.capacity:
+            print ( f'Залили бак полностью. Осталось лишних {liters + self.gas - self.capacity} литров' )
+            self.gas = self.capacity
+        else:
+            self.gas += liters
+
+    def ride(self, km):
+
+        init_gas = self.gas
+        self.gas -= km * self.gas_per_km
+
+        if self.gas < 0:
+            print(f'Бензин закончился. Удалось проехать лишь {init_gas * self.gas_per_km} километров')
+            self.mileage = self.mileage + init_gas * self.gas_per_km
+            self.gas = 0
+        else:
+            print(f'Проехали {km} километров, потратили {km * self.gas_per_km} литров топлива')
+            self.mileage = self.mileage + km * self.gas_per_km
