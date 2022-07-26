fe6c83f6abf16bcfb618a13b9022c9e1bbe98b2f
ngospodarik <109979798+ngospodarik@users.noreply.github.com>
2022-07-26 11:12:32 +0300

    Update 01_hw_coins.py

--- Module1/home_work/01_hw_coins.py
+++ Module1/home_work/01_hw_coins.py
@@ -10,7 +10,12 @@ class Coin:
         """
         Подбрасывание монетки
         """
-        self.side = ...  # random: heads/tails
+        if random.randint(0,1):
+            self.side = 'heads'
+        else:
+            self.side = 'tails'
+
+        return self.side   # random: heads/tails
 
 
 # Задание: создайте список из n-монеток. Подбросьте(flip) все монетки.
@@ -19,3 +24,22 @@ class Coin:
 # Пояснение: когда вы создаете монетку, то она находится в неопределенном состоянии self.side = None, т.е.
 # она находится у вас в руке и не выпала ни орлом ни решкой. Монетка "определеяется" со стороной(орел/решка),
 # только после того, как вы ее подбрасываете(вызываете метод flip())
+
+n = random.randint(1,100)
+
+coins = []
+
+for _ in range(n):
+    coins.append((Coin()))
+
+tails = heads = 0
+
+for coin in coins:
+    if Coin.flip(coin) == 'heads':
+        heads += 1
+    else:
+        tails += 1
+
+print(f'total number of flips - {n}')
+print(f'% of tails - {round((tails / (heads+tails) * 100),2)}%')
+print(f'% of heads - {round((heads / (heads+tails) * 100),2)}%')
