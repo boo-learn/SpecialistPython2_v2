5b3971a1e4d1c42b38a2e7d3019bbed37adeacb5
Sym0n92 <109950407+Sym0n92@users.noreply.github.com>
2022-07-26 16:56:28 +0400

    Update 07_deck_part3.py

--- Module3/practice/deck/07_deck_part3.py
+++ Module3/practice/deck/07_deck_part3.py
@@ -1,17 +1,62 @@
-class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
+# Начнем с создания карты
+import random
 
-    # TODO-1: реализуем новые методы
+
+class Card:
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+        self.suit_dict = {
+            'Hearts': '\u2665',
+            'Diamonds': '\u2666',
+            'Spades': '\u2660',
+            'Clubs': '\u2663'
+        }
+
+
+    def to_str(self):
+        return self.value + self.suit_dict.get(self.suit)
+
+    def equal_suit(self, other_card):
+        # TODO-1: метод возвращает True - если масти карт равны или False - если нет
+        return self.suit == other_card.suit
+
     def more(self, other_card):
-        ...
+        if Deck.values.index(self.value) == Deck.values.index(other_card.value):
+            return Deck.suits.index(self.suit) > Deck.suits.index(other_card.suit)
+        return Deck.values.index(self.value) > Deck.values.index(other_card.value)
+
+    def equal(self,other_card):
+        return self.value == other_card.value and self.suit == other_card.suit
 
     def less(self, other_card):
-        ...
+        return not self.more(other_card) and not self.equal(other_card)
 
 
 class Deck:
-    # TODO-0: сюда копируем реализацию класса колоды из предыдущего задания
-    ...
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    suits = ["Spades", "Clubs", "Diamonds", "Hearts"]
+    def __init__(self):
+        # Список карт в колоде. Каждым элементом списка будет объект класса Card
+
+        self.cards = []
+        # TODO-1: конструктор добавляет в список self.cards все(52) карты
+        self.cards = [Card(value=value, suit=suit) for value in self.values for suit in self.suits]
+
+    def show(self):
+        return f"cards[{len(self.cards)}]{', '.join([card.to_str() for card in self.cards])}"
+
+    def draw(self, x):
+        # TODO-1: Принцип работы данного метода прописан в 00_task_deck.md
+        self.x = x
+        x_list = []
+        for card in self.cards[:x]:
+            x_list.append(card.to_str())
+            self.cards.remove(card)
+        return x_list
+
+    def shuffle(self):
+        random.shuffle(self.cards)
 
 
 # Создаем колоду
@@ -21,6 +66,8 @@ deck.shuffle()
 print(deck.show())
 # Берем две карты из колоды
 card1, card2 = deck.draw(2)
+card1 = Card('10', 'Hearts')
+card2 = Card('10', 'Hearts')
 
 # Тестируем методы .less() и .more()
 if card1.more(card2):
