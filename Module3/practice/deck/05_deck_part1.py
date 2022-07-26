934d403b5af4da92eb6b2b09c7350d1faceffcc0
LyubimovVladimir <59981519+LyubimovVladimir@users.noreply.github.com>
2022-07-26 14:44:50 +0300

    Update 05_deck_part1.py
    Любимов В.

--- Module3/practice/deck/05_deck_part1.py
+++ Module3/practice/deck/05_deck_part1.py
@@ -1,7 +1,13 @@
 class Card:
     # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
 
+    def to_str(self):
+        # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
+        suits = {'Hearts': '\u2665', 'Diamonds': '\u2666', 'Clubs': '\u2667', 'Spades': '\u2664'}
+        return f'{self.value}{suits[self.suit]}'
 
 class Deck:
     def __init__(self):
@@ -9,11 +15,14 @@ class Deck:
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
         self.cards = []
+        for suit in suits:
+            for value in values:
+                self.cards.append(Card(value, suit))
         # TODO-1: конструктор добавляет в список self.cards все(52) карты
 
     def show(self):
         # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+        return f"cards[{len(self.cards)}]{', '.join([card.to_str() for card in self.cards])}"
 
 
 # Создаем колоду
