e736043736b13ccf0e3fc588270988cc25278ec0
Scherbakova Natalya <19natasha94@bk.ru>
2022-07-26 21:04:46 +0300

    Update 03_backpack_part2.py

--- Module2/practice/backpack/03_backpack_part2.py
+++ Module2/practice/backpack/03_backpack_part2.py
@@ -1,24 +1,53 @@
 class Item:
-    ...
-    # TODO: сюда копируем реализацию класса из предыдущего задания
+    def __init__(self, item_name, item_weight, item_cost):
+        self.name = item_name  # Название предмета
+        self.weight = item_weight  # Вес предмета, в килограммах
+        self.cost = item_cost  # Цена предмета, пусть будет, в рублях
+
+    def show(self):
+        return f"{self.name} вес: {self.weight} кг цена: {self.cost} руб"
 
 
 class BackPack:  # рюкзак
-    ...
-    # TODO: сюда копируем реализацию класса из предыдущего задания
-    # TODO: добавьте новое свойство .max_weight - максимальный суммарный вес предметов, которые можно положить в рюкзак
+    def __init__(self, max_weight=50):
+        self.items = []  # Предметы, которые хранятся в рюкзаке
+        self.max_weight = max_weight  # максимальный суммарный вес предметов, которые можно положить в рюкзак
+
+    def add_item(self, item: Item):
+        # TODO: Если предмет не помещается в рюкзак по весу - вывести сообщение "Предмет {name} слишком тяжелый",
+        #  и сам предмет не должен быть добавлен в рюкзак.
+        #  Если предмет помещает, то добавляем его в рюкзак.
+        max_weight = self.sum_weight()
+        if max_weight + item.weight <= self.max_weight:
+            self.items.append(item)
+        else:
+            print(f'Предмет {item.name} слишком тяжелый')
+
+    def show_items(self):
+        count = 1
+        for item in self.items:
+            print(f'{count}. {item.show()}')
+            count += 1
 
     def sum_weight(self):
         """
         Возвращает суммарный вес всех предметов в рюкзаке
         """
         # TODO: реализуйте метод
+        weight_items = 0
+        for item in self.items:
+            weight_items += item.weight
+        return weight_items
 
     def sum_cost(self):
         """
         Возвращает суммарную стоимость всех предметов в рюкзаке
         """
         # TODO: реализуйте метод
+        sum_items = 0
+        for item in self.items:
+            sum_items += item.cost
+        return sum_items
 
 
 # Создаем предметы
@@ -35,9 +64,6 @@ backpack.add_item(item1)
 backpack.add_item(item2)
 backpack.add_item(item3)
 backpack.add_item(item4)
-# TODO: Если предмет не помещается в рюкзак по весу - вывести сообщение "Предмет {name} слишком тяжелый",
-#  и сам предмет не должен быть добавлен в рюкзак.
-#  Если предмет помещает, то добавляем его в рюкзак.
 
 # Выводим все предметы в рюкзаке
 backpack.show_items()
