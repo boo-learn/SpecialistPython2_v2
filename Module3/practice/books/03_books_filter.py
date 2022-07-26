e7f65f27952711c1379e90f1e16fdc73742c344f
iLeyawiin <109950415+iLeyawiin@users.noreply.github.com>
2022-07-26 12:24:31 +0300

    Update 03_books_filter.py

--- Module3/practice/books/03_books_filter.py
+++ Module3/practice/books/03_books_filter.py
@@ -4,8 +4,7 @@ class Author:
         self.surname = surname
 
     def short_name(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return self.name[0] + "." + self.surname
 
 
 class Book:
@@ -16,8 +15,7 @@ class Book:
         self.pages = pages
 
     def to_str(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return f'"{self.name}" author: {self.author.short_name()} publish: {str(self.year)} год'
 
 
 author1 = Author("Михаил", "Булгаков")
@@ -32,5 +30,13 @@ books_catalog = [
 ]
 print("Найти все книги по фамилии автора")
 surname = input("Фамилия автора: ")
-# TODO-1: Выведите нумерованный список книг автора с фамилией surname
-#  примечание: если книг для автора с указанной фамилией нет - выведите "Книги не найдены"
+book_not_found = True
+row_num = 1
+for book in books_catalog:
+    if book.author.surname == surname:
+        book_not_found = False
+        print(f'{row_num} {book.to_str()}')
+        row_num += 1
+
+if book_not_found:
+    print(f"Книги не найдены")
