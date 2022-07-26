0e6a493f3b260b5f8416b9efaca34cb945217a1d
vln-oss <109944033+vln-oss@users.noreply.github.com>
2022-07-26 11:57:44 +0300

    Update 03_books_filter.py

--- Module3/practice/books/03_books_filter.py
+++ Module3/practice/books/03_books_filter.py
@@ -4,8 +4,7 @@ class Author:
         self.surname = surname
 
     def short_name(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return self.name[0] + "." + self.surname
 
 
 class Book:
@@ -16,8 +15,39 @@ class Book:
         self.pages = pages
 
     def to_str(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return f'"{self.name} " author: {self.author.short_name()} publish:{self.year} год'
+
+
+author1 = Author("Михаил", "Булгаков")
+author2 = Author(name="Стивен", surname="Кинг")
+
+books_catalog = [
+    Book("Вьюга", author1, 1926, 25),
+    Book("Мастер и Маргарита", author1, 1967, 480),
+    Book("Собачье сердце", author1, 1987, 352),
+    Book("Сияние", author2, 2014, 544),
+    Book("Оно", author2, 1986, 320),
+]
+
+
+class Author:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def short_name(self):
+        return self.name[0] + "." + self.surname
+
+
+class Book:
+    def __init__(self, name, author: Author, year, pages):
+        self.name = name
+        self.author = author
+        self.year = year
+        self.pages = pages
+
+    def to_str(self):
+        return f'"{self.name} " author: {self.author.short_name()} publish:{self.year} год'
 
 
 author1 = Author("Михаил", "Булгаков")
@@ -32,5 +62,13 @@ books_catalog = [
 ]
 print("Найти все книги по фамилии автора")
 surname = input("Фамилия автора: ")
-# TODO-1: Выведите нумерованный список книг автора с фамилией surname
-#  примечание: если книг для автора с указанной фамилией нет - выведите "Книги не найдены"
+
+author_list = list(filter(lambda x: x.author.surname == surname, books_catalog))
+
+if len(author_list) == 0:
+    print("Книги не найдены")
+    exit()
+
+for pos, book in enumerate(author_list):
+    print(pos + 1, ". ", book.to_str())
+
