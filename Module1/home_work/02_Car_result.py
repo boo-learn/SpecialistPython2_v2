a6739e360659ce99e2d89cd40525f3a01c917c54
ZzzNikolasZzz <109979786+ZzzNikolasZzz@users.noreply.github.com>
2022-07-26 11:04:05 +0300

    Update 02_Car_result.py

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -1,5 +1,36 @@
 # Сюда отправляем решение задачи "Автомобиль"
 # Само задание в файле 02_Сar_task.md
 
+
 class Car:
-    pass
+    def __init__(self, a, b, c):
+        self.gas = a
+        self.capacity = b
+        self.gas_per_km = c
+        self.distance = 0
+
+    def fill(self, a):
+        if self.gas + a > self.capacity:
+            self.gas = self.capacity
+            print(f"{a-self.capacity} лишних литра")
+            return self.gas
+        elif self.gas + a <= self.capacity:
+            self.gas += a
+        return self.gas
+
+    def ride(self, km):
+        if km * self.gas_per_km > self.gas:
+            self.distance += self.gas / self.gas_per_km
+            return print(
+                f" машина проехала {self.gas/self.gas_per_km} км, топливо закончилось"
+            )
+        else:
+            self.distance += km
+            return print(f" машина проехала {km} км")
+
+
+car1 = Car(40, 60, 0.5)
+
+car1.fill(20)
+
+car1.ride(100)
