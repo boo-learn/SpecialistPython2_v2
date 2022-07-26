212016a7e242244c3c58b846c3cded3a40b8540a
AnnaPyatniza <109980426+AnnaPyatniza@users.noreply.github.com>
2022-07-26 20:42:58 +0300

    Update 01_Item.py

--- Module2/practice/backpack/01_Item.py
+++ Module2/practice/backpack/01_Item.py
@@ -1,15 +1,17 @@
+#подскажите почему не срабатывает код?
 class Item:
-    def __init__(self, ):
-        self.name = ...  # Название предмета
-        self.weight = ...  # Вес предмета, в килограммах
-        self.cost = ...  # Цена предмета, пусть будет, в рублях
+    def __init__(self, name, weight, cost):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
 
 
-def show_item(item: Item):
+# def show_item(item: Item):
+def show(self):
     """
     Возвращает строковое представление объекта Item
     """
-    return f"{item.name} вес:{item.weight} цена:{item.cost}"
+    return f"{self.name} вес:{self.weight} цена:{self.cost}"
 
 
 # TODO-1: Дополните конструктор класса Item
@@ -19,19 +21,13 @@ item3 = Item("Ноутбук", 2.5, 22500)
 item4 = Item("Кот", 0.5, 250)
 
 # TODO-2: запустите программу, посмотрите результаты функции show_item()
-print(show_item(item1))
-print(show_item(item2))
-print(show_item(item3))
-print(show_item(item4))
+# print(show_item(item1))
+# print(show_item(item2))
+# print(show_item(item3))
+# print(show_item(item4))
 
 # TODO-3: сделайте функцию show_item(), методом show() класса Item
-# print(item1.show())
-# print(item2.show())
-# print(item3.show())
-# print(item4.show())
-
-# TODO-4: поместите все объекты item1, item2 ... itemN в список.
-#  Выведите элементы в виде нумерованного списка, при выводе используйте метод .show()
-items = []
-for item in items:
-    ...
+print(item1.show())
+print(item2.show())
+print(item3.show())
+print(item4.show())
