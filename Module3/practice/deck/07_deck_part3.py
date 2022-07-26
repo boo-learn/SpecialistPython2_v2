8b4b57a61c98b3cbfdd0538203c32e35b08680f1
utenkov-maxim <74677702+utenkov-maxim@users.noreply.github.com>
2022-07-26 14:35:32 +0300

    Update 07_deck_part3.py

--- Module3/practice/deck/07_deck_part3.py
+++ Module3/practice/deck/07_deck_part3.py
@@ -1,17 +1,64 @@
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        # TODO-0: скопируйте реализацию метода из предыдущей задачи
+        suits = {'Diamonds': '\u2666', 'Hearts': '\u2665', 'Spades': '\u2660', 'Clubs': '\u2663'}
+        return f"{self.value}{suits[self.suit]}"
+
+    def equal_suit(self, other_card):
+        # TODO-1: метод возвращает True - если масти карт равны или False - если нет
+        return self.suit == other_card.suit
 
     # TODO-1: реализуем новые методы
     def more(self, other_card):
-        ...
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+        self_value_index = values.index(self.value)
+        other_value_index = values.index(other_card.value)
+
+        self_suit_index = suits.index(self.suit)
+        other_suit_index = suits.index(other_card.suit)
+
+        if self_value_index > other_value_index:
+            return True
+        elif self_value_index == other_value_index and self_suit_index > other_suit_index:
+            return True
+        else:
+            return False
+
 
     def less(self, other_card):
-        ...
+        return not self.more(other_card)
 
 
 class Deck:
-    # TODO-0: сюда копируем реализацию класса колоды из предыдущего задания
-    ...
+    def __init__(self):
+        # Список карт в колоде. Каждым элементом списка будет объект класса Card
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+        self.cards = []
+        # TODO-0: конструктор копируем из предыдущей задачи
+        for s in suits:
+            for v in values:
+                self.cards.append(Card(v, s))
+
+    def show(self):
+        # TODO-0: копируем из предыдущей задачи
+        return f"deck[{len(self.cards)}]" + ', '.join([c.to_str() for c in self.cards])
+
+    def draw(self, x):
+        # TODO-1: Принцип работы данного метода прописан в 00_task_deck.md
+        return [self.cards.pop(0) for i in range(x)]
+
+    def shuffle(self):
+        # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
+        #   Подсказка: https://www.w3schools.com/python/ref_random_shuffle.asp
+        from random import shuffle
+        shuffle(self.cards)
 
 
 # Создаем колоду
