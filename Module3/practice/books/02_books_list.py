c05321442f1d03a0a27b274c0cb22ea88422e3dd
Anna12s <109950497+Anna12s@users.noreply.github.com>
2022-07-26 12:11:20 +0300

    Update 02_books_list.py

--- Module3/practice/books/02_books_list.py
+++ Module3/practice/books/02_books_list.py
@@ -4,7 +4,9 @@ class Author:
         self.surname = surname
 
     def short_name(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
+        return f'{self.name[0]}. {self.surname}'
+        # TODO-1: метод возвращает строку в формате: М.Булгаков
+        #  пояснение: Первую букву имени, фамилию целиком
         ...
 
 
@@ -16,9 +18,10 @@ class Book:
         self.pages = pages
 
     def to_str(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
+        return f'"{self.name}", author: {self.author.short_name()}, publish: {self.year}'
+        # TODO-2: метод возвращает строку в формате: "Вьюга" author:М.Булгаков publish:1926 год
+        #  пояснение: Название книги выводим в кавычках(""), у имени автора только первую букву
         ...
-
 
 author1 = Author("Михаил", "Булгаков")
 author2 = Author(name="Стивен", surname="Кинг")
@@ -30,5 +33,6 @@ books_catalog = [
     Book("Сияние", author2, 2014, 544),
     Book("Оно", author2, 1986, 320),
 ]
-
-# TODO-1: Выведите нумерованный список книг, используя для каждой книги ее строковое представление(метод .to_str())
+for n, book in enumerate(books_catalog):
+    print(f"{n+1}{book.to_str()}")
+    
