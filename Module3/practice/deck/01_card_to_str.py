54b8692d98f68a2047ddcd09f23a9b4cda255822
Vnnmnk <109950421+Vnnmnk@users.noreply.github.com>
2022-07-26 12:56:27 +0300

    Update 01_card_to_str.py

--- Module3/practice/deck/01_card_to_str.py
+++ Module3/practice/deck/01_card_to_str.py
@@ -4,9 +4,20 @@ class Card:
         self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
         self.suit = suit  # Масть карты
 
+    def unicode_suit(self):
+        if self.suit == "Hearts":
+            return '\u2665'
+        if self.suit == "Diamonds":
+            return '\u2666'
+        if self.suit == "Spades":
+            return '\u2660'
+        if self.suit == "Clubs":
+            return '\u2663'
+        else:
+            return 'Suit error'
+
     def to_str(self):
-        # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
-        ...
+        return f'{self.value}{self.unicode_suit()}'
 
 
 # Создадим несколько карт
@@ -17,8 +28,8 @@ card2 = Card("A", "Diamonds")
 print(card1.to_str())
 print(card2.to_str())
 
-# Пример, вывод иконок мастей:
-print('\u2661', '\u2665')
-print('\u2662', '\u2666')
-print('\u2667', '\u2663')
-print('\u2664', '\u2660')
+# # Пример, вывод иконок мастей:
+# print('\u2661', '\u2665')
+# print('\u2662', '\u2666')
+# print('\u2667', '\u2663')
+# print('\u2664', '\u2660')
