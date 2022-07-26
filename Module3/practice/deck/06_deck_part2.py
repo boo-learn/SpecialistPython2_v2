86e6999d4dc5e8591d9211995c59717b3004e60a
Marina <47941439+Remarina@users.noreply.github.com>
2022-07-26 14:36:52 +0300

    Update 06_deck_part2.py

--- Module3/practice/deck/06_deck_part2.py
+++ Module3/practice/deck/06_deck_part2.py
@@ -1,6 +1,18 @@
+import random
+
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        # TODO-0: скопируйте реализацию метода из предыдущей задачи
+        suits = {'Hearts':'\u2665','Diamonds':'\u2666','Clubs':'\u2663','Spades':'\u2660'}
+        return f'{self.value}{suits[self.suit]}'
+
+    def equal_suit(self, other_card):
+        # TODO-1: метод возвращает True - если масти карт равны или False - если нет
+        return self.suit == other_card.suit
 
 
 class Deck:
@@ -8,34 +20,35 @@ class Deck:
         # Список карт в колоде. Каждым элементом списка будет объект класса Card
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-        self.cards = []
         # TODO-0: конструктор копируем из предыдущей задачи
+        self.cards = [Card(value, suit) for suit in suits for value in values]
 
     def show(self):
         # TODO-0: копируем из предыдущей задачи
-        ...
+        return f"deck[{len(self.cards)}]{', '.join([card.to_str() for card in self.cards])}"
 
     def draw(self, x):
         # TODO-1: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+        return [self.cards.pop() for _ in range(x)]
 
     def shuffle(self):
         # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
         #   Подсказка: https://www.w3schools.com/python/ref_random_shuffle.asp
-        ...
+        return random.shuffle(self.cards)
 
 
 # Создаем колоду
 deck = Deck()
-# Выводим колоду в формате указанном в основном задании
+print('Выводим колоду в формате указанном в основном задании')
 print(deck.show())
-# Тусуем колоду
+print('Тусуем колоду')
 deck.shuffle()
 print(deck.show())
 
 # Возьмем 5 карт "в руку"
 hand = deck.draw(5)
-# Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
+print('Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют')
 print(deck.show())
-# Выводим список карт "в руке"(список hand)
-print(...)
+
+print('Выводим список карт "в руке"(список hand)')
+print(', '.join([card.to_str() for card in hand]))
