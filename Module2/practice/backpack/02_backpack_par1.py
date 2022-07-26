4f4d092be0eb00b7f6b886f2b25f2df73afbf157
ASidorovskaya <asidorovskaya@mail.ru>
2022-07-26 20:59:05 +0300

    Update 02_backpack_par1.py

--- Module2/practice/backpack/02_backpack_par1.py
+++ Module2/practice/backpack/02_backpack_par1.py
@@ -1,6 +1,11 @@
 class Item:
-    ...
-    # TODO: сюда копируем реализацию класса из предыдущего задания
+    def __init__(self, name, weight, cost):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
+
+    def show(self):
+        return f"{self.name} вес:{self.weight} цена:{self.cost}"
 
 
 class BackPack:  # рюкзак
@@ -8,16 +13,14 @@ class BackPack:  # рюкзак
         self.items = []  # Предметы, которые хранятся в рюкзаке
 
     def add_item(self, item: Item):
-        """
-        Добавляет предмет(item) в этот рюкзак
-        """
-        # TODO: реализуйте метод
+        self.items.append(item)
 
     def show_items(self):
-        """
-        Вывод все предметы, содержащиеся в рюкзаке в виде нумерованного списка
-        """
-        # TODO: реализуйте метод
+        print("Backpack contains: ")
+        i = 1
+        for item in self.items:
+            print(f"{i} {item.show()}")
+            i += 1
 
 
 # Создаем предметы
@@ -33,3 +36,4 @@ backpack.add_item(item2)
 
 # Выводим все предметы в рюкзаке
 backpack.show_items()
+
