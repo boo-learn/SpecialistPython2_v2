fb142a8861eff4bbaf9666ad64bb1ba6fccfdf73
Fotum <MaDMaDDneSS@gmail.com>
2022-07-26 14:39:33 +0300

    Update 08_deck_part4[add].py
    Соколов Юрий

--- Module3/practice/deck/08_deck_part4[add].py
+++ Module3/practice/deck/08_deck_part4[add].py
@@ -1,16 +1,63 @@
+import random
+
+
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value: str, suit: str):
+        self.value = value
+        self.suit = suit
+
+    def to_str(self):
+        suit_pic = {"Hearts": "\u2665", "Diamonds": "\u2666", "Clubs": "\u2663", "Spades": "\u2660"}
+        return f"{self.value}{suit_pic.get(self.suit)}"
+
+    def equal_suit(self, other):
+        return self.suit == other.suit
+
+    def more(self, other_card):
+        symbol_values = {"J": 11, "Q": 12, "K": 13, "A": 14}
+        this_value = symbol_values.get(self.value) or int(self.value)
+        other_value = symbol_values.get(other_card.value) or int(other_card.value)
+
+        # Если значения не равны, значит сравниваем по значению
+        if this_value != other_value:
+            return this_value > other_value
+
+        # Здесь значения равны, значит сравниваем масти
+        suits_values = {"Spades": 1, "Clubs": 2, "Diamonds": 3, "Hearts": 4}
+        this_suit_value = suits_values.get(self.suit)
+        other_suit_value = suits_values.get(other_card.suit)
+
+        return this_suit_value > other_suit_value
+
+    def less(self, other_card):
+        return not self.more(other_card)
 
 
 class Deck:
-    # TODO-0: сюда копируем реализацию класса колоды из предыдущего задания
-    ...
+    def __init__(self):
+        # Список карт в колоде. Каждым элементом списка будет объект класса Card
+        self.cards = []
+
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+        for st in suits:
+            suit_cards = [Card(value, st) for value in values]
+            self.cards.extend(suit_cards)
+
+    def show(self):
+        return f"deck[{len(self.cards)}]: {', '.join([card.to_str() for card in self.cards])}"
+
+    def draw(self, x):
+        if x > len(self.cards):
+            raise ValueError("Нельзя взять в руку больше карт, чем осталось в колоде")
+
+        return [self.cards.pop(0) for _ in range(x)]
+
+    def shuffle(self):
+        random.shuffle(self.cards)
 
     def shift(self, num_card):
-        # TODO-1: реализуем новый метод "сдвиг"
-        #  Принцип работы: перемещает num_card с верха колоды под низ
-        ...
+        self.cards.extend([self.cards.pop(0) for _ in range(num_card)])
 
 
 # Создаем колоду
