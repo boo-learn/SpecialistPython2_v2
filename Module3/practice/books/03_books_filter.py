5bd0162f593cd602b552b4a8ff8c312295aff406
LyubimovVladimir <59981519+LyubimovVladimir@users.noreply.github.com>
2022-07-26 12:17:23 +0300

    Update 03_books_filter.py
    Любимов В.

--- Module3/practice/books/03_books_filter.py
+++ Module3/practice/books/03_books_filter.py
@@ -5,7 +5,7 @@ class Author:
 
     def short_name(self):
         # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return f'{self.name[0]}.{self.surname}'
 
 
 class Book:
@@ -17,7 +17,7 @@ class Book:
 
     def to_str(self):
         # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return f'"{self.name}" author: {self.author.short_name()} publish: {self.year} год'
 
 
 author1 = Author("Михаил", "Булгаков")
@@ -34,3 +34,12 @@ print("Найти все книги по фамилии автора")
 surname = input("Фамилия автора: ")
 # TODO-1: Выведите нумерованный список книг автора с фамилией surname
 #  примечание: если книг для автора с указанной фамилией нет - выведите "Книги не найдены"
+i = 0
+cnt = 0
+for book in books_catalog:
+    if book.author.surname == surname:
+        print(i + 1, book.to_str())
+        i += 1
+        cnt += 1
+if cnt == 0:
+    print('Книги не найдены')
