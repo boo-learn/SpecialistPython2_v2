363b44c73bb599e38e5b737fe6f081f582c00b9f
Marina <47941439+Remarina@users.noreply.github.com>
2022-07-27 11:52:14 +0300

    Update 02_base_sort.py

--- Module5/practice/01_bases_sort/02_base_sort.py
+++ Module5/practice/01_bases_sort/02_base_sort.py
@@ -1,15 +1,67 @@
-# Все алгоритмы сортировки из examples/ оберните в функции
+def bubble_sort(nums):
+    #print("before sort = ", nums)
+    swapped = True
+    j = len(nums)-1
+    while swapped:
+        swapped = False
+        for i in range(j):
+            if nums[i] > nums[i + 1]:
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+        j -=1
+    return nums
 
-def bubble_sort():
-    pass
+def sort_choice(nums):
+    i = 0
+    while i < len(nums) - 1:
+        m = i
+        j = i + 1
+        while j < len(nums):
+            if nums[j] < nums[m]:
+                m = j
+            j += 1
+        nums[i], nums[m] = nums[m], nums[i]
+        i += 1
+    return nums
 
 
-def sort_choice():
-    pass
+def quick_sort(nums):
+    
+    def partition(nums, low, high):
+        # Выбираем средний элемент в качестве опорного
+        # Также возможен выбор первого, последнего
+        # или произвольного элементов в качестве опорного
+        pivot = nums[(low + high) // 2]
+        i = low - 1
+        j = high + 1
+        while True:
+            i += 1
+            while nums[i] < pivot:
+                i += 1
 
+            j -= 1
+            while nums[j] > pivot:
+                j -= 1
 
-def quick_sort():
-    pass
+            if i >= j:
+                return j
+
+            # Если элемент с индексом i (слева от опорного) больше, чем
+            # элемент с индексом j (справа от опорного), меняем их местами
+            nums[i], nums[j] = nums[j], nums[i]
+            
+    # Создадим вспомогательную функцию, которая вызывается рекурсивно
+    def _quick_sort(items, low, high):
+        if low < high:
+            # Индекс опорного элемента
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
+    return nums
 
 
 # Напишите функцию для заполнения списка случайными числами
@@ -21,6 +73,12 @@ def gen_list(size, at=-100, to=100):
     :param to: максимально возможное значение элементов
     :return: список из size произвольных элементов в диапазоне at..to 
     """
-    pass
+    return [random.randint(at,to) for _ in range(size)]
+    
 
 # протестируйте функции сортировки, используя gen_list() для создания сортируемых списков
+my_list = gen_list(size=10)
+print(my_list)
+print('bubble sort = ', bubble_sort(my_list))
+print('sort_choice = ', sort_choice(my_list))
+print('quick_sort = ', quick_sort(my_list))
