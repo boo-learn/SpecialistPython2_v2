1c17bdf84fcb02b7fe3120ac49e9c2ab26bff5e9
SergeySbornikov <70229550+SergeySbornikov@users.noreply.github.com>
2022-07-27 19:41:49 +0300

    Update 11_task_sort.py

--- Module5/practice/02_tasks_sort/11_task_sort.py
+++ Module5/practice/02_tasks_sort/11_task_sort.py
@@ -1,2 +1,13 @@
 # Анаграммы*
 # Задается словарь (список слов). Найти в нем все анаграммы (слова, составленные из одних и тех же букв).
+
+words = ['abba', 'baba', 'dog', 'GOD', 'house', 'Dog']
+d_words = dict()
+for word in words:
+    val = ''.join(sorted(word.lower()))
+    if val not in d_words:
+        d_words[val] = set()
+    d_words[val].add(word.lower())
+for k, v in d_words.items():
+    if len(v) > 1:
+        print(v)
