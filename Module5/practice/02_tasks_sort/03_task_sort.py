436eafc89a9a4b3e42a2e41cce5ff6b490759885
Sym0n92 <109950407+Sym0n92@users.noreply.github.com>
2022-07-27 14:04:05 +0400

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -2,5 +2,29 @@
 # Дан список чисел.
 # Найти: сумму 5-ти самых больших элементов
 
-numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
+def bubble_sort(list: list):
+    j = 1
+    nums = list
+    # print("before sort = ", nums)
+    swapped = True
+    while swapped:
+        swapped = False
+        # print("*****")
+        for i in range(len(nums) - j):
+            # print("i = ", i)
+            if nums[i] > nums[i + 1]:
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+        j += 1
+    # print("after sort = ", nums)
+    return nums
+
+
+numbers = [1,2,3,4,50,50,50,50,50]
+bubble_sort(numbers)
+summ = sum(numbers[:3:-1])
+print(numbers[::-1])
+print(summ)
 
