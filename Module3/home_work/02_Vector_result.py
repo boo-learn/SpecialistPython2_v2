1138763f35879815f2b61fa69c3433f0798c8093
PerlovAlexandr <109950471+PerlovAlexandr@users.noreply.github.com>
2022-07-27 08:33:42 +0300

    Update 02_Vector_result.py

--- Module3/home_work/02_Vector_result.py
+++ Module3/home_work/02_Vector_result.py
@@ -1,5 +1,30 @@
-# Сюда отправляем решение задачи "Вектор"
-# Само задание в файле 01_Vector_task.md
-
 class Vector:
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Vector(self.x * other, self.y * other)
+
+
+vector1 = Vector(2, 5)
+vector2 = Vector(1, 15)
+
+vector_result = vector1 + vector2
+print(vector_result)
+
+vector_result = vector1 - vector2
+print(vector_result)
+
+vector_result = vector1 * -2
+print(vector_result)
+
