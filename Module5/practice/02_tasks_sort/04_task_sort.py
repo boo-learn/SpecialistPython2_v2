0321cb6fb35d16a51ccb83213034222c2cf6d72b
vln-oss <109944033+vln-oss@users.noreply.github.com>
2022-07-27 13:06:54 +0300

    Update 04_task_sort.py

--- Module5/practice/02_tasks_sort/04_task_sort.py
+++ Module5/practice/02_tasks_sort/04_task_sort.py
@@ -1,3 +1,19 @@
+def bubble_sort2(nums):
+    print("before sort = ", nums)
+    swapped = True
+    while swapped:
+        swapped = False
+        # print("*****")
+        for i in range(len(nums) - 1):
+            # print("i = ", i)
+            if abs(nums[i]) > abs(nums[i + 1]):
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+    print("after sort = ", nums)
+
+
 # Сумма наибольших по модулю
 # Дан массив(список) чисел.
 # Найти: сумму 5-ти самых больших элементов по модулю.
@@ -5,3 +21,6 @@
 # В примере ниже, два самых больших по модулю числа это: -22.4 и 21.1. Они самые большие по модулю, а их сумма = -1.3
 
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, 21.1, 0]
+bubble_sort2(numbers)
+res = sum(numbers[-5:])
+print(res)
