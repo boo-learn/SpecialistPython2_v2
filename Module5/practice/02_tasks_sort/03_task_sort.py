6c7abe8e190da633e796c2aee9945d485e4c28e7
Vnnmnk <109950421+Vnnmnk@users.noreply.github.com>
2022-07-27 14:01:21 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -19,7 +19,7 @@ numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
 cnt = 5
 
 # print(numbers)
-# bubble_sort(numbers)
+bubble_sort(numbers)    # забыл оставить сортировку
 # print(numbers)
 sum_max = sum(numbers[:cnt])
 print(sum_max)
