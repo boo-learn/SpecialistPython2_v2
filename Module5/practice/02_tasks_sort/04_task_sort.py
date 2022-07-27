e17ee8d05891c207065ad04bb7b08448eec97e00
LyubimovVladimir <59981519+LyubimovVladimir@users.noreply.github.com>
2022-07-27 14:04:38 +0300

    Update 04_task_sort.py

--- Module5/practice/02_tasks_sort/04_task_sort.py
+++ Module5/practice/02_tasks_sort/04_task_sort.py
@@ -1,7 +1,58 @@
+def quick_sort(nums):
+    def partition(nums, low, high):
+        # Выбираем средний элемент в качестве опорного
+        # Также возможен выбор первого, последнего
+        # или произвольного элементов в качестве опорного
+        pivot = nums[(low + high) // 2]
+        i = low - 1
+        j = high + 1
+        while True:
+            i += 1
+            while nums[i] < pivot:
+                i += 1
+
+            j -= 1
+            while nums[j] > pivot:
+                j -= 1
+
+            if i >= j:
+                return j
+
+            # Если элемент с индексом i (слева от опорного) больше, чем
+            # элемент с индексом j (справа от опорного), меняем их местами
+            nums[i], nums[j] = nums[j], nums[i]
+
+    # Создадим вспомогательную функцию, которая вызывается рекурсивно
+    def _quick_sort(items, low, high):
+        if low < high:
+            # Индекс опорного элемента
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
+    return nums
+
 # Сумма наибольших по модулю
 # Дан массив(список) чисел.
 # Найти: сумму 5-ти самых больших элементов по модулю.
 # Пояснение: сравниваем элементы по модулю, а в сумму добавляем сами значения элементов(без модуля)
 # В примере ниже, два самых больших по модулю числа это: -22.4 и 21.1. Они самые большие по модулю, а их сумма = -1.3
 
+
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, 21.1, 0]
+abs_numbers = []
+sum_final = 0
+for i in numbers:
+    abs_numbers.append(abs(i))
+
+sorted_list = quick_sort(abs_numbers)
+for i in sorted_list[-5:]:
+    for j in numbers:
+        if abs(j) == i:
+            sum_final += j
+
+print(numbers)
+print(abs_numbers)
+print(quick_sort(abs_numbers))
+print(sum_final)
