47e8b9e9833ec950b40e636cf0993f8bdc503f97
vln-oss <109944033+vln-oss@users.noreply.github.com>
2022-07-27 16:28:13 +0300

    Update 05_nodes_find.py

--- Module6/practice/nodes/05_nodes_find.py
+++ Module6/practice/nodes/05_nodes_find.py
@@ -1,7 +1,4 @@
 class Node:
-    """
-    Класс для узла списка. Хранит значение и указатель на следующий узел.
-    """
 
     def __init__(self, value=None, next=None):
         self.value = value
@@ -9,11 +6,22 @@ class Node:
 
 
 def find_node_by_value(start_node, value):
-    """
-    Возвращает индекс ноды с заданным значением(value).
-    Если нода с указанным значение(value) не найдена, дублируйте поведение методы .index() списка
-    """
-    pass
+    current_node = start_node
+    idx = 0
+    while current_node:
+        if current_node.value == value:
+            print(f"Index of value {value} is {idx}")
+            exit()
+        idx += 1
+        current_node = current_node.next
+    print(f"Value {value} is not found")
+
+
+def print_nodes_value(start_node):
+    current_node = start_node
+    while current_node:
+        print(current_node.value)
+        current_node = current_node.next
 
 
 def gen_names_list(size=None):
@@ -25,16 +33,60 @@ def gen_names_list(size=None):
     return names[:size]
 
 
+def len_nodes(start_node):
+    """
+    Возвращает целое число - кол-во нод у цепочке
+    """
+    counter = 0
+    if not start_node is None:
+        counter += 1
+
+    while not start_node.next is None:
+        counter += 1
+        start_node = start_node.next
+
+    return counter
+
+
+def print_node_by_index(start_node, index):
+    cur_index = 0
+
+    while cur_index < index:
+        start_node = start_node.next
+        cur_index += 1
+
+    print(f"Node with index {index} = {start_node.value}")
+
+
 # Дан список из произвольного количества имен
 names = gen_names_list()
-# print(names)
+print(names)
 
-# TODO: скопируйте цепочку нод из предыдущей задачи
-...
+current_node = Node(names[0])
+first_node = current_node
+for name in names[1:]:
+    next_node = Node(name)
+    current_node.next = next_node
+    current_node = next_node
 
+# TODO: Передайте первую ноду в функцию print_nodes_value(), чтобы получить значения всех нод в цепочке
+
+
+print_nodes_value(first_node)
+
+# TODO: Передайте первую ноду в функцию len_nodes(), чтобы получить количество нод в цепочке
+
+length = len_nodes(first_node)
+print(f"length {length}")
+
+# TODO: Передайте первую ноду и индекс ноды, значение которой нужно вывести, в функцию print_node_by_index()
+#  напишите реализацию функции print_node_by_index()
+
+index = length - 2
+
+print_node_by_index(first_node, index)
 
 # TODO: Передайте первую ноду и искомое значение в функцию find_node_by_value()
 #  напишите реализацию функции find_node_by_value()
-first_node = ...
-value = ...
+value = 'Марина'
 find_node_by_value(first_node, value)
