ae8bc7bd9c7d59143db2278014d209170570b70b
SergeySbornikov <70229550+SergeySbornikov@users.noreply.github.com>
2022-07-27 11:00:47 +0300

    Update 01_base_sort.py

--- Module5/practice/01_bases_sort/01_base_sort.py
+++ Module5/practice/01_bases_sort/01_base_sort.py
@@ -1,4 +1,16 @@
-# Возьмите алгоритм сортировки пузырьком из examples/bubble_sort.py
-# Раскомментируйте print-ы, изучите вывод в консоли.
-# Вспомнив теорию, оптимизируйте алгоритм сортировки...
-
+nums = [5, 2, 1, 8, 4, 9, 10, 2]
+print("before sort = ", nums)
+swapped = True
+for j in range(len(nums)):
+    swapped = False
+    # print("*****")
+    for i in range(len(nums) - 1 - j):
+        # print("i = ", i)
+        if nums[i] > nums[i + 1]:
+            # Меняем элементы
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            # Устанавливаем swapped в True для следующей итерации
+            swapped = True
+    if not swapped:
+        break
+print("after sort = ", nums)
