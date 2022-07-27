88f1286e2ce5ada83888a5dacb936ea467752fc3
ZzzNikolasZzz <109979786+ZzzNikolasZzz@users.noreply.github.com>
2022-07-27 19:39:04 +0300

    Update 03_books_filter.py

--- Module3/practice/books/03_books_filter.py
+++ Module3/practice/books/03_books_filter.py
@@ -4,9 +4,10 @@ class Author:
         self.surname = surname
 
     def short_name(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
+        # TODO-1: метод возвращает строку в формате: М.Булгаков
+        #  пояснение: Первую букву имени, фамилию целиком
         ...
-
+        return f"{self.name[0]}.{self.surname}"
 
 class Book:
     def __init__(self, name, author: Author, year, pages):
@@ -16,8 +17,10 @@ class Book:
         self.pages = pages
 
     def to_str(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
+        # TODO-2: метод возвращает строку в формате: "Вьюга" author:М.Булгаков publish:1926 год
+        #  пояснение: Название книги выводим в кавычках(""), у имени автора только первую букву
         ...
+        return f'"{self.name}" author:{self.author.short_name()} publish:{self.year} год'
 
 
 author1 = Author("Михаил", "Булгаков")
@@ -34,3 +37,13 @@ print("Найти все книги по фамилии автора")
 surname = input("Фамилия автора: ")
 # TODO-1: Выведите нумерованный список книг автора с фамилией surname
 #  примечание: если книг для автора с указанной фамилией нет - выведите "Книги не найдены"
+
+for i in range(len(books_catalog)):
+    if surname == books_catalog[i].to_str()[1]:
+        print(books_catalog[i].to_str())
+    else:
+        print("Книги не найдены")
+#
+
+print(books_catalog[1].to_str())
+
