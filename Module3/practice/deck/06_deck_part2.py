3924fb91bd0d133b5bd081a87467a2292babb176
ASidorovskaya <asidorovskaya@mail.ru>
2022-07-28 00:10:25 +0300

    Update 06_deck_part2.py

--- Module3/practice/deck/06_deck_part2.py
+++ Module3/practice/deck/06_deck_part2.py
@@ -1,6 +1,16 @@
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        suits_list = ["Hearts", "Clubs", "Diamonds", "Spades"]
+        suits_list_code = ['\u2661', '\u2667', '\u2662', '\u2664']
+        return f'{self.value}{suits_list_code[suits_list.index(self.suit)]}'
+
+    def equal_suit(self, other_card):
+        # TODO-1: метод возвращает True - если масти карт равны или False - если нет
+        return self.suit == other_card.suit
 
 
 class Deck:
@@ -9,21 +19,23 @@ class Deck:
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
         self.cards = []
-        # TODO-0: конструктор копируем из предыдущей задачи
+        for suit in suits:
+            for value in values:
+                self.cards.append(Card(value, suit))
 
     def show(self):
-        # TODO-0: копируем из предыдущей задачи
-        ...
+        return f"Deck [{len (self.cards)} {', '.join(card.to_str() for card in self.cards)}"
 
     def draw(self, x):
-        # TODO-1: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+        drawn_cards = []
+        for card in self.cards [:x]:
+            drawn_cards.append (card)
+            self.cards.remove(card)
+        return drawn_cards
 
     def shuffle(self):
-        # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
-        #   Подсказка: https://www.w3schools.com/python/ref_random_shuffle.asp
-        ...
-
+        import random
+        return random.shuffle(self.cards)
 
 # Создаем колоду
 deck = Deck()
@@ -38,4 +50,4 @@ hand = deck.draw(5)
 # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
 print(deck.show())
 # Выводим список карт "в руке"(список hand)
-print(...)
+print(f"Карты в руке: всего = {len (hand)}, в том числе: {', '.join(card.to_str() for card in hand)}")
