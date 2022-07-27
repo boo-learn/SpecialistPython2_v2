08a582e1837f76c9e26fecdd08776c5228397d34
fluffypupp <109951288+fluffypupp@users.noreply.github.com>
2022-07-27 14:48:11 +0300

    Update 05_task_sort.py

--- Module5/practice/02_tasks_sort/05_task_sort.py
+++ Module5/practice/02_tasks_sort/05_task_sort.py
@@ -1,4 +1,22 @@
 # Дан список сотрудников:
+
+def bubble_sort(staff):
+    swapped = True
+    k = 1
+    while swapped:
+        swapped = False
+        for i in range(len(staff) - 1):
+            if staff[i]['salary'] > staff[i + 1]['salary']:
+                staff[i], staff[i + 1] = staff[i + 1], staff[i]
+                swapped = True
+            elif staff[i]['salary'] == staff[i + 1]['salary']:
+                if staff[i]['name'] > staff[i + 1]['name']:
+                    staff[i], staff[i + 1] = staff[i + 1], staff[i]
+
+        k += 1
+    return staff
+
+
 staff = [
     {
         'name': 'Григорий',
@@ -28,7 +46,22 @@ staff = [
 ]
 
 # 1. Выведите список сотрудников, отсортированный по уменьшению их заработной платы.
+# res = []
+# for i in staff:
+#     res.append(i['salary'])
+#
+# res.sort(reverse=True)
+# print(res)
+#
+# for j in range(len(res)):
+#     for i in staff:
+#         if i['salary'] == res[j]:
+#             print(i['surname'], i['name'])
+#     j += 1
+
+# print(type(staff))
 # Если у нескольких сотрудников одинаковая ЗП, то добавьте сортировку по Фамилии
-print("Список сотрудников отсортированный по уменьшению ЗП:")
+print("Список сотрудников отсортированный по уменьшению ЗП:", bubble_sort(staff)[::-1])
 
 # 2. Найдите сумму зарплат трех самых низкооплачиваемых сотрудников:
+
