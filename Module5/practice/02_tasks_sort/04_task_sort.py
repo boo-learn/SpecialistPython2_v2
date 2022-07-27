fe273e293184faf50a73cfd118baece2285c89ea
Vnnmnk <109950421+Vnnmnk@users.noreply.github.com>
2022-07-27 14:18:26 +0300

    Update 04_task_sort.py

--- Module5/practice/02_tasks_sort/04_task_sort.py
+++ Module5/practice/02_tasks_sort/04_task_sort.py
@@ -4,4 +4,24 @@
 # Пояснение: сравниваем элементы по модулю, а в сумму добавляем сами значения элементов(без модуля)
 # В примере ниже, два самых больших по модулю числа это: -22.4 и 21.1. Они самые большие по модулю, а их сумма = -1.3
 
+
+def bubble_sort_abs(nums):
+    swapped = True
+    stage = 0
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1 - stage):
+            if abs(nums[i]) < abs(nums[i + 1]):
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                swapped = True
+        stage += 1
+
+
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, 21.1, 0]
+cnt = 5
+
+# print(numbers)
+bubble_sort_abs(numbers)
+# print(numbers)
+sum_max = sum(numbers[:cnt])
+print(sum_max)
