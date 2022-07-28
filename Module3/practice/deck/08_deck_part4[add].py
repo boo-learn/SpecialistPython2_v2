701eb859588e2e648a70de0e39c7c1c311270c03
Scherbakova Natalya <19natasha94@bk.ru>
2022-07-28 09:15:15 +0300

    Update 08_deck_part4[add].py

--- Module3/practice/deck/08_deck_part4[add].py
+++ Module3/practice/deck/08_deck_part4[add].py
@@ -1,16 +1,54 @@
+import random
+
+
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+        self.suit_icons = {'Spades': '\u2660', 'Clubs': '\u2663', 'Diamonds': '\u2666', 'Hearts': '\u2665'}
+        self.card_values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+    def to_str(self):
+        return f'{self.value}{self.suit_icons[self.suit]}'
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
+
+    def more(self, other_card):
+        if self.value == other_card.value:
+            return list(self.suit_icons.keys()).index(self.suit) > list(other_card.suit_icons.keys()).index(
+                other_card.suit)
+        return self.card_values.index(self.value) > self.card_values.index(other_card.value)
+
+    def less(self, other_card):
+        if self.value == other_card.value:
+            return list(self.suit_icons.keys()).index(self.suit) < list(other_card.suit_icons.keys()).index(
+                other_card.suit)
+        return self.card_values.index(self.value) < self.card_values.index(other_card.value)
 
 
 class Deck:
-    # TODO-0: сюда копируем реализацию класса колоды из предыдущего задания
-    ...
+    def __init__(self):
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+        self.cards = [Card(value, suit) for suit in suits for value in values]
+
+    def show(self):
+        return f'cards[{len(self.cards)}]: {", ".join([card.to_str() for card in self.cards])}'
+
+    def draw(self, x):
+        first_cards = []
+        for _ in range(x):
+            first_cards.append(self.cards[0])
+            self.cards.pop(0)
+        return first_cards
+
+    def shuffle(self):
+        random.shuffle(self.cards)
 
     def shift(self, num_card):
-        # TODO-1: реализуем новый метод "сдвиг"
-        #  Принцип работы: перемещает num_card с верха колоды под низ
-        ...
+        for _ in range(num_card):
+            self.cards.append(self.cards.pop(0))
 
 
 # Создаем колоду
