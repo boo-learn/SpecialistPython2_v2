1f7ec51849d11fff54d4ef439519021a7b26ddd6
AlexeyMilkh <107917572+AlexeyMilkh@users.noreply.github.com>
2022-07-28 20:58:30 +0300

    Update 01_task_deck.py

--- Module3/practice/deck/deck_tasks/01_task_deck.py
+++ Module3/practice/deck/deck_tasks/01_task_deck.py
@@ -4,3 +4,17 @@ from deck_total import Card, Deck
 
 # TODO: Создайте колоду из 52 карт. Перемешайте ее. Вытяните две карты сверху.
 #   Сравните эти карты и выведите сообщение формата: “карта A♦ больше J♣”
+
+deck = Deck()
+print(deck)
+
+deck.shuffle()
+print(deck)
+
+card1, card2 = deck.draw(2)
+print(card1, card2)
+
+if card1 > card2:
+    print("карта A♦ больше J♣")
+else:
+    print("карта A♦ меньше J♣")
