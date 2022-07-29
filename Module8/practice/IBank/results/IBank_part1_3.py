af9073a324c4e875cfde0f7a43479b215786a1a0
PerlovAlexandr <109950471+PerlovAlexandr@users.noreply.github.com>
2022-07-29 12:39:18 +0300

    Update IBank_part1_3.py

--- Module8/practice/IBank/results/IBank_part1_3.py
+++ Module8/practice/IBank/results/IBank_part1_3.py
@@ -12,33 +12,40 @@ class Account:
         :param amount: сумма перевода
         :return:
         """
-        pass
+        if self.__balance > amount:
+            self.__balance -=amount
+            target_account.__balance +=amount
 
+        else:
+            print(f"Недостаточно средств на счету")
     def deposit(self, amount):
         """
         Внесение суммы на текущий счет
         :param amount: сумма
         """
-        pass
+        self.__balance += amount
 
     def withdraw(self, amount):
         """
         Снятие суммы с текущего счета
         :param amount: сумма
         """
-        pass
+        if self.__balance > amount:
+            self.__balance -= amount
+        else:
+            print(f"Недостаточно средств на счету")
 
     def full_info(self):
         """
         Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
         """
-        return f"..."
+        return f"{self.name} баланс: {self.__balance} руб. паспорт: {self.passport} т. {self.phone_number}"
 
     def __repr__(self):
         """
         :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
         """
-        return f"..."
+        return f"{self.name} баланс: {self.__balance} руб."
 
 
 account1 = Account("Иван", "3230 634563", "+7-900-765-12-34", 1000)
@@ -59,4 +66,5 @@ account1.transfer(account2, 600)
 
 # Проверяем изменения баланса:
 print(account1)
+print(account2)
 print(account2)
