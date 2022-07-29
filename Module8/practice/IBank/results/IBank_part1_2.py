3ec062daf7039db8b7da8bd8136c20312a1a8ad4
Vnnmnk <109950421+Vnnmnk@users.noreply.github.com>
2022-07-29 13:50:17 +0300

    Update IBank_part1_2.py

--- Module8/practice/IBank/results/IBank_part1_2.py
+++ Module8/practice/IBank/results/IBank_part1_2.py
@@ -5,56 +5,53 @@ class Account:
         self.phone_number = phone_number
         self.__balance = start_balance  # Закрываем прямой доступ к балансу
 
-    # TODO: совместно с преподавателем реализуйте getter для просмотра баланса
-    #  Можете попробовать самостоятельно: https://pythobyte.com/using-getters-and-setters-in-python-5205-840ed13f/
+    @property
+    def balance(self):
+        return self.__balance
 
-    def deposit(self, amount):
-        """
-        Внесение суммы на текущий счет
-        :param amount: сумма
-        """
-        pass
+    def __full_info(self):  # Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
+        return f"{self.name} баланс: {self.__balance} руб. паспорт: {self.passport} т. {self.phone_number}"
 
-    def withdraw(self, amount):
-        """
-        Снятие суммы с текущего счета
-        :param amount: сумма
-        """
-        pass
+    def __repr__(self):  # Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
+        return f"{self.name} баланс: {self.__balance} руб."
 
-    def full_info(self):
-        """
-        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
-        """
-        return f"..."
+    def deposit(self, amount):  # Внесение суммы на текущий счет: amount - сумма внесения
+        self.__balance += amount
 
-    def __repr__(self):
-        """
-        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
-        """
-        return f"..."
+    def withdraw(self, amount):  # Снятие суммы с текущего счета: amount - сумма снятия
+        if self.__balance >= amount:
+            self.__balance -= amount
+        else:
+            raise ValueError (f"Недостаточно средств")
 
 
 # Создаем тестовый аккаунт:
 account1 = Account("Алексей", "+7-901-744-22-99", "3232 456124", start_balance=500)
 
 # Смотрим баланс:
-print(account1)
+print(account1.balance)
 
 # Вносим сумму на счет:
-account1.deposit(600)
-print(account1)
+account1.deposit(200)
+print(account1.balance)
 
 # Снимаем деньги со счета:
 try:
-    account1.withdraw(1000)
+    account1.withdraw(600)
 except ValueError as e:
     print(e)
-print(account1)
+print(account1.balance)
 
 # Пробуем снять еще:
 try:
-    account1.withdraw(1000)
+    account1.withdraw(300)
 except ValueError as e:
     print(e)
-print(account1)
+print(account1.balance)
+
+# Пробуем снять еще:
+try:
+    account1.withdraw(100)
+except ValueError as e:
+    print(e)
+print(account1.balance)
