2f4e84ab61fe2b8b4fb86bf63d136ed073a7c514
AlexeyMilkh <107917572+AlexeyMilkh@users.noreply.github.com>
2022-07-29 19:48:29 +0300

    Update 02_base_sort.py

--- Module5/practice/01_bases_sort/02_base_sort.py
+++ Module5/practice/01_bases_sort/02_base_sort.py
@@ -1,12 +1,25 @@
 # Все алгоритмы сортировки из examples/ оберните в функции
 
 def bubble_sort():
-    pass
 
 
-def sort_choice():
-    pass
+def sort_choice(nums):
+    i = 0
+    while i < len(nums) - 1:
+        m = i
+        j = i + 1
+        while j < len(nums):
+            if nums[j] < nums[m]:
+                m = j
+            j += 1
+        nums[i], nums[m] = nums[m], nums[i]
+        i += 1
 
+
+nums = [5, 2, -1, 8, 4, -4, 7]
+print("before sort = ", nums)
+bubble_sort(nums)
+print("after sort = ", nums)
 
 def quick_sort():
     pass
