b71959ce55d8bbeeb15fda383d8633c74e741c2e
vln-oss <109944033+vln-oss@users.noreply.github.com>
2022-07-29 12:34:53 +0300

    Update IBank_part1_3.py

--- Module8/practice/IBank/results/IBank_part1_3.py
+++ Module8/practice/IBank/results/IBank_part1_3.py
@@ -5,41 +5,37 @@ class Account:
         self.phone_number = phone_number
         self.__balance = start_balance
 
-    def transfer(self, target_account, amount):
+    def full_info(self):
         """
-        Перевод денег на счет другого клиента
-        :param target_account: счет клиента для перевода
-        :param amount: сумма перевода
-        :return:
+        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
         """
-        pass
+        return f"{self.name} баланс: {self.__balance} руб. паспорт: {self.passport} т. {self.phone_number}"
+
+    def __repr__(self):
+        """
+        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.__balance} руб."
 
     def deposit(self, amount):
         """
         Внесение суммы на текущий счет
         :param amount: сумма
         """
-        pass
+        self.__balance += amount
 
     def withdraw(self, amount):
         """
         Снятие суммы с текущего счета
         :param amount: сумма
         """
-        pass
+        if amount > self.__balance:
+            raise ValueError(f"Сумма списания ({amount}) превышает остаток на счете {self.__balance}")
+        self.__balance -= amount
 
-    def full_info(self):
-        """
-        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
-        """
-        return f"..."
-
-    def __repr__(self):
-        """
-        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
-        """
-        return f"..."
-
+    def transfer(self, target_account, amount):
+        self.withdraw(amount)
+        target_account.deposit(amount)
 
 account1 = Account("Иван", "3230 634563", "+7-900-765-12-34", 1000)
 account2 = Account("Алексей", "+7-901-744-22-99", "3232 456124", 200)
@@ -55,8 +51,9 @@ print(account1)
 print(account2)
 
 # Переводим еще с первого аккаунт на второй:
-account1.transfer(account2, 600)
-
-# Проверяем изменения баланса:
-print(account1)
-print(account2)
+try:
+ account1.transfer(account2, 600)
+finally:
+ # Проверяем изменения баланса:
+ print(account1)
+ print(account2)
