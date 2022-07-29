d8eb3d8dd601a19fdc6a7d4b9b1c2982ff4df338
AlexeyMilkh <107917572+AlexeyMilkh@users.noreply.github.com>
2022-07-29 16:38:12 +0300

    Update 01_Money_result.py

--- Module3/home_work/01_Money_result.py
+++ Module3/home_work/01_Money_result.py
@@ -1,3 +1,32 @@
 class Money:
-    pass
-    # TODO: your code here
+    
+    def __init__(self, rubles, penny):
+        self.rubles = rubles + penny // 100
+        self.penny = penny % 100
+
+    def __str__(self):
+        return f'{str(self.rubles) + " руб"} {str(self.penny) + " коп"}'
+
+    def __add__(self, other_money):
+        return Money(self.rubles + other_money.rubles, self.penny + other_money.penny)
+
+    def __sub__(self, other_money):
+        return Money(self.rubles - other_money.rubles, self.penny - other_money.penny)
+
+    def __mul__(self, other_int):
+        return Money(self.rubles * other_int, self.penny * other_int)
+
+    def __gt__(self, other_money):
+        return self.rubles > other_money.rubles
+
+    def __lt__(self, other_money):
+        return self.rubles < other_money.rubles
+
+    def __eq__(self, other_money):
+        return self.rubles == other_money.rubles
+
+    def __ne__(self, other_money):
+        return self.rubles != other_money.rubles
+
+    def __mod__(self, other_int):
+        return Money(round(self.rubles * (other_int / 100)), round(self.penny * (other_int/100)) + self.rubles % 100)
