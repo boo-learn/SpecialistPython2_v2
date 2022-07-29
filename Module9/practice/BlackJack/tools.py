fb025ab5a9d803cba33c3d2eb80b0e48f026779b
SergeySbornikov <70229550+SergeySbornikov@users.noreply.github.com>
2022-07-29 16:20:30 +0300

    Update tools.py

--- Module9/practice/BlackJack/tools.py
+++ Module9/practice/BlackJack/tools.py
@@ -4,12 +4,21 @@ def sum_points(cards):
     :param cards: список карт(рука игрока или диллера)
     :return: сумму очков
     """
-    # Совет: храните кол-во очков за карту внутри класса Колоды(колода "знает", сколько дает очков каждая карта)
 
-    #  Сначала считаем сумму карт, считая ТУЗ за 11-очков
-    sum_points = ...
-    # Если сумма > 21, то перечитываем сумму, считая ТУЗ за 1(единицу)
-    if sum_points > 21:
-        ...
+    def calc_points(points, cards):
+        sum_points = 0
+        have_ace = False
+        for card in cards:
+            sum_points += points[card.value]
+            if card.value == "A":
+                have_ace = True
+        return sum_points, have_ace
 
+    points = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10,
+              "K": 10, "A": 11}
+    sum_points, have_ace = calc_points(points, cards)
+
+    if sum_points > 21 and have_ace:
+        points["A"] = 1
+        sum_points = calc_points(points, cards)
     return sum_points
