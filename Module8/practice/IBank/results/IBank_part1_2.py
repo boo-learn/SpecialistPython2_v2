30b4e0b71b4ab9a2b062fabd525964fb929ff60f
vln-oss <109944033+vln-oss@users.noreply.github.com>
2022-07-29 12:30:36 +0300

    Update IBank_part1_2.py

--- Module8/practice/IBank/results/IBank_part1_2.py
+++ Module8/practice/IBank/results/IBank_part1_2.py
@@ -3,39 +3,37 @@ class Account:
         self.name = name
         self.passport = passport
         self.phone_number = phone_number
-        self.__balance = start_balance  # Закрываем прямой доступ к балансу
+        self.balance = start_balance
 
-    # TODO: совместно с преподавателем реализуйте getter для просмотра баланса
-    #  Можете попробовать самостоятельно: https://pythobyte.com/using-getters-and-setters-in-python-5205-840ed13f/
+    def full_info(self):
+        """
+        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
+        """
+        return f"{self.name} баланс: {self.balance} руб. паспорт: {self.passport} т. {self.phone_number}"
+
+    def __repr__(self):
+        """
+        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.balance} руб."
 
     def deposit(self, amount):
         """
         Внесение суммы на текущий счет
         :param amount: сумма
         """
-        pass
+        self.balance += amount
 
     def withdraw(self, amount):
         """
         Снятие суммы с текущего счета
         :param amount: сумма
         """
-        pass
+        if amount > self.balance:
+            raise ValueError(f"Сумма списания ({amount}) превышает остаток на счете {self.balance}")
+        self.balance -= amount
 
-    def full_info(self):
-        """
-        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
-        """
-        return f"..."
-
-    def __repr__(self):
-        """
-        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
-        """
-        return f"..."
-
-
-# Создаем тестовый аккаунт:
+#Создаем тестовый аккаунт:
 account1 = Account("Алексей", "+7-901-744-22-99", "3232 456124", start_balance=500)
 
 # Смотрим баланс:
