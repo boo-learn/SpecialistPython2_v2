d51c5756d3f845a0f14540c0dae7fecbd2b52b31
AlexeyMilkh <107917572+AlexeyMilkh@users.noreply.github.com>
2022-07-29 21:15:59 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -4,3 +4,9 @@
 
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
 
+numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
+sort_choice(numbers)
+print(numbers)
+numbers = numbers[::-1]
+print(numbers)
+print(sum(numbers[:5]))
