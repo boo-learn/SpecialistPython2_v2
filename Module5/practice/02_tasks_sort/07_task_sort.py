9d701fb193b9bceff4bcdae31aa8c3b91cc3e357
ngospodarik <109979798+ngospodarik@users.noreply.github.com>
2022-08-01 20:15:31 +0300

    Update 07_task_sort.py

--- Module5/practice/02_tasks_sort/07_task_sort.py
+++ Module5/practice/02_tasks_sort/07_task_sort.py
@@ -11,3 +11,15 @@
 # 10 1 3 4 3 5 6 7 7 6 1
 # Выход
 # 5
+winners = [1, 3, 4, 3, 5, 6, 7, 7, 6, 1]
+
+winners_set = set(winners)
+winners_sorted = sorted(winners_set, reverse=True)
+
+
+count = 0
+for winner in winners:
+    if winner in list(winners_sorted)[:3]:
+        count += 1
+
+print(count)
