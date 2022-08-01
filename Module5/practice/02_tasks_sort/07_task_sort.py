fd8497e8a3d8fd208cd2b4f5d84aec8b963eb90b
ASidorovskaya <asidorovskaya@mail.ru>
2022-08-01 22:52:04 +0300

    Update 07_task_sort.py

--- Module5/practice/02_tasks_sort/07_task_sort.py
+++ Module5/practice/02_tasks_sort/07_task_sort.py
@@ -1,13 +1,25 @@
-# Призеры олимпиады
-# По результатам олимпиады участники награждаются дипломами.
-# Набравшие одинаковые  баллы  получают дипломы  одинаковой степени.
-# Призером олимпиады считается участник, получивший диплом  не хуже III степени.
-# По результатам олимпиады определите количество призеров.
-# Вход: натуральное число участников(N < 100) и далее N натуральных# чисел – результаты участников.
-# Выход: одно число – число призеров.
-# Пример:
-# Вход
-#
-# 10 1 3 4 3 5 6 7 7 6 1
-# Выход
-# 5
+result_list = [7, 7, 7, 4, 5, 6, 6, 7, 1, 8]
+
+result_list.sort()
+result_list.reverse()
+
+print(result_list)
+
+i = 0
+j = 0
+res = 0
+
+while i < 3:
+    j = 0
+    while j < len(result_list):
+        if j == len(result_list) - 1:
+            res = res + result_list.count(result_list[j])
+            result_list = []
+        elif result_list[j] > result_list[j + 1]:
+            res = res + result_list.count(result_list[j])
+            result_list = result_list[j + 1:]
+            break
+        j += 1
+    i += 1
+
+print(f"Total price holders = {res}")
