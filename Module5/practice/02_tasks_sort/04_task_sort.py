b8edfe81d838adc399261691cd4de5320c0fb4c1
Scherbakova Natalya <19natasha94@bk.ru>
2022-08-01 18:47:31 +0300

    Update 04_task_sort.py

--- Module5/practice/02_tasks_sort/04_task_sort.py
+++ Module5/practice/02_tasks_sort/04_task_sort.py
@@ -1,7 +1,36 @@
-# Сумма наибольших по модулю
-# Дан массив(список) чисел.
-# Найти: сумму 5-ти самых больших элементов по модулю.
-# Пояснение: сравниваем элементы по модулю, а в сумму добавляем сами значения элементов(без модуля)
-# В примере ниже, два самых больших по модулю числа это: -22.4 и 21.1. Они самые большие по модулю, а их сумма = -1.3
+def sort_choice(nums, reserved=False):
+    i = 0
+    while i < len(nums) - 1:
+        m = i
+        j = i + 1
+        while j < len(nums):
+            if reserved:
+                condition = nums[j] > nums[m]
+            else:
+                condition = nums[j] < nums[m]
+            if condition:
+                m = j
+            j += 1
+        nums[i], nums[m] = nums[m], nums[i]
+        i += 1
+
 
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, 21.1, 0]
+
+new_numbers = []
+
+for num in numbers:
+    new_numbers.append(abs(num))
+
+sort_choice(new_numbers)
+
+for i in range(len(numbers)):
+    if numbers[i] < 0:
+        index = new_numbers.index((numbers[i] * -1))
+        new_numbers[index] = numbers[i]
+
+sum_num = 0
+for num in new_numbers[-5:]:
+    sum_num += num
+
+print(f'Сумма самых максимальных по модулю чисел списка {new_numbers[-5::]}: {round(sum_num, 1)}')
