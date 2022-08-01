c2afdbcf927f9c31a3034b332fc776b1aa5d0acd
AlexeyMilkh <107917572+AlexeyMilkh@users.noreply.github.com>
2022-08-01 13:16:27 +0300

    Update 02_Vector_result.py

--- Module3/home_work/02_Vector_result.py
+++ Module3/home_work/02_Vector_result.py
@@ -2,4 +2,15 @@
 # Само задание в файле 01_Vector_task.md
 
 class Vector:
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other_vector):
+        return self.x + other_vector.x,  self.y + other_vector.y
+
+    def __sub__(self, other_vector):
+        return self.x - other_vector.x,  self.y - other_vector.y
+
+    def __mul__(self, scalar):
+        return self.x * scalar, self.y * scalar
