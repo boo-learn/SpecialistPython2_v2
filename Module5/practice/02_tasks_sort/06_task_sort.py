c1199f735c9656f24b86a663f94559f9f5d3212b
AlexeyMilkh <107917572+AlexeyMilkh@users.noreply.github.com>
2022-08-01 20:14:49 +0300

    Update 06_task_sort.py

--- Module5/practice/02_tasks_sort/06_task_sort.py
+++ Module5/practice/02_tasks_sort/06_task_sort.py
@@ -13,3 +13,13 @@
 # 70
 # Пояснение:
 # Возможен такой порядок: 10 2 50 1 10
+
+ValueIn = [2, 1, 10, 50, 10]
+N = len(ValueIn) + 1
+ValueIn.sort(reverse=True)
+print(ValueIn)
+result = 0
+for value in ValueIn:
+    result = sum(ValueIn[:round(N/2)])
+
+print(result)
