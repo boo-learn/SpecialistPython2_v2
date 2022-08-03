867949e745fdb14eb722675786a29275067c2834
ASidorovskaya <asidorovskaya@mail.ru>
2022-08-03 19:55:31 +0300

    Update IBank_part1_2.py

--- Module8/practice/IBank/IBank_part1_2.py
+++ Module8/practice/IBank/IBank_part1_2.py
@@ -3,43 +3,37 @@ class Account:
         self.name = name
         self.passport = passport
         self.phone_number = phone_number
-        # self.balance = start_balance
         self.__balance = start_balance  # TODO: Закрываем прямой доступ к балансу
 
     def full_info(self) -> str:
         """
         Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
         """
-        return f"..."
+        return f"{self.name} баланс: {self.__balance} руб. паспорт: {self.passport} т.{self.phone_number}"
 
     def __repr__(self) -> str:
         """
         :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
         """
-        return f"..."
+        return f"{self.name} баланс: {self.__balance} руб."
 
-    # TODO: совместно с преподавателем реализуйте getter для просмотра баланса
-    #  Можете попробовать самостоятельно: https://pythobyte.com/using-getters-and-setters-in-python-5205-840ed13f/
-    @property
     def balance(self) -> int:
-        return ...
+        return self.__balance
+
 
     def deposit(self, amount: int) -> None:
-        """
-        Внесение суммы на текущий счет
-        :param amount: сумма
-        """
-        pass
+        return self.__balance + amount
+
 
     def withdraw(self, amount: int) -> None:
-        """
-        Снятие суммы с текущего счета
-        :param amount: сумма
-        """
-        pass
+        if self.__balance < amount:
+            print("На счете недостаточно денег")
+        else:
+            return self.__balance - amount
+
+    # Создаем тестовый аккаунт:
 
 
-# Создаем тестовый аккаунт:
 account1 = Account("Алексей", "+7-901-744-22-99", "3232 456124", start_balance=500)
 
 # Смотрим баланс:
