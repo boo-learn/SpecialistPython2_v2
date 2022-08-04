c93b1052062975bbe01ec04acbaadc1947ac5f26
ngospodarik <109979798+ngospodarik@users.noreply.github.com>
2022-08-04 20:50:22 +0300

    Update IBank_part3.py

--- Module8/practice/IBank/IBank_part3.py
+++ Module8/practice/IBank/IBank_part3.py
@@ -1,10 +1,104 @@
-# Сюда отправляем готовое решение IBank часть-3
+from typing import List
+
+# класс для хранение информации об операциях
 class Operation:
-    # TODO: сюда копируем реализацию класса Operation из предыдущей задачи
-    pass
+    # Типы операций храним в свойствах класса
+    DEPOSIT = 'Пополнение'
+    WITHDRAW = 'Снятие'
+    TRANSFER_OUT = 'Перевод'
+    TRANSFER_IN = 'Поступление'
+
+    # Напоминаю: обращение к этим переменным происходит через имя класса, пример: Operation.WITHDRAW
+
+    def __init__(self, amount: int, oper_type: str, target_account = None, commission = None):
+        self.type = oper_type
+        self.amount = amount
+        self.target_account = target_account
+        self.commission = commission
+
+    def __repr__(self) -> None:
+        """
+        :return: возвращает строковое представление операции. Формат указан в 02_IBank_part2.md
+        """
+        if self.type == Operation.DEPOSIT:
+            return f'{self.type} {self.amount} руб.'
+        if self.type == Operation.WITHDRAW:
+            return f'{self.type} {self.amount} руб. (комиссия {self.commission * self.amount} руб.)'
+        elif self.type == Operation.TRANSFER_IN:
+            return f'{self.type} {self.amount} руб. со счета клиента: {self.target_account.name}'
+        else:
+            return f'{self.type} {self.amount} руб. на счет клиента: {self.target_account.name} (комиссия {self.commission * self.amount} руб.)'
 
 
 class Account:
     # TODO: сюда копируем реализацию класса Account из предыдущей задачи
-    pass
+    # TODO: историю храним как список объектов класса Operation, добавив свойство в конструктор:
+    #   self.__history = []
 
+    def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
+        self.name = name
+        self.passport = passport
+        self.phone_number = phone_number
+        self.__balance = start_balance
+        self.__history = []
+        self.commission = 0.02
+
+    def full_info(self) -> str:
+        """
+        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
+        """
+        return f"{self.name} баланс: {self.balance} руб. паспорт: {self.passport} т.{self.phone_number}"
+
+    def __repr__(self) -> str:
+        """
+        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.balance} руб."
+
+    @property
+    def balance(self) -> int:
+        return self.__balance
+
+    # Данный метод дан в готовом виде. Изучите его и используйте как пример, для доработки других
+    def deposit(self, amount: int, to_history: bool = True) -> None:
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        :param to_history: True - записывать операцию в историю, False - не записывать
+        """
+        self.__balance += amount
+        if to_history:
+            self.__history.append(Operation(amount, Operation.DEPOSIT))
+
+    def withdraw(self, amount: int, to_history: bool = True) -> None:
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        """
+
+        withdraw_amount = int(amount * (1 + self.commission))
+
+        if self.__balance < withdraw_amount:
+            raise ValueError("Недостаточно средств на счете. Операция отклонена")
+        self.__balance -= withdraw_amount
+        if to_history:
+            self.__history.append(Operation(amount, Operation.WITHDRAW, self.commission))
+
+    def transfer(self, target_account: 'Account', amount: int, to_history: bool = True) -> None:
+        """
+        Перевод денег на счет другого клиента
+        :param target_account: счет клиента для перевода
+        :param amount: сумма перевода
+        :return:
+        """
+        self.withdraw(amount, to_history=False)
+        target_account.deposit(amount, to_history=False)
+        if to_history:
+            self.__history.append(Operation(amount, Operation.TRANSFER_OUT, target_account, self.commission))
+            target_account.__history.append(Operation(amount, Operation.TRANSFER_IN, self, self.commission))
+
+    def get_history(self) -> List[str]:
+        """
+        :return: возвращаем историю операций  виде списка строковых представлений
+        """
+        return [str(operation) for operation in self.__history]
