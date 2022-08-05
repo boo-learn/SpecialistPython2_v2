400683ce965b741d50b9b3179895034d7ec59dd5
Skandy-git <61092155+Skandy-git@users.noreply.github.com>
2022-08-05 20:33:35 +0300

    Update tools.py

--- Module9/practice/BlackJack/tools.py
+++ Module9/practice/BlackJack/tools.py
@@ -4,12 +4,33 @@ def sum_points(cards):
     :param cards: список карт(рука игрока или диллера)
     :return: сумму очков
     """
-    # Совет: храните кол-во очков за карту внутри класса Колоды(колода "знает", сколько дает очков каждая карта)
+    card_points = {
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "10": 10,
+        "J": 10,
+        "Q": 10,
+        "K": 10,
+        "A": 11
+    }
 
     #  Сначала считаем сумму карт, считая ТУЗ за 11-очков
-    sum_points = ...
+
+    sum_points = 0
+
+    for card in cards:
+        sum_points += card_points[card]
     # Если сумма > 21, то перечитываем сумму, считая ТУЗ за 1(единицу)
-    if sum_points > 21:
-        ...
+    if sum_points > 21 and 'A' in cards:
+        sum_points = 0
+        card_points['A'] = 1
+        for card in cards:
+            sum_points += card_points[card]
 
     return sum_points
