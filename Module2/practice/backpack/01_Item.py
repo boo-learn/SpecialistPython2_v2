083b74132512354c81eb60598e9a95827779e66e
VladislavLoseff <71550680+VladislavLoseff@users.noreply.github.com>
2022-08-15 16:08:41 +0300

    Update 01_Item.py

--- Module2/practice/backpack/01_Item.py
+++ Module2/practice/backpack/01_Item.py
@@ -1,8 +1,11 @@
 class Item:
-    def __init__(self, ):
-        self.name = ...  # Название предмета
-        self.weight = ...  # Вес предмета, в килограммах
-        self.cost = ...  # Цена предмета, пусть будет, в рублях
+    def __init__(self, name, weight, cost):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
+
+    def show(self):
+        return f" {self.name} вес:{self.weight} цена:{self.cost}"
 
 
 def show_item(item: Item):
@@ -25,13 +28,16 @@ print(show_item(item3))
 print(show_item(item4))
 
 # TODO-3: сделайте функцию show_item(), методом show() класса Item
-# print(item1.show())
-# print(item2.show())
-# print(item3.show())
-# print(item4.show())
+print(item1.show())
+print(item2.show())
+print(item3.show())
+print(item4.show())
 
 # TODO-4: поместите все объекты item1, item2 ... itemN в список.
 #  Выведите элементы в виде нумерованного списка, при выводе используйте метод .show()
-items = []
-for item in items:
-    ...
+items = [Item("Гиря", 25, 500),
+         Item("Арбуз", 4, 300),
+         Item("Ноутбук", 2.5, 22500),
+         Item("Кот", 0.5, 250)]
+for item in range(len(items)):
+    print(items[item].show())
