f5ca26677095d5b56208e4031701615c2198aa68
Sergey Mikhaylenko <sergey.mikhaylenko@gmail.com>
2022-08-15 16:55:00 +0300

    Update 03_backpack_part2.py

--- Module2/practice/backpack/03_backpack_part2.py
+++ Module2/practice/backpack/03_backpack_part2.py
@@ -1,24 +1,57 @@
 class Item:
-    ...
-    # TODO: сюда копируем реализацию класса из предыдущего задания
+    def __init__(self, name,weight,cost):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
+
+    def show(self):
+        """
+        Возвращает строковое представление объекта Item
+        """
+        return f"{self.name} вес:{self.weight} цена:{self.cost}"
 
 
 class BackPack:  # рюкзак
-    ...
-    # TODO: сюда копируем реализацию класса из предыдущего задания
-    # TODO: добавьте новое свойство .max_weight - максимальный суммарный вес предметов, которые можно положить в рюкзак
+    def __init__(self,max_weight):
+        self.items = []  # Предметы, которые хранятся в рюкзаке
+        self.max_weight = max_weight #максимальный суммарный вес предметов, которые можно положить в рюкзак
 
+    def add_item(self, it: Item):
+        """
+        Добавляет предмет(item) в этот рюкзак
+        """
+        self.items.append(it)
+        #print(it.show())
+
+    def show_items(self):
+        """
+        Вывод все предметы, содержащиеся в рюкзаке в виде нумерованного списка
+        """
+        #print(self.items)
+        for it in self.items:
+            #print(it.name)
+            print(it.show())
+
+    # TODO: добавьте новое свойство .max_weight - максимальный суммарный вес предметов, которые можно положить в рюкзак
     def sum_weight(self):
         """
         Возвращает суммарный вес всех предметов в рюкзаке
         """
-        # TODO: реализуйте метод
+        res_sum=0
+        for it in self.items:
+            #print(it.name)
+            res_sum=res_sum + it.weight
+        return res_sum
 
     def sum_cost(self):
         """
         Возвращает суммарную стоимость всех предметов в рюкзаке
         """
-        # TODO: реализуйте метод
+        res_cost=0
+        for it in self.items:
+            #print(it.name)
+            res_cost=res_cost + it.cost
+        return res_cost
 
 
 # Создаем предметы
@@ -41,3 +74,6 @@ backpack.add_item(item4)
 
 # Выводим все предметы в рюкзаке
 backpack.show_items()
+
+print(backpack.sum_weight())
+print(backpack.sum_cost())
