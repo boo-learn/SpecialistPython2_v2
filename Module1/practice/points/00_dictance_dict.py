6b8de4dadf683c282a2f05b81a747870774e75f4
Degtyarskiy <111269758+Degtyarskiy@users.noreply.github.com>
2022-08-15 11:39:23 +0300

    Update 00_dictance_dict.py

--- Module1/practice/points/00_dictance_dict.py
+++ Module1/practice/points/00_dictance_dict.py
@@ -1,13 +1,17 @@
+from math import sqrt
+
+
 def distance(p1, p2):
     """
     Расстояние между двумя точками
     """
-    # TODO: напишите тело функции
-
+    
+    dist_1 = sqrt((p2["x"] - p1["x"]) ** 2 + (p2["y"] - p1["y"]) ** 2)
+    return dist_1
 
 # Даны две точки на координатной плоскости
 point1 = {"x": 2, "y": 5}
-point2 = {"x": -2, "y": 4}
+point2 = {"x": 3, "y": 4}
 
 dist = distance(point1, point2)
 
