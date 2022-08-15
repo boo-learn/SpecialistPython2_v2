b546cd949ed37ae0453899d562d567167b0b8af0
Talivad <111274061+Talivad@users.noreply.github.com>
2022-08-15 13:30:47 +0300

    Update 05_line.py

--- Module1/practice/points/05_line.py
+++ Module1/practice/points/05_line.py
@@ -3,10 +3,18 @@ class Point:
         self.x = x
         self.y = y
 
+    def dist_to(self, other_point):
+        # TODO-1: реализуйте метод
+        # Подсказка: смотри пример, Module1/examples/06_object_methods.py
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
+
 
 # Ломаная линия задана произвольным количеством последовательных точек
 points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 
-# TODO: Найдите длину ломаной линии
+dist = 0
 
-print("Длина ломаной линии = ", ...)
+for point in range(len(points) - 1):
+    dist += points[point].dist_to(points[point + 1])
+
+print("Длина ломаной линии = ", dist)
