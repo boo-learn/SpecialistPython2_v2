b75a7208661a54f01c0f8a1ca2504f5e447f39c9
EdenHazard8 <97665053+EdenHazard8@users.noreply.github.com>
2022-08-15 15:20:49 +0300

    Update 00_people.py

--- Module1/practice/peoples/00_people.py
+++ Module1/practice/peoples/00_people.py
@@ -2,34 +2,35 @@ class People:
     def __init__(self, name, surname, age):
         self.name = name
         self.surname = surname
-        self.age = age
+        self.__age = age
+
+    def change_age(self, new_age):
+        if type(new_age) == int and 0 < new_age < 101:
+            self.__age = new_age
+        else:
+            print("некорректное значение для возраста")
 
     def full_name(self):
         return f"{self.surname} {self.name}"
+
+    def full_info(self):
+        return f"Человек: {self.surname} {self.name} и ему {self.__age} лет"
 
 
 # Создадим двух человек:
 people1 = People("Иван", "Уткин", 27)
 people2 = People("Алексей", "Перов", 35)
+people3 = People("Василий", "Быстров", 65)
 
-# Определим кто старше:
-if people1.age < people2.age:
-    print(f"{people1.full_name()} младше {people2.full_name()}")
-elif people1.age > people2.age:
-    print(f"{people2.full_name()} младше {people1.full_name()}")
-else:
-    print(f"{people1.full_name()} и {people2.full_name()} одногодки")
+print(people1.full_info())
+print(people2.full_info())
+print(people3.full_info())
 
-print("Меняем возраст")
-people1.age = 45  # Меняем возраст первого человека
-people2.age = "hello"  # Меняем возраст второго, на некорректное значение
+print("Меняем возраст людей")
+people1.change_age(45)
+people2.change_age("help")
+people2.change_age(-30)
 
-# Снова пытаемся определить кто старше:
-if people1.age < people2.age:  # И падаем с ошибкой...
-    print(f"{people1.full_name()} младше {people2.full_name()}")
-elif people1.age > people2.age:
-    print(f"{people2.full_name()} младше {people1.full_name()}")
-else:
-    print(f"{people1.full_name()} и {people2.full_name()} одногодки")
-
-# Как защититься от установки некорректных свойств?
+print(people1.full_info())
+print(people2.full_info())
+print(people3.full_info())
