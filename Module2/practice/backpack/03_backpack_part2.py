098c2ff2e8614f6a9cb4354a3bb1f06e3e92ded9
Andrey_Gorbi <agorbi@ya.ru>
2022-08-15 16:56:53 +0300

    Update 03_backpack_part2.py

--- Module2/practice/backpack/03_backpack_part2.py
+++ Module2/practice/backpack/03_backpack_part2.py
@@ -1,10 +1,37 @@
 class Item:
-    ...
+    def __init__(self, name, weight, cost):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
+
+    def show(self):
+        return f"{self.name} вес: {self.weight} цена: {self.cost}"
     # TODO: сюда копируем реализацию класса из предыдущего задания
 
 
 class BackPack:  # рюкзак
-    ...
+    def __init__(self, max_weight):
+        self.items = []  # Предметы, которые хранятся в рюкзаке
+        self.max_weight = max_weight
+
+    def add_item(self, item: Item):
+        """
+        Добавляет предмет(item) в этот рюкзак
+        """
+        # TODO: реализуйте метод
+        if item.weight > self.max_weight:
+            print(f"Предмет {item.name} слишком тяжелый")
+        else:
+            self.items.append(item)
+
+    def show_items(self):
+        """
+        Вывод все предметы, содержащиеся в рюкзаке в виде нумерованного списка
+        """
+        # TODO: реализуйте метод
+        for n, item in enumerate(self.items, 1):
+            print(n, item.show())
+
     # TODO: сюда копируем реализацию класса из предыдущего задания
     # TODO: добавьте новое свойство .max_weight - максимальный суммарный вес предметов, которые можно положить в рюкзак
 
@@ -13,12 +40,20 @@ class BackPack:  # рюкзак
         Возвращает суммарный вес всех предметов в рюкзаке
         """
         # TODO: реализуйте метод
+        sum_weight = 0
+        for item in self.items:
+            sum_weight += item.weight
+        print(sum_weight)
 
     def sum_cost(self):
         """
         Возвращает суммарную стоимость всех предметов в рюкзаке
         """
         # TODO: реализуйте метод
+        sum_cost = 0
+        for item in self.items:
+            sum_cost += item.cost
+        print(sum_cost)
 
 
 # Создаем предметы
@@ -41,3 +76,5 @@ backpack.add_item(item4)
 
 # Выводим все предметы в рюкзаке
 backpack.show_items()
+backpack.sum_weight()
+backpack.sum_cost()
