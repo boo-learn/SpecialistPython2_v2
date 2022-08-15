d076f47d37768537e4eaff58781297e10dc89ba3
EdenHazard8 <97665053+EdenHazard8@users.noreply.github.com>
2022-08-15 13:04:58 +0300

    Update 04_long_points.py

--- Module1/practice/points/04_long_points.py
+++ Module1/practice/points/04_long_points.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -7,6 +10,19 @@ class Point:
 # Дан список из произвольного количества точек:
 points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)]
 
-# TODO: найдите точку наиболее удаленную от начала координат и выведите ее координаты
 
-print("Координаты наиболее удаленной точки = ", ...)
+def dist_to(points):
+    list_points = []
+
+    for point in points:
+        list_points.append(math.sqrt(((0 - point.x) ** 2) + ((0 - point.y) ** 2)))
+    return list_points
+
+
+# TODO: найдите точку наиболее удаленную от начала координат и выведите ее координаты
+def main():
+    print("Координаты наиболее удаленной точки = ", max(dist_to(points)))
+
+
+if __name__ == '__main__':
+    main()
