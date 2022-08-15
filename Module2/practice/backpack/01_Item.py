e87a11126b3e63c9edbf6b435f1a37f2815af6bf
Sergey Mikhaylenko <sergey.mikhaylenko@gmail.com>
2022-08-15 16:08:47 +0300

    Update 01_Item.py

--- Module2/practice/backpack/01_Item.py
+++ Module2/practice/backpack/01_Item.py
@@ -1,16 +1,17 @@
+from ast import Pass
+
+
 class Item:
-    def __init__(self, ):
-        self.name = ...  # Название предмета
-        self.weight = ...  # Вес предмета, в килограммах
-        self.cost = ...  # Цена предмета, пусть будет, в рублях
+    def __init__(self, name,weight,cost):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
 
-
-def show_item(item: Item):
-    """
-    Возвращает строковое представление объекта Item
-    """
-    return f"{item.name} вес:{item.weight} цена:{item.cost}"
-
+    def show(self):
+        """
+        Возвращает строковое представление объекта Item
+        """
+        return f"{self.name} вес:{self.weight} цена:{self.cost}"
 
 # TODO-1: Дополните конструктор класса Item
 item1 = Item("Гиря", 25, 500)
@@ -19,10 +20,10 @@ item3 = Item("Ноутбук", 2.5, 22500)
 item4 = Item("Кот", 0.5, 250)
 
 # TODO-2: запустите программу, посмотрите результаты функции show_item()
-print(show_item(item1))
-print(show_item(item2))
-print(show_item(item3))
-print(show_item(item4))
+print(item1.show())
+print(item2.show())
+print(item3.show())
+print(item4.show())
 
 # TODO-3: сделайте функцию show_item(), методом show() класса Item
 # print(item1.show())
@@ -32,6 +33,6 @@ print(show_item(item4))
 
 # TODO-4: поместите все объекты item1, item2 ... itemN в список.
 #  Выведите элементы в виде нумерованного списка, при выводе используйте метод .show()
-items = []
+items = [item1,item2,item3,item4]
 for item in items:
-    ...
+    item.show()
