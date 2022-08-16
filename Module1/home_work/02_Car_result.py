f4b3c71652bda3746bc4b297d398ed0176414b58
Steelrat1997 <kondrashov.konstantin@mail.ru>
2022-08-16 04:21:42 +0300

    Update 02_Car_result.py
    Done

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -1,5 +1,38 @@
-# Сюда отправляем решение задачи "Автомобиль"
-# Само задание в файле 02_Сar_task.md
-
 class Car:
-    pass
+    def __init__(self, gas, capacity, gas_per_km, mileage = 0):
+        self.gas = gas
+        self.capacity = capacity
+        self.gas_per_km = gas_per_km
+        self.mileage = 0
+    
+    def fill(self, gas_added):
+        if self.gas + gas_added <= self.capacity:
+            self.gas += gas_added
+        else:
+            excess_gas = self.gas + gas_added - self.capacity
+            print(f"Лишние литры - {excess_gas} ")
+            self.gas = self.capacity
+    
+    def ride(self, path_length):
+        gas_needs = path_length * self.gas_per_km
+        if gas_needs <= self.gas:
+            self.gas -= gas_needs
+            self.mileage += path_length
+            print(f"Проехали {path_length} километров")
+        else:
+            print(f"Проехали {self.gas / self.gas_per_km} километров\n")
+            self.mileage += self.gas / self.gas_per_km
+            self.gas = 0
+    
+    def show(self):
+        return f"Остаток бензина {self.gas}л     Вместимость бака {self.capacity}л     Расход {self.gas_per_km}л/км     Пробег {self.mileage}км\n"
+            
+
+print()
+car1 = Car(60, 100, 0.5)
+
+car1.fill(55)  # залили 5 литров
+print(car1.show())
+
+car1.ride(100)
+print(car1.show())
