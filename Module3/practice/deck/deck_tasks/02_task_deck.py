8f31d0b669d4fc7ede3471ec1b2ee73570fa0383
EdenHazard8 <97665053+EdenHazard8@users.noreply.github.com>
2022-08-16 16:54:41 +0300

    Update 02_task_deck.py

--- Module3/practice/deck/deck_tasks/02_task_deck.py
+++ Module3/practice/deck/deck_tasks/02_task_deck.py
@@ -1,4 +1,22 @@
 from deck_total import Card, Deck
+from collections import Counter
 
 # TODO: Создайте колоду из 52 карт. Перемешайте ее. Вытяните 10 карт сверху
 #   и посчитайте карт какой/каких мастей среди вытянутых карт оказалось больше всего?
+
+# Создаем колоду
+deck = Deck()
+# Тусуем колоду
+deck.shuffle()
+print(deck)
+# Берем две карты из колоды
+
+def suit_count():
+    suits = []
+    cards = deck.draw(10)
+    for card in cards:
+        suits.append(card.suit)
+    counter = Counter(suits)
+    return max(counter, key=counter.get)
+
+print(suit_count())
