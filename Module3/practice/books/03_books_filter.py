c79b3f35596a7ef83a3c4dc174fb232ca51b4c12
VladislavLoseff <71550680+VladislavLoseff@users.noreply.github.com>
2022-08-16 11:32:59 +0300

    Update 03_books_filter.py

--- Module3/practice/books/03_books_filter.py
+++ Module3/practice/books/03_books_filter.py
@@ -4,8 +4,7 @@ class Author:
         self.surname = surname
 
     def short_name(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return f"  {self.name[0]}. {self.surname} "
 
 
 class Book:
@@ -16,8 +15,7 @@ class Book:
         self.pages = pages
 
     def to_str(self):
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return f' "{self.name}" author:{self.author.short_name()} publish:{self.year} год'
 
 
 author1 = Author("Михаил", "Булгаков")
@@ -32,5 +30,13 @@ books_catalog = [
 ]
 print("Найти все книги по фамилии автора")
 surname = input("Фамилия автора: ")
-# TODO-1: Выведите нумерованный список книг автора с фамилией surname
-#  примечание: если книг для автора с указанной фамилией нет - выведите "Книги не найдены"
+n = 0
+for book in books_catalog:
+    if book.author.surname == surname:
+        print(book.to_str())
+        n += 1
+
+
+if n == 0:
+    print('нет книг такого автора')
+
