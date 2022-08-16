61961251da2963ca4f66b6d937d4607cbba21ca9
Steelrat1997 <kondrashov.konstantin@mail.ru>
2022-08-16 22:43:47 +0300

    Update 06_deck_part2.py
    Done

--- Module3/practice/deck/06_deck_part2.py
+++ Module3/practice/deck/06_deck_part2.py
@@ -1,41 +1,62 @@
-class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+import random
 
+class Card:
+    DIAMONDS = 'Diamonds'
+    HEARTS = 'Hearts'
+    SPADES = 'Spades'
+    CLUBS = 'Clubs'
+        
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        suits = { 
+            Card.DIAMONDS : '\u2666',
+            Card.HEARTS : '\u2665',
+            Card.SPADES : '\u2660',
+            Card.CLUBS : '\u2663'
+            }
+        return self.value + suits[self.suit]
+
+    def equal_suit(self, other_card):
+        if self.suit == other_card.suit:
+            return True
+        else:
+            return False
 
 class Deck:
     def __init__(self):
         # Список карт в колоде. Каждым элементом списка будет объект класса Card
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-        self.cards = []
-        # TODO-0: конструктор копируем из предыдущей задачи
+        self.cards = [Card(value, suit) for suit in suits for value in values]
 
     def show(self):
-        # TODO-0: копируем из предыдущей задачи
-        ...
+        print(f"Deck[{len(self.cards)}]: ", end = '')
+        print(*[card.to_str() for card in self.cards], sep = ', ')
 
     def draw(self, x):
-        # TODO-1: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+        drawed_cards = self.cards[:x]
+        self.cards = self.cards[x:]
+        return drawed_cards
 
     def shuffle(self):
-        # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
-        #   Подсказка: https://www.w3schools.com/python/ref_random_shuffle.asp
-        ...
+        random.shuffle(self.cards)
+
 
 
 # Создаем колоду
 deck = Deck()
 # Выводим колоду в формате указанном в основном задании
-print(deck.show())
+deck.show()
 # Тусуем колоду
 deck.shuffle()
-print(deck.show())
+deck.show()
 
 # Возьмем 5 карт "в руку"
 hand = deck.draw(5)
 # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
-print(deck.show())
+deck.show()
 # Выводим список карт "в руке"(список hand)
-print(...)
+print(f"hand[{len(hand)}]:" + ' '.join([card.to_str() for card in hand]))
