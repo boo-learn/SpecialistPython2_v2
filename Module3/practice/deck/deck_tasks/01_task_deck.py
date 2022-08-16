f644ec29bf4539fa003a03a2e77c97184982e67f
Sergey Mikhaylenko <sergey.mikhaylenko@gmail.com>
2022-08-16 16:34:30 +0300

    Update 01_task_deck.py

--- Module3/practice/deck/deck_tasks/01_task_deck.py
+++ Module3/practice/deck/deck_tasks/01_task_deck.py
@@ -4,3 +4,16 @@ from deck_total import Card, Deck
 
 # TODO: Создайте колоду из 52 карт. Перемешайте ее. Вытяните две карты сверху.
 #   Сравните эти карты и выведите сообщение формата: “карта A♦ больше J♣”
+
+deck = Deck()
+deck.shuffle()
+# TODO-final: реализовать нативную работу с объектами:
+# 1. Вывод колоды в терминал:
+print(deck)  # вместо print(deck.show())
+
+card1, card2 = deck.draw(2)
+
+if card1 > card2: # card1.__gt__(card2)
+    print(f"{card1} больше {card2}")
+else:
+    print(f"{card1} меньше {card2}")
