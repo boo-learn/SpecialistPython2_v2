67fa33c2db468a2545637657348692b3693da963
Steelrat1997 <kondrashov.konstantin@mail.ru>
2022-08-16 20:55:13 +0300

    Update 03_cards_list.py
    Done

--- Module3/practice/deck/03_cards_list.py
+++ Module3/practice/deck/03_cards_list.py
@@ -1,19 +1,38 @@
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    DIAMONDS = 'Diamonds'
+    HEARTS = 'Hearts'
+    SPADES = 'Spades'
+    CLUBS = 'Clubs'
+        
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
 
+    def to_str(self):
+        suits = { 
+            Card.DIAMONDS : '\u2666',
+            Card.HEARTS : '\u2665',
+            Card.SPADES : '\u2660',
+            Card.CLUBS : '\u2663'
+            }
+        return self.value + suits[self.suit]
+
+    def equal_suit(self, other_card):
+        if self.suit == other_card.suit:
+            return True
+        else:
+            return False
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 hearts_cards = []
-# TODO-1: добавьте в список hearts_cards все червовые карты(от 2-ки до туза)
-
 diamonds_cards = []
-# TODO-2: добавьте в список diamonds_cards все бубновые карты(от туза до 2-ки)
 
-# TODO-3: выведите все карты из списка hearts_cards в терминал через запятую в одну строку:
-# Пример вывода: 2♥, 3♥, 4♥ ... A♥
+for value in values:
+    hearts_cards.append(Card(value, Card.HEARTS))
+    diamonds_cards.append(Card(value, Card.DIAMONDS))
 
-
+print(*[card.to_str() for card in hearts_cards], sep = ', ')
+    
 cards = [
     Card("2", "Hearts"),
     Card("3", "Hearts"),
