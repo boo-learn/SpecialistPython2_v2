322704dc1b479d0aa024c2b5f79a30d7f201f39c
kruglovdm <110811400+kruglovdm@users.noreply.github.com>
2022-08-16 12:08:12 +0300

    Update 01_card_to_str.py

--- Module3/practice/deck/01_card_to_str.py
+++ Module3/practice/deck/01_card_to_str.py
@@ -6,7 +6,24 @@ class Card:
 
     def to_str(self):
         # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
-        ...
+        res = f'{self.value}{self.to_suit()}'
+        return res
+
+    def to_suit(self):
+        if self.suit == 'Diamonds':
+            # print('\u2662', '\u2666')
+            res = '\u2666'
+        elif self.suit == 'Hearts':
+            # print('\u2661', '\u2665')
+            res = '\u2665'
+        elif self.suit == 'Spades ':
+            # print('\u2664', '\u2660')
+            res = '\u2660'
+        else: #Clubs
+            # print('\u2667', '\u2663')
+            res = '\u2663'
+
+        return res
 
 
 # Создадим несколько карт
