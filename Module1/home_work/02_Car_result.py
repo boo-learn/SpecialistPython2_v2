1a4a53cbc11cc062d6ab376e0a004c3fb364d24e
Degtyarskiy <111269758+Degtyarskiy@users.noreply.github.com>
2022-08-16 09:55:42 +0300

    Update 02_Car_result.py

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -1,5 +1,38 @@
-# Сюда отправляем решение задачи "Автомобиль"
-# Само задание в файле 02_Сar_task.md
-
 class Car:
-    pass
+    def __init__(self, gas, capicity, gas_per_km, total_ride):
+        self.gas = gas
+        self.capicity = capicity
+        self.gas_per_km = gas_per_km
+        self.total_ride = total_ride
+
+    # def show_gas(self):
+    #     print(self.gas)
+
+    # def show_total_ride(self):
+    #     print(self.total_ride)
+
+    def fill(self, f):
+        if f + self.gas <= self.capicity:
+            self.gas += f
+        else:            
+            print('Лишние литры: ', (f + self.gas) - self.capicity)
+            self.gas = self.capicity
+
+    def ride(self, r):
+        gas_consup = r / self.gas_per_km
+        if gas_consup <= self.gas:
+            print('Пройденное расстояние: ', r, ', ',  'расход: ', round(gas_consup, 1), sep = '')
+            self.total_ride += r
+        else:
+            print('Проехали всего: ', self.gas * self.gas_per_km)
+            self.total_ride += self.gas * self.gas_per_km
+  
+car1 = Car(10, 70, 7, 10)
+
+car1.fill(65)
+
+# car1.show_gas()
+
+car1.ride(700)
+
+# car1.show_total_ride()
