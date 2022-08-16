70ea2a563ef12d9de9bc15d5a691168db518666b
Andrey_Gorbi <agorbi@ya.ru>
2022-08-16 12:56:24 +0300

    Update 03_cards_list.py

--- Module3/practice/deck/03_cards_list.py
+++ Module3/practice/deck/03_cards_list.py
@@ -1,21 +1,27 @@
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
 
+    def to_str(self):
+        card_suits = {
+            "Diamonds": '\u2666',
+            "Hearts": '\u2665',
+            "Spades": '\u2663',
+            "Clubs": '\u2660'
+        }
+        return f"{self.value}{card_suits[self.suit]}"
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-hearts_cards = []
-# TODO-1: добавьте в список hearts_cards все червовые карты(от 2-ки до туза)
 
-diamonds_cards = []
-# TODO-2: добавьте в список diamonds_cards все бубновые карты(от туза до 2-ки)
+hearts_cards = [Card(value, "Hearts") for value in values]
+diamonds_cards = [Card(value, "Diamonds") for value in values[::-1]]
 
-# TODO-3: выведите все карты из списка hearts_cards в терминал через запятую в одну строку:
-# Пример вывода: 2♥, 3♥, 4♥ ... A♥
+hearts_str = [card.to_str() for card in hearts_cards]
+diamonds_str = [card.to_str() for card in diamonds_cards]
 
-
-cards = [
-    Card("2", "Hearts"),
-    Card("3", "Hearts"),
-    Card("4", "Hearts"),
-    ...]
+print(",".join(hearts_str))
+print(",".join(diamonds_str))
