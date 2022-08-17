aa52cf5a9f6b867e44407b710055535cd0f55a3c
VladislavLoseff <71550680+VladislavLoseff@users.noreply.github.com>
2022-08-17 12:58:26 +0300

    Update 01_task_sort.py

--- Module5/practice/02_tasks_sort/01_task_sort.py
+++ Module5/practice/02_tasks_sort/01_task_sort.py
@@ -1,6 +1,27 @@
+def sort_choice(nums):
+    i = 0
+    while i < len(nums) - 1:
+        m = i
+        j = i + 1
+        while j < len(nums):
+            if nums[j] < nums[m]:
+                m = j
+            j += 1
+        nums[i], nums[m] = nums[m], nums[i]
+        i += 1
+    return nums
+
+
 # Сумма с условием
 # Дан список чисел.
 # Найти: сумму элементов списка, больших данного числа a
 
 numbers = [2.5, 13.6, 13, -22.4, -12.8, 6.7, 12.8, 21, 4, -22.1]
-a = ...  # Задайте самостоятельно, выбрав произвольное число
+a = 10
+s = []
+for i in range(len(numbers)):
+    if numbers[i] > a:
+        s.append(numbers[i])
+
+print(sum(s))
+# Задайте самост
