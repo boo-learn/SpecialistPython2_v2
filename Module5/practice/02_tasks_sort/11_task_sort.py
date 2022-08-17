a5edc78c47c8452f764f2b2f7aec1afe7ad3c3a5
kruglovdm <110811400+kruglovdm@users.noreply.github.com>
2022-08-18 01:23:49 +0300

    Update 11_task_sort.py

--- Module5/practice/02_tasks_sort/11_task_sort.py
+++ Module5/practice/02_tasks_sort/11_task_sort.py
@@ -1,2 +1,12 @@
 # Анаграммы*
 # Задается словарь (список слов). Найти в нем все анаграммы (слова, составленные из одних и тех же букв).
+
+words = ['камень', 'береза', 'Василий', 'лунь', 'мошкара', 'ромашка', 'удар', 'руда']
+
+for wrd1 in words:
+    for wrd2 in words:
+        if len(wrd1)==len(wrd2) and wrd1 != wrd2:
+            wrd1_sorted = sorted(wrd1.lower())
+            wrd2_sorted = sorted(wrd2.lower())
+            if wrd1_sorted == wrd2_sorted:
+                print(f'{wrd1}')
