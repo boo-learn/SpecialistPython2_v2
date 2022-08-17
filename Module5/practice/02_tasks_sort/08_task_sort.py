cbd53e884362d175624e16e3322f8ef0d924914d
Degtyarskiy <111269758+Degtyarskiy@users.noreply.github.com>
2022-08-17 20:54:22 +0300

    Update 08_task_sort.py

--- Module5/practice/02_tasks_sort/08_task_sort.py
+++ Module5/practice/02_tasks_sort/08_task_sort.py
@@ -1,5 +1,16 @@
-# Телефонная база
-# Отсортируйте список телефонов по возрастанию и используя сортировку выбором.
-# Телефон задан в виде строки формата nn-nn-nn. Например, 23-45-67
-
 phones = ["25-17-58", "11-34-85", "54-61-56", "34-61-72", "25-17-55", "34-56-56"]
+
+def sort_phones(baza):
+    i = 0
+    while i < len(baza) - 1:
+        m = i
+        j = i + 1
+        while j < len(baza):
+            if baza[j] < baza[m]:
+                m = j
+            j += 1
+        baza[i], baza[m] = baza[m], baza[i]
+        i += 1
+    return baza
+    
+print(sort_phones(phones))
