f8c62fb1a80439e2ab8df07a0960476d35f8ff00
Steelrat1997 <kondrashov.konstantin@mail.ru>
2022-08-17 16:06:54 +0300

    Update 09_task_sort.py

--- Module5/practice/02_tasks_sort/09_task_sort.py
+++ Module5/practice/02_tasks_sort/09_task_sort.py
@@ -1,11 +1,30 @@
-# Список сотрудников
-# Дан список, элементами которого являются сотрудники, представленные в виде словаря(dict).
-# Пример данных:
+def choice_sort(nums, reverse = False, key = lambda n: n):
+    i = 0
+    while i < len(nums) - 1:
+        m = i
+        j = i + 1
+        while j < len(nums):
+            if reverse:
+                condition = key(nums[j]) > key(nums[m])
+            else:
+                condition = key(nums[j]) < key(nums[m])
+            if condition:
+                m = j
+            j += 1
+        nums[i], nums[m] = nums[m], nums[i]
+        i += 1
+
+
 employees = [
    {"name": "Петр", "surname": "Алексеев", "position": "Инженер"},
    {"name": "Иван", "surname": "Петров", "position": "Прораб"},
    {"name": "Алексей", "surname": "Петров", "position": "Строитель"},
    {"name": "Иван", "surname": "Сидоров", "position": "Строитель"},
 ]
+
+choice_sort(employees, key = lambda emp : (emp["surname"], emp["name"]))
+for employee in employees:
+    print(employee['surname'], employee['name'])
+    
 # Выведите список сотрудников(без указания должности) в формате: Фамилия Имя, в отсортированном порядке.
 # Примечание: если фамилии сотрудников совпадают, при сортировке учесть имя.
