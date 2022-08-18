d5be39614ab266e4b4ed0b15b90e798940b7f815
EdenHazard8 <97665053+EdenHazard8@users.noreply.github.com>
2022-08-18 16:36:03 +0300

    Update IBank_part1_4[add].py

--- Module8/practice/IBank/IBank_part1_4[add].py
+++ Module8/practice/IBank/IBank_part1_4[add].py
@@ -1,12 +1,69 @@
+import re
+
+
 class Account:
-    # TODO-0: сюда копируем реализацию класса Account из предыдущей задачи
-    # TODO-1: добавьте проверку паспорта и телефона(в конструкторе) на соответствие заданным форматам
-    #  В случае несоответствия выбрасываем исключение ValueError("Неверный формат телефона/паспорта")
-    #  Проверка информации на корректность - валидация
-    #  Готовые валидаторы можете взять в директории helpers
-    pass
+    pattern_phone = r"[+]7-\d{3}-\d{3}-\d{2}-\d{2}"
+    pattern_pasport = r"\d{4} \d{6}"
+
+    def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
+        self.name = name
+        self.passport = passport
+        self.phone_number = phone_number
+        # self.balance = start_balance
+        self.__balance = start_balance  # TODO: Закрываем прямой доступ к балансу
+        self.__validator_phone()
+
+    def __validator_phone(self):
+        if not re.match(Account.pattern_phone, self.phone_number):
+            raise ValueError(f"Phone number {phone_number} is not correct")
+        if not re.match(Account.pattern_pasport, self.passport):
+            raise ValueError(f"Passport number {phone_number} is not correct")
+
+    def full_info(self) -> str:
+        """
+        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
+        """
+        return f"{self.name} баланс: {self.__balance} руб." \
+               f" паспорт: {self.passport} т.{self.phone_number}"
+
+    def __repr__(self) -> str:
+        """
+        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.__balance} руб."
+
+    # TODO: совместно с преподавателем реализуйте getter для просмотра баланса
+    #  Можете попробовать самостоятельно: https://pythobyte.com/using-getters-and-setters-in-python-5205-840ed13f/
+    @property
+    def balance(self) -> int:
+        return self.__balance
+
+    def deposit(self, amount: int) -> None:
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        """
+        self.__balance += amount
+
+    def withdraw(self, amount: int) -> None:
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        """
+        self.__balance -= amount
+
+    # TODO-1: напишите реализацию метода transfer()
+    def transfer(self, target_account: 'Account', amount: int) -> None:
+        """
+        Перевод денег на счет другого клиента
+        :param target_account: счет клиента для перевода
+        :param amount: сумма перевода
+        :return:
+        """
+        self.withdraw(amount)
+        target_account.deposit(amount)
 
 
 account1 = Account("Иван", "3230 634563", "+7-900-765-12-34", 1000)  # аккаунт с корректными данными
-account2 = Account("Алексей", "+7-901-744-22-99", "323 456124", 200)  # номер паспорта задан не верно
-account3 = Account("Петр", "+7-904-745-47", "3232 456124", 200)  # номер телефона задан не верно
+account2 = Account("Алексей", "3523 456124", "+7-901-744-22-99", 200)  # номер паспорта задан не верно
+account3 = Account("Петр", "3232 456124", "+7-904-745-47-45", 200)  # номер телефона задан не верно
