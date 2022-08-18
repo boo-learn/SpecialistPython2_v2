1b6ebfa329878819f027e31c81d19f64d03a631e
Steelrat1997 <kondrashov.konstantin@mail.ru>
2022-08-18 10:24:54 +0300

    Update 02_Vector_result.py
    Сделал умножение в любом порядке

--- Module3/home_work/02_Vector_result.py
+++ Module3/home_work/02_Vector_result.py
@@ -1,5 +1,35 @@
-# Сюда отправляем решение задачи "Вектор"
-# Само задание в файле 01_Vector_task.md
-
 class Vector:
-    pass
+    def __init__(self, x, y):
+        self.x = x 
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Vector(self.x * other, self.y * other)
+
+    def __rmul__(self, other):
+        return Vector(self.x * other, self.y * other)
+
+vector1 = Vector(10, 3)
+vector2 = Vector(-5, 6)
+vector3 = vector1 + vector2
+print(vector1, ' + ', vector2, ' = ', vector3)
+
+vector3 = vector1 - vector2
+print(vector1, ' - ', vector2, ' = ', vector3)
+
+k = 2
+
+vector3 = vector1 * k
+print(vector1, ' * ', k, ' = ', vector3)
+
+vector3 = k * vector1
+print(k, ' * ', vector1, ' = ', vector3)
