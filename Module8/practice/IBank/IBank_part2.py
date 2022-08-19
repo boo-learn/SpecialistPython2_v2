549e3eba940e1b7d9512300efe7ca8977314238e
Andrey_Gorbi <agorbi@ya.ru>
2022-08-19 11:21:46 +0300

    Update IBank_part2.py

--- Module8/practice/IBank/IBank_part2.py
+++ Module8/practice/IBank/IBank_part2.py
@@ -1,6 +1,5 @@
 from typing import List
 # Сюда отправляем готовое решение IBank часть-2
-
 # класс для хранение информации об операциях
 class Operation:
     # Типы операций храним в свойствах класса
@@ -11,19 +10,65 @@ class Operation:
 
     # Напоминаю: обращение к этим переменным происходит через имя класса, пример: Operation.WITHDRAW
 
-    def __init__(self, ):
+    def __init__(self, amount, type, related_account=None):
         # TODO: Тут добавляем свойства для хранение информации об операции
-        pass
+        self.amount = amount
+        self.type = type
+        self.related_account = related_account
+
 
     def __repr__(self) -> str:
         """
         :return: возвращает строковое представление операции. Формат указан в 02_IBank_part2.md
         """
-        pass
+        if self.type == Operation.DEPOSIT or self.type == Operation.WITHDRAW:
+            return f"{self.type} {self.amount} руб."
+        elif self.type == Operation.TRANSFER_OUT:
+            return f"{self.type} {self.amount} руб. на счет клиента: {self.related_account.name}"
+        elif self.type == Operation.TRANSFER_IN:
+            return f"{self.type} {self.amount} руб. со счета клиента: {self.related_account.name}"
 
 
 class Account:
     # TODO: сюда копируем реализацию класса Account из предыдущей задачи
+    def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
+        self.name = name
+        self.passport = passport
+        self.phone_number = phone_number
+        self.__balance = start_balance
+        self.__history = []
+
+    def full_info(self) -> str:
+        return f"{self.name} баланс: {self.balance} руб. паспорт: {self.passport} т.{self.phone_number}"
+
+    def __repr__(self) -> str:
+        return f"{self.name} баланс: {self.balance} руб."
+
+    @property
+    def balance(self) -> int:
+        return self.__balance
+
+
+    def withdraw(self, amount: int, to_history: bool = True) -> None:
+        if self.__balance < amount:
+            raise ValueError("У Вас не достаточно средств...")        
+        self.__balance -= amount
+        if to_history:
+            self.__history.append(Operation(amount, Operation.WITHDRAW))
+
+
+    def transfer(self, target_account: 'Account', amount: int, to_history: bool = True) -> None:
+        self.withdraw(amount, to_history=False)    
+        target_account.deposit(amount, to_history=False)
+        
+        transfer_in = Operation(amount, Operation.TRANSFER_IN, self)
+        transfer_out = Operation(amount, Operation.TRANSFER_OUT, target_account)
+
+        if to_history:
+            self.__history.append(transfer_out)
+            target_account.__history.append(transfer_in)
+            
+
     # TODO: историю храним как список объектов класса Operation, добавив свойство в конструктор:
     #   self.__history = []
 
@@ -34,6 +79,15 @@ class Account:
         :param amount: сумма
         :param to_history: True - записывать операцию в историю, False - не записывать
         """
+        self.__balance += amount
+        if to_history:
+            self.__history.append(Operation(amount, Operation.DEPOSIT))
+
+    def get_history(self) -> List[str]:
+        """
+        :return: возвращаем историю операций  виде списка строковых представлений
+        """
+        return [str(operation) for operation in self.__history]
         self.__balance += amount
         if to_history:
             self.__history.append(Operation(amount, Operation.DEPOSIT))
