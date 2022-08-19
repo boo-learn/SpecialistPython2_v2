e43c4e57fd53d3b5dfa1a900f364ca672277e658
kruglovdm <110811400+kruglovdm@users.noreply.github.com>
2022-08-19 16:58:15 +0300

    Update tools.py

--- Module9/practice/BlackJack/tools.py
+++ Module9/practice/BlackJack/tools.py
@@ -1,3 +1,5 @@
+from classes import  Card, Deck
+
 def sum_points(cards):
     """
     Напишите отдельную функцию для нахождения суммы очков всех карт в списке
@@ -5,11 +7,27 @@ def sum_points(cards):
     :return: сумму очков
     """
     # Совет: храните кол-во очков за карту внутри класса Колоды(колода "знает", сколько дает очков каждая карта)
-
+    card_points = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
+                   'J': 10, 'Q': 10, 'K': 10, 'A': 11}
     #  Сначала считаем сумму карт, считая ТУЗ за 11-очков
-    sum_points = ...
+    sum_points = card_points[cards[0].value]+card_points[cards[1].value]+card_points[cards[2].value]
     # Если сумма > 21, то перечитываем сумму, считая ТУЗ за 1(единицу)
+
     if sum_points > 21:
-        ...
+        if cards[0].value == 'A':
+            sum_points -= 10
+        if cards[1].value == 'A':
+            sum_points -= 10
+        if cards[2].value == 'A':
+            sum_points -= 10
 
     return sum_points
+
+
+card1 = Card('10',Card.HEARTS)
+card2 = Card('2',Card.HEARTS)
+card3 = Card('A',Card.HEARTS)
+
+hands = [card1,card2,card3]
+print(sum_points(hands))
+
