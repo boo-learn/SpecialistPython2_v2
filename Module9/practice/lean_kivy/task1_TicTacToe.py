b3f9a77308792235ad1a48d27932a6b2c0dffc5d
EdenHazard8 <97665053+EdenHazard8@users.noreply.github.com>
2022-08-19 15:27:56 +0300

    Update task1_TicTacToe.py

--- Module9/practice/lean_kivy/task1_TicTacToe.py
+++ Module9/practice/lean_kivy/task1_TicTacToe.py
@@ -9,20 +9,24 @@ Window.size = (400, 500)
 
 
 class MyApp(App):
+    move = "X"
+    waiting = "O"
+    count = 0
+
     def build(self):
         grid = GridLayout(padding=15, cols=3)
         # GridLayout: https://kivy.org/doc/stable/api-kivy.uix.gridlayout.html
         # Widgets: https://kivy.org/doc/stable/api-kivy.uix.html
         self.cell1 = Button(on_press=self.on_btn_click, font_size=40)
-        self.cell2 = Button(font_size=40)
-        self.cell3 = Button(font_size=40)
-        self.cell4 = Button(font_size=40)
-        self.cell5 = Button(font_size=40)
-        self.cell6 = Button(font_size=40)
-        self.cell7 = Button(font_size=40)
-        self.cell8 = Button(font_size=40)
-        self.cell9 = Button(font_size=40)
-        self.lbl_status = Label(text="Ходит: X")
+        self.cell2 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell3 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell4 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell5 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell6 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell7 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell8 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell9 = Button(on_press=self.on_btn_click, font_size=40)
+        self.lbl_status = Label(text=f"Ходит: {MyApp.move}")
         grid.add_widget(self.cell1)
         grid.add_widget(self.cell2)
         grid.add_widget(self.cell3)
@@ -36,9 +40,12 @@ class MyApp(App):
         return grid
 
     def on_btn_click(self, instance):
-        instance.text = 'X'
-        self.lbl_status.text = "Ходит: O"
-
+        instance.text = MyApp.move
+        self.lbl_status.text = f"Ходит: {MyApp.waiting}"
+        MyApp.waiting, MyApp.move = MyApp.move, MyApp.waiting
+        MyApp.count += 1
+        if MyApp.count == 9:
+            self.lbl_status.text = 'Ничья'
 
 # TODO: дана заготовка игры "Крестики-нолики". Закончите игру.
 # TODO: Когда кто-то выстраивает вряд/по диагонали кри крестика или три нолика, в поле self.lbl_status, вывести: "победил Х".
