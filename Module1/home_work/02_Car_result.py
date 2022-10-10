bed1f51732a5ff71db428c58a3c432dbd2af0daf
JornGoran <115464462+JornGoran@users.noreply.github.com>
2022-10-11 01:56:25 +0300

    Update 02_Car_result.py

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -2,4 +2,26 @@
 # Само задание в файле 02_Сar_task.md
 
 class Car:
-    pass
+    def __init__(self, gas, capacity, gas_per_km, run):
+        self.gas = gas
+        self.capacity = capacity
+        self.gas_per_km = gas_per_km
+        self.run = run
+
+    def fill(self, litres):
+        if self.capacity - self.gas >= litres:
+            self.gas += litres
+        else:
+            print(f'Не вместилось {litres - (self.capacity - self.gas)} литров')
+            self.gas = self.capacity
+
+    def ride(self, km):
+        if self.gas < (km * self.gas_per_km):
+            print(f'Проехали {self.gas / self.gas_per_km} километров')
+            self.run += self.gas / self.gas_per_km
+            self.gas = 0
+        else:
+            print(f'Проехали {km} километров' )
+            self.run += km
+            self.gas -= km * self.gas_per_km
+            
