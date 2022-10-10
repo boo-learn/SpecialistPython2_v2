10a874e9287e80cfbf6a9da7cd123840abb97488
MihaBro <110846905+MihaBro@users.noreply.github.com>
2022-10-10 14:15:54 +0300

    Update 06_color_points[add].py

--- Module1/practice/points/06_color_points[add].py
+++ Module1/practice/points/06_color_points[add].py
@@ -1,14 +1,15 @@
 from __future__ import annotations
 
 
+
 class Point:
     def __init__(self, x: int, y: int, color: str):
         self.x = x
         self.y = y
-        self.color = ...
+        self.color = color
 
     def dist_to(self, other_point: Point) -> float:
-        ...
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
 
 
 # Дан список точек нарисованных красным(red) и зеленым(green) цветами
@@ -27,7 +28,7 @@ points = [
 # TODO-2: реализуйте метод dist_to()
 # TODO-3: вычислите площади треугольников образованных точками разных цветов
 
-# your core here...
-
-print("Площадь красного треугольника = ", ...)
-print("Площадь зеленого треугольника = ", ...)
+red = [point for point in points if point.color == 'red']
+green = [point for point in points if point.color == 'green']
+print("Площадь красного треугольника = ", abs(((red[0].x - red[2].x) * (red[1].y - red[2].y) - (red[1].x - red[2].x) * (red[0].y - red[2].y)) * 0.5))
+print("Площадь зеленого треугольника = ", abs(((green[0].x - green[2].x) * (green[1].y - green[2].y) - (green[1].x - green[2].x) * (green[0].y - green[2].y)) * 0.5))
