592dd2cc3da42ccb4bf3d10d7188c73d7b5f0ace
Kelpyre <atrom@list.ru>
2022-10-10 18:27:37 +0300

    Update 01_hw_coins.py
    Филимонов Дмитрий, решение задачи

--- Module1/home_work/01_hw_coins.py
+++ Module1/home_work/01_hw_coins.py
@@ -9,7 +9,21 @@ class Coin:
         """
         Подбрасывание монетки. # heads-орел/tails-решка
         """
-        self.side = ...  # random: heads/tails
+        choices = ['heads', 'tails']
+        self.side = random.choices(choices)  # random: heads/tails
+
+
+if __name__ == '__main__':
+    val = int(input('Введите количество монет : '))
+    counter = 0
+    coin_list = [Coin() for _ in range(val)]
+    for i in coin_list:
+        i.flip()
+        if i.side == ['heads']:
+            counter += 1
+    print(f'Процент орлов : {int(counter/val * 100)}')
+    print(f'Процент решек : {int((val-counter)/val * 100)}')
+
 
 # Задание:
 # 1. Создайте список из n-монеток, n - вводится с клавиатуры
