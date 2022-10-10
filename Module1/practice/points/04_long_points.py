c32f5808da9140f31b8c16272742a4392ba41adb
iscradly <101692879+iscradly@users.noreply.github.com>
2022-10-10 14:17:18 +0300

    Update 04_long_points.py

--- Module1/practice/points/04_long_points.py
+++ Module1/practice/points/04_long_points.py
@@ -1,19 +1,20 @@
 from __future__ import annotations
 
-
 class Point:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
 
     def dist_to(self, other_point: Point) -> float:
-        # TODO-0: скопируйте реализацию из предыдущей задачи
-        ...
-
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
 
 # Дан список из произвольного количества точек:
 points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)]
+point2 = Point(0, 0)
 
-# TODO-1: найдите точку наиболее удаленную от начала координат и выведите ее координаты
+b = []
+for point in points:
+    # print("Расстояние между точками = ",  point.dist_to((point2)))
+    b.append(point.dist_to((point2)))
 
-print("Координаты наиболее удаленной точки = ", ...)
+print("Координаты наиболее удаленной точки = ", max(b))
