cecaf64a387226147a4e3a041a16e8bbd4553a70
JamieMarch <99137853+JamieMarch@users.noreply.github.com>
2022-10-11 01:07:18 +0300

    Update 02_Car_result.py
    Остановился на цикле в 3 условии

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -2,4 +2,27 @@
 # Само задание в файле 02_Сar_task.md
 
 class Car:
-    pass
+    def __init__(self, gas: int, capacity: int, gas_per_km: int):
+        self.gas = gas
+        self.capacity = capacity
+        self.gas_per_km = gas_per_km
+
+    def fill(self, benz: int):
+        self.benz = benz
+    def __str__(self):
+        return f'Gas: {self.gas}, capacity: {self.capacity}, gas_per_km: {self.gas_per_km}'
+
+    def ride(self, distance: int):
+        self.dist = distance
+        result = self.gas - (distance * self.gas_per_km)
+        while self.gas != 0:
+            self.gas = result
+            print("Едем дальше")
+        else:
+            print("stop")
+
+car1 = Car(15, 20, 1)
+print(car1)
+car1.fill(10)
+print(car1)
+car1.ride(10)
