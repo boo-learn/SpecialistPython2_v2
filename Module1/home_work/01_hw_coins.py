798b1a0ec005aa6765503e92c94f75b925ea9214
Лев Харьков <kirsan94@mail.ru>
2022-10-10 17:17:49 +0300

    Update 01_hw_coins.py

--- Module1/home_work/01_hw_coins.py
+++ Module1/home_work/01_hw_coins.py
@@ -9,12 +9,27 @@ class Coin:
         """
         Подбрасывание монетки. # heads-орел/tails-решка
         """
-        self.side = ...  # random: heads/tails
+        sides = ["heads", "tails"]
+        self.side = random.choice(sides)  # random: heads/tails
 
 # Задание:
 # 1. Создайте список из n-монеток, n - вводится с клавиатуры
 # 2. Подбросьте(flip) все монетки. У каждой монетки в списке вызовите метод .flip()
 # 3. Выведите соотношение выпавших орлов и решек в процентах
+
+n = int(input("Количество монеток: "))
+coins = [Coin] * n
+heads = 0
+tails = 0
+for coin in coins:
+    coin.flip(coin)
+    if coin.side == "heads":
+        heads += 1
+    else:
+        tails += 1
+print(int(heads/n*100), '%')
+print(int(tails/n*100), '%')
+
 
 # Пояснение: когда вы создаете монетку, то она находится в неопределенном состоянии self.side = None, т.е.
 # она находится у вас в руке и не выпала ни орлом ни решкой. Монетка "определеяется" со стороной(орел/решка),
