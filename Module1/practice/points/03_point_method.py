bb693486a5e9c4b6b7a576206967a378c59b3c7c
Grigorus <82019468+Grigorus@users.noreply.github.com>
2022-10-10 15:10:16 +0400

    Update 03_point_method.py

--- Module1/practice/points/03_point_method.py
+++ Module1/practice/points/03_point_method.py
@@ -1,20 +1,2 @@
-from __future__ import annotations
 
-
-class Point:
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-
-    def dist_to(self, other_point: Point) -> float:
-        # TODO-1: реализуйте метод
-        # Подсказка: смотри пример, Module1/examples/06_object_methods.py
-        ...
-
-
-# Дано две точки на координатной плоскости
-point1 = Point(2, 4)
-point2 = Point(5, -2)
-
-# TODO-2: выведите расстояние между точками используя метод dist_to()
-print(f"Расстояние между точками = {...}")
+#тест отправки
