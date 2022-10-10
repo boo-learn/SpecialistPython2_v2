c0ea939c020a0020c38eac515a89d497d87fe0dd
trds42 <trds42@gmail.com>
2022-10-10 14:22:26 +0300

    Update 06_color_points[add].py

--- Module1/practice/points/06_color_points[add].py
+++ Module1/practice/points/06_color_points[add].py
@@ -5,11 +5,18 @@ class Point:
     def __init__(self, x: int, y: int, color: str):
         self.x = x
         self.y = y
-        self.color = ...
+        self.color = color
 
     def dist_to(self, other_point: Point) -> float:
-        ...
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
 
+
+def triangle_area(triangle_points):
+    a = triangle_points[0].dist_to(triangle_points[1])
+    b = triangle_points[0].dist_to(triangle_points[2])
+    c = triangle_points[1].dist_to(triangle_points[2])
+    p = (a + b + c) / 2
+    return (p * (p - a) * (p - b) * (p - c))**0.5
 
 # Дан список точек нарисованных красным(red) и зеленым(green) цветами
 # Точно известно что точек каждого цвета ровно три, но порядок точек в списке произвольный
@@ -28,6 +35,14 @@ points = [
 # TODO-3: вычислите площади треугольников образованных точками разных цветов
 
 # your core here...
+red_triangle = []
+green_triangle = []
 
-print("Площадь красного треугольника = ", ...)
-print("Площадь зеленого треугольника = ", ...)
+for point in points:
+    if point.color == 'red':
+        red_triangle.append(point)
+    elif point.color == 'green':
+        green_triangle.append(point)
+
+print("Площадь красного треугольника = ", triangle_area(red_triangle))
+print("Площадь зеленого треугольника = ", triangle_area(green_triangle))
