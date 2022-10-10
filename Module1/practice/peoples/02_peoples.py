2287ac09778a9cb4e133ff615d0e30c0e788b5c6
trds42 <trds42@gmail.com>
2022-10-10 15:23:05 +0300

    Update 02_peoples.py

--- Module1/practice/peoples/02_peoples.py
+++ Module1/practice/peoples/02_peoples.py
@@ -6,7 +6,10 @@ class People:
 
     def change_age(self, new_age: int) -> None:
         # TODO: скопируйте реализацию метода из предыдущей задачи
-        ...
+        if type(new_age) == int and 1 < new_age < 100:
+            self.age = new_age
+        else:
+            print("Некорректный возраст")
 
     def full_name(self) -> str:
         return f"{self.surname} {self.name}"
@@ -25,6 +28,24 @@ peoples = [
 
 # TODO-1: найдите самого молодого человека и выведите его Фамилию и Имя
 #  Примечание: Если самых молодых несколько, выведите любого
+the_youngest = peoples[0]
+for person in peoples:
+    if person.age < the_youngest.age:
+        the_youngest = person
+print(f'Самый молодой человек - это {the_youngest.full_name()}')
 
 # TODO-2: найдите всех одногодок и выведите их Фамилии и Имена
 #  Примечание: Если одногодок нет, выведите сообщение "одногодок нет"
+same_age_peoples = []
+for i in range(len(peoples)):
+    current_age = peoples[i].age
+    for j in range(i+1, len(peoples)):
+        if peoples[j].age == current_age:
+            same_age_peoples.append(peoples[i])
+            same_age_peoples.append(peoples[j])
+
+if len(same_age_peoples) > 0:
+    for person in same_age_peoples:
+        print(person.full_name())
+else:
+    print('Одногодок нет')
