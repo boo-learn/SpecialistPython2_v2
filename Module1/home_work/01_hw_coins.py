547a3734bae8f3b1fedfea38daedf0954c8865d1
stdio666 <115464395+stdio666@users.noreply.github.com>
2022-10-10 21:48:19 +0300

    Update 01_hw_coins.py
    Тишкин

--- Module1/home_work/01_hw_coins.py
+++ Module1/home_work/01_hw_coins.py
@@ -1,16 +1,33 @@
 import random
 
-
 class Coin:
     def __init__(self):
         self.side = None
 
     def flip(self) -> None:
-        """
-        Подбрасывание монетки. # heads-орел/tails-решка
-        """
-        self.side = ...  # random: heads/tails
+        self.side = random.choice(['heads','tails'])
 
+n = int(input("Введите число: "))
+print(n)
+coinlist = []
+
+for i in range(0, n): # создание списка монет
+    coin = Coin()
+    coinlist.append(coin)
+
+for i in range(0, n): # подбрасывание каждой монеты
+    coinlist[i].flip()
+    print(coinlist[i].side)
+h = 0
+t = 0
+for coin_i in coinlist:
+    if coin_i.side == "heads":
+        h += 1
+    else:
+        t += 1
+
+print("Tails = ", t/n*100, "%")
+print("Heads = ", h/n*100, "%")
 # Задание:
 # 1. Создайте список из n-монеток, n - вводится с клавиатуры
 # 2. Подбросьте(flip) все монетки. У каждой монетки в списке вызовите метод .flip()
