2e057bd571397aeb353bbb7d1c1bf96619252abe
MihaBro <110846905+MihaBro@users.noreply.github.com>
2022-10-11 12:38:13 +0300

    Update 03_cards_list.py

--- Module4/practice/deck/03_cards_list.py
+++ Module4/practice/deck/03_cards_list.py
@@ -1,17 +1,47 @@
+# Начнем с создания карты
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
+        if self.suit == "Hearts":
+            card_suit = '\u2665'
+        elif self.suit == "Diamonds":
+            card_suit = '\u2666'
+        elif self.suit == "Clubs":
+            card_suit = '\u2663'
+        elif self.suit == "Spades":
+            card_suit = '\u2660'
+        return f'{self.value}{card_suit}'
+
+    def equal_suit(self, other_card):
+        # TODO-1: метод возвращает True - если масти карт равны или False - если нет
+        if self.suit == other_card.suit:
+            return True
+        else:
+            return False
 
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 hearts_cards = []
 # TODO-1: добавьте в список hearts_cards все червовые карты(от 2-ки до туза)
+for value in values:
+    value = Card(value,'Hearts')
+    hearts_cards.append(value)
 
 diamonds_cards = []
 # TODO-2: добавьте в список diamonds_cards все бубновые карты(от туза до 2-ки)
+for value in values:
+    value = Card(value,'Diamonds')
+    diamonds_cards.append(value)
+    diamonds_cards = diamonds_cards[::-1]
 
 # TODO-3: выведите все карты из списка hearts_cards в терминал через запятую в одну строку:
 # Пример вывода: 2♥, 3♥, 4♥ ... A♥
+for card in hearts_cards:
+    print(f'{card.to_str()}', end=', ')
 
 
 cards = [
