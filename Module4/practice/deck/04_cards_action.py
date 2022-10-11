b01a7ceb0e9d5fe0e53f855cf0f3a7fab4733e0a
Mikhail32r <115464481+Mikhail32r@users.noreply.github.com>
2022-10-11 12:51:10 +0300

    Update 04_cards_action.py

--- Module4/practice/deck/04_cards_action.py
+++ Module4/practice/deck/04_cards_action.py
@@ -1,12 +1,51 @@
+# Начнем с создания карты
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        suit_icon = {
+        'Hearts':'\u2665',
+        'Diamonds':'\u2666',
+        'Clubs':'\u2663',
+        'Spades':'\u2660',
+        }
+
+        return f"{self.value}{suit_icon[self.suit]}"
+        # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
+
+def equal_suit(card1:Card, card2:Card):
+    if card1.suit==card2.suit:
+        return True
+    else:
+        return False
 
 
-cards = []
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-# TODO-1: в список cards добавьте ВСЕ карты всех мастей
+hearts_cards = []
+diamonds_cards = []
+clubs_cards = []
+spades_cards = []
+deck_card = []
 
-# TODO-2: Выведите карты в формате: cards[кол-во]2♥, 3♥, 4♥ ... A♥, 2♦, 3♦ ... A♦, ...
-#  кол-во берем от размера списка cards
+for i in values:
+    hearts_cards.append(Card(i,suits[0]))
+    diamonds_cards.append(Card(i,suits[1]))
+    clubs_cards.append(Card(i,suits[2]))
+    spades_cards.append(Card(i,suits[3]))
+
+deck_card.append(hearts_cards)
+deck_card.append(diamonds_cards)
+deck_card.append(clubs_cards)
+deck_card.append(spades_cards)
+
+suits = []
+for i in deck_card:
+    for j in i:
+        suits.append(j.to_str())
+    print("Количество карт ", len(i),*suits, sep=', ')
+    suits.clear()
+
+
