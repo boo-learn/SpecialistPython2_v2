f14c2bdfc290c1766a8cdd4910bc1fb0a5967f3f
MihaBro <110846905+MihaBro@users.noreply.github.com>
2022-10-11 17:06:02 +0300

    Update 03_task_deck.py

--- Module4/practice/deck/deck_tasks/03_task_deck.py
+++ Module4/practice/deck/deck_tasks/03_task_deck.py
@@ -4,3 +4,19 @@ from deck_total import Card, Deck
 #   Снова перемешайте колоду и вытяните еще одну. Если вторая карта меньше первой, повторите “перемешать + вытянуть”,
 #   до тех пор, пока не вытяните карту больше предыдущей карты.
 #   В качестве результата выведи все вытягиваемые карты в консоль.
+
+deck = Deck()
+cards = []
+while deck:
+    deck.shuffle()
+    card1 = deck.draw(1)
+    deck.shuffle()
+    card2 = deck.draw(1)
+    if card1 < card2:
+        cards.append(card1)
+        cards.append(card2)
+        break
+    else:
+        cards.append(card1)
+        cards.append(card2)
+print(cards)
