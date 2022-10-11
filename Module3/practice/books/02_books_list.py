b2a3e5ccc9019796f8cb692dde1ca2c4c71c48c0
Grigorus <82019468+Grigorus@users.noreply.github.com>
2022-10-11 12:50:21 +0400

    Update 02_books_list.py

--- Module3/practice/books/02_books_list.py
+++ Module3/practice/books/02_books_list.py
@@ -1,11 +1,14 @@
+
+
 class Author:
     def __init__(self, name: str, surname: str):
         self.name = name
         self.surname = surname
 
     def short_name(self) -> str:
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        # TODO-1: метод возвращает строку в формате: М.Булгаков
+        #  пояснение: Первую букву имени, фамилию целиком
+        return f'{self.name[0]}. {self.surname}'
 
 
 class Book:
@@ -16,9 +19,21 @@ class Book:
         self.pages = pages
 
     def to_str(self) -> str:
-        # TODO-0: скопируйте реализацию метода из предыдущей задачи
+        # TODO-2: метод возвращает строку в формате: "Вьюга" author:М.Булгаков publish:1926 год
+        #  пояснение: Название книги выводим в кавычках(""), у имени автора только первую букву
         ...
+        return f'"{self.name}" author: {author.short_name()} publish: {book.year} год'
 
+
+author = Author("Михаил", "Булгаков")
+book = Book("Вьюга", author, 1926, 25)
+print(author.short_name())
+
+# TODO-3: Измените имя, фамилию автора и название книги, проверьте, что программа корректно работает с новыми значениями
+author = Author("Афанасий", "Фет")
+book = Book("Чума", author, 1901, 250)
+
+print(book.to_str())
 
 author1 = Author("Михаил", "Булгаков")
 author2 = Author(name="Стивен", surname="Кинг")
@@ -30,5 +45,7 @@ books_catalog = [
     Book("Сияние", author2, 2014, 544),
     Book("Оно", author2, 1986, 320),
 ]
-
-# TODO-1: Выведите нумерованный список книг, используя для каждой книги ее строковое представление(метод .to_str())
+n=1
+for unit in books_catalog:
+    print(f'{n} {unit.to_str()}')
+    n += 1
