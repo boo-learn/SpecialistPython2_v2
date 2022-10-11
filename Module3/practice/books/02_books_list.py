1e061102ba2990c8002fae004bec7219485dbd0f
Tobby15 <115464512+Tobby15@users.noreply.github.com>
2022-10-11 11:48:41 +0300

    Update 02_books_list.py

--- Module3/practice/books/02_books_list.py
+++ Module3/practice/books/02_books_list.py
@@ -5,7 +5,7 @@ class Author:
 
     def short_name(self) -> str:
         # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return f"{self.surname[:1]}.{self.name}"
 
 
 class Book:
@@ -17,7 +17,7 @@ class Book:
 
     def to_str(self) -> str:
         # TODO-0: скопируйте реализацию метода из предыдущей задачи
-        ...
+        return f'"{self.name}" author:{self.author.short_name()} publish: {self.year} год'
 
 
 author1 = Author("Михаил", "Булгаков")
@@ -32,3 +32,8 @@ books_catalog = [
 ]
 
 # TODO-1: Выведите нумерованный список книг, используя для каждой книги ее строковое представление(метод .to_str())
+
+i = 1
+for book in books_catalog:
+    print(i, book.to_str())
+    i += 1
