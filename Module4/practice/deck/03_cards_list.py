f6b53056de987dc831ca486635684310ee06ddd1
Mikhail32r <115464481+Mikhail32r@users.noreply.github.com>
2022-10-11 12:39:18 +0300

    Update 03_cards_list.py

--- Module4/practice/deck/03_cards_list.py
+++ Module4/practice/deck/03_cards_list.py
@@ -1,21 +1,42 @@
+# Начнем с создания карты
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        suit_icon = {
+        'Hearts':'\u2665',
+        'Diamonds':'\u2666',
+        'Clubs':'\u2663',
+        'Spades':'\u2660',
+        }
+
+        return f"{self.value}{suit_icon[self.suit]}"
+        # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
+
+def equal_suit(card1:Card, card2:Card):
+    if card1.suit==card2.suit:
+        return True
+    else:
+        return False
 
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
 hearts_cards = []
-# TODO-1: добавьте в список hearts_cards все червовые карты(от 2-ки до туза)
-
 diamonds_cards = []
-# TODO-2: добавьте в список diamonds_cards все бубновые карты(от туза до 2-ки)
 
-# TODO-3: выведите все карты из списка hearts_cards в терминал через запятую в одну строку:
-# Пример вывода: 2♥, 3♥, 4♥ ... A♥
+for i in values:
+    hearts_cards.append(Card(i,'Hearts'))
+    diamonds_cards.append(Card(i, 'Diamonds'))
 
+suits = []
+for i in hearts_cards:
+    suits.append(i.to_str())
+print(*suits, sep=', ')
+suits = []
+for i in diamonds_cards:
+    suits.append(i.to_str())
+print(*suits, sep=', ')
 
-cards = [
-    Card("2", "Hearts"),
-    Card("3", "Hearts"),
-    Card("4", "Hearts"),
-    ...]
