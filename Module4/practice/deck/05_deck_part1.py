6e09f3695c179c6fab42c4bbaa09e5bc62522f5b
Fr1zgum <53056922+Fr1zgum@users.noreply.github.com>
2022-10-11 14:43:20 +0300

    Update 05_deck_part1.py

--- Module4/practice/deck/05_deck_part1.py
+++ Module4/practice/deck/05_deck_part1.py
@@ -1,20 +1,37 @@
 class Card:
     # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
     ...
+def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
 
+    def to_str(self):
+        suit_icon = {
+            'Hearts': '\u2665',
+            'Diamonds': '\u2666',
+            'Clubs': '\u2663',
+            'Spades': '\u2660'
+        }
+        return f"{self.value}{suit_icon[self.suit]}"
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
 
 class Deck:
-    def __init__(self):
-        # Список карт в колоде. Каждым элементом списка будет объект класса Card
-        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+    d suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
         self.cards = []
         # TODO-1: конструктор добавляет в список self.cards все(52) карты
+        for suit in suits:
+            for value in values:
+                self.cards.append(Card(value,suit))
 
     def show(self):
         # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
         ...
-
+        cards_str = []
+        for card in self.cards:
+            cards_str.append(card.to_str())
+        return f'cards[{len(self.cards)}]' + ', '.join(cards_str)
 
 # Создаем колоду
 deck = Deck()
