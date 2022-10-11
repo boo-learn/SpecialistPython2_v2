c131f8a508afdfc2fa116ed73acfaa0ac33e5109
trds42 <trds42@gmail.com>
2022-10-11 13:08:15 +0300

    Update 04_cards_action.py

--- Module4/practice/deck/04_cards_action.py
+++ Module4/practice/deck/04_cards_action.py
@@ -1,12 +1,30 @@
 class Card:
     # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def __str__(self):
+        suit_icon = {
+            'Diamonds': '\u2666',
+            'Hearts':   '\u2665',
+            'Spades':   '\u2664',
+            'Clubs':    '\u2667'
+        }
+        return f'{self.value}{suit_icon[self.suit]}'
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
 
 
 cards = []
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
 # TODO-1: в список cards добавьте ВСЕ карты всех мастей
+for suit in suits:
+    for value in values:
+        cards.append(Card(value, suit))
 
 # TODO-2: Выведите карты в формате: cards[кол-во]2♥, 3♥, 4♥ ... A♥, 2♦, 3♦ ... A♦, ...
 #  кол-во берем от размера списка cards
+print(f'cards[{len(cards)}]{", ".join(map(str, cards))}')
