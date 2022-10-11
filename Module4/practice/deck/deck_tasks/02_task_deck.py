ed5dc65368d719d09491a58e52df9380ca79428b
Tobby15 <115464512+Tobby15@users.noreply.github.com>
2022-10-11 17:00:11 +0300

    Update 02_task_deck.py

--- Module4/practice/deck/deck_tasks/02_task_deck.py
+++ Module4/practice/deck/deck_tasks/02_task_deck.py
@@ -2,3 +2,34 @@ from deck_total import Card, Deck
 
 # TODO: Создайте колоду из 52 карт. Перемешайте ее. Вытяните 10 карт сверху
 #   и посчитайте карт какой/каких мастей среди вытянутых карт оказалось больше всего?
+deck = Deck()
+# Тусуем колоду
+deck.shuffle()
+print(deck.show())
+# Берем две карты из колоды
+suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+cards = deck.draw(10)
+sum_Hearts = 0
+sum_Diamonds = 0
+sum_Spades = 0
+sum_Clubs = 0
+for card in cards:
+    if card.suit == 'Hearts':
+        sum_Hearts += 1
+    if card.suit == 'Diamonds':
+        sum_Diamonds += 1
+    if card.suit == 'Spades':
+        sum_Spades += 1
+    if card.suit == 'Clubs':
+        sum_Clubs += 1
+#print(sum_Hearts, sum_Diamonds, sum_Spades, sum_Clubs)
+#print(max(sum_Hearts, sum_Diamonds, sum_Spades, sum_Clubs))
+if sum_Hearts == max(sum_Hearts, sum_Diamonds, sum_Spades, sum_Clubs):
+    print(f"Hearts")
+if sum_Diamonds == max(sum_Hearts, sum_Diamonds, sum_Spades, sum_Clubs):
+    print(f"Diamonds")
+if sum_Spades == max(sum_Hearts, sum_Diamonds, sum_Spades, sum_Clubs):
+    print(f"Spades")
+if sum_Clubs == max(sum_Hearts, sum_Diamonds, sum_Spades, sum_Clubs):
+    print(f"Clubs")
+    
