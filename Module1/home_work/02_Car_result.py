b00e742941a46e2c7a2be01ecd8a7827cde14c6d
iscradly <101692879+iscradly@users.noreply.github.com>
2022-10-11 09:15:12 +0300

    Update 02_Car_result.py

--- Module1/home_work/02_Car_result.py
+++ Module1/home_work/02_Car_result.py
@@ -1,5 +1,35 @@
-# Сюда отправляем решение задачи "Автомобиль"
-# Само задание в файле 02_Сar_task.md
-
 class Car:
-    pass
+    def __init__(self, gas: float, capacity: float, gas_per_km: float, run: float):
+        self.gas = gas # сколько бензина в баке
+        self.capacity = capacity # вместимость бака
+        self.gas_per_km = gas_per_km # расход топлива на км\
+        self.run = run # пробег
+
+    def fill(self, new_gas) -> float: # метод "залить столько-то литров в бак"
+        if (self.gas + new_gas) > self.capacity:
+            print(f"Лишние литры: {(self.gas + new_gas) - self.capacity}")
+            self.gas = self.capacity
+        else:
+            self.gas = self.gas + new_gas
+
+    def ride(self, km) -> float: # проехать сколько-то км"
+        if km <= self.gas / self.gas_per_km:
+            self.gas = self.gas - km * self.gas_per_km
+            print(f"Проехали {km} километров")
+            self.run = self.run + km
+        else:
+            self.gas = 0
+            print(f"Проехали {self.gas / self.gas_per_km} километров")
+            self.run = self.run + self.gas / self.gas_per_km
+
+
+    # def show(self) -> str:
+    #     return f"{self.gas} вес:{self.capacity} цена:{self.gas_per_km}"
+
+car1 = Car(0, 100, 2, 10)
+print(car1.gas)
+car1.fill(108)
+print(car1.gas)
+car1.ride(25)
+print(car1.run)
+print(car1.gas)
