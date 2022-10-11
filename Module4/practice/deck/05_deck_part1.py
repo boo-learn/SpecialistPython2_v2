c4acd2d1d9c3d60d80da660dbfc6ec18d756ac71
Mikhail32r <115464481+Mikhail32r@users.noreply.github.com>
2022-10-11 13:19:54 +0300

    Update 05_deck_part1.py

--- Module4/practice/deck/05_deck_part1.py
+++ Module4/practice/deck/05_deck_part1.py
@@ -1,7 +1,25 @@
+# Начнем с создания карты
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
 
+    def to_str(self):
+        suit_icon = {
+        'Hearts':'\u2665',
+        'Diamonds':'\u2666',
+        'Clubs':'\u2663',
+        'Spades':'\u2660',
+        }
+
+        return f"{self.value}{suit_icon[self.suit]}"
+        # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
+
+def equal_suit(card1:Card, card2:Card):
+    if card1.suit==card2.suit:
+        return True
+    else:
+        return False
 
 class Deck:
     def __init__(self):
@@ -9,14 +27,17 @@ class Deck:
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
         self.cards = []
+        for svit in suits:
+            for value in values:
+                self.cards.append(Card(value,svit))
         # TODO-1: конструктор добавляет в список self.cards все(52) карты
 
-    def show(self):
-        # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+    def show(self)->str:
+        card_str = []
+        for card in self.cards:
+            card_str.append(card.to_str())
+        return f"Количество карт в колоде {len(self.cards)}: {', '.join(card_str)}"
 
 
-# Создаем колоду
-deck = Deck()
-# Выводим колоду в формате указанном в основном задании
-print(deck.show())
+deck1 = Deck()
+print(deck1.show())
