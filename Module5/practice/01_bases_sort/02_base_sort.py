08c85444c32c5db1e2a2e3b9248e8eb46cb1278f
trds42 <trds42@gmail.com>
2022-10-12 11:46:32 +0300

    Update 02_base_sort.py

--- Module5/practice/01_bases_sort/02_base_sort.py
+++ Module5/practice/01_bases_sort/02_base_sort.py
@@ -1,15 +1,66 @@
 # Все алгоритмы сортировки из examples/ оберните в функции
 
-def bubble_sort():
-    pass
+def bubble_sort(nums):
+    swapped = True
+    n = len(nums) - 1
+    while swapped:
+        swapped = False
+        for i in range(n):
+            if nums[i] > nums[i + 1]:
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+        n -= 1
 
 
-def sort_choice():
-    pass
+def sort_choice(nums):
+    i = 0
+    while i < len(nums) - 1:
+        m = i
+        j = i + 1
+        while j < len(nums):
+            if nums[j] < nums[m]:
+                m = j
+            j += 1
+        nums[i], nums[m] = nums[m], nums[i]
+        i += 1
 
 
-def quick_sort():
-    pass
+def quick_sort(nums):
+    # Создадим вспомогательную функцию, которая вызывается рекурсивно
+    def _quick_sort(items, low, high):
+        if low < high:
+            # Индекс опорного элемента
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
+
+
+def partition(nums, low, high):
+    # Выбираем средний элемент в качестве опорного
+    # Также возможен выбор первого, последнего
+    # или произвольного элементов в качестве опорного
+    pivot = nums[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while nums[i] < pivot:
+            i += 1
+
+        j -= 1
+        while nums[j] > pivot:
+            j -= 1
+
+        if i >= j:
+            return j
+
+        # Если элемент с индексом i (слева от опорного) больше, чем
+        # элемент с индексом j (справа от опорного), меняем их местами
+        nums[i], nums[j] = nums[j], nums[i]
 
 
 # Напишите функцию для заполнения списка случайными числами
@@ -21,6 +72,22 @@ def gen_list(size, at=-100, to=100):
     :param to: максимально возможное значение элементов
     :return: список из size произвольных элементов в диапазоне at..to 
     """
-    pass
+    new_list = []
+    for i in range(size):
+        new_list.append(random.randint(at, to))
+    return new_list
+
 
 # протестируйте функции сортировки, используя gen_list() для создания сортируемых списков
+nums = gen_list(50)
+print("before sort = ", nums)
+bubble_sort(nums)
+print("after bubble_sort = ", nums)
+nums = gen_list(50)
+print("before sort = ", nums)
+sort_choice(nums)
+print("after sort_choice = ", nums)
+test_list = gen_list(50)
+print("before sort = ", nums)
+quick_sort(nums)
+print("after quick_sort = ", test_list)
