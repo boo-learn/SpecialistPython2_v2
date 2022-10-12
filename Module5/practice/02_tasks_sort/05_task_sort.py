e133f90bcbb913d6c61d7b8709a1ee307fb3b208
MihaBro <110846905+MihaBro@users.noreply.github.com>
2022-10-12 15:25:26 +0300

    Update 05_task_sort.py

--- Module5/practice/02_tasks_sort/05_task_sort.py
+++ Module5/practice/02_tasks_sort/05_task_sort.py
@@ -1,3 +1,4 @@
+
 # Дан список сотрудников:
 staff = [
     {
@@ -29,6 +30,32 @@ staff = [
 
 # 1. Выведите список сотрудников, отсортированный по уменьшению их заработной платы.
 # Если у нескольких сотрудников одинаковая ЗП, то добавьте сортировку по Фамилии
-print("Список сотрудников отсортированный по уменьшению ЗП:")
+
+
+def sort_by_salary(staff, reverse=False):
+    salary_list = []
+    result = []
+    for person in staff:
+        if person['salary'] not in salary_list:
+            salary_list.append(person['salary'])
+    if reverse:
+        salary_list = sorted(salary_list, reverse=True)
+    else:
+        salary_list = sorted(salary_list, reverse=False)
+    print(salary_list)
+    for salary in salary_list:
+        for person in staff:
+            if salary == person['salary']:
+                result.append(person)
+    return result
+
+
+print("Список сотрудников отсортированный по уменьшению ЗП:", sort_by_salary(staff, reverse=True))
 
 # 2. Найдите сумму зарплат трех самых низкооплачиваемых сотрудников:
+result = sort_by_salary(staff)
+summa = 0
+for person in result[:3]:
+    summa += person['salary']
+print(f'{summa} рублей')
+
