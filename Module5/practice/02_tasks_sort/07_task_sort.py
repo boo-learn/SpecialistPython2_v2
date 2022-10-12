c5991196bc0deb8872e261be123561b2c65063c9
trds42 <trds42@gmail.com>
2022-10-12 16:36:11 +0300

    Update 07_task_sort.py

--- Module5/practice/02_tasks_sort/07_task_sort.py
+++ Module5/practice/02_tasks_sort/07_task_sort.py
@@ -11,3 +11,28 @@
 # 10 1 3 4 3 5 6 7 7 6 1
 # Выход
 # 5
+
+import random
+
+n = 11
+points = [random.randint(0, 10) for _ in range(n)]
+print(points)
+
+points.sort(reverse=True)
+print(points)
+
+prize = points[0]
+diploma = 1
+num_prizes = 0
+for point in points:
+    if point >= prize:
+        num_prizes += 1
+    else:
+        prize = point
+        diploma += 1
+        if diploma <= 3:
+            num_prizes += 1
+        else:
+            break
+print(num_prizes)
+
