a11d9d4243c2fdaa3c97be01168cc6a7e1916f5b
Mikhail32r <115464481+Mikhail32r@users.noreply.github.com>
2022-10-12 10:53:23 +0300

    Update 02_bubble_sort.py

--- Module5/examples/02_bubble_sort.py
+++ Module5/examples/02_bubble_sort.py
@@ -1,21 +1,7 @@
-# Алгоритм
-# Сначала сравниваются первые два элемента списка.
-# Если первый элемент больше, они меняются местами.
-# Если они уже в нужном порядке, оставляем их как есть. Затем переходим к следующей паре элементов,
-# сравниваем их значения и меняем местами при необходимости.
-# Этот процесс продолжается до последней пары элементов в списке.
-
 nums = [5, 2, 1, 8, 4]
-print("before sort = ", nums)
-swapped = True
-while swapped:
-    swapped = False
-    # print("*****")
-    for i in range(len(nums) - 1):
-        # print("i = ", i)
-        if nums[i] > nums[i + 1]:
-            # Меняем элементы
-            nums[i], nums[i + 1] = nums[i + 1], nums[i]
-            # Устанавливаем swapped в True для следующей итерации
-            swapped = True
+for i in range(len(nums) - 1):
+    for j in range(len(nums) - i - 1):
+        if nums[j] > nums[j + 1]:
+            nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
 print("after sort = ", nums)
