ba37904ebb2ad12c969c5dbbaf72f30cd049219b
Tobby15 <115464512+Tobby15@users.noreply.github.com>
2022-10-12 12:39:24 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -4,3 +4,22 @@
 
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
 
+sum = 0
+for i in range(5):
+    max = numbers[1]
+    index_max = 0
+    i = 0
+    for num in numbers:
+        if num > max:
+            max = num
+            index_max = i
+        i += 1
+    sum = sum + max
+    numbers.pop(index_max)
+
+print(f"SUM = {sum}")
+
+stri = ''
+for num in numbers:
+    stri = stri + ',' + str(num)
+print(stri[1:])
