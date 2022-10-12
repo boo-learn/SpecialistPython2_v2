a3c1ff7c424ec84eda24fba996bba159b6af89e8
trds42 <trds42@gmail.com>
2022-10-12 15:37:23 +0300

    Update 05_task_sort.py

--- Module5/practice/02_tasks_sort/05_task_sort.py
+++ Module5/practice/02_tasks_sort/05_task_sort.py
@@ -22,13 +22,22 @@ staff = [
     },
     {
         'name': 'Василий',
-        'surname': 'Павлов',
+        'surname': 'Иванов',
         'salary': 34500
     },
 ]
 
 # 1. Выведите список сотрудников, отсортированный по уменьшению их заработной платы.
 # Если у нескольких сотрудников одинаковая ЗП, то добавьте сортировку по Фамилии
+staff.sort(key=lambda x: x['surname'])
+staff.sort(reverse=True, key=lambda x: x['salary'])
 print("Список сотрудников отсортированный по уменьшению ЗП:")
+for person in staff:
+    print(person['name'], person['surname'], person['salary'])
 
 # 2. Найдите сумму зарплат трех самых низкооплачиваемых сотрудников:
+staff.sort(key=lambda x: x['salary'])
+sum_salary = 0
+for person in staff[:3]:
+    sum_salary += person['salary']
+print("Сумма зарплат трех самых низкооплачиваемых сотрудников:", sum_salary)
