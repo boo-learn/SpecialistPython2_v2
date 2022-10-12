71f3e7dd9ac15237a12543528a67e031b434b0ce
Aleksandr <115464601+Mox1eSylas@users.noreply.github.com>
2022-10-12 14:05:36 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -4,3 +4,11 @@
 
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
 
+
+num_summ = 0
+numbers.sort(reverse=True)
+
+for i in range(5):
+    num_summ += numbers[i]
+    
+print(num_summ)
