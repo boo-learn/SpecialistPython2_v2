3274c671f849e064d3edcc197db86c0bfdd669c6
JornGoran <115464462+JornGoran@users.noreply.github.com>
2022-10-13 16:01:16 +0300

    Update IBank_part1_1.py

--- Module8/practice/IBank/IBank_part1_1.py
+++ Module8/practice/IBank/IBank_part1_1.py
@@ -9,13 +9,13 @@ class Account:
         """
         Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
         """
-        return f"..."
+        return f"{self.name} баланс: {self.balance} руб. паcпорт: {self.passport} т.{self.phone_number}"
 
     def __repr__(self) -> str:
         """
         :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
         """
-        return f"..."
+        return f"{self.name} баланс: {self.balance} руб."
 
 
 account1 = Account("Иван", "3230 634563", "+7-900-765-12-34")
@@ -25,3 +25,4 @@ print(account1.full_info())
 account2 = Account(name="Алексей", phone_number="+7-901-744-22-99", passport="3232 456124", start_balance=500)
 print(account2)  # При отсутствии .__str__(), вызывает __repr__()
 print(account2.full_info())
+
