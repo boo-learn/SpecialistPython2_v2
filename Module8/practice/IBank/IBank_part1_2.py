b34d7e9989f7f2768c69f79fe9648d1c5a431796
MihaBro <110846905+MihaBro@users.noreply.github.com>
2022-10-13 16:18:37 +0300

    Update IBank_part1_2.py

--- Module8/practice/IBank/IBank_part1_2.py
+++ Module8/practice/IBank/IBank_part1_2.py
@@ -1,42 +1,47 @@
 class Account:
     def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
-        self.name = name
-        self.passport = passport
-        self.phone_number = phone_number
-        # self.balance = start_balance
-        self.__balance = start_balance  # TODO: Закрываем прямой доступ к балансу
+        self.__name = name
+        self.__passport = passport
+        self.__phone_number = phone_number
+        self.__balance = start_balance
 
     def full_info(self) -> str:
         """
         Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
         """
-        return f"..."
+        return f'{self.__name} баланс: {self.__balance} руб. паспорт: {self.__passport} т.{self.__phone_number}'
 
     def __repr__(self) -> str:
         """
         :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
         """
-        return f"..."
+        return f'{self.__name} баланс: {self.__balance} руб.'
+
 
     # TODO: совместно с преподавателем реализуйте getter для просмотра баланса
     #  Можете попробовать самостоятельно: https://pythobyte.com/using-getters-and-setters-in-python-5205-840ed13f/
     @property
     def balance(self) -> int:
-        return ...
+        return f'{self.__balance} руб.'
 
     def deposit(self, amount: int) -> None:
         """
         Внесение суммы на текущий счет
         :param amount: сумма
         """
-        pass
+        self.__balance += amount
+        return self.__balance
 
     def withdraw(self, amount: int) -> None:
         """
         Снятие суммы с текущего счета
         :param amount: сумма
         """
-        pass
+        if self.__balance - amount < 0:
+            print(f'Недостаточно средств')
+        else:
+            self.__balance -= amount
+            return self.__balance
 
 
 # Создаем тестовый аккаунт:
