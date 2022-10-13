d99f58e40544c179cd5ad15e804d62cf8f544115
Aleksandr <115464601+Mox1eSylas@users.noreply.github.com>
2022-10-13 16:18:59 +0300

    Update IBank_part1_2.py

--- Module8/practice/IBank/IBank_part1_2.py
+++ Module8/practice/IBank/IBank_part1_2.py
@@ -10,33 +10,27 @@ class Account:
         """
         Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
         """
-        return f"..."
+        return f"{self.__repr__()} паспорт: {self.passport} т.{self.phone_number}"
 
     def __repr__(self) -> str:
         """
         :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
         """
-        return f"..."
+        return f"{self.name} {self.balance}"
 
     # TODO: совместно с преподавателем реализуйте getter для просмотра баланса
     #  Можете попробовать самостоятельно: https://pythobyte.com/using-getters-and-setters-in-python-5205-840ed13f/
     @property
     def balance(self) -> int:
-        return ...
+        return f"баланс {self.__balance} руб."
 
     def deposit(self, amount: int) -> None:
-        """
-        Внесение суммы на текущий счет
-        :param amount: сумма
-        """
-        pass
+        self.__balance = self.__balance+amount
 
     def withdraw(self, amount: int) -> None:
-        """
-        Снятие суммы с текущего счета
-        :param amount: сумма
-        """
-        pass
+        if self.__balance >= amount:
+            self.__balance = self.__balance-amount
+
 
 
 # Создаем тестовый аккаунт:
