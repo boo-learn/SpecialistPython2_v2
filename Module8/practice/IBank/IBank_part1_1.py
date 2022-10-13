34ae1fc7e5d56266c9c880600fd0188529453d6e
Mikhail32r <115464481+Mikhail32r@users.noreply.github.com>
2022-10-13 16:13:53 +0300

    Update IBank_part1_1.py

--- Module8/practice/IBank/IBank_part1_1.py
+++ Module8/practice/IBank/IBank_part1_1.py
@@ -3,25 +3,43 @@ class Account:
         self.name = name
         self.passport = passport
         self.phone_number = phone_number
-        self.balance = start_balance
+        if start_balance < 0:
+            raise ValueError("Балланс не может быть отрицательным")
+        else:
+            self.balance = start_balance
 
-    def full_info(self) -> str:
+    def up_balance(self,up_bal):
+        self.balance+=up_bal
+
+    def down_balance(self,up_bal):
+        if (self.balance-up_bal) < 0:
+            raise ValueError("Балланс не может быть отрицательным")
+        else:
+            self.balance-=up_bal
+
+    def full_info(self):
         """
         Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
         """
-        return f"..."
+        return f"{self.name} баланс: {self.balance} руб. Паспорт: {self.passport} т. {self.phone_number}"
 
     def __repr__(self) -> str:
         """
         :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
         """
-        return f"..."
+        return f"{self.name} баланс: {self.balance} руб. Паспорт: {self.passport} т. {self.phone_number}"
 
 
 account1 = Account("Иван", "3230 634563", "+7-900-765-12-34")
 print(account1)  # При отсутствии .__str__(), вызывает __repr__()
 print(account1.full_info())
 
-account2 = Account(name="Алексей", phone_number="+7-901-744-22-99", passport="3232 456124", start_balance=500)
+
+#account1.down_balance(100)
+
+account2 = Account(name="Алексей", phone_number="+7-901-744-22-99", passport="3232 456124", start_balance=-500)
 print(account2)  # При отсутствии .__str__(), вызывает __repr__()
+print(account2.full_info())
+
+account2.up_balance(100)
 print(account2.full_info())
