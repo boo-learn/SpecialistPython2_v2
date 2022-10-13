1adee7e7805add398e1b56b43e70bce66fcb3878
iscradly <101692879+iscradly@users.noreply.github.com>
2022-10-13 20:56:38 +0300

    Update IBank_part1_3.py

--- Module8/practice/IBank/IBank_part1_3.py
+++ Module8/practice/IBank/IBank_part1_3.py
@@ -1,5 +1,43 @@
 class Account:
     # TODO-0: скопируйте реализацию из предыдущего решения
+    def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
+        self.name = name
+        self.passport = passport
+        self.phone_number = phone_number
+        # self.balance = start_balance
+        self.__balance = start_balance  # TODO: Закрываем прямой доступ к балансу
+
+    def full_info(self) -> str:
+        """
+        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
+        """
+        return f"{self.name} баланс: {self.balance} руб. паcпорт: {self.passport} т.{self.phone_number}"
+
+    def __repr__(self) -> str:
+        """
+        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.balance} руб."
+
+    @property
+    def balance(self) -> int:
+        return self.__balance
+
+    def deposit(self, amount: int) -> None:
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        """
+        self.__balance += amount
+
+    def withdraw(self, amount: int) -> None:
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        """
+        if self.balance < amount:
+            raise ValueError("Недостаточно средств")
+        self.__balance -= amount
 
     # TODO-1: напишите реализацию метода transfer()
     def transfer(self, target_account: 'Account', amount: int) -> None:
@@ -9,31 +47,11 @@ class Account:
         :param amount: сумма перевода
         :return:
         """
-        pass
-
-
-account1 = Account("Иван", "3230 634563", "+7-900-765-12-34", 1000)
-account2 = Account("Алексей", "+7-901-744-22-99", "3232 456124", 200)
-
-print(account1)
-print(account2)
-
-# Переводим деньги с первого аккаунт на второй:
-try:
-    account1.transfer(account2, 500)
-except ValueError as e:
-    print(e)
-
-# Проверяем изменения баланса:
-print(account1)
-print(account2)
-
-# Переводим еще с первого аккаунт на второй:
-try:
-    account1.transfer(account2, 600)
-except ValueError as e:
-    print(e)
-
-# Проверяем изменения баланса:
-print(account1)
-print(account2)
+    ####Вариант 1
+        # if self.__balance < amount:
+        #     raise ValueError("Недостаточно средств")
+        # self.__balance -= amount
+        # target_account.__balance += amount
+    ####Вариант 2
+        self.withdraw(amount)
+        target_account.deposit(amount)
