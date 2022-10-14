144dcf4caa8f2ba156736b420bca8cf0c1808eea
trds42 <trds42@gmail.com>
2022-10-14 11:05:31 +0300

    Update IBank_part2.py

--- Module8/practice/IBank/IBank_part2.py
+++ Module8/practice/IBank/IBank_part2.py
@@ -1,7 +1,6 @@
 from typing import List
-# Сюда отправляем готовое решение IBank часть-2
 
-# класс для хранение информации об операциях
+
 class Operation:
     # Типы операций храним в свойствах класса
     DEPOSIT = 'Пополнение'
@@ -9,25 +8,47 @@ class Operation:
     TRANSFER_OUT = 'Перевод'
     TRANSFER_IN = 'Поступление'
 
-    # Напоминаю: обращение к этим переменным происходит через имя класса, пример: Operation.WITHDRAW
-
-    def __init__(self, ):
-        # TODO: Тут добавляем свойства для хранение информации об операции
-        pass
+    def __init__(self, amount, type, target_account=None):
+        self.type = type
+        self.amount = amount
+        self.target_account = target_account
 
     def __repr__(self) -> str:
         """
         :return: возвращает строковое представление операции. Формат указан в 02_IBank_part2.md
         """
-        pass
+        info = ""
+        if self.type == Operation.TRANSFER_IN:
+            info = f' со счета клиента: {self.target_account}'
+        elif self.type == Operation.TRANSFER_OUT:
+            info = f' на счет клиента: {self.target_account}'
+        return f'{self.type} {self.amount} руб.{info}'
 
 
 class Account:
-    # TODO: сюда копируем реализацию класса Account из предыдущей задачи
-    # TODO: историю храним как список объектов класса Operation, добавив свойство в конструктор:
-    #   self.__history = []
+    def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
+        self.name = name
+        self.passport = passport
+        self.phone_number = phone_number
+        self.__balance = start_balance
+        self.__history = []
 
-    # Данный метод дан в готовом виде. Изучите его и используйте как пример, для доработки других
+    def full_info(self) -> str:
+        """
+        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
+        """
+        return f"{self.name} баланс: {self.balance} руб. паспорт: {self.passport} т.{self.phone_number}"
+
+    def __repr__(self) -> str:
+        """
+        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.balance} руб."
+
+    @property
+    def balance(self) -> int:
+        return self.__balance
+
     def deposit(self, amount: int, to_history: bool = True) -> None:
         """
         Внесение суммы на текущий счет
@@ -37,6 +58,31 @@ class Account:
         self.__balance += amount
         if to_history:
             self.__history.append(Operation(amount, Operation.DEPOSIT))
+
+    def withdraw(self, amount: int, to_history: bool = True) -> None:
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        :param to_history: True - записывать операцию в историю, False - не записывать
+        """
+        if amount <= self.__balance:
+            self.__balance -= amount
+            if to_history:
+                self.__history.append(Operation(amount, Operation.WITHDRAW))
+        else:
+            raise ValueError("Not enough money")
+
+    def transfer(self, target_account: 'Account', amount: int) -> None:
+        """
+        Перевод денег на счет другого клиента
+        :param target_account: счет клиента для перевода
+        :param amount: сумма перевода
+        :return:
+        """
+        self.withdraw(amount, False)
+        self.__history.append(Operation(amount, Operation.TRANSFER_OUT, target_account))
+        target_account.deposit(amount, False)
+        target_account.__history.append(Operation(amount, Operation.TRANSFER_IN, self))
 
     def get_history(self) -> List[str]:
         """
