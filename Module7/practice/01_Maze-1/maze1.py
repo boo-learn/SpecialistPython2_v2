4651e9bcd9b30767891cfae395a9c9888b44ad03
Aleksandr <115464601+Mox1eSylas@users.noreply.github.com>
2022-10-14 14:53:07 +0300

    Update maze1.py

--- Module7/practice/01_Maze-1/maze1.py
+++ Module7/practice/01_Maze-1/maze1.py
@@ -1,8 +1,41 @@
-# Выверите и скопируйте сюда наиболее подходяший алгоритм для работы с графом(bfs/dfs)
+def dfs(start_point, graph):
+    visited = [False] * len(graph)
+
+    def _dfs(v):
+        visited[v] = True
+        for w in graph[v]:
+            if not visited[w]:  # посещён ли текущий сосед?
+                _dfs(w)
+
+    _dfs(start_point)
+    return visited
 
 # Опишите список смежности по изображению лабиринта из файла task.md
 graph = [
-    ...
+    [4],  # 0
+    [2],  # 1
+    [1,3],  # 2
+    [2],  # 3
+    [0],  # 4
+    [6,9],  # 5
+    [5,7],  # 6
+    [6,11],  # 7
+    [],  # 8
+    [5,13],  # 9
+    [],  # 10
+    [7,15],  # 11
+    [13],  # 12
+    [12,9,14],  # 13
+    [13],  # 14
+    [11]  # 15
 ]
 
-# Решите задачу и выведите ответ в нужном формате
+
+start_point = 14
+vis = dfs(start_point, graph)
+for num, vi in enumerate(vis,1):
+    if vi:
+        print(f"Из точки S-{num} можно дойти до финиша")
+    else:
+        print(f"Из точки S-{num} нельзя дойти до финиша")
+print(vis)
