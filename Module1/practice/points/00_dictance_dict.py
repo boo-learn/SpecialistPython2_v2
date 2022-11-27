b6f24d210ec3d9fde2a4326d582f1ca54051af70
Haski21 <81989841+Haski21@users.noreply.github.com>
2022-11-27 11:53:35 +0300

    Update 00_dictance_dict.py
    Фокин Илья

--- Module1/practice/points/00_dictance_dict.py
+++ Module1/practice/points/00_dictance_dict.py
@@ -1,8 +1,12 @@
+import math
+
 def distance(p1: dict, p2: dict) -> float:
     """
     Расстояние между двумя точками
     """
     # TODO: напишите тело функции
+    #a = math.sqrt((p2['x'] - p1['x']) ** 2) + math.sqrt((p2['y'] - p1['x']) ** 2)
+    return math.sqrt((p2['x'] - p1['x']) ** 2) + math.sqrt((p2['y'] - p1['y']) ** 2)
 
 
 # Даны две точки на координатной плоскости
