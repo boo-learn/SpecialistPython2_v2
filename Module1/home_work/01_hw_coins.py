b2d9b2082cd249a5e591a212b1405b373e49ffbe
Ivan Tikhomirov <54577016+Neumark1982@users.noreply.github.com>
2022-11-28 20:17:57 +0300

    Update 01_hw_coins.py
    Home_Work - Тихомиров Иван

--- Module1/home_work/01_hw_coins.py
+++ Module1/home_work/01_hw_coins.py
@@ -9,8 +9,31 @@ class Coin:
         """
         Подбрасывание монетки. # heads-орел/tails-решка
         """
-        self.side = ...  # random: heads/tails
+        side_tuple = ('heads', 'tails')
+        self.side = random.choice(side_tuple)  # random: heads/tails
 
+        
+coin_list = []
+flip_results_dict = {'heads':0, 'tails':0}
+
+quantity_of_coins = int(input('Enter quantity of coins you need: '))
+
+for i in range(quantity_of_coins):
+    coin = Coin()
+    coin_list.append(coin)
+
+print(flip_results_dict.keys())
+
+
+for value in coin_list:
+    value.flip()
+    if value.side == 'heads':
+        flip_results_dict['heads'] +=1
+    else:
+        flip_results_dict['tails'] +=1
+
+print('Процентное соотношение выпавших орлов {:.2f}'.format((flip_results_dict['heads'] / quantity_of_coins) * 100))
+print('Процентное соотношение выпавших решек {:.2f}'.format((flip_results_dict['tails'] / quantity_of_coins) * 100))
 # Задание:
 # 1. Создайте список из n-монеток, n - вводится с клавиатуры
 # 2. Подбросьте(flip) все монетки. У каждой монетки в списке вызовите метод .flip()
