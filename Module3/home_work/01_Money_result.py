638e405a30a598bd5a07941832e207c0b380ceaa
Haski21 <81989841+Haski21@users.noreply.github.com>
2022-12-05 23:29:25 +0300

    Update 01_Money_result.py

--- Module3/home_work/01_Money_result.py
+++ Module3/home_work/01_Money_result.py
@@ -1,3 +1,78 @@
+import urllib.request
+
 class Money:
-    pass
-    # TODO: your code here
+    
+        def __init__(self, rubls = 0, kops = 0) -> None:
+            self.rubls = rubls
+            self.kops = kops
+            self.conversion_money()
+            
+        def conversion_money(self) -> None:
+            while self.kops >= 100:
+                self.rubls += 1
+                self.kops -= 100
+
+        def __str__(self):
+            self.conversion_money()
+            return f'{self.rubls} руб {self.kops} коп'
+        
+        def __add__(self, other):
+            temp_m = Money()
+            temp_m.rubls = self.rubls + other.rubls
+            temp_m.kops = self.kops + other.kops
+            return f'{str(temp_m)}'
+
+    
+        def  __sub__(self, other):
+            temp_m = Money()
+            if self.kops < other.kops:
+                temp_m.rubls = self.rubls - other.rubls - 1
+                temp_m.kops = self.kops - other.kops + 100
+                return f'{str(temp_m)}'
+            else:
+                temp_m.rubls = self.rubls - other.rubls
+                temp_m.kops = self.kops - other.kops
+                return f'{str(temp_m)}'
+         
+        def __mul__(self, number):
+            self.rubls *= number
+            self.kops *= number
+            return f'{str(self)}'
+
+        def __lt__(self, other):
+            return self.rubls <= other.rubls and self.kops < other.kops
+        
+        def __gt__(self,other):
+            return self.rubls >= other.rubls and self.kops > other.kops
+        
+        def __eq__(self,other):
+            return self.rubls == other.rubls and self.kops == other.kops
+        
+        def __ne__(self,other):
+            return not self.rubls == other.rubls and self.kops == other.kops
+        
+        def __mod__(self, procent):
+            temp_M = Money()
+            temp_M.kops = round((self.rubls * 100 + self.kops) * procent / 100)
+            return f'{str(temp_M)}'
+
+#  Не понимаю реализацию
+#         def convert(self): 
+#             data = urllib.request.urlopen(url).read()
+            
+
+
+sum1 = Money(18, 15)
+sum2 = Money(12, 420)
+
+print(sum1 + sum2)
+print(sum1 - sum2)
+print(sum1 * 5)
+
+print(sum1 > sum2)
+print(sum1 < sum2)
+print(sum1 == sum2)
+print(sum1 != sum2)
+
+
+print(sum2 % 21)
