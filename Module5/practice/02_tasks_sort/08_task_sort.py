0f25d8e04c3908be17b75d3ce753d9d3010cc70c
Ivan Tikhomirov <54577016+Neumark1982@users.noreply.github.com>
2022-12-14 23:52:26 +0300

    Update 08_task_sort.py

--- Module5/practice/02_tasks_sort/08_task_sort.py
+++ Module5/practice/02_tasks_sort/08_task_sort.py
@@ -3,3 +3,17 @@
 # Телефон задан в виде строки формата nn-nn-nn. Например, 23-45-67
 
 phones = ["25-17-58", "11-34-85", "54-61-56", "34-61-72", "25-17-55", "34-56-56"]
+
+i = 0
+
+while i < len(phones) - 1:
+    m = i
+    j = i + 1
+    while j < len(phones):
+        if phones[j] < phones[m]:
+            m = j
+        j += 1
+    phones[i], phones[m] = phones[m], phones[i]
+    i += 1
+
+print(phones)
