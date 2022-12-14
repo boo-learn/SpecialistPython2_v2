5917502c12a04f3381e86ae38943c01d12fa62ef
Ivan Tikhomirov <54577016+Neumark1982@users.noreply.github.com>
2022-12-14 23:55:27 +0300

    Update 06_task_sort.py

--- Module5/practice/02_tasks_sort/06_task_sort.py
+++ Module5/practice/02_tasks_sort/06_task_sort.py
@@ -13,3 +13,27 @@
 # 70
 # Пояснение:
 # Возможен такой порядок: 10 2 50 1 10
+
+goods_price = [2, 1, 10, 50, 100, 70, 25, 56, 36, 99,30]
+max_check_sum = 0
+
+i = 0
+while i < len(goods_price) - 1:
+    j = i + 1
+    while j < len(goods_price):
+        m = i
+        if goods_price[j] > goods_price[m] and m % 2 == 0:
+            m = j
+            goods_price[i], goods_price[m] = goods_price[m], goods_price[i]
+        elif goods_price[j] < goods_price[m] and m % 2 != 0:
+            m = j
+            goods_price[i], goods_price[m] = goods_price[m], goods_price[i]
+        j += 1
+    i += 1
+
+i = 0    
+while i < len(goods_price):
+    max_check_sum += goods_price[i]
+    i += 2
+
+print(max_check_sum)
