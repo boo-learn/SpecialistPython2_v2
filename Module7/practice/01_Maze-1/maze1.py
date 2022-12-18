b98349f5b7d36bfbfb8926cd3dac8227532ad0d1
Diana <106874021+Melifo@users.noreply.github.com>
2022-12-18 14:28:48 +0300

    Update maze1.py

--- Module7/practice/01_Maze-1/maze1.py
+++ Module7/practice/01_Maze-1/maze1.py
@@ -1,8 +1,40 @@
-# Выверите и скопируйте сюда наиболее подходяший алгоритм для работы с графом(bfs/dfs)
+def dfs(start_vertex, graph):
+    visited = [False] * len(graph)
 
-# Опишите список смежности по изображению лабиринта из файла task.md
-graph = [
-    ...
+    def _dfs(v):
+        visited[v] = True
+        for w in graph[v]:
+            if not visited[w]:
+                _dfs(w)
+
+    _dfs(start_vertex)
+    return visited
+
+
+maze = [
+    [1],  # 0
+    [0, 5],  # 1
+    [6],  # 2
+    [7],  # 3
+    [5, 8],  # 4
+    [1, 4],  # 5
+    [2, 10],  # 6
+    [3],  # 7
+    [4, 9, 12],  # 8
+    [8, 10],  # 9
+    [6, 9, 11, 14],  # 10
+    [10],  # 11
+    [8],  # 12
+    [],  # 13
+    [10, 15],  # 14
+    [14]  # 15
 ]
+start = [0, 3, 12]
+finish = 14
 
-# Решите задачу и выведите ответ в нужном формате
+for point in start:
+    run = dfs(point, maze)
+    if run [finish] == True:
+        print(f"Из точки {point} можно дойти до финиша")
+    else:
+        print(f"Из точки {point} нельзя дойти до финиша")
