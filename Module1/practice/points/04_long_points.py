ee025dac339d4f575a334a9c3b24e0859f87e660
VladislaffP <120783153+VladislaffP@users.noreply.github.com>
2022-12-19 22:00:49 +0300

    Update 04_long_points.py
    Как можно оптимизировать такое решение?

--- Module1/practice/points/04_long_points.py
+++ Module1/practice/points/04_long_points.py
@@ -7,12 +7,24 @@ class Point:
         self.y = y
 
     def dist_to(self, other_point: Point) -> float:
-        # TODO-0: скопируйте реализацию из предыдущей задачи
-        ...
+        return ((other_point.x - self.x) ** 2 + (other_point.y - self.y) ** 2) ** 0.5
 
 
 # Дан список из произвольного количества точек:
 points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)]
+point0 = Point(0,0)
+
+maxd = 0
+maxn = 0
+
+for p in points:
+    dist = round(p.dist_to(point0), 3)
+    if dist > maxd:
+        maxd = dist
+        maxn += 1
+    print(f"x = {p.x}, y = {p.y}, d = {round(p.dist_to(point0), 3)}")
+    
+print(f"Координаты наиболее удаленной точки = ({points[maxn].x}, {points[maxn].y}, Дистанция: {maxd}")
 
 # TODO-1: найдите точку наиболее удаленную от начала координат и выведите ее координаты
 
