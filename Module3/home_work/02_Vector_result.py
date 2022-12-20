67b7c124819bfe89667822b307bdde2c8df1e212
Dmitry Garkavchenko <55814047+dmitrygar@users.noreply.github.com>
2022-12-21 01:03:29 +0300

    Update 02_Vector_result.py

--- Module3/home_work/02_Vector_result.py
+++ Module3/home_work/02_Vector_result.py
@@ -2,4 +2,40 @@
 # Само задание в файле 01_Vector_task.md
 
 class Vector:
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return Vector(x, y)
+
+    def __mul__(self, other):
+        x = self.x * other
+        y = self.y * other
+        return Vector(x, y)
+
+    def __rmul__(self, other):
+        x = self.x * other
+        y = self.y * other
+        return Vector(x, y)
+
+    def __str__(self):
+        return f"({self.x};{self.y})"
+
+
+a = Vector(0, 1)
+b = Vector(1, 0)
+print(a, b)
+print(a+b)
+print(b+a)
+print(a-b)
+print((a+b)*2)
+print(2*a + 2*b)
+
