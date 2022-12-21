e1f0947cacdb7c4bf3b0cb3c028392737906f856
Dmitry Garkavchenko <55814047+dmitrygar@users.noreply.github.com>
2022-12-21 19:22:55 +0300

    Update 03_cards_list.py

--- Module4/practice/deck/03_cards_list.py
+++ Module4/practice/deck/03_cards_list.py
@@ -1,21 +1,26 @@
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        icons = {"Hearts": '\u2665',
+                 "Diamonds": '\u2666',
+                 "Clubs": '\u2667',
+                 "Spades": '\u2664'
+                 }
+        return f"{self.value}{icons.get(self.suit)}"
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
 
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-hearts_cards = []
-# TODO-1: добавьте в список hearts_cards все червовые карты(от 2-ки до туза)
+# добавьте в список hearts_cards все червовые карты(от 2-ки до туза)
+hearts_cards = [Card(val, "Hearts") for val in values]
 
-diamonds_cards = []
-# TODO-2: добавьте в список diamonds_cards все бубновые карты(от туза до 2-ки)
+# добавьте в список diamonds_cards все бубновые карты(от туза до 2-ки)
+diamonds_cards = [Card(val, "Diamonds") for val in reversed(values)]
 
-# TODO-3: выведите все карты из списка hearts_cards в терминал через запятую в одну строку:
-# Пример вывода: 2♥, 3♥, 4♥ ... A♥
-
-
-cards = [
-    Card("2", "Hearts"),
-    Card("3", "Hearts"),
-    Card("4", "Hearts"),
-    ...]
+# выведите все карты из списка hearts_cards в терминал через запятую в одну строку:
+print(', '.join([card.to_str() for card in hearts_cards]))
