c822659eaa3ec6d20ea203a4a5cbf0a05828617a
Dmitry Garkavchenko <55814047+dmitrygar@users.noreply.github.com>
2022-12-21 20:18:48 +0300

    Update 06_deck_part2.py

--- Module4/practice/deck/06_deck_part2.py
+++ Module4/practice/deck/06_deck_part2.py
@@ -1,28 +1,40 @@
+import itertools
+import random
+
+
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        icons = {"Hearts": '\u2665',
+                 "Diamonds": '\u2666',
+                 "Clubs": '\u2667',
+                 "Spades": '\u2664'
+                 }
+        return f"{self.value}{icons.get(self.suit)}"
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
 
 
 class Deck:
     def __init__(self):
-        # Список карт в колоде. Каждым элементом списка будет объект класса Card
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
-        self.cards = []
-        # TODO-0: конструктор копируем из предыдущей задачи
+        self.cards = [Card(value, suite) for suite, value in itertools.product(suits, values)]
 
     def show(self):
-        # TODO-0: копируем из предыдущей задачи
-        ...
+        return f'deck[{len(self.cards)}]: {", ".join([card.to_str() for card in self.cards])}'
 
     def draw(self, x):
-        # TODO-1: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+        cards = self.cards[0:x]
+        del(self.cards[0:x])
+        return cards
 
     def shuffle(self):
-        # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
-        #   Подсказка: https://www.w3schools.com/python/ref_random_shuffle.asp
-        ...
+        random.shuffle(self.cards)
 
 
 # Создаем колоду
@@ -35,7 +47,9 @@ print(deck.show())
 
 # Возьмем 5 карт "в руку"
 hand = deck.draw(5)
+
 # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
 print(deck.show())
+
 # Выводим список карт "в руке"(список hand)
-print(...)
+print(f'hand[{len(hand)}]: {", ".join([card.to_str() for card in hand])}')
