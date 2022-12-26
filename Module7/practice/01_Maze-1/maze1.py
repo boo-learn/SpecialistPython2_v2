4fcf601f192cee8c33834a7bff40d50db0de5683
VladislaffP <120783153+VladislaffP@users.noreply.github.com>
2022-12-26 20:14:25 +0300

    Update maze1.py

--- Module7/practice/01_Maze-1/maze1.py
+++ Module7/practice/01_Maze-1/maze1.py
@@ -1,8 +1,29 @@
-# Выверите и скопируйте сюда наиболее подходяший алгоритм для работы с графом(bfs/dfs)
-
-# Опишите список смежности по изображению лабиринта из файла task.md
 graph = [
-    ...
+    # список смежности
+    [1],         # 0
+    [0, 4],   # 1
+    [5],         # 2
+    [4, 6],      # 3
+    [1, 3, 5],         # 4
+    [2, 4],      # 5
+    [3],            # 6
+    [8],            # 7
+    [7]             # 8
 ]
 
-# Решите задачу и выведите ответ в нужном формате
+def dfs(graph, start_point):
+    visited = [False] * (len(graph))
+
+    def _dfs(v):
+        visited[v] = True
+        for w in graph[v]:
+            if not visited[w]:  # посещён ли текущий сосед?
+                _dfs(w)
+    _dfs(start_point)
+    return visited
+
+#print(dfs(graph, 0))
+if dfs(graph, 0)[7]:
+    print("Сan go to the bank")
+else:
+    print("Can't go to the bank")
