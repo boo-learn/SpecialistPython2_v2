456fd6fc2908c29dcdec9c33cd4efe136e9e1e6a
Dmitry Garkavchenko <55814047+dmitrygar@users.noreply.github.com>
2022-12-27 23:01:36 +0300

    Update IBank_part1_3.py

--- Module8/practice/IBank/IBank_part1_3.py
+++ Module8/practice/IBank/IBank_part1_3.py
@@ -1,39 +1,41 @@
 class Account:
-    # TODO-0: скопируйте реализацию из предыдущего решения
+    def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
+        self.name = name
+        self.passport = passport
+        self.phone_number = phone_number
+        self.__balance = start_balance  # закрываем прямой доступ к балансу
 
-    # TODO-1: напишите реализацию метода transfer()
-    def transfer(self, target_account: 'Account', amount: int) -> None:
+    def full_info(self) -> str:
         """
-        Перевод денег на счет другого клиента
-        :param target_account: счет клиента для перевода
-        :param amount: сумма перевода
-        :return:
+        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
         """
-        pass
+        return f"{self.name} баланс: {self.balance} руб. паспорт: {self.passport} т.{self.phone_number}"
 
+    def __repr__(self) -> str:
+        """
+        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.balance} руб."
 
-account1 = Account("Иван", "3230 634563", "+7-900-765-12-34", 1000)
-account2 = Account("Алексей", "3232 456124", "+7-901-744-22-99", 200)
+    #  реализуйте getter для просмотра баланса
+    #  Подробнее тут: https://pythobyte.com/using-getters-and-setters-in-python-5205-840ed13f/
+    @property
+    def balance(self) -> int:
+        return self.__balance
 
-print(account1)
-print(account2)
+    def deposit(self, amount: int) -> None:
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        """
+        self.__balance += amount
 
-# Переводим деньги с первого аккаунт на второй:
-try:
-    account1.transfer(account2, 500)
-except ValueError as e:
-    print(e)
-
-# Проверяем изменения баланса:
-print(account1)
-print(account2)
-
-# Переводим еще с первого аккаунт на второй:
-try:
-    account1.transfer(account2, 600)
-except ValueError as e:
-    print(e)
-
-# Проверяем изменения баланса:
-print(account1)
-print(account2)
+    def withdraw(self, amount: int) -> None:
+        """
+        Снятие суммы с текущего счета
+        :param amount: сумма
+        """
+        if amount <= self.balance:
+            self.__balance -= amount
+        else:
+            raise ValueError("Недостаточно средств")
