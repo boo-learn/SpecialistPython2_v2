6211a29eaad8f7b4348e3c8bba4850f64940febe
boo <eyurchenko@specialist.ru>
2022-12-27 19:17:57 +0700

    change inheritance example

--- Module8/examples/1_inheritance.py
+++ Module8/examples/1_inheritance.py
@@ -1,101 +1,39 @@
-# Наследование подразумевает, что дочерний класс содержит все атрибуты родительского класса,
-# при этом некоторые из них могут быть переопределены или добавлены в дочернем.
-
-
 # Рассмотрим два класса
-class Student:
-    def __init__(self, name, surname, birth_date, school, class_room):
+class Person:
+    def __init__(self, name):
         self.name = name
-        self.surname = surname
-        self.birth_date = birth_date
-        self.school = school
-        self._class_room = {'class_num': int(class_room.split()[0]),
-                            'class_char': class_room.split()[1]}
 
-    @property
-    def class_room(self):
-        return "{} {}".format(self._class_room['class_num'], self._class_room['class_char'])
-
-    def next_class(self):
-        self._class_room['class_num'] += 1
-
-    def get_full_name(self):
-        return self.name + ' ' + self.surname
-
-    def set_name(self, new_name):
-        self.name = new_name
+    def greet(self):
+        return f"Hi, it's {self.name}"
 
 
-class Teacher:
-    def __init__(self, name, surname, birth_date, school, teach_classes):
+class Employee:
+    def __init__(self, name, job_title):
         self.name = name
-        self.surname = surname
-        self.birth_date = birth_date
-        self.school = school
-        self.teach_classes = list(map(self.convert_class, teach_classes))
+        self.job_title = job_title
 
-    def convert_class(self, class_room):
-        """
-        '<class_num> <class_int>' --> {'class_num': <class_num>, 'class_int': <class_int>}
-        """
-        return {'class_num': int(class_room.split()[0]),
-                'class_char': class_room.split()[1]}
+    def greet(self):
+        return f"Hi, it's {self.name}"
 
-    def get_full_name(self):
-        return self.name + ' ' + self.surname
-
-    def set_name(self, new_name):
-        self.name = new_name
-
-# Эти Классы описывают два различных объекта
-# Но часть информации у них общая(атрибуты, методы)
+    def my_job(self):
+        return f"I'm {self.job_title}"
 
 
-# Общую информацию выносим в Класс-предок (родитель)
-class People:
-    def __init__(self, name, surname, birth_date, school):
-        self.name = name
-        self.surname = surname
-        self.birth_date = birth_date
-        self.school = school
-
-    def get_full_name(self):
-        return self.name + ' ' + self.surname
-
-    def set_name(self, new_name):
-        self.name = new_name
+# Эти Классы описывают два различных типа объектов
+# Часть информации у них общая(атрибуты, методы), а часть отличается
 
 
-# Сами классы наследуем
-class Student(People):
-    def __init__(self, name, surname, birth_date, school, class_room):
-        # Явно вызываем конструктор родительского класса
-        People.__init__(self, name, surname, birth_date, school)
-        # Добавляем уникальные атрибуты
-        self._class_room = {'class_num': int(class_room.split()[0]),
-                            'class_char': class_room.split()[1]}
+# Чтобы избежать дублирования кода, воспользуемся наследованием.
+# Наследуем класс Employee от Person:
+class Employee(Person):
+    def __init__(self, name, job_title):
+        # Вызываем конструктор родителя, чтобы создать атрибут .name
+        super().__init__(name)
+        # Добавляем новый атрибут, которого не было у родителя:
+        self.job_title = job_title
 
-    # И уникальные методы
-    @property
-    def class_room(self):
-        return "{} {}".format(self._class_room['class_num'], self._class_room['class_char'])
+    # Метод greet() - описывать не нужно, он будет унаследован от родителя
 
-    def next_class(self):
-        self._class_room['class_num'] += 1
-
-
-class Teacher(People):
-    def __init__(self, name, surname, birth_date, school, teach_classes):
-        People.__init__(self, name, surname, birth_date, school)
-        self.teach_classes = list(map(self.convert_class, teach_classes))
-
-    # Уникальный метод Учителя
-    def convert_class(self, class_room):
-        """
-        '<class_num> <class_int>' --> {'class_num': <class_num>, 'class_int': <class_int>}
-        """
-        return {'class_num': int(class_room.split()[0]),
-                'class_char': class_room.split()[1]}
-
-# Наследование позволяет избежать дублирования и кода
-        # и повторно использовать уже готовые реализации
+    # Описываем новый метод, которого не было у родителя:
+    def my_job(self):
+        return f"I'm {self.job_title}"
