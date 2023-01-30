cfac8557c1ad00b01bb553e3755d383e90bf28bf
iakrapivin <122812457+iakrapivin@users.noreply.github.com>
2023-01-30 20:29:29 +0300

    Update 01_distance.py

--- Module1/practice/points/01_distance.py
+++ Module1/practice/points/01_distance.py
@@ -1,3 +1,4 @@
+# TASK 1 ----------------------------------------------------------------
 class Point:
     def __init__(self, x: int, y: int):
         self.x = x
@@ -8,13 +9,11 @@ def distance(p1: Point, p2: Point) -> float:
     """
     Расстояние между двумя точками
     """
-    # TODO: напишите тело функции
+    return round((((p1.x - p2.x)**2 + (p1.y - p2.y)**2)**0.5), 2)
 
 
 # Даны две точки на координатной плоскости
 point1 = Point(2, 4)
 point2 = Point(5, -2)
 
-dist = distance(point1, point2)  # Передаем объекты point1 и point2 в функцию
-
-print("Расстояние между точками = ", dist)
+dist = distance(point1, point2)
