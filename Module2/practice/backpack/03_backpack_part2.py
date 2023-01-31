122c655827a349edb61adad47d8f9063e5b63e91
iakrapivin <122812457+iakrapivin@users.noreply.github.com>
2023-01-31 20:31:11 +0300

    Update 03_backpack_part2.py

--- Module2/practice/backpack/03_backpack_part2.py
+++ Module2/practice/backpack/03_backpack_part2.py
@@ -1,12 +1,40 @@
 class Item:
-    ...
-    # TODO: сюда копируем реализацию класса из предыдущего задания
+    def __init__(self, name: str, weight: float, cost: int):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
+
+    def show(self) -> str:
+        """
+        Возвращает строковое представление объекта Item
+        """
+        return f"{self.name} вес:{self.weight} цена:{self.cost}"
 
 
 class BackPack:  # рюкзак
-    ...
+    def __init__(self, max_weight: int):
+        self.items = []  # Предметы, которые хранятся в рюкзаке
+        self.max_weight = max_weight # максимальный суммарный вес предметов, которые можно положить в рюкзак
 
     # TODO: сюда копируем реализацию класса из предыдущего задания
+
+    def add_item(self, item: Item) -> None:
+        """
+        Добавляет предмет(item) в этот рюкзак
+        """
+        if self.max_weight - self.sum_weight() >= item.weight :
+            self.items.append(item)
+        else:
+            print(f"Предмет {item.name} слишком тяжелый")
+
+    def show_items(self) -> None:
+        """
+        Выводит(print'ом) все предметы, содержащиеся в рюкзаке в виде нумерованного списка
+        """
+        # TODO: реализуйте метод
+        for i, item in enumerate(self.items):
+            print(f"{i+1}) {item.name}")
+
     # TODO: добавьте новое свойство .max_weight - максимальный суммарный вес предметов, которые можно положить в рюкзак
 
     def sum_weight(self) -> float:
@@ -14,6 +42,10 @@ class BackPack:  # рюкзак
         Возвращает суммарный вес всех предметов в рюкзаке
         """
         # TODO: реализуйте метод
+        summa = 0
+        for i in self.items:
+            summa += i.weight
+        return summa
 
     def sum_cost(self) -> int:
         """
