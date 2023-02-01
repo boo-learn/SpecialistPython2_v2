26f40973d3a61ee7d2a5fad0cdc60f9a58f9bf61
iakrapivin <122812457+iakrapivin@users.noreply.github.com>
2023-02-01 19:53:08 +0300

    Update 01_card_to_str.py

--- Module4/practice/deck/01_card_to_str.py
+++ Module4/practice/deck/01_card_to_str.py
@@ -4,9 +4,16 @@ class Card:
         self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
         self.suit = suit  # Масть карты
 
-    def to_str(self):
+    def to_str(self) -> str:
         # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
-        ...
+        if self.suit == "Hearts":
+            return f"{self.value}\u2665"
+        elif self.suit == "Diamonds":
+            return f"{self.value}\u2666"
+        elif self.suit == "Clubs":
+            return f"{self.value}\u2663"
+        elif self.suit == "Spades":
+            return f"{self.value}\u2660"
 
 
 # Создадим несколько карт
@@ -16,9 +23,3 @@ card2 = Card("A", "Diamonds")
 # Выведем карты на экран в виде: 10♥ и A♦
 print(card1.to_str())
 print(card2.to_str())
-
-# Пример, вывод иконок мастей:
-print('\u2661', '\u2665')
-print('\u2662', '\u2666')
-print('\u2667', '\u2663')
-print('\u2664', '\u2660')
