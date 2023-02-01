1fe752d7bc18f5081f753b26f1b6680ec5c05600
cheshirskiy <doma-kot@ya.ru>
2023-02-01 21:11:31 +0300

    Update 05_deck_part1.py

--- Module4/practice/deck/05_deck_part1.py
+++ Module4/practice/deck/05_deck_part1.py
@@ -1,6 +1,26 @@
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        icon = None
+        if self.suit == "Hearts":
+            icon = '\u2665'
+        elif self.suit == "Diamonds":
+            icon = '\u2666'
+        elif self.suit == "Spades":
+            icon = '\u2660'
+        else:
+            icon = '\u2663'
+        return f'{self.value}{icon}'
+
+    def equal_suit(self, other_card):
+
+        if self.suit == other_card.suit:
+            return True
+        return False
+
 
 
 class Deck:
@@ -9,11 +29,18 @@ class Deck:
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
         self.cards = []
-        # TODO-1: конструктор добавляет в список self.cards все(52) карты
+        for value in values:
+            for suit in suits:
+                self.cards.append(Card(value, suit))
 
     def show(self):
         # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+       
+        final_str = f'deck[{len(self.cards)}]'
+        str_cards = []
+        for card in self.cards:
+            str_cards.append(card.to_str())
+        return final_str + ", ".join(str_cards)
 
 
 # Создаем колоду
