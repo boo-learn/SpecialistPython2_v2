5f24658528ba1be228009d279dd8cce12dac2628
cheshirskiy <doma-kot@ya.ru>
2023-02-01 20:10:58 +0300

    Update 03_cards_list.py

--- Module4/practice/deck/03_cards_list.py
+++ Module4/practice/deck/03_cards_list.py
@@ -1,21 +1,42 @@
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
 
+    def to_str(self):
+        icon = None
+        if self.suit == "Hearts":
+            icon = '\u2665'
+        elif self.suit == "Diamonds" :
+            icon = '\u2666'
+        elif self.suit == "Spades":
+            icon = '\u2660'
+        else:
+            icon = '\u2663'
+        return f'{self.value}{icon}'
+
+    def equal_suit(self, other_card):
+        
+        if self.suit == other_card.suit:
+            return True
+        return False
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 hearts_cards = []
-# TODO-1: добавьте в список hearts_cards все червовые карты(от 2-ки до туза)
+
+for value in values:
+    hearts_cards.append(Card(value, "Hearts"))
 
 diamonds_cards = []
-# TODO-2: добавьте в список diamonds_cards все бубновые карты(от туза до 2-ки)
+values.reverse()
+for value in values:
+    diamonds_cards.append(Card(value, "Diamond"))
 
-# TODO-3: выведите все карты из списка hearts_cards в терминал через запятую в одну строку:
-# Пример вывода: 2♥, 3♥, 4♥ ... A♥
 
-
-cards = [
-    Card("2", "Hearts"),
-    Card("3", "Hearts"),
-    Card("4", "Hearts"),
-    ...]
+for heart_card in hearts_cards:
+    print(heart_card.to_str())
+# cards = [
+#     Card("2", "Hearts"),
+#     Card("3", "Hearts"),
+#     Card("4", "Hearts"),
+#     ...]
