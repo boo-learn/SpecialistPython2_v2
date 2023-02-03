435419c79266d84f73c7c77a94bbcc70a363ab0e
iozbsdvs <100926106+iozbsdvs@users.noreply.github.com>
2023-02-03 13:00:42 +0300

    Update 01_task_Currency.py
    Добавил исключения, как требовалось в задании

--- Module4/home_work/Currency/01_task_Currency.py
+++ Module4/home_work/Currency/01_task_Currency.py
@@ -3,13 +3,34 @@
 # Нас будут интересовать только курсы доллара и евро.
 # Как получить курс по API со стороннего сайта - смотри в helpers/request_currency.py
 
+import requests
+
+
 class Currency:
     def __init__(self, type):
-        pass
+        self.currency = type
+
+    def __getitem__(self, date):
+        date = date.split('.')
+        date = date[::-1]
+        date = '/'.join(date)
+
+        url = f'https://www.cbr-xml-daily.ru/archive/{date}/daily_json.js'
+        response = requests.get(url)
+        response_result = response.json()
+
+        try:
+            if self.currency == "usd":
+                return response_result['Valute']['USD']['Value']
+            elif self.currency == "euro":
+                return response_result['Valute']['EUR']['Value']
+        except:
+            return response_result['explanation']
 
 
 usd = Currency("usd")  # Создаем валюту "Доллар"
 euro = Currency("euro")  # Создаем валюту "Евро"
+
 print(usd['02.09.2020'])  # ← получение курса доллара на указанную дату
 print(euro['12.10.2018'])  # ← получение курса евро на указанную дату
 print(euro['12.14.2018'])  # ← в случае некорректной выбрасываем исключение
