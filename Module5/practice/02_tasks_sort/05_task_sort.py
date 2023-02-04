98d0b88b1cc9f2650524299e9e4b5ea017d851da
dmitriypo1989 <87150543+dmitriypo1989@users.noreply.github.com>
2023-02-04 16:30:08 +0300

    Update 05_task_sort.py

--- Module5/practice/02_tasks_sort/05_task_sort.py
+++ Module5/practice/02_tasks_sort/05_task_sort.py
@@ -2,7 +2,7 @@
 staff = [
     {
         'name': 'Григорий',
-        'surname': 'Петров',
+        'surname': 'Зуев',
         'salary': 124300
     },
     {
@@ -12,7 +12,7 @@ staff = [
     },
     {
         'name': 'Иван',
-        'surname': 'Павлов',
+        'surname': 'Абвов',
         'salary': 34500
     },
     {
@@ -22,13 +22,47 @@ staff = [
     },
     {
         'name': 'Василий',
-        'surname': 'Павлов',
+        'surname': 'Иванов',
         'salary': 34500
     },
 ]
 
+def sort_choice(staff):
+    i = 0
+    while i < len(staff) - 1:
+        m = i
+        j = i + 1
+        while j < len(staff):
+            if staff[j]['salary'] > staff[m]['salary']: #Скрипт сортировки скорректиров в части зарплат
+                m = j
+            elif staff[j]['salary'] == staff[m]['salary']: #Скрипт сортировки скорректиров в части фамилий
+                if staff[j]['surname'][0] < staff[m]['surname'][0]: #Скрипт сортировки скорректиров в части фамилий
+                    m = j
+            j += 1
+        staff[i], staff[m] = staff[m], staff[i]
+        i += 1
+#print(staff)
+sort_choice(staff)
+#print(staff)
 # 1. Выведите список сотрудников, отсортированный по уменьшению их заработной платы.
 # Если у нескольких сотрудников одинаковая ЗП, то добавьте сортировку по Фамилии
+
+persons = []
+for person in staff:
+    persons.append(person['surname'])
+
 print("Список сотрудников отсортированный по уменьшению ЗП:")
+print(*persons, sep=', ')
 
 # 2. Найдите сумму зарплат трех самых низкооплачиваемых сотрудников:
+sumv = 0
+staff.reverse()
+
+number_of_persons = 0
+for person in staff:
+    sumv += person['salary']
+    number_of_persons += 1
+    if number_of_persons == 3:
+        break
+
+print(sumv)
