6a4d4685c33336360ee13bdd5b66f6f9d9b76bf1
iakrapivin <122812457+iakrapivin@users.noreply.github.com>
2023-02-04 19:25:25 +0300

    Update 10_task_sort.py

--- Module5/practice/02_tasks_sort/10_task_sort.py
+++ Module5/practice/02_tasks_sort/10_task_sort.py
@@ -1,6 +1,55 @@
-# Камни*
-# Имеется N камней веса А1,А2,...,АN.
-#
-# Необходимо разбить их на две кучи таким образом, чтобы веса куч отличались не более чем в 2 раза.
-# Если этого сделать нельзя, то указать это.
+import random as r
 
+
+def sort_choice(nums: list) -> None:
+    i = 0
+    while i < len(nums) - 1:
+        m = i
+        j = i + 1
+        while j < len(nums):
+            if nums[j] < nums[m]:
+                m = j
+            j += 1
+        nums[i], nums[m] = nums[m], nums[i]
+        i += 1
+
+
+n = 10
+stones = [r.randint(1, 100) for _ in range(n)]
+mountains = {"small": [], "big": []}
+
+sort_choice(stones)
+print(stones,"\n")
+
+mountains["big"].append(stones.pop(-1))
+
+while True:
+    small = sum(mountains["small"])
+    others = sum(stones)
+    big = sum(mountains["big"])
+    if small < big:
+        if 0.5 < big/(others + small) < 2:
+            mountains["small"] += stones
+            break
+        elif big/(others + small) >= 2:
+            print("It's UNREAL!")
+            mountains["small"] += stones
+            break
+        else:
+            try:
+                mountains["small"].append(stones.pop(-1))
+            except IndexError:
+                mountains["small"] += stones
+            if 0.5 < (others + small)/big < 2:
+                mountains["small"] += stones
+                break
+    else:
+        try:
+            mountains["big"].append(stones.pop(0))
+        except IndexError:
+            mountains["big"] += stones
+            break
+
+print(mountains,"\n")
+print(f" small / big = {sum(mountains['big'])/sum(mountains['small'])}")
+# получилось не очень красиво но работает)
