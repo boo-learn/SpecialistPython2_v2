8795d366e91af66270a324af104ec733d26b10f8
cheshirskiy <doma-kot@ya.ru>
2023-02-05 01:12:21 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -1,6 +1,15 @@
 # Сумма наибольших
 # Дан список чисел.
 # Найти: сумму 5-ти самых больших элементов
+from base_sort import sort_choice
 
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
 
+sort_choice(numbers)
+
+sum_number = 0
+
+for number in numbers[-5:]:
+    sum_number += number
+
+print(sum_number)
