d13958983f3c3327f0f7754626998065b67a6ffd
Aleksandr Kolchak <123981265+Kolchaks@users.noreply.github.com>
2023-02-06 00:06:42 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -1,6 +1,21 @@
-# Сумма наибольших
-# Дан список чисел.
-# Найти: сумму 5-ти самых больших элементов
+def sort_choise(nums: list) -> None:
+    len_nums = len(nums)
+    
+    for i in range(0, len_nums - 1):
+        min_num = i
+    
+        for j in range(i + 1, len_nums):
+            if nums[j] < nums[min_num]:
+                min_num = j
+                
+        nums[i], nums[min_num] = nums[min_num], nums[i]
 
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
+
+quontity_max_numbers = int(input())
+sort_choise(numbers)
+numbers.reverse()
+
+print(numbers)
+print(sum(numbers[:quontity_max_numbers]))
 
