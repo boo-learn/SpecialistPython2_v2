18e5fafb09acfef2538b129f7a18090e1217acae
Aleksandr Kolchak <123981265+Kolchaks@users.noreply.github.com>
2023-02-06 01:16:55 +0300

    Update 06_task_sort.py

--- Module5/practice/02_tasks_sort/06_task_sort.py
+++ Module5/practice/02_tasks_sort/06_task_sort.py
@@ -1,15 +1,30 @@
-# Рекламная акция
-# В сети магазинов "Н-Аудио" проводится рекламная акция. Каждый второй товар – бесплатно.
-# Естественно, кассирам дано указание пробивать товары в таком порядке, чтобы магазин потерял как можно меньше денег.
-# По списку товаров определите максимальную сумму в чеке.
-#
-# Вход:дано N натуральных чисел – цены товаров.
-# Выход: одно число – максимальная сумма чека.
+from random import randint
 
-# Пример
-# Вход:
-# 2 1 10 50 10
-# Выход:
-# 70
-# Пояснение:
-# Возможен такой порядок: 10 2 50 1 10
+def sort_choise(nums: list) -> None:
+    len_nums = len(nums)
+    
+    for i in range(0, len_nums - 1):
+        min_num = i
+    
+        for j in range(i + 1, len_nums):
+            if nums[j] < nums[min_num]:
+                min_num = j
+                
+        nums[i], nums[min_num] = nums[min_num], nums[i]
+        
+N = int(input())       
+prices = [randint(1, 50) for _ in range(N)]
+cash_receipt = []
+sort_choise(prices)
+print(prices)
+
+for i in prices:
+    if len(prices) > 1:
+        cash_receipt.append(prices[-1])
+        cash_receipt.append(prices[0])
+        prices = prices[1:-1]
+    elif len(prices) == 1:
+        cash_receipt.append(prices[0])
+        prices.clear()
+        
+print(*cash_receipt)
