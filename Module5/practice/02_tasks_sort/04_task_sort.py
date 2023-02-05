ce05f25bcdb70ed0fdce8d7e5e4a7b13bc93155a
Aleksandr Kolchak <123981265+Kolchaks@users.noreply.github.com>
2023-02-06 00:11:09 +0300

    Update 04_task_sort.py

--- Module5/practice/02_tasks_sort/04_task_sort.py
+++ Module5/practice/02_tasks_sort/04_task_sort.py
@@ -1,7 +1,22 @@
-# Сумма наибольших по модулю
-# Дан массив(список) чисел.
-# Найти: сумму 5-ти самых больших элементов по модулю.
-# Пояснение: сравниваем элементы по модулю, а в сумму добавляем сами значения элементов(без модуля)
-# В примере ниже, два самых больших по модулю числа это: -22.4 и 21.1. Они самые большие по модулю, а их сумма = -1.3
+def sort_choise_abs(nums: list) -> None:
+    len_nums = len(nums)
+    
+    for i in range(0, len_nums - 1):
+        min_num = i
+    
+        for j in range(i + 1, len_nums):
+            if abs(nums[j]) < abs(nums[min_num]):
+                min_num = j
+                
+        nums[i], nums[min_num] = nums[min_num], nums[i]
+        
 
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, 21.1, 0]
+sum_modules = 0
+
+sort_choise_abs(numbers)
+print(numbers)
+numbers.reverse()
+
+quontity_max_modules = int(input())
+print(sum(numbers[:quontity_max_modules]))
