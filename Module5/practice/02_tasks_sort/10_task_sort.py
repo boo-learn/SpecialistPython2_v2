73b368a334bc8a848f2a15f727e97bf7824ab79a
Aleksandr Kolchak <123981265+Kolchaks@users.noreply.github.com>
2023-02-07 16:10:26 +0300

    Update 10_task_sort.py

--- Module5/practice/02_tasks_sort/10_task_sort.py
+++ Module5/practice/02_tasks_sort/10_task_sort.py
@@ -1,6 +1,28 @@
-# Камни*
-# Имеется N камней веса А1,А2,...,АN.
-#
-# Необходимо разбить их на две кучи таким образом, чтобы веса куч отличались не более чем в 2 раза.
-# Если этого сделать нельзя, то указать это.
+from random import randint
+
+N = int(input())
+rocks = list(set([randint(1, 50) for _ in range(N)]))
+
+rocks.sort(reverse=True)
+print(rocks)
+
+middle = sum_rocks // 2
+bunch_of_rocks = []
+
+if max(rocks) / (sum(rocks) - max(rocks)) > 2:
+    print('Этого сделать нельзя')
+
+
+while sum(bunch_of_rocks) < middle:
+    bunch_of_rocks.append(rocks[0])
+    rocks = rocks[1:]
+
+if sum(bunch_of_rocks) < sum(rocks):
+    check = sum(bunch_of_rocks) / sum(rocks)
+else:
+    check = sum(rocks) / sum(bunch_of_rocks)
+
+print(rocks)
+print(bunch_of_rocks)
+print(check)
 
