cad8f45ce747053df5e01b28ea19cbb718b1f6c2
iakrapivin <122812457+iakrapivin@users.noreply.github.com>
2023-02-09 19:15:04 +0300

    Update IBank_part1_3.py

--- Module8/practice/IBank/IBank_part1_3.py
+++ Module8/practice/IBank/IBank_part1_3.py
@@ -1,39 +1,31 @@
+
 class Account:
-    # TODO-0: скопируйте реализацию из предыдущего решения
+    def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
+        self.name = name
+        self.passport = passport
+        self.phone_number = phone_number
+        self.__balance = start_balance
 
-    # TODO-1: напишите реализацию метода transfer()
-    def transfer(self, target_account: 'Account', amount: int) -> None:
-        """
-        Перевод денег на счет другого клиента
-        :param target_account: счет клиента для перевода
-        :param amount: сумма перевода
-        :return:
-        """
-        pass
+    @property
+    def balance(self) -> int:
+        return self.__balance
 
+    def full_info(self) -> str:
+        return f"{self.name} баланс: {self.__balance} руб. паспорт: {self.passport} т.{self.phone_number}"
 
-account1 = Account("Иван", "3230 634563", "+7-900-765-12-34", 1000)
-account2 = Account("Алексей", "3232 456124", "+7-901-744-22-99", 200)
+    def __repr__(self) -> str:
+        return f"{self.name} баланс: {self.__balance} руб."
 
-print(account1)
-print(account2)
+    def deposit(self, money: int) -> None:
+        self.__balance += money
 
-# Переводим деньги с первого аккаунт на второй:
-try:
-    account1.transfer(account2, 500)
-except ValueError as e:
-    print(e)
+    def withdraw(self, money: int) -> int:
+        if self.__balance >= money:
+            self.__balance -= money
+            return money
+        else:
+            raise ValueError
 
-# Проверяем изменения баланса:
-print(account1)
-print(account2)
-
-# Переводим еще с первого аккаунт на второй:
-try:
-    account1.transfer(account2, 600)
-except ValueError as e:
-    print(e)
-
-# Проверяем изменения баланса:
-print(account1)
-print(account2)
+    def transfer(self, target_account: 'Account', money: int) -> None:
+        to_transaction = self.withdraw(money)
+        target_account.__balance += to_transaction
