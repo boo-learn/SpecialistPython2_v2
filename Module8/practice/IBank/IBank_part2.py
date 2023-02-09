ec3581a03ef4847fad2b343641b079e5d869f123
iakrapivin <122812457+iakrapivin@users.noreply.github.com>
2023-02-09 21:09:36 +0300

    Update IBank_part2.py

--- Module8/practice/IBank/IBank_part2.py
+++ Module8/practice/IBank/IBank_part2.py
@@ -1,9 +1,66 @@
-from typing import List
+import re
 
 
-# класс для хранения информации об операциях
+class Account:
+
+    def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
+        passport_reg = r"\d{4} \d{6}"
+        phone_number_reg = r"[+]7-\d{3}-\d{3}-\d{2}-\d{2}"
+        if re.match(passport_reg, passport):
+            self.passport = passport
+        else:
+            raise ValueError("Неверный формат паспорта")
+        if re.match(phone_number_reg, phone_number):
+            self.phone_number = phone_number
+        else:
+            raise ValueError("Неверный формат телефона")
+        self.name = name
+        self.__balance = start_balance
+        self.__history: list[Operation] = []
+
+    @property
+    def balance(self) -> int:
+        return self.__balance
+
+    def get_history(self) -> list['Operation']:
+        return self.__history
+
+    def full_info(self) -> str:
+        return f"{self.name} баланс: {self.__balance} руб. паспорт: {self.passport} т.{self.phone_number}"
+
+    def __repr__(self) -> str:
+        return f"{self.name} баланс: {self.__balance} руб."
+
+    def deposit(self, money: int, to_history: bool = True) -> None:
+        self.__balance += money
+        if to_history:
+            operation = Operation(Operation.DEPOSIT, money)
+            self.__history.append(operation)
+
+    def withdraw(self, money: int, to_history: bool = True) -> None:
+        if self.__balance >= money:
+            self.__balance -= money
+            if to_history:
+                operation = Operation(Operation.WITHDRAW, money)
+                self.__history.append(operation)
+        else:
+            raise ValueError("На счете недостаточно средств")
+
+    def transfer(self, target_acc: 'Account', money: int, to_history: bool = True) -> None:
+        if self.__balance >= money:
+            self.__balance -= money
+            target_acc.__balance += money
+            if to_history:
+                operation_out = Operation(Operation.TRANSFER_OUT, money, target_account=target_acc)
+                operation_in = Operation(Operation.TRANSFER_IN, money, target_account=self)
+                self.__history.append(operation_out)
+                target_acc.__history.append(operation_in)
+        else:
+            raise ValueError("На счете недостаточно средств")
+
+
 class Operation:
-    # Типы операций храним в свойствах класса
+
     DEPOSIT = 'Пополнение'
     WITHDRAW = 'Снятие'
     TRANSFER_OUT = 'Перевод'
@@ -28,44 +85,16 @@ class Operation:
         return str_out
 
 
-class Account:
-    def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
-        self.name = name
-        self.passport = passport
-        self.phone_number = phone_number
-        self.__balance = start_balance
-        # историю храним как список объектов класса Operation, добавив свойство в конструктор:
-        self.__history: List[Operation] = []
-
-    # TODO: сюда копируем реализацию методом класса Account из предыдущей задачи
-    #  и реализуем добавление в историю
-
-    # Данный метод дан в готовом виде. Изучите его и используйте как пример, для доработки других
-    def deposit(self, amount: int, to_history: bool = True) -> None:
-        """
-        Внесение суммы на текущий счет
-        :param amount: сумма
-        :param to_history: True - записывать операцию в историю, False - не записывать
-        """
-        self.__balance += amount
-        if to_history:
-            operation = Operation(amount, Operation.DEPOSIT)
-            self.__history.append(operation)
-
-    def get_history(self) -> List[Operation]:
-        """
-        :return: возвращаем историю операций в виде списка операций
-        """
-        return self.__history
-
-
-# Создаем тестовый аккаунт:
-account1 = Account("Алексей", "3232 456124", "+7-901-744-22-99", start_balance=500)
-
+account1 = Account("Алексей", "3232 456124",
+                   "+7-901-744-22-99", start_balance=500)
+account2 = Account("Алексей2", "3232 456124",
+                   "+7-901-744-22-99", start_balance=500)
 # Выполняем пару операций пополнения:
-account1.deposit(200)
-account1.deposit(500)
 
+account1.transfer(account2,100)
 # Выводим историю операций:
 for operation in account1.get_history():
+    print(operation)
+
+for operation in account2.get_history():
     print(operation)
