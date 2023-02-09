4b3fcd5866508c8bd5284f8b7fbe3749e0da408c
PozhitkovDI <87150543+dmitriypo1989@users.noreply.github.com>
2023-02-09 18:57:16 +0300

    Update IBank_part1_3.py

--- Module8/practice/IBank/IBank_part1_3.py
+++ Module8/practice/IBank/IBank_part1_3.py
@@ -1,15 +1,39 @@
 class Account:
-    # TODO-0: скопируйте реализацию из предыдущего решения
+    def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
+        self.name = name
+        self.passport = passport
+        self.phone_number = phone_number
+        #self.balance = start_balance
+        self.__balance = start_balance  # TODO: Закрываем прямой доступ к балансу
+
+    def full_info(self) -> str:
+        return f"{self.name} баланc: {self.balance}руб. паспорт: {self.passport} т.{self.phone_number}"
+
+    def __repr__(self) -> str:
+        return f"{self.name} баланc: {self.balance}руб."
+
+    # TODO: реализуйте getter для просмотра баланса
+    #  Подробнее тут: https://pythobyte.com/using-getters-and-setters-in-python-5205-840ed13f/
+    @property
+    def balance(self) -> int:
+        return self.__balance
+
+    def deposit(self, amount: int) -> None:
+        self.__balance += amount
+
+    def withdraw(self, amount: int) -> None:
+        if self.__balance >= amount:
+            self.__balance -= amount
+        else:
+            raise ValueError('Недостаточно денег на счёте.')
 
     # TODO-1: напишите реализацию метода transfer()
     def transfer(self, target_account: 'Account', amount: int) -> None:
-        """
-        Перевод денег на счет другого клиента
-        :param target_account: счет клиента для перевода
-        :param amount: сумма перевода
-        :return:
-        """
-        pass
+        if self.__balance >= amount:
+            self.__balance -= amount
+            target_account.__balance += amount
+        else:
+            raise ValueError('Недостаточно денег на счёте для перевода.')
 
 
 account1 = Account("Иван", "3230 634563", "+7-900-765-12-34", 1000)
