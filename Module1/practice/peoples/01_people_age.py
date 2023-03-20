3e9c5b01263c505594837cb1de14923cfe1fa0b1
RPortnov <128364428+RPortnov@users.noreply.github.com>
2023-03-20 13:06:07 +0300

    Update 01_people_age.py
    Добавил дополнительно еще 2 кейса ввода некорректного возраста: вне границ и float

--- Module1/practice/peoples/01_people_age.py
+++ Module1/practice/peoples/01_people_age.py
@@ -2,20 +2,23 @@ class People:
     def __init__(self, name: str, surname: str, age: int):
         self.name = name
         self.surname = surname
-        self.age = age
+        self.__age = age
 
     def change_age(self, new_age: int) -> None:
         # Корректным возрастом считаем целое число от 1 до 100
         # Если задан некорректный возраст, выводим "некорректное значение для возраста" и оставляем старое значение
         # Метод меняет свойство self.age на значение new_age
         # TODO: напишите реализацию данного метода
-        ...
+        if type(new_age) == int and 1 < new_age < 100:
+            self.__age = new_age
+        else:
+            print("Некорректное значение для возраста")
 
     def full_name(self) -> str:
         return f"{self.surname} {self.name}"
 
     def full_info(self) -> str:
-        return f"Человек: {self.surname} {self.name} и ему {self.age} лет"
+        return f"Человек: {self.surname} {self.name} и ему {self.__age} лет"
 
 
 # Создадим двух человек:
@@ -31,6 +34,9 @@ print("Меняем возраст людей")
 people1.change_age(45)
 people2.change_age("help")
 people2.change_age(-30)
+# добавил еще 2 кейса
+people2.change_age(1)
+people2.change_age(5.5)
 
 print(people1.full_info())
 print(people2.full_info())
