8c62c7a33c439504485f3c9ab93f5ba9ce2caa4e
andreev-aleksey <94741962+andreev-aleksey@users.noreply.github.com>
2023-03-20 12:33:46 +0300

    Update 05_line.py
    Не понял, как можно сделать from_class = Point(0, 0) без нулевых координат.
    Если создать просто from_class = Point() - просит x и y, так как в классе заданы self.x = x и self.y = y

--- Module1/practice/points/05_line.py
+++ Module1/practice/points/05_line.py
@@ -8,7 +8,16 @@ class Point:
 
     def dist_to(self, other_point: Point) -> float:
         # TODO-0: скопируйте реализацию из предыдущей задачи
-        ...
+        return (((self.x - other_point.x)**2) + ((self.y - other_point.y)**2))**0.5
+    
+    def length_line(self, points_list: list[Point]):
+        length = []
+        for element in range(len(points_list)):
+            if element+1 != len(points_list):
+                p2 = points_list[element+1]
+                p1 = points_list[element]
+                length.append(p1.dist_to(p2))
+        return sum(length)
 
 
 # Ломаная линия задана произвольным количеством последовательных точек
@@ -16,4 +25,16 @@ points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 
 # TODO: Найдите длину ломаной линии
 
-print("Длина ломаной линии = ", ...)
+length = []
+for element in range(len(points)):
+    if element+1 != len(points):
+        p2 = points[element+1]
+        p1 = points[element]
+        length.append(p1.dist_to(p2))
+
+from_func = sum(length)
+
+from_class = Point(0, 0)
+
+print("Длина ломаной линии = ", from_func)
+print("Длина ломаной линии = ", from_class.length_line(points))
