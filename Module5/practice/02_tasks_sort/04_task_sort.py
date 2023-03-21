dc60907fb732657adf6559d21c37ee5672c704d6
andreev-aleksey <94741962+andreev-aleksey@users.noreply.github.com>
2023-03-21 16:43:22 +0300

    Update 04_task_sort.py

--- Module5/practice/02_tasks_sort/04_task_sort.py
+++ Module5/practice/02_tasks_sort/04_task_sort.py
@@ -5,3 +5,28 @@
 # В примере ниже, два самых больших по модулю числа это: -22.4 и 21.1. Они самые большие по модулю, а их сумма = -1.3
 
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, 21.1, 0]
+
+def sort_choice(nums, reverse=False, abs_flag=False) -> None:
+    i = 0
+    while i < len(nums) - 1:
+        m = i
+        j = i + 1
+        while j < len(nums):
+            if abs_flag:
+                if reverse:
+                    rule_sort = abs(nums[j]) > abs(nums[m])
+                else:
+                    rule_sort = abs(nums[j]) < abs(nums[m])
+            else:
+                if reverse:
+                    rule_sort = nums[j] > nums[m]
+                else:
+                    rule_sort = nums[j] < nums[m]
+            if rule_sort:
+                m = j
+            j += 1
+        nums[i], nums[m] = nums[m], nums[i]
+        i += 1
+
+sort_choice(numbers, reverse=True, abs_flag=True)
+print(sum(numbers[:5]))
