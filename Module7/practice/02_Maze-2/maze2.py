a04b434cf07e8eeb78382f5f6a105741273f033a
upper87 <123666741+upper87@users.noreply.github.com>
2023-03-22 14:24:52 +0300

    Update maze2.py

--- Module7/practice/02_Maze-2/maze2.py
+++ Module7/practice/02_Maze-2/maze2.py
@@ -1,5 +1,49 @@
-# Скопируйте решение из предыдущей задачи(Maze-1) и адаптируйте под условия текущей задачи
-# Чем меньше пришлось вносить изменений в код программы, тем лучше было решение предыдущей задачи
+def dfs(graph, start_vertex):
+    visited = [False] * len(graph)
+
+    def _dfs(v):
+        visited[v] = True
+        for w in graph[v]:
+            if not visited[w]:  # посещён ли текущий сосед?
+                _dfs(w)
+
+    _dfs(start_vertex)
+    return visited
 
 
-# Решите задачу и выведите ответ в нужном формате
+def bfs(graph, start_vertex):
+    lengths = [None] * len(graph)
+    lengths[start_vertex] = 0
+    queue = [start_vertex]
+
+    while queue:
+        cur_vertex = queue.pop(0)
+        for vertex in graph[cur_vertex]:
+            if lengths[vertex] is None:
+                lengths[vertex] = lengths[cur_vertex] + 1
+                queue.append(vertex)
+    return lengths
+
+graph = [
+    [1, 3],     # 0
+    [0, 4],     # 1
+    [5],        # 2
+    [0, 4, 6],  # 3
+    [1, 3],     # 4
+    [2, 8],     # 5
+    [3],        # 6
+    [8],        # 7
+    [7, 5],     # 8
+]
+
+
+start_vertex = 7
+bank = 2
+
+lengths = bfs(graph, start_vertex=start_vertex)
+print(lengths)
+
+if lengths[bank] == None:
+    print("Мы не придем в Банк")
+else:
+    print("Мы придем в банк")
