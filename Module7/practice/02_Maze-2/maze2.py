3ae45bb6ef7a0da792f59ffd661092bf925fb23e
DinaraY <106914008+DinaraY@users.noreply.github.com>
2023-03-22 14:13:39 +0300

    Update maze2.py

--- Module7/practice/02_Maze-2/maze2.py
+++ Module7/practice/02_Maze-2/maze2.py
@@ -3,3 +3,27 @@
 
 
 # Решите задачу и выведите ответ в нужном формате
+graph = [
+    # список смежности
+    [1, 3],  # 0
+    [0, 4],  # 1
+    [5],  # 2
+    [0, 4, 6],  # 3
+    [1, 3],  # 4
+    [2, 8],  # 5
+    [3],  # 6
+    [8],  # 7
+    [7, 5]  # 8
+]
+start = 0
+lengths = [None] * len(graph)
+lengths[start] = 0
+queue = [start]
+while queue:
+    cur_vertex = queue.pop(0)
+    for vertex in graph[cur_vertex]:
+        if lengths[vertex] is None:
+            lengths[vertex] = lengths[cur_vertex] + 1
+            queue.append(vertex)
+
+print(lengths)
