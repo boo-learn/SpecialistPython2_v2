51a9707dc5f9b5cb473a668197ae17c03f163c95
iisaev7 <128364984+iisaev7@users.noreply.github.com>
2023-03-22 14:51:48 +0300

    Update maze4.py

--- Module7/practice/04_Maze-4/maze4.py
+++ Module7/practice/04_Maze-4/maze4.py
@@ -1,5 +1,36 @@
-# Скопируйте решение из предыдущей задачи и адаптируйте под условия текущей задачи
-# Чем меньше пришлось вносить изменений в код программы, тем лучше было решение предыдущей задачи
+def dfs(graph, start_vertex):
+    visited = [False] * len(graph)
+
+    def _dfs(v):
+        visited[v] = True
+        for w in graph[v]:
+            if not visited[w]:  # посещён ли текущий сосед?
+                _dfs(w)
+
+    _dfs(start_vertex)
+    return visited
 
 
-# Решите задачу и выведите ответ в нужном формате
+graph = [
+    [1], #0
+    [0,4], #1
+    [5], #2
+    [4], #3
+    [1,3,7], #4
+    [2], #5
+    [], #6
+    [4,8], #7
+    [7], #8
+]
+
+start = 1
+points = {"shop":5, "bank":3, "bar":8}
+
+visited= dfs(graph, start)
+
+for point in points:
+    if visited[points.get(point)]:
+        print(f"Сan go to the {point}")
+    else:
+        print(f"Сan't go to the {point}")
+
