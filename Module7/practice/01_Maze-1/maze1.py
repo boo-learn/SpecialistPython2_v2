0f18ba8e5eacaa596c006071119405868c0bd06e
RPortnov <128364428+RPortnov@users.noreply.github.com>
2023-03-22 14:21:27 +0300

    Update maze1.py

--- Module7/practice/01_Maze-1/maze1.py
+++ Module7/practice/01_Maze-1/maze1.py
@@ -2,7 +2,24 @@
 
 # Опишите список смежности по изображению лабиринта из файла task.md
 graph = [
-    ...
+    # список смежности
+    [1],     # 0   Home
+    [0,4],   # 1
+    [5],     # 2
+    [4,6],   # 3
+    [1,3,5], # 4
+    [2,4],   # 5
+    [3],     # 6
+    [8] ,    # 7  Bank
+    [7]      # 8
 ]
 
-# Решите задачу и выведите ответ в нужном формате
+
+Home = 0
+Bank = 7
+visited = dfs(graph, start_vertex=Home)
+# print(visited)
+if  visited[Bank]:
+    print("Сan go to the bank")
+else:
+    print("Can't go to the bank")
