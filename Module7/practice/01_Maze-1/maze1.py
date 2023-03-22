49e7e14517109ad0af67a1c5aa617132c2094a48
upper87 <123666741+upper87@users.noreply.github.com>
2023-03-22 14:19:23 +0300

    Update maze1.py

--- Module7/practice/01_Maze-1/maze1.py
+++ Module7/practice/01_Maze-1/maze1.py
@@ -1,8 +1,49 @@
-# Выверите и скопируйте сюда наиболее подходяший алгоритм для работы с графом(bfs/dfs)
+def dfs(graph, start_vertex):
+    visited = [False] * len(graph)
 
-# Опишите список смежности по изображению лабиринта из файла task.md
+    def _dfs(v):
+        visited[v] = True
+        for w in graph[v]:
+            if not visited[w]:  # посещён ли текущий сосед?
+                _dfs(w)
+
+    _dfs(start_vertex)
+    return visited
+
+
+def bfs(graph, start_vertex):
+    lengths = [None] * len(graph)
+    lengths[start_vertex] = 0
+    queue = [start_vertex]
+
+    while queue:
+        cur_vertex = queue.pop(0)
+        for vertex in graph[cur_vertex]:
+            if lengths[vertex] is None:
+                lengths[vertex] = lengths[cur_vertex] + 1
+                queue.append(vertex)
+    return lengths
+
 graph = [
-    ...
+    [1],        # 0
+    [0, 4],     # 1
+    [5],        # 2
+    [4, 6],     # 3
+    [1, 3, 5],  # 4
+    [2, 4],     # 5
+    [3],        # 6
+    [8],        # 7
+    [7],        # 8
 ]
 
-# Решите задачу и выведите ответ в нужном формате
+
+start_vertex = 0
+bank = 5
+
+lengths = bfs(graph, start_vertex=start_vertex)
+print(lengths)
+
+if lengths[bank] == None:
+    print("Мы не предем в Банк")
+else:
+    print("Мы придем в банк")
