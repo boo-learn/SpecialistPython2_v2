3fe0e74097a02bc5e8292c877407dcfb1ce71886
iisaev7 <128364984+iisaev7@users.noreply.github.com>
2023-03-23 16:01:41 +0300

    Update IBank_part1_2.py

--- Module8/practice/IBank/IBank_part1_2.py
+++ Module8/practice/IBank/IBank_part1_2.py
@@ -3,59 +3,26 @@ class Account:
         self.name = name
         self.passport = passport
         self.phone_number = phone_number
-        # self.balance = start_balance
-        self.__balance = start_balance  # TODO: Закрываем прямой доступ к балансу
+        self.__balance = start_balance
 
     def full_info(self) -> str:
-        """
-        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
-        """
-        return f"..."
+        return f"{self.name} баланс: {self.__balance} руб. паспорт: {self.passport} т.{self.phone_number}"
 
     def __repr__(self) -> str:
-        """
-        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
-        """
-        return f"..."
-
-    # TODO: реализуйте getter для просмотра баланса
-    #  Подробнее тут: https://pythobyte.com/using-getters-and-setters-in-python-5205-840ed13f/
+        return f"{self.name} баланс: {self.__balance} руб."
+    
     @property
     def balance(self) -> int:
-        return ...
+        return self.__balance
 
     def deposit(self, amount: int) -> None:
-        """
-        Внесение суммы на текущий счет
-        :param amount: сумма
-        """
-        pass
+        self.__balance += amount
 
     def withdraw(self, amount: int) -> None:
-        """
-        Снятие суммы с текущего счета
-        :param amount: сумма
-        """
-        pass
-
-
-# Создаем тестовый аккаунт:
-account1 = Account("Алексей", "3232 456124", "+7-901-744-22-99", start_balance=500)
-
-# Вносим сумму на счет:
-# account1.deposit(600)
-# print(account1)
-
-# Снимаем деньги со счета:
-# try:
-#     account1.withdraw(1000)
-# except ValueError as e:
-#     print(e)
-# print(account1)
-
-# Пробуем снять еще:
-# try:
-#     account1.withdraw(1000)
-# except ValueError as e:
-#     print(e)
-# print(account1)
+        if type(amount) == int and amount > 0:
+            if self.__balance >= amount:
+                self.__balance -= amount
+            else:
+                raise ValueError("Операция не выполнена! Не дсотаточно средств!")
+        else:
+            raise ValueError("Операция не выполнена! только положительные целые числа!")
