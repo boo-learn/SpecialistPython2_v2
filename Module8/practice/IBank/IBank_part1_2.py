677a746c1cd1393b8e3e130d5dc12c079701458c
andreev-aleksey <94741962+andreev-aleksey@users.noreply.github.com>
2023-03-23 16:05:38 +0300

    Update IBank_part1_2.py

--- Module8/practice/IBank/IBank_part1_2.py
+++ Module8/practice/IBank/IBank_part1_2.py
@@ -10,52 +10,57 @@ class Account:
         """
         Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
         """
-        return f"..."
+        return f"{self.name} баланс: {self.__balance} руб. паспорт: {self.passport} т.{self.phone_number}"
 
     def __repr__(self) -> str:
         """
         :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
         """
-        return f"..."
+        return f"{self.name} баланс: {self.__balance} руб."
 
     # TODO: реализуйте getter для просмотра баланса
     #  Подробнее тут: https://pythobyte.com/using-getters-and-setters-in-python-5205-840ed13f/
     @property
     def balance(self) -> int:
-        return ...
+        return self.__balance
 
     def deposit(self, amount: int) -> None:
         """
         Внесение суммы на текущий счет
         :param amount: сумма
         """
-        pass
+        self.__balance += amount 
 
     def withdraw(self, amount: int) -> None:
         """
         Снятие суммы с текущего счета
         :param amount: сумма
         """
-        pass
+        new_balance = self.__balance - amount
+        if new_balance > 0:
+            self.__balance = new_balance
+        else:
+            raise ValueError("Недостаточно денег на счете")
 
 
 # Создаем тестовый аккаунт:
 account1 = Account("Алексей", "3232 456124", "+7-901-744-22-99", start_balance=500)
 
+
 # Вносим сумму на счет:
-# account1.deposit(600)
+account1.deposit(300)
 # print(account1)
 
 # Снимаем деньги со счета:
-# try:
-#     account1.withdraw(1000)
-# except ValueError as e:
-#     print(e)
-# print(account1)
+try:
+    account1.withdraw(500)
+except ValueError as e:
+    print(e)
+print(account1)
 
 # Пробуем снять еще:
-# try:
-#     account1.withdraw(1000)
-# except ValueError as e:
-#     print(e)
-# print(account1)
+try:
+    account1.withdraw(1000)
+except ValueError as e:
+    print(e)
+print(account1)
