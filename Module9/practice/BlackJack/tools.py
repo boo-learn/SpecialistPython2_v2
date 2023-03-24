1a8fdaf167a17f05c72a4dd73f665cac655a75fb
iisaev7 <128364984+iisaev7@users.noreply.github.com>
2023-03-24 14:52:42 +0300

    Update tools.py

--- Module9/practice/BlackJack/tools.py
+++ Module9/practice/BlackJack/tools.py
@@ -1,15 +1,18 @@
 def sum_points(cards):
-    """
-    Напишите отдельную функцию для нахождения суммы очков всех карт в списке
-    :param cards: список карт(рука игрока или диллера)
-    :return: сумму очков
-    """
     # Совет: храните кол-во очков за карту внутри класса Колоды(колода "знает", сколько дает очков каждая карта)
 
     #  Сначала считаем сумму карт, считая ТУЗ за 11-очков
-    sum_points = ...
+    sum_points = 0
+    for card in cards:
+        sum_points += card.points()
     # Если сумма > 21, то перечитываем сумму, считая ТУЗ за 1(единицу)
+
     if sum_points > 21:
-        ...
+        sum_points = 0
+        for card in cards:
+            if card.points() == 11:
+                sum_points += 1
+                continue
+            sum_points += card.points()
 
     return sum_points
