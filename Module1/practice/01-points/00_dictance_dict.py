43f42fa70d08ca86d23a7376193b45ec78ea6d71
naatalik <127740269+naatalik@users.noreply.github.com>
2023-04-17 19:45:40 +0300

    Update 00_dictance_dict.py

--- Module1/practice/01-points/00_dictance_dict.py
+++ Module1/practice/01-points/00_dictance_dict.py
@@ -1,14 +1,11 @@
 def distance(p1: dict, p2: dict) -> float:
-    """
-    Расстояние между двумя точками
-    """
-    # TODO: напишите тело функции
-
+    import math
+    return math.sqrt((p1['x'] - p2['x'])**2 + (p1['y'] - p2['y'])**2)
 
 # Даны две точки на координатной плоскости
 point1 = {"x": 2, "y": 5}
 point2 = {"x": -2, "y": 4}
 
-dist = distance(point1, point2)
+dist = distance(point1, point2) 
 
 print("Расстояние между точками = ", dist)
