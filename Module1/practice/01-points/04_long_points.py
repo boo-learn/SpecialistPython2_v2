5c7d0f603f362b63785f27c63703f60253f995b1
rundect <93350655+rundect@users.noreply.github.com>
2023-04-17 21:25:21 +0300

    Update 04_long_points.py

--- Module1/practice/01-points/04_long_points.py
+++ Module1/practice/01-points/04_long_points.py
@@ -7,13 +7,21 @@ class Point:
         self.y = y
 
     def dist_to(self, other_point: Point) -> float:
-        # TODO-0: скопируйте реализацию из предыдущей задачи
-        ...
+        return (((self.x - other_point.x) ** 2) + ((self.y - other_point.y) ** 2)) ** 0.5
 
 
 # Дан список из произвольного количества точек:
 points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)]
 
-# TODO-1: найдите точку наиболее удаленную от начала координат и выведите ее координаты
+origin_point = Point(0, 0)
 
-print("Координаты наиболее удаленной точки = ", ...)
+# TODO-1: найдите точку наиболее удаленную от начала координат и выведите ее координаты
+max_point = origin_point
+max_dist = 0
+for point in points:
+    distance = point.dist_to(origin_point)
+    if distance > max_dist:
+        max_dist = distance
+        max_point = point
+
+print("Координаты наиболее удаленной точки = ", max_point.x, max_point.y)
