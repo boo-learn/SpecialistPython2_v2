325b4e73e7b369c88a0b413f852235ccfe48bbd6
PythonEngine <130360297+PythonEngine@users.noreply.github.com>
2023-04-18 21:04:59 +0300

    Update 03_backpack_part2.py

--- Module2/practice/backpack/03_backpack_part2.py
+++ Module2/practice/backpack/03_backpack_part2.py
@@ -1,25 +1,54 @@
 class Item:
-    ...
-    # TODO: сюда копируем реализацию класса из предыдущего задания
+    def __init__(self, name: str, weight: float, cost: int):
+        self.name = name
+        self.weight = weight
+        self.cost = cost
+
+    def show(self):
+        return f"{self.name}; вес:{self.weight}; цена:{self.cost}"
 
 
 class BackPack:  # рюкзак
-    ...
+    def __init__(self, max_weight = 0):
+        self.items = []  # Предметы, которые хранятся в рюкзаке
+        self.max_weight = max_weight
+    def add_item(self, item: Item) -> bool:
+        """
+        Добавляет предмет(item) в этот рюкзак
+        """
 
-    # TODO: сюда копируем реализацию класса из предыдущего задания
+        if self.sum_weight() + item.weight < self.max_weight:
+            self.items.append(item)
+        else:
+            print(f"Предмет {item.name} слишком тяжелый")
+
+
+    def show_items(self) -> None:
+        """
+        Выводит(print'ом) все предметы, содержащиеся в рюкзаке в виде нумерованного списка
+        """
+        for n, item in enumerate(self.items):
+            print(f"{n + 1}. {item.show()}")
+
     # TODO: добавьте новое свойство .max_weight - максимальный суммарный вес предметов, которые можно положить в рюкзак
 
     def sum_weight(self) -> float:
         """
         Возвращает суммарный вес всех предметов в рюкзаке
         """
-        # TODO: реализуйте метод
+        total_weight = 0
+        for item in self.items:
+            total_weight += item.weight
+        return total_weight
 
     def sum_cost(self) -> int:
         """
         Возвращает суммарную стоимость всех предметов в рюкзаке
         """
-        # TODO: реализуйте метод
+        total_cost = 0
+        for item in self.items:
+            total_cost += item.cost
+        return total_cost
 
 
 # Создаем предметы
