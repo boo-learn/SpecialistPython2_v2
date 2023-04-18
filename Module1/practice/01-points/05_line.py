3bff21f48dc598c2c09b434ed4c69e5d03e526db
rundect <93350655+rundect@users.noreply.github.com>
2023-04-18 16:47:25 +0300

    Update 05_line.py

--- Module1/practice/01-points/05_line.py
+++ Module1/practice/01-points/05_line.py
@@ -7,13 +7,17 @@ class Point:
         self.y = y
 
     def dist_to(self, other_point: Point) -> float:
-        # TODO-0: скопируйте реализацию из предыдущей задачи
-        ...
+        return (((self.x - other_point.x) ** 2) + ((self.y - other_point.y) ** 2)) ** 0.5
 
 
 # Ломаная линия задана произвольным количеством последовательных точек
 points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
+first_point = points[0]
+result = 0
 
 # TODO: Найдите длину ломаной линии
-
-print("Длина ломаной линии = ", ...)
+for point in points:
+    distance = point.dist_to(first_point)
+    result = result + distance
+    first_point = point
+print("Длина ломаной линии = ", result)
