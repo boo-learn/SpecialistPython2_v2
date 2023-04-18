17169c448c4788d5a689fcc13435e13e2e162348
rundect <93350655+rundect@users.noreply.github.com>
2023-04-18 21:06:43 +0300

    Update 02_backpack_par1.py

--- Module2/practice/backpack/02_backpack_par1.py
+++ Module2/practice/backpack/02_backpack_par1.py
@@ -1,6 +1,14 @@
 class Item:
-    ...
-    # TODO: сюда копируем реализацию класса из предыдущего задания
+    def __init__(self, name: str, weight: float, cost: int):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
+
+    def show(self) -> str:
+        """
+        Возвращает строковое представление объекта Item
+        """
+        return f"{self.name} вес:{self.weight} цена:{self.cost}"
 
 
 class BackPack:  # рюкзак
@@ -11,13 +19,14 @@ class BackPack:  # рюкзак
         """
         Добавляет предмет(item) в этот рюкзак
         """
-        # TODO: реализуйте метод
+        self.items.append(item)
 
     def show_items(self) -> None:
         """
         Выводит(print'ом) все предметы, содержащиеся в рюкзаке в виде нумерованного списка
         """
-        # TODO: реализуйте метод
+        for i, item in enumerate(self.items):
+            print(i + 1, item.show())
 
 
 # Создаем предметы
@@ -33,3 +42,4 @@ backpack.add_item(item2)
 
 # Выводим все предметы в рюкзаке
 backpack.show_items()
+
