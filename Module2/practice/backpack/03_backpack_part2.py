ab21be4d2627f040803c823f9bae70a3aa59288e
rundect <93350655+rundect@users.noreply.github.com>
2023-04-18 21:19:59 +0300

    Update 03_backpack_part2.py

--- Module2/practice/backpack/03_backpack_part2.py
+++ Module2/practice/backpack/03_backpack_part2.py
@@ -1,25 +1,52 @@
 class Item:
-    ...
-    # TODO: сюда копируем реализацию класса из предыдущего задания
+    def __init__(self, name: str, weight: float, cost: int):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
+
+    def show(self) -> str:
+        """
+        Возвращает строковое представление объекта Item
+        """
+        return f"{self.name} вес:{self.weight} цена:{self.cost}"
 
 
 class BackPack:  # рюкзак
-    ...
+    def __init__(self, max_weight):
+        self.items = []  # Предметы, которые хранятся в рюкзаке
+        self.max_weight = max_weight
+        self.__items_weight = 0
+        self.__items_cost = 0
 
-    # TODO: сюда копируем реализацию класса из предыдущего задания
-    # TODO: добавьте новое свойство .max_weight - максимальный суммарный вес предметов, которые можно положить в рюкзак
+    def add_item(self, item: Item) -> None:
+        """
+        Добавляет предмет(item) в этот рюкзак
+        """
+        if self.__items_weight + item.weight > self.max_weight:
+            print(f"Предмет {item.name} слишком тяжелый")
+        else:
+            self.__items_weight += item.weight
+            self.__items_cost += item.cost
+            self.items.append(item)
+
+    def show_items(self) -> None:
+        """
+        Выводит(print'ом) все предметы, содержащиеся в рюкзаке в виде нумерованного списка
+        """
+        for i, item in enumerate(self.items):
+            print(i + 1, item.show())
 
     def sum_weight(self) -> float:
         """
         Возвращает суммарный вес всех предметов в рюкзаке
         """
-        # TODO: реализуйте метод
+        return self.__items_weight
 
     def sum_cost(self) -> int:
         """
         Возвращает суммарную стоимость всех предметов в рюкзаке
         """
-        # TODO: реализуйте метод
+        return self.__items_cost
 
 
 # Создаем предметы
@@ -42,3 +69,5 @@ backpack.add_item(item4)
 
 # Выводим все предметы в рюкзаке
 backpack.show_items()
+
+
