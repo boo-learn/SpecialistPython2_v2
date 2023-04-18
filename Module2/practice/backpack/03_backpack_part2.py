84b3b72406d1d267b9d843a51c8b6770fb586ff2
Zvonkin Kirill <110783681+slip686@users.noreply.github.com>
2023-04-18 21:19:51 +0300

    Update 03_backpack_part2.py

--- Module2/practice/backpack/03_backpack_part2.py
+++ Module2/practice/backpack/03_backpack_part2.py
@@ -1,25 +1,52 @@
 class Item:
-    ...
-    # TODO: сюда копируем реализацию класса из предыдущего задания
+    def __init__(self, name: str, weight: float | int, cost: int):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
+
+    def show(self):
+        return f"{self.name} вес:{self.weight} цена:{self.cost}"
 
 
 class BackPack:  # рюкзак
-    ...
+    def __init__(self, max_weight=30):
+        self.items = []  # Предметы, которые хранятся в рюкзаке
+        self.max_weight = max_weight
+        self.items_weight = 0
+        self.items_cost = 0
 
-    # TODO: сюда копируем реализацию класса из предыдущего задания
-    # TODO: добавьте новое свойство .max_weight - максимальный суммарный вес предметов, которые можно положить в рюкзак
+    def add_item(self, item: Item) -> None:
+        """
+        Добавляет предмет(item) в этот рюкзак
+        """
+        if self.items_weight + item.weight <= self.max_weight:
+            self.items.append(item)
+            self.items_weight += item.weight
+            self.items_cost += item.cost
+        else:
+            print(f'Предмет {item.name} слишком тяжелый')
+
+
+    def show_items(self) -> None:
+        """
+        Выводит(print'ом) все предметы, содержащиеся в рюкзаке в виде нумерованного списка
+        """
+        for i, item in enumerate(self.items, 1):
+            print(f'{i}: {item.show()}')
 
     def sum_weight(self) -> float:
         """
         Возвращает суммарный вес всех предметов в рюкзаке
         """
-        # TODO: реализуйте метод
+        return self.items_weight
 
     def sum_cost(self) -> int:
         """
         Возвращает суммарную стоимость всех предметов в рюкзаке
         """
-        # TODO: реализуйте метод
+        return self.items_cost
+
+
 
 
 # Создаем предметы
