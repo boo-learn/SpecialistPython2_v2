f39030a81bd77cf0b537c9ff96d9c737cf0aecac
PythonEngine <130360297+PythonEngine@users.noreply.github.com>
2023-04-19 20:37:18 +0300

    Update 01_card_to_str.py

--- Module4/practice/deck/01_card_to_str.py
+++ Module4/practice/deck/01_card_to_str.py
@@ -3,10 +3,18 @@ class Card:
     def __init__(self, value, suit):
         self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
         self.suit = suit  # Масть карты
+        self.suit_symbol = "x"
+        if self.suit == "Hearts":
+            self.suit_symbol = '\u2665'
+        elif self.suit == "Diamonds":
+            self.suit_symbol = '\u2666'
+        elif self.suit == "Clubs":
+            self.suit_symbol = '\u2665'
+        else:
+            self.suit_symbol = '\u2665'
 
     def to_str(self):
-        # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
-        ...
+        return f"{self.value}{self.suit_symbol}"
 
 
 # Создадим несколько карт
@@ -17,8 +25,8 @@ card2 = Card("A", "Diamonds")
 print(card1.to_str())
 print(card2.to_str())
 
-# Пример, вывод иконок мастей:
-print('\u2661', '\u2665')
-print('\u2662', '\u2666')
-print('\u2667', '\u2663')
-print('\u2664', '\u2660')
+# # Пример, вывод иконок мастей:
+# print('\u2661', '\u2665')
+# print('\u2662', '\u2666')
+# print('\u2667', '\u2663')
+# print('\u2664', '\u2660')
