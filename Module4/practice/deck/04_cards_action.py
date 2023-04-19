68ea8f0e0f925941266c326b6e50784e34f6d58b
Zvonkin Kirill <110783681+slip686@users.noreply.github.com>
2023-04-19 21:26:34 +0300

    Update 04_cards_action.py

--- Module4/practice/deck/04_cards_action.py
+++ Module4/practice/deck/04_cards_action.py
@@ -1,12 +1,28 @@
+# Начнем с создания карты
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
+        translator = {'Hearts': '\u2665', 'Diamonds': '\u2666', 'Spades': '\u2660', 'Clubs': '\u2663'}
+        return(f'{self.value}{translator.get(self.suit)}')
+
+    def equal_suit(self, other_card) -> bool:
+        # TODO-1: метод возвращает True - если масти карт равны или False - если нет
+        return self.suit == other_card.suit
 
 
 cards = []
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
 # TODO-1: в список cards добавьте ВСЕ карты всех мастей
+for suit in suits:
+    for value in values:
+        cards.append(Card(value=value, suit=suit))
 
 # TODO-2: Выведите карты в формате: cards[кол-во]2♥, 3♥, 4♥ ... A♥, 2♦, 3♦ ... A♦, ...
 #  кол-во берем от размера списка cards
+cards_line = [card.to_str() for card in cards]
+print(f'cards[{len(cards_line)}]{", ".join(cards_line)}')
