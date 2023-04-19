7a2ee5342e565d8ba0ec7d929b4781dd9083b12c
Zvonkin Kirill <110783681+slip686@users.noreply.github.com>
2023-04-19 20:56:13 +0300

    Update 03_cards_list.py

--- Module4/practice/deck/03_cards_list.py
+++ Module4/practice/deck/03_cards_list.py
@@ -1,18 +1,36 @@
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
+        translator = {'Hearts': '\u2665', 'Diamonds': '\u2666', 'Spades': '\u2660', 'Clubs': '\u2663'}
+        return(f'{self.value}{translator.get(self.suit)}')
+
+    def equal_suit(self, other_card) -> bool:
+        # TODO-1: метод возвращает True - если масти карт равны или False - если нет
+        if self.suit == other_card.suit:
+            return True
+        else:
+            return False
 
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 hearts_cards = []
 # TODO-1: добавьте в список hearts_cards все червовые карты(от 2-ки до туза)
+for value in values:
+    hearts_cards.append(Card(value, 'Hearts'))
 
 diamonds_cards = []
 # TODO-2: добавьте в список diamonds_cards все бубновые карты(от туза до 2-ки)
+for value in reversed(values):
+    hearts_cards.append(Card(value, 'Diamonds'))
 
 # TODO-3: выведите все карты из списка hearts_cards в терминал через запятую в одну строку:
 # Пример вывода: 2♥, 3♥, 4♥ ... A♥
-
+cards_line = [card.to_str() for card in hearts_cards]
+print(', '.join(cards_line))
 
 cards = [
     Card("2", "Hearts"),
