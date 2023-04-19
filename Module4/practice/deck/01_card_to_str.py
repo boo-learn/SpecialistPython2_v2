0b093918c03ff46000cedc782a7f427e09ac3d71
Paul Mazenko <67897709+PaulMazuka@users.noreply.github.com>
2023-04-19 20:40:04 +0300

    Update 01_card_to_str.py

--- Module4/practice/deck/01_card_to_str.py
+++ Module4/practice/deck/01_card_to_str.py
@@ -6,9 +6,14 @@ class Card:
 
     def to_str(self):
         # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
-        ...
-
-
+        if self.suit == "Hearts":
+            return f'{self.value}. \u2661 '
+        if self.suit == "Diamonds":
+            return f'{self.value}. \u2662 '
+        if self.suit == "Spades":
+            return f'{self.value}. \u2664 '
+        if self.suit == "Clubs":
+            return f'{self.value}.\u2667 '
 # Создадим несколько карт
 card1 = Card("10", "Hearts")
 card2 = Card("A", "Diamonds")
