ba9e3e14d7304a16e737c504754fe12354423dad
Nikoaly <74960807+Nikoaly@users.noreply.github.com>
2023-04-19 22:23:18 +0300

    Update 04_cards_action.py

--- Module4/practice/deck/04_cards_action.py
+++ Module4/practice/deck/04_cards_action.py
@@ -1,12 +1,29 @@
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+       def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        # Символы для отображения мастей
+        suit_symbols = {"Hearts": "\u2665", "Diamonds": "\u2666",
+                        "Clubs": "\u2663", "Spades": "\u2660"}
+        # Строковое представление карты вида "значение масть"
+        return self.value + suit_symbols[self.suit]
+
+    def equal_suit(self, other_card):
+        return self.suit == other_card.suit
 
 
 cards = []
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
 # TODO-1: в список cards добавьте ВСЕ карты всех мастей
-
+for suit in suits:
+    for value in values:
+        card = Card(value, suit)
+        cards.append(card)
 # TODO-2: Выведите карты в формате: cards[кол-во]2♥, 3♥, 4♥ ... A♥, 2♦, 3♦ ... A♦, ...
 #  кол-во берем от размера списка cards
+card_strings = [card.to_str() for card in cards]
+cards_string = ", ".join(card_strings)
+print(f"cards[{len(cards)}]: {cards_string}")
