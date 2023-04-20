4e479767b89d32a2d119d65e88d90910d144bb0a
Nikoaly <74960807+Nikoaly@users.noreply.github.com>
2023-04-20 19:19:42 +0300

    Update 06_deck_part2.py

--- Module4/practice/deck/06_deck_part2.py
+++ Module4/practice/deck/06_deck_part2.py
@@ -1,6 +1,19 @@
+import random
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        # Символы для отображения мастей
+        suit_symbols = {"Hearts": "\u2665", "Diamonds": "\u2666",
+                        "Clubs": "\u2663", "Spades": "\u2660"}
+        # Строковое представление карты вида "значение масть"
+        return self.value + suit_symbols[self.suit]
+
+    def equal_suit(self, other_card):
+        # TODO-1: метод возвращает True - если масти карт равны или False - если нет
+        return self.suit == other_card.suit
 
 
 class Deck:
@@ -9,20 +22,30 @@ class Deck:
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
         self.cards = []
-        # TODO-0: конструктор копируем из предыдущей задачи
+        # TODO-1: конструктор добавляет в список self.cards все(52) карты
+        for suit in suits:
+            for value in values:
+                self.cards.append(Card(value, suit))
 
     def show(self):
-        # TODO-0: копируем из предыдущей задачи
-        ...
+        cards_str = []
+        for card in self.cards:
+            cards_str.append(card.to_str())
+        return f"cards[{len(self.cards)}]:" + ", ".join(cards_str)
 
     def draw(self, x):
         # TODO-1: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+        if x > len(self.cards):
+            return None
+        hand = []
+        for i in range(x):
+            hand.append(self.cards.pop(0))
+        return hand
 
     def shuffle(self):
         # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
         #   Подсказка: https://www.w3schools.com/python/ref_random_shuffle.asp
-        ...
+        random.shuffle(self.cards)
 
 
 # Создаем колоду
@@ -38,4 +61,6 @@ hand = deck.draw(5)
 # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
 print(deck.show())
 # Выводим список карт "в руке"(список hand)
-print(...)
+if hand is not None:
+    for card in hand:
+        print(card.to_str())
