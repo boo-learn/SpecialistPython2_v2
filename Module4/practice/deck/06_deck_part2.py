4aacacb5cb8adaea3a4efb51db25ff95c47d40f9
rundect <93350655+rundect@users.noreply.github.com>
2023-04-20 19:01:20 +0300

    Update 06_deck_part2.py

--- Module4/practice/deck/06_deck_part2.py
+++ Module4/practice/deck/06_deck_part2.py
@@ -1,6 +1,29 @@
+# Начнем с создания карты
+import random
+
+
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value, suit):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self):
+        # TODO-1: метод возвращает строковое представление карты в виде: 10♥ и A♦
+        return f'{self.value}{self.__suit_mapping[self.suit]}'
+
+    __suit_mapping = {
+        'Diamonds': '\u2666',
+        'Hearts': '\u2665',
+        'Spades': '\u2660',
+        'Clubs': '\u2663'
+    }
+
+    def equal_suit(self, other_card):
+        # TODO-1: метод возвращает True - если масти карт равны или False - если нет
+        if self.suit == other_card.suit:
+            return True
+        else:
+            return False
 
 
 class Deck:
@@ -9,20 +32,28 @@ class Deck:
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
         self.cards = []
-        # TODO-0: конструктор копируем из предыдущей задачи
+        # TODO-1: конструктор добавляет в список self.cards все(52) карты
+        for suit in suits:
+            for value in values:
+                self.cards.append(Card(value, suit))
 
     def show(self):
-        # TODO-0: копируем из предыдущей задачи
-        ...
+        # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
+        cards_str = []
+        for card in self.cards:
+            cards_str.append(card.to_str())
+        return f"cards[{len(self.cards)}]:" + ", ".join(cards_str)
 
     def draw(self, x):
         # TODO-1: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+        hand_deck = list(self.cards[:x])
+        self.cards = self.cards[x:]
+        return hand_deck
 
     def shuffle(self):
         # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
         #   Подсказка: https://www.w3schools.com/python/ref_random_shuffle.asp
-        ...
+        return random.shuffle(self.cards)
 
 
 # Создаем колоду
@@ -38,4 +69,4 @@ hand = deck.draw(5)
 # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
 print(deck.show())
 # Выводим список карт "в руке"(список hand)
-print(...)
+print(hand)
