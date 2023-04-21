b73f4ca7c717a12672237368de0cd17a93b10e18
Роман Афанасьев <roman.afanasev@innostage-group.ru>
2023-04-21 20:11:31 +0300

    Update 02_task_deck.py

--- Module4/practice/deck/deck_tasks/02_task_deck.py
+++ Module4/practice/deck/deck_tasks/02_task_deck.py
@@ -1,4 +1,14 @@
-from deck_total import Card, Deck
+from deck_total import Deck
 
 # TODO: Создайте колоду из 52 карт. Перемешайте ее. Вытяните 10 карт сверху
 #   и посчитайте карт какой/каких мастей среди вытянутых карт оказалось больше всего?
+deck_52_cards = Deck()
+deck_52_cards.shuffle()
+cards_10 = deck_52_cards.draw(10)
+cards_suit = {}
+for card in cards_10:
+    cards_suit[card.suit] = cards_suit.get(card.suit, 0) + 1
+print(cards_suit)
+most_suits = []
+for suit in cards_suit:
+    pass
