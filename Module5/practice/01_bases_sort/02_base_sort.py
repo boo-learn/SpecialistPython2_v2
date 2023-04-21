d612d53b575f7b1ceb222d298b67a9fef559b237
Nikoaly <74960807+Nikoaly@users.noreply.github.com>
2023-04-21 21:06:56 +0300

    Update 02_base_sort.py

--- Module5/practice/01_bases_sort/02_base_sort.py
+++ Module5/practice/01_bases_sort/02_base_sort.py
@@ -1,26 +1,47 @@
-# Все алгоритмы сортировки из examples/ оберните в функции
+import random
 
-def bubble_sort():
-    pass
+def bubble_sort(num):
+    n = len(num)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if num[j] > num[j + 1]:
+                num[j], num[j + 1] = num[j + 1], num[j]
+    return num
 
+def selection_sort(num):
+    n = len(num)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if num[j] < num[min_idx]:
+                min_idx = j
+        num[i], num[min_idx] = num[min_idx], num[i]
+    return num
 
-def sort_choice():
-    pass
-
-
-def quick_sort():
-    pass
-
+def quick_sort(num):
+    if len(num) <= 1:
+        return num
+    pivot = num[0]
+    left = []
+    right = []
+    for i in range(1, len(num)):
+        if num[i] < pivot:
+            left.append(num[i])
+        else:
+            right.append(num[i])
+    return quick_sort(left) + [pivot] + quick_sort(right)
 
 # Напишите функцию для заполнения списка случайными числами
 def gen_list(size, at=-100, to=100):
-    import random
-    """
-    :param size: кол-во элементов списка
-    :param at: минимально возможное значение элементов
-    :param to: максимально возможное значение элементов
-    :return: список из size произвольных элементов в диапазоне at..to 
-    """
-    pass
+    return [random.randint(at, to) for i in range(size)]
 
-# протестируйте функции сортировки, используя gen_list() для создания сортируемых списков
+
+arr1 = gen_list(10)
+print("Unsorted list 1:", arr1)
+sorted_arr1 = bubble_sort(arr1)
+print("Bubble Sorted list 1:", sorted_arr1)
+sorted_arr1 = selection_sort(arr1)
+print("Selection Sorted list 1:", sorted_arr1)
+sorted_arr1 = quick_sort(arr1)
+print("Quick Sorted list 1:", sorted_arr1)
+
