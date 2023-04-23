ec4eaa63036f59953389f3759e8d33f2da3200cb
rundect <93350655+rundect@users.noreply.github.com>
2023-04-24 02:27:18 +0300

    Update 01_task_Currency.py

--- Module4/home_work/Currency/01_task_Currency.py
+++ Module4/home_work/Currency/01_task_Currency.py
@@ -2,10 +2,26 @@
 # Курсы валют будем брать с этого сайта: https://www.cbr-xml-daily.ru/ .
 # Нас будут интересовать только курсы доллара и евро.
 # Как получить курс по API со стороннего сайта - смотри в helpers/request_currency.py
+import requests
+from requests import RequestException
+
 
 class Currency:
     def __init__(self, type):
-        pass
+        self.type = type.upper()[:3]
+
+    def __get_exchange_value(self, day, month, year):
+        url = f'https://www.cbr-xml-daily.ru/archive/{year}/{month}/{day}/daily_json.js'
+        request = requests.get(url)
+        if request.status_code == 404:
+            request.raise_for_status()
+        request_json = request.json()
+        return request_json['Valute'][self.type]['Value']
+
+    def __getitem__(self, date):
+        self.day, self.month, self.year = date.split('.')
+        exchange_value = self.__get_exchange_value(self.day, self.month, self.year)
+        return f'{date}: 1{self.type} = {exchange_value}RUB'
 
 
 usd = Currency("usd")  # Создаем валюту "Доллар"
