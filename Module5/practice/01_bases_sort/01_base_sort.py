33040004e8c0f76814db0c8c3cb4078711f09264
Роман Афанасьев <roman.afanasev@innostage-group.ru>
2023-04-24 18:42:57 +0300

    Update 01_task_Currency.py

--- Module5/practice/01_bases_sort/01_base_sort.py
+++ Module5/practice/01_bases_sort/01_base_sort.py
@@ -11,17 +11,17 @@
 nums = [5, 2, 1, 8, 4]
 print("before sort = ", nums)
 swapped = True
-n = 1
+j = 0
 while swapped:
     swapped = False
     print("*****")
 
-    for i in range(len(nums) - n):
+    for i in range(len(nums) - 1 - j):
         print("i = ", i)
         if nums[i] > nums[i + 1]:
             # Меняем элементы
             nums[i], nums[i + 1] = nums[i + 1], nums[i]
             # Устанавливаем swapped в True для следующей итерации
             swapped = True
-    n += 1
+    j += 1
 print("after sort = ", nums)
