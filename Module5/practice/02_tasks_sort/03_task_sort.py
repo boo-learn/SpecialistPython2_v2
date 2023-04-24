6905517074b614a1632c95c902f21979e46c7c95
PythonEngine <130360297+PythonEngine@users.noreply.github.com>
2023-04-24 19:12:54 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -1,6 +1,54 @@
+
 # Сумма наибольших
 # Дан список чисел.
 # Найти: сумму 5-ти самых больших элементов
 
+def partition(nums, low, high):
+    # Выбираем средний элемент в качестве опорного
+    # Также возможен выбор первого, последнего
+    # или произвольного элементов в качестве опорного
+    pivot = nums[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while nums[i] < pivot:
+            i += 1
+        j -= 1
+
+        while nums[j] > pivot:
+            j -= 1
+
+        if i >= j:
+            return j
+
+        # Если элемент с индексом i (слева от опорного) больше, чем
+        # элемент с индексом j (справа от опорного), меняем их местами
+        nums[i], nums[j] = nums[j], nums[i]
+
+def quick_sort(nums):
+    # Создадим вспомогательную функцию, которая вызывается рекурсивно
+    def _quick_sort(items, low, high):
+        if low < high:
+            # Индекс опорного элемента
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
+
+
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
 
+quick_sort(numbers)
+
+total = 0
+i = 1
+
+while i <= 5:
+    print(i)
+    total += numbers[-i]
+    i += 1
+
+print(numbers)
+print(total)
