d1c19a03b897942434e2ff14564f4f8c9e6c54be
PythonEngine <130360297+PythonEngine@users.noreply.github.com>
2023-04-24 03:12:26 +0300

    Update 02_Vector_result.py

--- Module3/home_work/02_Vector_result.py
+++ Module3/home_work/02_Vector_result.py
@@ -1,5 +1,24 @@
-# Сюда отправляем решение задачи "Вектор"
-# Само задание в файле 01_Vector_task.md
 
 class Vector:
-    pass
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self) -> str:
+        return f"{self.x}, {self.y}"
+
+    def __add__(self, vector2):
+        __x = self.x + vector2.x
+        __y = self.y + vector2.y
+        return Vector(__x, __y)
+
+    def __sub__(self, vector2):
+        __x = self.x - vector2.x
+        __y = self.y - vector2.y
+        return Vector(__x, __y)
+
+    def __mul__(self, n):
+        __x = self.x * n
+        __y = self.y * n
+        return Vector(__x, __y)
