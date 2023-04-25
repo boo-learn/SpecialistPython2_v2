b55794741a3797e220d4b0495e079a3f564ac64d
PythonEngine <130360297+PythonEngine@users.noreply.github.com>
2023-04-25 20:29:46 +0300

    Update maze1.py

--- Module7/practice/01_Maze-1/maze1.py
+++ Module7/practice/01_Maze-1/maze1.py
@@ -1,8 +1,37 @@
-# Выверите и скопируйте сюда наиболее подходяший алгоритм для работы с графом(bfs/dfs)
 
-# Опишите список смежности по изображению лабиринта из файла task.md
-graph = [
-    ...
+def dfs(graph: list[list], start_point: int) -> list[bool]:
+    """
+    Алгоритм поиска в глубину (DFS)
+    """
+    visited = [False] * len(graph)
+
+    def _dfs(v):
+        visited[v] = True
+        for w in graph[v]:
+            if not visited[w]:  # посещён ли текущий сосед?
+                _dfs(w)
+
+    _dfs(start_point)
+    return visited
+
+graph1 = [
+    [1],            # 0
+    [4],            # 1
+    [5],            # 2
+    [4, 6],         # 3
+    [1, 3, 5],      # 4
+    [2, 4],         # 5
+    [3],            # 6
+    [8],            # 7
+    [7]             # 8
 ]
 
-# Решите задачу и выведите ответ в нужном формате
+MSG1 = "Сan go to the bank"
+MSG2 = "Сan't go to the bank"
+
+visited = dfs(graph1, start_point=0)
+
+if visited[0] and visited[7]:
+    print(MSG1)
+else:
+    print(MSG2)
