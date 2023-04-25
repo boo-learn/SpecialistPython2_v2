62de505826cb29e4d7d4f66e8f7294bc07691b50
Pluzhnikov Mikhail <106968636+mihaisVFR@users.noreply.github.com>
2023-04-25 20:47:06 +0300

    Update maze3.py

--- Module7/practice/03_Maze-3/maze3.py
+++ Module7/practice/03_Maze-3/maze3.py
@@ -1,5 +1,39 @@
-# Скопируйте решение из предыдущей задачи и адаптируйте под условия текущей задачи
-# Чем меньше пришлось вносить изменений в код программы, тем лучше было решение предыдущей задачи
+graph = [
+    [1, ],    # 0
+    [0, 4],  #1
+    [5],    # 2
+    [6, 4], # 3
+    [1, 3, 5],     # 4
+    [4, 2],  # 5
+    [3],  # 6
+    [8],  # 7
+    [7], # 8
+]
+
+def dfs(graph: list, start_point: int) -> list[bool]:
+    """
+    Алгоритм поиска в глубину
+    """
+    visited = [False] * len(graph)
+
+    def _dfs(v):
+        visited[v] = True
+        for w in graph[v]:
+            if not visited[w]:  # посещён ли текущий сосед?
+                _dfs(w)
+
+    _dfs(start_point)
+    return visited
 
 
-# Решите задачу и выведите ответ в нужном формате
+shop = (dfs(graph, 0)[2], "shop")
+bank = (dfs(graph, 0)[7], "bank")
+
+def answer(var):
+    if var[0]:
+        print(f"Can go to the {var[1]}")
+    else:
+        print(f"Can't go to the {var[1]}")
+
+answer(shop)
+answer(bank)
