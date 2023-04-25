9d0beebd86f7e248c850fa3a98d385d77128a6f7
Pluzhnikov Mikhail <106968636+mihaisVFR@users.noreply.github.com>
2023-04-25 11:07:41 +0300

    Update 09_task_sort.py

--- Module5/practice/02_tasks_sort/09_task_sort.py
+++ Module5/practice/02_tasks_sort/09_task_sort.py
@@ -1,11 +1,34 @@
-# Список сотрудников
-# Дан список, элементами которого являются сотрудники, представленные в виде словаря(dict).
-# Пример данных:
+def sort_choice(lst, reverse=False, key=lambda n: n):
+    i = 0
+    while i < len(lst) - 1:
+        m = i
+        j = i + 1
+        while j < len(lst):
+            if reverse:
+                condition = key(lst[j]) > key(lst[m])
+            else:
+                condition = key(lst[j]) < key(lst[m])
+
+            if condition:
+                m = j
+            j += 1
+        lst[i], lst[m] = lst[m], lst[i]
+        i += 1
+
+
 employees = [
    {"name": "Петр", "surname": "Алексеев", "position": "Инженер"},
    {"name": "Иван", "surname": "Петров", "position": "Прораб"},
    {"name": "Алексей", "surname": "Петров", "position": "Строитель"},
    {"name": "Иван", "surname": "Сидоров", "position": "Строитель"},
 ]
-# Выведите список сотрудников(без указания должности) в формате: Фамилия Имя, в отсортированном порядке.
-# Примечание: если фамилии сотрудников совпадают, при сортировке учесть имя.
+
+employees_copy = employees
+for emp in employees_copy:
+    emp.pop("position")
+
+sort_choice(employees, key=lambda employee: (employee["surname"], employee["name"]))
+num = 1
+for name_surname in employees:
+    print(f"{num}. {name_surname['surname']} {name_surname['name']}")
+    num += 1
