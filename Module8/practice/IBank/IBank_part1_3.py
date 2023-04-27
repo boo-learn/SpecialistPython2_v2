bee971ba373bbf6f35cb3ad29f42ac1a72c81f20
PythonEngine <130360297+PythonEngine@users.noreply.github.com>
2023-04-27 20:20:34 +0300

    Update IBank_part1_3.py

--- Module8/practice/IBank/IBank_part1_3.py
+++ Module8/practice/IBank/IBank_part1_3.py
@@ -1,7 +1,43 @@
 class Account:
-    # TODO-0: скопируйте реализацию из предыдущего решения
+    def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
+        self.name = name
+        self.passport = passport
+        self.phone_number = phone_number
+        self.__balance = start_balance
 
-    # TODO-1: напишите реализацию метода transfer()
+    def full_info(self) -> str:
+        """
+        Полная информация о счете в формате: "Иван баланс: 100 руб. паспорт: 3200 123456 т.+7-900-200-02-03"
+        """
+        return f"{self.name} баланс: {self.__balance} руб. паспорт: {self.passport} т.{self.phone_number}"
+
+    def __repr__(self) -> str:
+        """
+        :return: Информацию о счете в виде строки в формате "Иван баланс: 100 руб."
+        """
+        return f"{self.name} баланс: {self.__balance} руб."
+
+    @property
+    def balance(self) -> int:
+        return self.__balance
+
+    @balance.getter
+    def balance(self) -> int:
+        return self.__balance
+
+    def deposit(self, amount: int) -> None:
+        """
+        Внесение суммы на текущий счет
+        :param amount: сумма
+        """
+        self.__balance += amount
+
+    def withdraw(self, amount: int) -> None:
+        if amount <= self.__balance:
+            self.__balance -= amount
+        else:
+            raise ValueError("Денег на счету недостаточно")
+
     def transfer(self, target_account: 'Account', amount: int) -> None:
         """
         Перевод денег на счет другого клиента
@@ -9,7 +45,11 @@ class Account:
         :param amount: сумма перевода
         :return:
         """
-        pass
+        if self.__balance >= amount:
+            self.__balance -= amount
+            target_account.__balance += self.__balance
+        else:
+            raise ValueError("Недостаточно денег на счёте")
 
 
 account1 = Account("Иван", "3230 634563", "+7-900-765-12-34", 1000)
@@ -34,6 +74,3 @@ try:
 except ValueError as e:
     print(e)
 
-# Проверяем изменения баланса:
-print(account1)
-print(account2)
