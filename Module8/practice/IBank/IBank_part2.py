b256846427c65db498df97984d90ad9d8b52b913
Zvonkin Kirill <110783681+slip686@users.noreply.github.com>
2023-04-27 21:40:54 +0300

    Update IBank_part2.py

--- Module8/practice/IBank/IBank_part2.py
+++ Module8/practice/IBank/IBank_part2.py
@@ -1,15 +1,12 @@
 from typing import List
+import re
 
 
-# класс для хранения информации об операциях
 class Operation:
-    # Типы операций храним в свойствах класса
     DEPOSIT = 'Пополнение'
     WITHDRAW = 'Снятие'
     TRANSFER_OUT = 'Перевод'
     TRANSFER_IN = 'Поступление'
-
-    # Напоминаю: обращение к этим переменным происходит через имя класса, пример: Operation.WITHDRAW
 
     def __init__(self, type, amount, target_account=None):
         self.type = type
@@ -17,9 +14,6 @@ class Operation:
         self.target_account = target_account
 
     def __repr__(self) -> str:
-        """
-        :return: возвращает строковое представление операции. Формат указан в 02_IBank_part2.md
-        """
         str_out = f"{self.type} {self.amount} руб."
         if self.type == Operation.TRANSFER_OUT:
             str_out += f" на счет клиента: {self.target_account.name}"
@@ -31,41 +25,59 @@ class Operation:
 class Account:
     def __init__(self, name: str, passport: str, phone_number: str, start_balance: int = 0):
         self.name = name
-        self.passport = passport
-        self.phone_number = phone_number
+        self.passport = None
+        self.phone_number = None
+        self.__check_passport(passport)
+        self.__check_phone(phone_number)
         self.__balance = start_balance
-        # историю храним как список объектов класса Operation, добавив свойство в конструктор:
+        self.info = self.full_info()
         self.__history: List[Operation] = []
 
-    # TODO: сюда копируем реализацию методом класса Account из предыдущей задачи
-    #  и реализуем добавление в историю
+    def __check_passport(self, passport):
+        passport_pattern = r"\d{4} \d{6}"
+        if re.match(passport_pattern, passport):
+            self.passport = passport
+        else:
+            raise ValueError('Неверный формат паспортных данных')
 
-    # Данный метод дан в готовом виде. Изучите его и используйте как пример, для доработки других
-    def deposit(self, amount: int, to_history: bool = True) -> None:
-        """
-        Внесение суммы на текущий счет
-        :param amount: сумма
-        :param to_history: True - записывать операцию в историю, False - не записывать
-        """
-        self.__balance += amount
-        if to_history:
-            operation = Operation(amount, Operation.DEPOSIT)
-            self.__history.append(operation)
+    def __check_phone(self, phone):
+        phone_pattern = r"[+]7-\d{3}-\d{3}-\d{2}-\d{2}"
+        if re.match(phone_pattern, phone):
+            self.phone_number = phone
+        else:
+            raise ValueError('Неверный формат номера телефона')
+
+    def full_info(self) -> str:
+        return f"{self.name} баланс: {self.__balance} руб. паспорт: {self.passport} т.{self.phone_number}"
+
+    def __repr__(self) -> str:
+        return self.info
 
     def get_history(self) -> List[Operation]:
-        """
-        :return: возвращаем историю операций в виде списка операций
-        """
         return self.__history
 
+    @property
+    def balance(self):
+        return self.__balance
 
-# Создаем тестовый аккаунт:
-account1 = Account("Алексей", "3232 456124", "+7-901-744-22-99", start_balance=500)
+    def deposit(self, amount: int, to_history: bool = True):
+        self.__balance += amount
+        if to_history:
+            self.__history.append(Operation('Пополнение', amount))
 
-# Выполняем пару операций пополнения:
-account1.deposit(200)
-account1.deposit(500)
+    def withdraw(self, amount: int, to_history: bool = True):
+        if self.__balance - amount >= 0:
+            self.__balance -= amount
+            if to_history:
+                self.__history.append(Operation('Снятие', amount))
+        else:
+            raise ValueError('Недостаточно средств')
 
-# Выводим историю операций:
-for operation in account1.get_history():
-    print(operation)
+    def transfer(self, target_account: 'Account', amount: int) -> None:
+        try:
+            self.withdraw(amount, to_history=False)
+            target_account.deposit(amount, to_history=False)
+            self.__history.append(Operation('Перевод', amount, target_account))
+            target_account.__history.append(Operation('Поступление', amount, self))
+        except ValueError:
+            raise ValueError('Недостаточно средств')
